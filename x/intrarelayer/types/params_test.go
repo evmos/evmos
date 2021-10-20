abc58c3754789e6b9fa5b73d5ee3157668e2b7bc
ramacarlucho <ramirocarlucho@gmail.com>
2021-10-20 12:45:59 -0300

    Validate genesis fix. genesis and params tests

---
+++ x/intrarelayer/types/params_test.go
@@ -0,0 +1,44 @@
+package types
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestParamsValidate(t *testing.T) {
+	testCases := []struct {
+		name     string
+		params   Params
+		expError bool
+	}{
+		{"default", DefaultParams(), false},
+		{
+			"valid",
+			NewParams(true, 100),
+			false,
+		},
+		{
+			"empty",
+			Params{},
+			true,
+		},
+		{
+			"invalid param duration",
+			Params{
+				TokenPairVotingPeriod: -10,
+			},
+			true,
+		},
+	}
+
+	for _, tc := range testCases {
+		err := tc.params.Validate()
+
+		if tc.expError {
+			require.Error(t, err, tc.name)
+		} else {
+			require.NoError(t, err, tc.name)
+		}
+	}
+}
