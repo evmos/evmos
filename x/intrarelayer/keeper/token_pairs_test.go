62bd5431011033839db8f29e8bb6f0dbafda5eb8
Daniel Burckhardt <daniel.m.burckhardt@gmail.com>
2021-10-20 17:48:27 +0200

    Add TestGetTokenPairID

--- x/intrarelayer/keeper/token_pairs_test.go
+++ x/intrarelayer/keeper/token_pairs_test.go
@@ -6,6 +6,29 @@ import (
 	"github.com/tharsis/evmos/x/intrarelayer/types"
 )
 
+func (suite *KeeperTestSuite) TestGetTokenPairID() {
+	pair := types.NewTokenPair(tests.GenerateAddress(), evmtypes.DefaultEVMDenom, true)
+	suite.app.IntrarelayerKeeper.SetTokenPair(suite.ctx, pair)
+
+	testCases := []struct {
+		name  string
+		token string
+		expId []byte
+	}{
+		{"nil token", "", nil},
+		{"valid hex token", tests.GenerateAddress().Hex(), []byte{}},
+		{"valid hex token", tests.GenerateAddress().String(), []byte{}},
+	}
+	for _, tc := range testCases {
+		id := suite.app.IntrarelayerKeeper.GetTokenPairID(suite.ctx, tc.token)
+		if id != nil {
+			suite.Require().Equal(tc.expId, id, tc.name)
+		} else {
+			suite.Require().Nil(id)
+		}
+	}
+}
+
 func (suite *KeeperTestSuite) TestGetTokenPair() {
 	pair := types.NewTokenPair(tests.GenerateAddress(), evmtypes.DefaultEVMDenom, true)
 	suite.app.IntrarelayerKeeper.SetTokenPair(suite.ctx, pair)
