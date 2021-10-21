0710001fe08a144469730632821b8edfdf9d54b0
Daniel Burckhardt <daniel.m.burckhardt@gmail.com>
2021-10-21 17:57:22 +0200

    WIP Keepertests(TestRegisterTokenPair)

---
+++ x/intrarelayer/keeper/proposals_test.go
@@ -0,0 +1,72 @@
+package keeper_test
+
+import (
+	"fmt"
+
+	"github.com/tharsis/ethermint/tests"
+	"github.com/tharsis/evmos/x/intrarelayer/types"
+)
+
+func (suite KeeperTestSuite) TestRegisterTokenPair() {
+	pair := types.NewTokenPair(tests.GenerateAddress(), "coin", true)
+	id := pair.GetID()
+
+	testCases := []struct {
+		name     string
+		malleate func()
+		expPass  bool
+	}{
+		{
+			"intrarelaying is disabled globally",
+			func() {
+				params := types.DefaultParams()
+				params.EnableIntrarelayer = false
+				suite.app.IntrarelayerKeeper.SetParams(suite.ctx, params)
+			},
+			false,
+		},
+		{
+			"token ERC20 already registered",
+			func() {
+				suite.app.IntrarelayerKeeper.SetERC20Map(suite.ctx, pair.GetERC20Contract(), id)
+			},
+			false,
+		},
+		{
+			"denom already registered",
+			func() {
+				suite.app.IntrarelayerKeeper.SetDenomMap(suite.ctx, pair.Denom, id)
+			},
+			false,
+		},
+		{
+			"meta data already stored",
+			func() {
+				suite.app.IntrarelayerKeeper.SetTokenPair(suite.ctx, pair)
+			},
+			false,
+		},
+		{
+			"ok",
+			func() {
+				pair.Enabled = true
+			},
+			true,
+		},
+	}
+	for _, tc := range testCases {
+		suite.Run(fmt.Sprintf("Case %s", tc.name), func() {
+			suite.SetupTest() // reset
+
+			tc.malleate()
+
+			err := suite.app.IntrarelayerKeeper.RegisterTokenPair(suite.ctx, pair)
+			if tc.expPass {
+				suite.Require().NoError(err, tc.name)
+			} else {
+				suite.Require().Error(err, tc.name)
+			}
+		})
+	}
+
+}
