b4e6c3fa1f5b7882090dbdde7a020387e3927c3e
ramacarlucho <ramirocarlucho@gmail.com>
2021-10-26 15:21:11 -0300

    check correct execution

--- x/intrarelayer/keeper/evm_hooks_test.go
+++ x/intrarelayer/keeper/evm_hooks_test.go
@@ -3,6 +3,7 @@ package keeper_test
 import (
 	"math/big"
 
+	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/tharsis/evmos/x/intrarelayer/types"
 )
 
@@ -10,18 +11,25 @@ import (
 func (suite *KeeperTestSuite) TestEvmHooks() {
 	suite.SetupTest()
 
-	// Module account is missing mint/burn permits
-
 	contractAddr := suite.DeployContract("coin", "token")
 	suite.Commit()
 	pair := types.NewTokenPair(contractAddr, "coinevm", true)
 	err := suite.app.IntrarelayerKeeper.RegisterTokenPair(suite.ctx, pair)
 	suite.Require().NoError(err)
+
+	// Mint 10 tokens to suite.address (owner)
 	_ = suite.MintERC20Token(contractAddr, suite.address, suite.address, big.NewInt(10))
 	suite.Commit()
 
+	// Burn the 10 tokens of suite.address (owner)
 	msg := suite.BurnERC20Token(contractAddr, suite.address, big.NewInt(10))
 	logs := suite.app.EvmKeeper.GetTxLogsTransient(msg.AsTransaction().Hash())
+
+	// After this execution, the burned tokens will be available on the cosmos chain
 	err = suite.app.IntrarelayerKeeper.PostTxProcessing(suite.ctx, msg.AsTransaction().Hash(), logs)
+
 	// Check correct execution
+	suite.Require().NoError(err)
+	// Acc Address should have the 10 coins that were burned in the transaction
+	suite.Require().Equal(suite.app.BankKeeper.GetBalance(suite.ctx, sdk.AccAddress(suite.address.Bytes()), "coinevm").Amount, sdk.NewIntFromBigInt(big.NewInt(10)))
 }
