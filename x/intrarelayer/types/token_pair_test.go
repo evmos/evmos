6e37e72d10b527289073802adcf4d8a343efb833
Daniel Burckhardt <daniel.m.burckhardt@gmail.com>
2021-11-12 18:09:15 +0100

    IRM(new Specs): Add tests for  isNativeCoin() and isNavtiveERC20() methods

--- x/intrarelayer/types/token_pair_test.go
+++ x/intrarelayer/types/token_pair_test.go
@@ -1,6 +1,7 @@
 package types
 
 import (
+	"fmt"
 	"strings"
 	"testing"
 
@@ -36,6 +37,7 @@ func (suite *TokenPairTestSuite) TestTokenPairNew() {
 		{msg: "Register token pair - invalid length token (0)", erc20Address: tests.GenerateAddress(), denom: "", enabled: true, owner: MODULE_OWNER, expectPass: false},
 		{msg: "Register token pair - invalid length token (1)", erc20Address: tests.GenerateAddress(), denom: "a", enabled: true, owner: MODULE_OWNER, expectPass: false},
 		{msg: "Register token pair - invalid length token (128)", erc20Address: tests.GenerateAddress(), denom: strings.Repeat("a", 129), enabled: true, owner: MODULE_OWNER, expectPass: false},
+		{msg: "Register token pair - pass", erc20Address: tests.GenerateAddress(), denom: "test", enabled: true, owner: MODULE_OWNER, expectPass: true},
 	}
 
 	for i, tc := range testCases {
@@ -59,6 +61,7 @@ func (suite *TokenPairTestSuite) TestTokenPair() {
 		{msg: "Register token pair - invalid address (no hex)", pair: TokenPair{"0x5dCA2483280D9727c80b5518faC4556617fb19ZZ", "test", true, MODULE_OWNER}, expectPass: false},
 		{msg: "Register token pair - invalid address (invalid length 1)", pair: TokenPair{"0x5dCA2483280D9727c80b5518faC4556617fb19", "test", true, MODULE_OWNER}, expectPass: false},
 		{msg: "Register token pair - invalid address (invalid length 2)", pair: TokenPair{"0x5dCA2483280D9727c80b5518faC4556617fb194FFF", "test", true, MODULE_OWNER}, expectPass: false},
+		{msg: "pass", pair: TokenPair{tests.GenerateAddress().String(), "test", true, MODULE_OWNER}, expectPass: true},
 	}
 
 	for i, tc := range testCases {
@@ -68,6 +71,75 @@ func (suite *TokenPairTestSuite) TestTokenPair() {
 			suite.Require().NoError(err, "valid test %d failed: %s, %v", i, tc.msg)
 		} else {
 			suite.Require().Error(err, "invalid test %d passed: %s, %v", i, tc.msg)
+		}
+	}
+}
+
+func (suite *TokenPairTestSuite) TestIsNativeCoin() {
+	testCases := []struct {
+		name       string
+		pair       TokenPair
+		expectPass bool
+	}{
+		{
+			"no owner",
+			TokenPair{tests.GenerateAddress().String(), "test", true, INVALID_OWNER},
+			false,
+		},
+		{
+			"external ERC20 owner",
+			TokenPair{tests.GenerateAddress().String(), "test", true, EXTERNAL_OWNER},
+			false,
+		},
+		{
+			"pass",
+			TokenPair{tests.GenerateAddress().String(), "test", true, MODULE_OWNER},
+			true,
+		},
+	}
+
+	for _, tc := range testCases {
+		res := tc.pair.IsNativeCoin()
+		fmt.Println(res)
+
+		if tc.expectPass {
+			suite.Require().True(res, tc.name)
+		} else {
+			suite.Require().False(res, tc.name)
+		}
+	}
+}
+func (suite *TokenPairTestSuite) TestIsNativeERC20() {
+	testCases := []struct {
+		name       string
+		pair       TokenPair
+		expectPass bool
+	}{
+		{
+			"no owner",
+			TokenPair{tests.GenerateAddress().String(), "test", true, INVALID_OWNER},
+			false,
+		},
+		{
+			"module owner",
+			TokenPair{tests.GenerateAddress().String(), "test", true, MODULE_OWNER},
+			false,
+		},
+		{
+			"pass",
+			TokenPair{tests.GenerateAddress().String(), "test", true, EXTERNAL_OWNER},
+			true,
+		},
+	}
+
+	for _, tc := range testCases {
+		res := tc.pair.IsNativeERC20()
+		fmt.Println(res)
+
+		if tc.expectPass {
+			suite.Require().True(res, tc.name)
+		} else {
+			suite.Require().False(res, tc.name)
 		}
 	}
 }
