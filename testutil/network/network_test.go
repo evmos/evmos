440120d7c0f13b61ff8134267523195be2500a5b
Federico Kunze Küllmer <31522760+fedekunze@users.noreply.github.com>
2021-11-23 11:30:41 +0100

    cmd: update testnet command (#105)
    * cmd: update testnet command
    
    * tests wip
    
    * tests
    
    * changelog
    
    * norace

--- testutil/network/network_test.go
+++ testutil/network/network_test.go
@@ -4,12 +4,16 @@
 package network_test
 
 import (
+	"fmt"
 	"testing"
 	"time"
 
 	"github.com/stretchr/testify/suite"
 
-	"github.com/cosmos/cosmos-sdk/testutil/network"
+	"github.com/ethereum/go-ethereum/ethclient"
+	"github.com/tharsis/ethermint/server/config"
+	"github.com/tharsis/ethermint/testutil/network"
+	evmosnetwork "github.com/tharsis/evmos/testutil/network"
 )
 
 type IntegrationTestSuite struct {
@@ -22,11 +26,22 @@ func (s *IntegrationTestSuite) SetupSuite() {
 	s.T().Log("setting up integration test suite")
 
 	var err error
-	s.network, err = network.New(s.T(), s.T().TempDir(), network.DefaultConfig())
+	cfg := evmosnetwork.DefaultConfig()
+	cfg.JSONRPCAddress = config.DefaultJSONRPCAddress
+	cfg.NumValidators = 1
+
+	s.network, err = network.New(s.T(), s.T().TempDir(), cfg)
+	s.Require().NoError(err)
+	s.Require().NotNil(s.network)
+
+	_, err = s.network.WaitForHeight(2)
 	s.Require().NoError(err)
 
-	_, err = s.network.WaitForHeight(1)
-	s.Require().NoError(err)
+	if s.network.Validators[0].JSONRPCClient == nil {
+		address := fmt.Sprintf("http://%s", s.network.Validators[0].AppConfig.JSONRPC.Address)
+		s.network.Validators[0].JSONRPCClient, err = ethclient.Dial(address)
+		s.Require().NoError(err)
+	}
 }
 
 func (s *IntegrationTestSuite) TearDownSuite() {
@@ -37,6 +52,10 @@ func (s *IntegrationTestSuite) TearDownSuite() {
 func (s *IntegrationTestSuite) TestNetwork_Liveness() {
 	h, err := s.network.WaitForHeightWithTimeout(10, time.Minute)
 	s.Require().NoError(err, "expected to reach 10 blocks; got %d", h)
+
+	latestHeight, err := s.network.LatestHeight()
+	s.Require().NoError(err, "latest height failed")
+	s.Require().GreaterOrEqual(latestHeight, h)
 }
 
 func TestIntegrationTestSuite(t *testing.T) {
