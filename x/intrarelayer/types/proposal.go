9f8fb776fe0ae976b24533e02270887b4dcf556a
Federico Kunze Küllmer <federico.kunze94@gmail.com>
2021-12-01 14:10:14 +0100

    changes from main

--- x/intrarelayer/types/proposal.go
+++ x/intrarelayer/types/proposal.go
@@ -47,7 +47,7 @@ func CreateDenomDescription(address string) string {
 
 // CreateDenom generates a string the module name plus the address to avoid conflicts with names staring with a number
 func CreateDenom(address string) string {
-	return fmt.Sprintf("%s-%s", ModuleName, address)
+	return fmt.Sprintf("%s/%s", ModuleName, address)
 }
 
 // NewRegisterCoinProposal returns new instance of RegisterCoinProposal
