3add5304bfcdc3d337f261a31e4f846ec9009821
Federico Kunze Küllmer <federico.kunze94@gmail.com>
2021-12-01 14:50:10 +0100

    gosec

--- x/intrarelayer/client/cli/utils.go
+++ x/intrarelayer/client/cli/utils.go
@@ -2,6 +2,7 @@ package cli
 
 import (
 	"io/ioutil"
+	"path/filepath"
 
 	"github.com/cosmos/cosmos-sdk/codec"
 	banktypes "github.com/cosmos/cosmos-sdk/x/bank/types"
@@ -11,7 +12,7 @@ import (
 func ParseMetadata(cdc codec.JSONCodec, metadataFile string) (banktypes.Metadata, error) {
 	metadata := banktypes.Metadata{}
 
-	contents, err := ioutil.ReadFile(metadataFile)
+	contents, err := ioutil.ReadFile(filepath.Clean(metadataFile))
 	if err != nil {
 		return metadata, err
 	}
