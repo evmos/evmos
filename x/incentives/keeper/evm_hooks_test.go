b052379342d78fd60f461dc6dbe2914b9bf158d8
Daniel Burckhardt <daniel.m.burckhardt@gmail.com>
2022-01-06 17:22:15 +0100

    feat(incentives): WIP evm_hook_test.go

--- x/incentives/keeper/evm_hooks_test.go
+++ x/incentives/keeper/evm_hooks_test.go
@@ -6,9 +6,6 @@ import (
 
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/ethereum/go-ethereum/common"
-
-	"github.com/tharsis/ethermint/tests"
-	"github.com/tharsis/evmos/x/erc20/types"
 )
 
 // ensureHooksSet tries to set the hooks on EVMKeeper, this will fail if the
@@ -21,6 +18,7 @@ func (suite *KeeperTestSuite) ensureHooksSet() {
 	suite.app.EvmKeeper.SetHooks(suite.app.IncentivesKeeper)
 }
 
+// TODO Fix test when the ethermint version is updated
 func (suite *KeeperTestSuite) TestEvmHooksStoreTxGasUsed() {
 	testCases := []struct {
 		name     string
@@ -29,90 +27,62 @@ func (suite *KeeperTestSuite) TestEvmHooksStoreTxGasUsed() {
 	}{
 		{
 			"correct execution",
-			func(contract common.Address) {
-				// _, err := suite.app.Erc20Keeper.RegisterERC20(suite.ctx, contractAddr)
-				// suite.Require().NoError(err)
+			func(contractAddr common.Address) {
+				// Submit contract Tx and make sure participant has enough tokens for tx
+				suite.MintERC20Token(contractAddr, suite.address, participant, big.NewInt(100))
+				suite.Commit()
+				res := suite.TransferERC20Token(contractAddr, participant, participant2, big.NewInt(10))
 
-				// // Mint 10 tokens to suite.address (owner)
-				// _ = suite.MintERC20Token(contractAddr, suite.address, suite.address, big.NewInt(10))
-				// suite.Commit()
-
-				// // Burn the 10 tokens of suite.address (owner)
-				// msg := suite.BurnERC20Token(contractAddr, suite.address, big.NewInt(10))
-				// hash := msg.AsTransaction().Hash()
-				// logs := suite.app.EvmKeeper.GetTxLogsTransient(hash)
-				// suite.Require().NotEmpty(logs)
+				// check if logs submitted
+				hash := res.AsTransaction().Hash()
+				logs := suite.app.EvmKeeper.GetTxLogsTransient(hash)
+				suite.Require().NotEmpty(logs)
 			},
 			true,
 		},
-		// {
-		// 	"unregistered pair",
-		// 	func(contractAddr common.Address) {
-		// 		// Mint 10 tokens to suite.address (owner)
-		// 		_ = suite.MintERC20Token(contractAddr, suite.address, suite.address, big.NewInt(10))
-		// 		suite.Commit()
-
-		// 		// Burn the 10 tokens of suite.address (owner)
-		// 		msg := suite.BurnERC20Token(contractAddr, suite.address, big.NewInt(10))
-		// 		hash := msg.AsTransaction().Hash()
-		// 		logs := suite.app.EvmKeeper.GetTxLogsTransient(hash)
-		// 		suite.Require().NotEmpty(logs)
-		// 	},
-		// 	false,
-		// },
-		// {
-		// 	"wrong event",
-		// 	func(contractAddr common.Address) {
-		// 		_, err := suite.app.Erc20Keeper.RegisterERC20(suite.ctx, contractAddr)
-		// 		suite.Require().NoError(err)
-
-		// 		// Mint 10 tokens to suite.address (owner)
-		// 		msg := suite.MintERC20Token(contractAddr, suite.address, suite.address, big.NewInt(10))
-		// 		hash := msg.AsTransaction().Hash()
-		// 		logs := suite.app.EvmKeeper.GetTxLogsTransient(hash)
-		// 		suite.Require().NotEmpty(logs)
-		// 	},
-		// 	false,
-		// },
+		// {"unincentivized contract", func(contractAddr common.Address) {}, false},
+		// {"wrong event", func(contractAddr common.Address) {}, false},
 	}
 	for _, tc := range testCases {
 		suite.Run(fmt.Sprintf("Case %s", tc.name), func() {
-			// suite.mintFeeCollector = true
+			suite.mintFeeCollector = true
 			suite.SetupTest()
 			suite.ensureHooksSet()
 
-			// Deploy contract and create incentive
-			contractAddr := suite.DeployContract("acoin", "COIN")
+			// Deploy contract, nint and create incentive
+			contractAddr := suite.DeployContract(denomCoin, "COIN")
 			suite.Commit()
 			in, err := suite.app.IncentivesKeeper.RegisterIncentive(
 				suite.ctx,
-				contract,
-				allocations,
+				contractAddr,
+				sdk.DecCoins{
+					sdk.NewDecCoinFromDec(denomMint, sdk.NewDecWithPrec(allocationRate, 2)),
+				},
 				epochs,
 			)
 			suite.Require().NoError(err)
 			suite.Commit()
 
-			mint := big.NewInt(100)
-			send := big.NewInt(10)
+			tc.malleate(contractAddr)
 
-			// Submit contract Tx and make sure participant has enough tokens
-			suite.MintERC20Token(contractAddr, suite.address, participant, mint)
+			expGasUsed := int64(10)
+			gm, found := suite.app.IncentivesKeeper.GetIncentiveGasMeter(
+				suite.ctx,
+				contractAddr,
+				participant,
+			)
+			suite.Require().True(found)
 			suite.Commit()
-			res := suite.TransferERC20Token(contractAddr, participant, participant2, send)
-
-			gasUsed := res.
-			// check if gas is logged in Gas Meter
-			gm, found := suite.app.IncentivesKeeper.GetIncentiveGasMeter(suite.ctx, contractAddr, participant)
-			suite.Commit()
-
+			// check if gasUsed is logged
 			if tc.expPass {
 				suite.Require().NoError(err)
-				suite.Require().Equal(int64(10), balance.Amount.Int64())
+				suite.Require().NotZero(gm)
+				suite.Require().Equal(expGasUsed, gm)
+				suite.Require().Equal(expGasUsed, in.TotalGas)
 			} else {
 				suite.Require().Error(err)
-				// Check that no changes were made to the account
-				suite.Require().Equal(int64(0), balance.Amount.Int64())
+				suite.Require().Zero(gm)
+				suite.Require().Zero(in.TotalGas)
 			}
 		})
 	}
