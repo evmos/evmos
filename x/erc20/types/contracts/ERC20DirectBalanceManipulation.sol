762a957857d23ff58cc13314c2872030e6af39ed
Daniel Burckhardt <daniel.m.burckhardt@gmail.com>
2022-01-07 22:00:30 +0100

    spacing

--- x/erc20/types/contracts/ERC20DirectBalanceManipulation.sol
+++ x/erc20/types/contracts/ERC20DirectBalanceManipulation.sol
@@ -1,7 +1,9 @@
 // SPDX-License-Identifier: MIT
 
 pragma solidity ^0.8.0;
+
 import "@openzeppelin/contracts/token/ERC20/presets/ERC20PresetMinterPauser.sol";
+
 // This is an evil token. Whenever an A -> B transfer is called, half of the amount goes to B
 // and half to a predefined C
 contract ERC20DirectBalanceManipulation is ERC20PresetMinterPauser {
