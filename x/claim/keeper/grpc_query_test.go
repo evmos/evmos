ab5881e0b4a0f5d925532d4d57a693b9587406bf
Federico Kunze Küllmer <federico.kunze94@gmail.com>
2022-01-08 16:39:09 +0100

    test setup

---
+++ x/claim/keeper/grpc_query_test.go
@@ -0,0 +1,16 @@
+package keeper_test
+
+import (
+	sdk "github.com/cosmos/cosmos-sdk/types"
+
+	"github.com/tharsis/evmos/x/claim/types"
+)
+
+func (suite *KeeperTestSuite) TestQueryParams() {
+	ctx := sdk.WrapSDKContext(suite.ctx)
+	expParams := types.DefaultParams()
+
+	res, err := suite.queryClient.Params(ctx, &types.QueryParamsRequest{})
+	suite.Require().NoError(err)
+	suite.Require().Equal(expParams, res.Params)
+}
