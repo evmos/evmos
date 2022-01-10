7e9f9a218325ee591f0dde45391c8e40e8bfe98e
Daniel Burckhardt <daniel.m.burckhardt@gmail.com>
2022-01-10 12:34:42 +0100

    fix contract indentation

--- x/erc20/types/contracts/ERC20MinterBurnerDecimals.sol
+++ x/erc20/types/contracts/ERC20MinterBurnerDecimals.sol
@@ -24,102 +24,102 @@ import "@openzeppelin/contracts/utils/Context.sol";
  * and pauser roles to other accounts.
  */
 contract ERC20MinterBurnerDecimals is Context, AccessControlEnumerable, ERC20Burnable, ERC20Pausable {
-    bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
-    bytes32 public constant PAUSER_ROLE = keccak256("PAUSER_ROLE");
-    bytes32 public constant BURNER_ROLE = keccak256("BURNER_ROLE");
-    uint8 private _decimals;
+  bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
+  bytes32 public constant PAUSER_ROLE = keccak256("PAUSER_ROLE");
+  bytes32 public constant BURNER_ROLE = keccak256("BURNER_ROLE");
+  uint8 private _decimals;
 
-    /**
-     * @dev Grants `DEFAULT_ADMIN_ROLE`, `MINTER_ROLE` and `PAUSER_ROLE` to the
-     * account that deploys the contract and customizes tokens decimals
-     *
-     * See {ERC20-constructor}.
-     */
-    constructor(string memory name, string memory symbol, uint8 decimals_)
-      ERC20(name, symbol) {
-        _setupRole(DEFAULT_ADMIN_ROLE, _msgSender());
+  /**
+    * @dev Grants `DEFAULT_ADMIN_ROLE`, `MINTER_ROLE` and `PAUSER_ROLE` to the
+    * account that deploys the contract and customizes tokens decimals
+    *
+    * See {ERC20-constructor}.
+    */
+  constructor(string memory name, string memory symbol, uint8 decimals_)
+    ERC20(name, symbol) {
+      _setupRole(DEFAULT_ADMIN_ROLE, _msgSender());
 
-        _setupRole(MINTER_ROLE, _msgSender());
-        _setupRole(PAUSER_ROLE, _msgSender());
-        _setupRole(BURNER_ROLE, _msgSender());
-        _setupDecimals(decimals_);
-    }
+      _setupRole(MINTER_ROLE, _msgSender());
+      _setupRole(PAUSER_ROLE, _msgSender());
+      _setupRole(BURNER_ROLE, _msgSender());
+      _setupDecimals(decimals_);
+  }
 
-    /**
-     * @dev Sets `_decimals` as `decimals_ once at Deployment'
-     */
-    function _setupDecimals(uint8 decimals_) private {
-      _decimals = decimals_;
-    }
+  /**
+    * @dev Sets `_decimals` as `decimals_ once at Deployment'
+    */
+  function _setupDecimals(uint8 decimals_) private {
+    _decimals = decimals_;
+  }
 
-    /**
-     * @dev Overrides the `decimals()` method with custom `_decimals`
-     */
-    function decimals() public view virtual override returns (uint8) {
-      return _decimals;
-    }
+  /**
+    * @dev Overrides the `decimals()` method with custom `_decimals`
+    */
+  function decimals() public view virtual override returns (uint8) {
+    return _decimals;
+  }
 
-    /**
-     * @dev Creates `amount` new tokens for `to`.
-     *
-     * See {ERC20-_mint}.
-     *
-     * Requirements:
-     *
-     * - the caller must have the `MINTER_ROLE`.
-     */
-    function mint(address to, uint256 amount) public virtual {
-        require(hasRole(MINTER_ROLE, _msgSender()), "ERC20MinterBurnerDecimals: must have minter role to mint");
-        _mint(to, amount);
-    }
+  /**
+    * @dev Creates `amount` new tokens for `to`.
+    *
+    * See {ERC20-_mint}.
+    *
+    * Requirements:
+    *
+    * - the caller must have the `MINTER_ROLE`.
+    */
+  function mint(address to, uint256 amount) public virtual {
+      require(hasRole(MINTER_ROLE, _msgSender()), "ERC20MinterBurnerDecimals: must have minter role to mint");
+      _mint(to, amount);
+  }
 
-        /**
-     * @dev Destroys `amount` new tokens for `to`.
-     *
-     * See {ERC20-_burn}.
-     *
-     * Requirements:
-     *
-     * - the caller must have the `BURNER_ROLE`.
-     */
-    function burnCoins(address from, uint256 amount) public virtual {
-        require(hasRole(BURNER_ROLE, _msgSender()), "ERC20MinterBurnerDecimals: must have burner role to burn");
-        _burn(from, amount);
-    }
+      /**
+    * @dev Destroys `amount` new tokens for `to`.
+    *
+    * See {ERC20-_burn}.
+    *
+    * Requirements:
+    *
+    * - the caller must have the `BURNER_ROLE`.
+    */
+  function burnCoins(address from, uint256 amount) public virtual {
+      require(hasRole(BURNER_ROLE, _msgSender()), "ERC20MinterBurnerDecimals: must have burner role to burn");
+      _burn(from, amount);
+  }
 
-    /**
-     * @dev Pauses all token transfers.
-     *
-     * See {ERC20Pausable} and {Pausable-_pause}.
-     *
-     * Requirements:
-     *
-     * - the caller must have the `PAUSER_ROLE`.
-     */
-    function pause() public virtual {
-        require(hasRole(PAUSER_ROLE, _msgSender()), "ERC20MinterBurnerDecimals: must have pauser role to pause");
-        _pause();
-    }
+  /**
+    * @dev Pauses all token transfers.
+    *
+    * See {ERC20Pausable} and {Pausable-_pause}.
+    *
+    * Requirements:
+    *
+    * - the caller must have the `PAUSER_ROLE`.
+    */
+  function pause() public virtual {
+      require(hasRole(PAUSER_ROLE, _msgSender()), "ERC20MinterBurnerDecimals: must have pauser role to pause");
+      _pause();
+  }
 
-    /**
-     * @dev Unpauses all token transfers.
-     *
-     * See {ERC20Pausable} and {Pausable-_unpause}.
-     *
-     * Requirements:
-     *
-     * - the caller must have the `PAUSER_ROLE`.
-     */
-    function unpause() public virtual {
-        require(hasRole(PAUSER_ROLE, _msgSender()), "ERC20MinterBurnerDecimals: must have pauser role to unpause");
-        _unpause();
-    }
+  /**
+    * @dev Unpauses all token transfers.
+    *
+    * See {ERC20Pausable} and {Pausable-_unpause}.
+    *
+    * Requirements:
+    *
+    * - the caller must have the `PAUSER_ROLE`.
+    */
+  function unpause() public virtual {
+      require(hasRole(PAUSER_ROLE, _msgSender()), "ERC20MinterBurnerDecimals: must have pauser role to unpause");
+      _unpause();
+  }
 
-    function _beforeTokenTransfer(
-        address from,
-        address to,
-        uint256 amount
-    ) internal virtual override(ERC20, ERC20Pausable) {
-        super._beforeTokenTransfer(from, to, amount);
-    }
+  function _beforeTokenTransfer(
+      address from,
+      address to,
+      uint256 amount
+  ) internal virtual override(ERC20, ERC20Pausable) {
+      super._beforeTokenTransfer(from, to, amount);
+  }
 }
