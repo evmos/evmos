b4297a23f4cfcbc3a153247d4c8f1b6ba2de9e0e
Marko Baricevic <markobaricevic3778@gmail.com>
2022-01-12 12:13:33 +0100

    custom config.toml

--- cmd/evmosd/root.go
+++ cmd/evmosd/root.go
@@ -99,7 +99,7 @@ func NewRootCmd() (*cobra.Command, params.EncodingConfig) {
 
 	rootCmd.AddCommand(
 		ethermintclient.ValidateChainID(
-			genutilcli.InitCmd(app.ModuleBasics, app.DefaultNodeHome),
+			InitCmd(app.ModuleBasics, app.DefaultNodeHome),
 		),
 		genutilcli.CollectGenTxsCmd(banktypes.GenesisBalancesIterator{}, app.DefaultNodeHome),
 		genutilcli.MigrateGenesisCmd(),
