d6aa046db9654d67ea8092ac23cde42d22415032
Daniel Burckhardt <daniel.m.burckhardt@gmail.com>
2022-01-20 18:45:49 +0100

    feat(inflation): fix genesis and module test

--- x/inflation/module_test.go
+++ x/inflation/module_test.go
@@ -2,24 +2,34 @@ package inflation_test
 
 import (
 	"testing"
+
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	authtypes "github.com/cosmos/cosmos-sdk/x/auth/types"
+	"github.com/stretchr/testify/require"
+	abcitypes "github.com/tendermint/tendermint/abci/types"
+	tmproto "github.com/tendermint/tendermint/proto/tendermint/types"
+	feemarkettypes "github.com/tharsis/ethermint/x/feemarket/types"
+
+	simapp "github.com/tharsis/evmos/app"
+	"github.com/tharsis/evmos/x/inflation/types"
 )
 
 func TestItCreatesModuleAccountOnInitBlock(t *testing.T) {
 	// setup feemarketGenesis params
-	// feemarketGenesis := feemarkettypes.DefaultGenesisState()
-	// feemarketGenesis.Params.EnableHeight = 1
-	// feemarketGenesis.Params.NoBaseFee = false
-	// feemarketGenesis.BaseFee = sdk.NewInt(feemarketGenesis.Params.InitialBaseFee)
-	// app := simapp.Setup(false, feemarketGenesis)
-	// ctx := app.BaseApp.NewContext(false, tmproto.Header{})
+	feemarketGenesis := feemarkettypes.DefaultGenesisState()
+	feemarketGenesis.Params.EnableHeight = 1
+	feemarketGenesis.Params.NoBaseFee = false
+	feemarketGenesis.BaseFee = sdk.NewInt(feemarketGenesis.Params.InitialBaseFee)
+	app := simapp.Setup(false, feemarketGenesis)
+	ctx := app.BaseApp.NewContext(false, tmproto.Header{})
 
-	// app.InitChain(
-	// 	abcitypes.RequestInitChain{
-	// 		AppStateBytes: []byte("{}"),
-	// 		ChainId:       "test-chain-id",
-	// 	},
-	// )
+	app.InitChain(
+		abcitypes.RequestInitChain{
+			AppStateBytes: []byte("{}"),
+			ChainId:       "test-chain-id",
+		},
+	)
 
-	// acc := app.AccountKeeper.GetAccount(ctx, authtypes.NewModuleAddress(types.ModuleName))
-	// require.NotNil(t, acc)
+	acc := app.AccountKeeper.GetAccount(ctx, authtypes.NewModuleAddress(types.ModuleName))
+	require.NotNil(t, acc)
 }
