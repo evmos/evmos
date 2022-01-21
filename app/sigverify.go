388c83664312e86f241a4e78d324cc1c0cbbab53
Federico Kunze Küllmer <federico.kunze94@gmail.com>
2022-01-21 17:53:05 +0100

    comments

--- app/sigverify.go
+++ app/sigverify.go
@@ -23,6 +23,13 @@ const (
 // SigVerificationGasConsumer is the Evmos implementation of SignatureVerificationGasConsumer. It consumes gas
 // for signature verification based upon the public key type. The cost is fetched from the given params and is matched
 // by the concrete type.
+// The types of keys supported are:
+//
+// - ethsecp256k1 (Ethereum keys)
+//
+// - ed25519 (Validators)
+//
+// - multisig (Cosmos SDK multisigs)
 func SigVerificationGasConsumer(
 	meter sdk.GasMeter, sig signing.SignatureV2, params authtypes.Params,
 ) error {
@@ -30,13 +37,16 @@ func SigVerificationGasConsumer(
 	switch pubkey := pubkey.(type) {
 
 	case *ethsecp256k1.PubKey:
+		// Ethereum keys
 		meter.ConsumeGas(secp256k1VerifyCost, "ante verify: eth_secp256k1")
 		return nil
 	case *ed25519.PubKey:
+		// Validator keys
 		meter.ConsumeGas(params.SigVerifyCostED25519, "ante verify: ed25519")
 		return sdkerrors.Wrap(sdkerrors.ErrInvalidPubKey, "ED25519 public keys are unsupported")
 
 	case multisig.PubKey:
+		// Multisig keys
 		multisignature, ok := sig.Data.(*signing.MultiSignatureData)
 		if !ok {
 			return fmt.Errorf("expected %T, got, %T", &signing.MultiSignatureData{}, sig.Data)
