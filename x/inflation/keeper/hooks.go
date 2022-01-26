93fcd3446b02fb2f191da467dba2f7e8d10aedaa
Prajjwol Gautam <not@acyb.org>
2022-01-26 13:05:04 -0800

    Update hooks to update after a epoch ends

--- x/inflation/keeper/hooks.go
+++ x/inflation/keeper/hooks.go
@@ -1,66 +1,53 @@
 package keeper
 
 import (
+	"fmt"
+
+	"github.com/cosmos/cosmos-sdk/telemetry"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	epochstypes "github.com/tharsis/evmos/x/epochs/types"
+	"github.com/tharsis/evmos/x/inflation/types"
 )
 
 func (k Keeper) BeforeEpochStart(_ sdk.Context, _ string, _ int64) {
 }
 
 func (k Keeper) AfterEpochEnd(ctx sdk.Context, epochIdentifier string, epochNumber int64) {
-	// TODO daily epoch logic
-	// return
-	// // check if epochIdentifier signal equals the identifier in the params
-	// if epochIdentifier != params.EpochIdentifier {
-	// 	return
-	// }
-
-	// // not distribute rewards if it's not time yet for rewards allocation
-	// if epochNumber < params.MintingRewardsAllocationStartEpoch {
-	// 	return
-	// } else if epochNumber == params.MintingRewardsAllocationStartEpoch {
-	// 	k.SetLastHalvenEpochNum(ctx, epochNumber)
-	// }
-	// // fetch stored minter & params
-	// minter := k.GetMinter(ctx)
-	// // params := k.GetParams(ctx)
-
-	// // Check if we have hit an epoch where we update the inflation parameter.
-	// // Since epochs only update based on BFT time data, it is safe to store the
-	// // "halvening period time" in terms of the number of epochs that have
-	// // transpired.
-	// if epochNumber >= k.GetParams(ctx).ReductionPeriodInEpochs+k.GetLastHalvenEpochNum(ctx) {
-	// 	// Halven the reward per halven period
-	// 	minter.EpochProvisions = minter.NextEpochProvisions(params)
-	// 	k.SetMinter(ctx, minter)
-	// 	k.SetLastHalvenEpochNum(ctx, epochNumber)
-	// }
+	params := k.GetParams(ctx)
 
 	// mint coins, update supply
-	// epochMintProvision, found := k.GetEpochMintProvision(ctx)
-	// if found {
-	// 	panic("the epochMintProvision has was not found")
-	// }
+	epochMintProvision, found := k.GetEpochMintProvision(ctx)
+	if !found {
+		panic("the epochMintProvision was not found")
+	}
 
-	// mintedCoin := sdk.NewCoin(params.MintDenom, epochMintProvision.TruncateInt())
-	// // We over-allocate by the developer vesting portion, and burn this later
-	// if err := k.MintAndAllocateInflation(ctx, mintedCoin); err != nil {
-	// 	panic(err)
-	// }
+	mintedCoin := sdk.NewCoin(params.MintDenom, epochMintProvision.TruncateInt())
+	if err := k.MintAndAllocateInflation(ctx, mintedCoin); err != nil {
+		panic(err)
+	}
 
-	// if mintedCoin.Amount.IsInt64() {
-	// 	defer telemetry.ModuleSetGauge(types.ModuleName, float32(mintedCoin.Amount.Int64()), "minted_tokens")
-	// }
+	// check if a period is over
+	period := k.GetPeriod(ctx)
 
-	// ctx.EventManager().EmitEvent(
-	// 	sdk.NewEvent(
-	// 		types.EventTypeMint,
-	// 		sdk.NewAttribute(types.AttributeEpochNumber, fmt.Sprintf("%d", epochNumber)),
-	// 		// sdk.NewAttribute(types.AttributeKeyEpochProvisions, minter.EpochProvisions.String()),
-	// 		sdk.NewAttribute(sdk.AttributeKeyAmount, mintedCoin.Amount.String()),
-	// 	),
-	// )
+	if epochNumber-params.EpochsPerPeriod*int64(period) > params.EpochsPerPeriod {
+		period++
+		k.SetPeriod(ctx, period)
+		newProvision := types.CalculateEpochMintProvision(params, k.GetPeriod(ctx))
+		k.SetEpochMintProvision(ctx, newProvision)
+	}
+
+	if mintedCoin.Amount.IsInt64() {
+		defer telemetry.ModuleSetGauge(types.ModuleName, float32(mintedCoin.Amount.Int64()), "minted_tokens")
+	}
+
+	ctx.EventManager().EmitEvent(
+		sdk.NewEvent(
+			types.EventTypeMint,
+			sdk.NewAttribute(types.AttributeEpochNumber, fmt.Sprintf("%d", epochNumber)),
+			// sdk.NewAttribute(types.AttributeKeyEpochProvisions, minter.EpochProvisions.String()),
+			sdk.NewAttribute(sdk.AttributeKeyAmount, mintedCoin.Amount.String()),
+		),
+	)
 }
 
 // ___________________________________________________________________________________________________
