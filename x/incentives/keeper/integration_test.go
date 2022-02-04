e023cefdd8b2d53a0c397e732ae2d5c20daaceaf
Daniel Burckhardt <daniel.m.burckhardt@gmail.com>
2022-02-04 14:51:52 +0100

    wip

---
+++ x/incentives/keeper/integration_test.go
@@ -0,0 +1,14 @@
+package keeper_test
+
+import (
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+)
+
+var _ = Describe("Integration", func() {
+
+})
+
+// ?
+// Run Gingko on after keeper tests
+// Have access to keeper suite app and keeper methods
