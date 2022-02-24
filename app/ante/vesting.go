9088d0c2cbf23e044439195fabfff1c3aac07e3a
Daniel Burckhardt <daniel.m.burckhardt@gmail.com>
2022-02-24 16:05:30 +0100

    refactor ante vesting errors

--- app/ante/vesting.go
+++ app/ante/vesting.go
@@ -34,12 +34,15 @@ func (vtd EthVestingTransactionDecorator) AnteHandle(ctx sdk.Context, tx sdk.Tx,
 	for _, msg := range tx.GetMsgs() {
 		msgEthTx, ok := msg.(*evmtypes.MsgEthereumTx)
 		if !ok {
-			return ctx, sdkerrors.Wrapf(sdkerrors.ErrUnknownRequest, "invalid message type %T, expected %T", msg, (*evmtypes.MsgEthereumTx)(nil))
+			return ctx, sdkerrors.Wrapf(sdkerrors.ErrUnknownRequest,
+				"invalid message type %T, expected %T", msg, (*evmtypes.MsgEthereumTx)(nil),
+			)
 		}
 
 		acc := vtd.ak.GetAccount(ctx, msgEthTx.GetFrom())
 		if acc == nil {
-			return ctx, sdkerrors.Wrapf(sdkerrors.ErrUnknownAddress, "account %s does not exist", acc)
+			return ctx, sdkerrors.Wrapf(sdkerrors.ErrUnknownAddress,
+				"account %s does not exist", acc)
 		}
 
 		// Check that this decorator only applies to clawback vesting accounts
@@ -53,7 +56,7 @@ func (vtd EthVestingTransactionDecorator) AnteHandle(ctx sdk.Context, tx sdk.Tx,
 		// grant while there are already vested coins on the account.
 		vested := clawbackAccount.GetVestedCoins(ctx.BlockTime())
 		if len(vested) == 0 {
-			return ctx, sdkerrors.Wrapf(sdkerrors.ErrUnknownRequest,
+			return ctx, sdkerrors.Wrapf(vestingtypes.ErrInsufficientVestedCoins,
 				"cannot perform Ethereum tx with clawback vesting account, that has no vested coins: %s", vested,
 			)
 		}
@@ -61,7 +64,7 @@ func (vtd EthVestingTransactionDecorator) AnteHandle(ctx sdk.Context, tx sdk.Tx,
 		// Error if account has locked coins (before surpassing all lockup periods)
 		islocked := clawbackAccount.HasLockedCoins(ctx.BlockTime())
 		if islocked {
-			return ctx, sdkerrors.Wrapf(sdkerrors.ErrUnknownRequest,
+			return ctx, sdkerrors.Wrapf(vestingtypes.ErrVestingLockup,
 				"cannot perform Ethereum tx with clawback vesting account, that has locked coins: %s", vested,
 			)
 		}
@@ -84,8 +87,9 @@ func NewVestingDelegationDecorator(ak evmtypes.AccountKeeper, sk vestingtypes.St
 	}
 }
 
-// AnteHandle checks if the tx contains a staking delegation
-// It error if the coins are still locked or the bond amount is greater than the coins already vested
+// AnteHandle checks if the tx contains a staking delegation.
+// It errors if the coins are still locked or the bond amount is greater than
+// the coins already vested
 func (vdd VestingDelegationDecorator) AnteHandle(ctx sdk.Context, tx sdk.Tx, simulate bool, next sdk.AnteHandler) (newCtx sdk.Context, err error) {
 	for _, msg := range tx.GetMsgs() {
 		// Continue only if delegation
@@ -98,7 +102,9 @@ func (vdd VestingDelegationDecorator) AnteHandle(ctx sdk.Context, tx sdk.Tx, sim
 
 			acc := vdd.ak.GetAccount(ctx, addr)
 			if acc == nil {
-				return ctx, sdkerrors.Wrapf(sdkerrors.ErrUnknownAddress, "account %s does not exist", addr)
+				return ctx, sdkerrors.Wrapf(sdkerrors.ErrUnknownAddress,
+					"account %s does not exist", addr,
+				)
 			}
 
 			clawbackAccount, isPeriodicVesting := acc.(*vestingtypes.ClawbackVestingAccount)
@@ -109,10 +115,9 @@ func (vdd VestingDelegationDecorator) AnteHandle(ctx sdk.Context, tx sdk.Tx, sim
 
 			// error if bond amount is > vested coins
 			coins := clawbackAccount.GetVestedOnly(ctx.BlockHeader().Time)
-			// vested := coins.AmountOf(stakingtypes.DefaultParams().BondDenom)
 			vested := coins.AmountOf(vdd.sk.BondDenom(ctx))
 			if vested.LT(delegateMsg.Amount.Amount) {
-				return ctx, sdkerrors.Wrapf(sdkerrors.ErrUnknownRequest,
+				return ctx, sdkerrors.Wrapf(vestingtypes.ErrInsufficientVestedCoins,
 					"cannot delegate unvested coins. Coins Vested: %x", vested,
 				)
 			}
