a59eeace404a9e21d0ddfcd82712640ff6438c5f
Daniel Burckhardt <daniel.m.burckhardt@gmail.com>
2022-02-26 20:16:26 +0100

    add grpc query tests

--- x/vesting/keeper/grpc_query_test.go
+++ x/vesting/keeper/grpc_query_test.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 
 	sdk "github.com/cosmos/cosmos-sdk/types"
+	authtypes "github.com/cosmos/cosmos-sdk/x/auth/types"
 
 	"github.com/tharsis/ethermint/tests"
 	"github.com/tharsis/evmos/testutil"
@@ -11,86 +12,62 @@ import (
 )
 
 func (suite *KeeperTestSuite) TestUnvested() {
-	ctx := sdk.WrapSDKContext(suite.ctx)
-
-	req := &types.QueryUnvestedRequest{}
+	var (
+		req    *types.QueryUnvestedRequest
+		expRes *types.QueryUnvestedResponse
+	)
 	addr := sdk.AccAddress(tests.GenerateAddress().Bytes())
 
 	testCases := []struct {
 		name     string
 		malleate func()
-		expErr   bool
+		expPass  bool
 	}{
-		// {
-		// 	"empty req", func() {}, true,
-		// },
-		// {
-		// 	"invalid address",
-		// 	func() {
-		// 		req = &types.QueryUnvestedRequest{
-		// 			Address: "evmos1",
-		// 		}
-		// 	},
-		// 	true,
-		// },
-		// {
-		// 	"uvnested Tokens not found for address",
-		// 	func() {
-		// 		req = &types.QueryUnvestedRequest{
-		// 			Address: addr.String(),
-		// 		}
-		// 	},
-		// 	true,
-		// },
-		// {
-		// 	"valid, without funding",
-		// 	func() {
-		// 		// Create and fund periodic vesting account
-		// 		vestingStart := s.ctx.BlockTime()
-		// 		baseAccount := authtypes.NewBaseAccountWithAddress(addr)
-		// 		funder := sdk.AccAddress(types.ModuleName)
-		// 		clawbackAccount := types.NewClawbackVestingAccount(
-		// 			baseAccount,
-		// 			funder,
-		// 			balances,
-		// 			vestingStart,
-		// 			lockupPeriods,
-		// 			vestingPeriods,
-		// 		)
-		// 		acc := s.app.AccountKeeper.NewAccount(s.ctx, clawbackAccount)
-		// 		s.app.AccountKeeper.SetAccount(s.ctx, acc)
+		{
+			"empty req",
+			func() {
+				req = &types.QueryUnvestedRequest{}
+			},
+			false,
+		},
+		{
+			"invalid address",
+			func() {
+				req = &types.QueryUnvestedRequest{
+					Address: "evmos1",
+				}
+			},
+			false,
+		},
+		{
+			"invalid account - not found",
+			func() {
+				req = &types.QueryUnvestedRequest{
+					Address: addr.String(),
+				}
+			},
+			false,
+		},
+		{
+			"invalid account - not clawback vesting account",
+			func() {
+				baseAccount := authtypes.NewBaseAccountWithAddress(addr)
+				acc := suite.app.AccountKeeper.NewAccount(suite.ctx, baseAccount)
+				suite.app.AccountKeeper.SetAccount(suite.ctx, acc)
 
-		// 		req = &types.QueryUnvestedRequest{
-		// 			Address: addr.String(),
-		// 		}
-		// 	},
-		// 	false,
-		// },
+				req = &types.QueryUnvestedRequest{
+					Address: addr.String(),
+				}
+			},
+			false,
+		},
 		{
 			"valid",
 			func() {
-				// Create and fund periodic vesting account
-
-				// vestingStart := s.ctx.BlockTime()
-				// baseAccount := authtypes.NewBaseAccountWithAddress(addr)
-				// funder := sdk.AccAddress(types.ModuleName)
-				// clawbackAccount := types.NewClawbackVestingAccount(
-				// 	baseAccount,
-				// 	funder,
-				// 	balances,
-				// 	vestingStart,
-				// 	lockupPeriods,
-				// 	vestingPeriods,
-				// )
-				// err := testutil.FundAccount(s.app.BankKeeper, s.ctx, addr, balances)
-				// s.Require().NoError(err)
-				// acc := s.app.AccountKeeper.NewAccount(s.ctx, clawbackAccount)
-				// s.app.AccountKeeper.SetAccount(s.ctx, acc)
-
 				vestingStart := s.ctx.BlockTime()
 				funder := sdk.AccAddress(types.ModuleName)
-				err := testutil.FundAccount(s.app.BankKeeper, s.ctx, funder, balances)
-				s.Require().NoError(err)
+				err := testutil.FundAccount(suite.app.BankKeeper, suite.ctx, funder, balances)
+				suite.Require().NoError(err)
 
 				msg := types.NewMsgCreateClawbackVestingAccount(
 					funder,
@@ -100,40 +77,224 @@ func (suite *KeeperTestSuite) TestUnvested() {
 					vestingPeriods,
 					false,
 				)
-				_, err = s.app.VestingKeeper.CreateClawbackVestingAccount(ctx, msg)
-				s.Require().NoError(err)
+				ctx := sdk.WrapSDKContext(suite.ctx)
+				_, err = suite.app.VestingKeeper.CreateClawbackVestingAccount(ctx, msg)
+				suite.Require().NoError(err)
 
 				req = &types.QueryUnvestedRequest{
+					Address: addr.String(),
+				}
+				expRes = &types.QueryUnvestedResponse{Unvested: balances}
+			},
+			true,
+		},
+	}
+
+	for _, tc := range testCases {
+		suite.Run(fmt.Sprintf("Case %s", tc.name), func() {
+			suite.SetupTest() // reset
+			ctx := sdk.WrapSDKContext(suite.ctx)
+			tc.malleate()
+
+			res, err := suite.queryClient.Unvested(ctx, req)
+			if tc.expPass {
+				suite.Require().NoError(err)
+				suite.Require().Equal(expRes, res)
+			} else {
+				suite.Require().Error(err)
+			}
+		})
+	}
+}
+
+func (suite *KeeperTestSuite) TestVested() {
+	var (
+		req    *types.QueryVestedRequest
+		expRes *types.QueryVestedResponse
+	)
+	addr := sdk.AccAddress(tests.GenerateAddress().Bytes())
+
+	testCases := []struct {
+		name     string
+		malleate func()
+		expPass  bool
+	}{
+		{
+			"empty req",
+			func() {
+				req = &types.QueryVestedRequest{}
+			},
+			false,
+		},
+		{
+			"invalid address",
+			func() {
+				req = &types.QueryVestedRequest{
+					Address: "evmos1",
+				}
+			},
+			false,
+		},
+		{
+			"invalid account - not found",
+			func() {
+				req = &types.QueryVestedRequest{
 					Address: addr.String(),
 				}
 			},
 			false,
 		},
-		// {
-		// 	"valid, non empty claimable amounts",
-		// 	func() {
-		// 		claimsRecord := types.NewUnvested(sdk.NewInt(1_000_000_000_000))
-		// 		suite.app.ClaimsKeeper.SetUnvested(suite.ctx, addr, claimsRecord)
-		// 		req = &types.QueryUnvestedRequest{
-		// 			Address: addr.String(),
-		// 		}
-		// 	},
-		// 	false,
-		// },
+		{
+			"invalid account - not clawback vesting account",
+			func() {
+				baseAccount := authtypes.NewBaseAccountWithAddress(addr)
+				acc := suite.app.AccountKeeper.NewAccount(suite.ctx, baseAccount)
+				suite.app.AccountKeeper.SetAccount(suite.ctx, acc)
+
+				req = &types.QueryVestedRequest{
+					Address: addr.String(),
+				}
+			},
+			false,
+		},
+		{
+			"valid",
+			func() {
+				vestingStart := s.ctx.BlockTime()
+				funder := sdk.AccAddress(types.ModuleName)
+				err := testutil.FundAccount(suite.app.BankKeeper, suite.ctx, funder, balances)
+				suite.Require().NoError(err)
+
+				msg := types.NewMsgCreateClawbackVestingAccount(
+					funder,
+					addr,
+					vestingStart,
+					lockupPeriods,
+					vestingPeriods,
+					false,
+				)
+				ctx := sdk.WrapSDKContext(suite.ctx)
+				_, err = suite.app.VestingKeeper.CreateClawbackVestingAccount(ctx, msg)
+				suite.Require().NoError(err)
+
+				req = &types.QueryVestedRequest{
+					Address: addr.String(),
+				}
+				expRes = &types.QueryVestedResponse{}
+			},
+			true,
+		},
 	}
 
 	for _, tc := range testCases {
-		fmt.Println(addr.String())
+		suite.Run(fmt.Sprintf("Case %s", tc.name), func() {
+			suite.SetupTest() // reset
+			ctx := sdk.WrapSDKContext(suite.ctx)
+			tc.malleate()
 
-		tc.malleate()
+			res, err := suite.queryClient.Vested(ctx, req)
+			if tc.expPass {
+				suite.Require().NoError(err)
+				suite.Require().Equal(expRes, res)
+			} else {
+				suite.Require().Error(err)
+			}
+		})
+	}
+}
 
-		res, err := suite.queryClient.Unvested(ctx, req)
+func (suite *KeeperTestSuite) TestLocked() {
+	var (
+		req    *types.QueryLockedRequest
+		expRes *types.QueryLockedResponse
+	)
+	addr := sdk.AccAddress(tests.GenerateAddress().Bytes())
 
-		if tc.expErr {
-			suite.Require().Error(err)
-		} else {
-			suite.Require().NoError(err)
-			suite.Require().Equal(balances, res.Unvested)
-		}
+	testCases := []struct {
+		name     string
+		malleate func()
+		expPass  bool
+	}{
+		{
+			"empty req",
+			func() {
+				req = &types.QueryLockedRequest{}
+			},
+			false,
+		},
+		{
+			"invalid address",
+			func() {
+				req = &types.QueryLockedRequest{
+					Address: "evmos1",
+				}
+			},
+			false,
+		},
+		{
+			"invalid account - not found",
+			func() {
+				req = &types.QueryLockedRequest{
+					Address: addr.String(),
+				}
+			},
+			false,
+		},
+		{
+			"invalid account - not clawback vesting account",
+			func() {
+				baseAccount := authtypes.NewBaseAccountWithAddress(addr)
+				acc := suite.app.AccountKeeper.NewAccount(suite.ctx, baseAccount)
+				suite.app.AccountKeeper.SetAccount(suite.ctx, acc)
+
+				req = &types.QueryLockedRequest{
+					Address: addr.String(),
+				}
+			},
+			false,
+		},
+		{
+			"valid",
+			func() {
+				vestingStart := s.ctx.BlockTime()
+				funder := sdk.AccAddress(types.ModuleName)
+				err := testutil.FundAccount(suite.app.BankKeeper, suite.ctx, funder, balances)
+				suite.Require().NoError(err)
+
+				msg := types.NewMsgCreateClawbackVestingAccount(
+					funder,
+					addr,
+					vestingStart,
+					lockupPeriods,
+					vestingPeriods,
+					false,
+				)
+				ctx := sdk.WrapSDKContext(suite.ctx)
+				_, err = suite.app.VestingKeeper.CreateClawbackVestingAccount(ctx, msg)
+				suite.Require().NoError(err)
+
+				req = &types.QueryLockedRequest{
+					Address: addr.String(),
+				}
+				expRes = &types.QueryLockedResponse{Locked: balances}
+			},
+			true,
+		},
+	}
+
+	for _, tc := range testCases {
+		suite.Run(fmt.Sprintf("Case %s", tc.name), func() {
+			suite.SetupTest() // reset
+			ctx := sdk.WrapSDKContext(suite.ctx)
+			tc.malleate()
+
+			res, err := suite.queryClient.Locked(ctx, req)
+			if tc.expPass {
+				suite.Require().NoError(err)
+				suite.Require().Equal(expRes, res)
+			} else {
+				suite.Require().Error(err)
+			}
+		})
 	}
 }
