5ba586cefe35c3182d4c094c571af4b95cb0f7c8
Federico Kunze Küllmer <federico.kunze94@gmail.com>
2022-02-28 14:28:29 +0100

    add missing modules

--- scripts/protoc-swagger-gen.sh
+++ scripts/protoc-swagger-gen.sh
@@ -5,17 +5,17 @@ set -eo pipefail
 mkdir -p ./tmp-swagger-gen
 proto_dirs=$(find ./proto ./third_party/proto -path -prune -o -name '*.proto' -print0 | xargs -0 -n1 dirname | sort | uniq)
 for dir in $proto_dirs; do
-    
-    # generate swagger files (filter query files)
-    query_file=$(find "${dir}" -maxdepth 1 -name 'query.proto')
-    if [[ ! -z "$query_file" ]]; then
-        protoc  \
-        -I "proto" \
-        -I "third_party/proto" \
-        "$query_file" \
-        --swagger_out ./tmp-swagger-gen \
-        --swagger_opt logtostderr=true --swagger_opt fqn_for_swagger_name=true --swagger_opt simple_operation_ids=true
-    fi
+
+  # generate swagger files (filter query files)
+  query_file=$(find "${dir}" -maxdepth 1 \( -name 'query.proto' -o -name 'service.proto' \))
+  if [[ ! -z "$query_file" ]]; then
+    buf protoc  \
+    -I "proto" \
+    -I "third_party/proto" \
+    "$query_file" \
+    --swagger_out=./tmp-swagger-gen \
+    --swagger_opt=logtostderr=true --swagger_opt=fqn_for_swagger_name=true --swagger_opt=simple_operation_ids=true
+  fi
 done
 
 # combine swagger files
