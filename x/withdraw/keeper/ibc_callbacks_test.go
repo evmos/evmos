0340ef78127d549a3d7fdc0bf71f27b73a42d8e8
Daniel Burckhardt <daniel.m.burckhardt@gmail.com>
2022-03-04 18:40:18 +0100

    tests(withdraw): integraition tests wip

---
+++ x/withdraw/keeper/ibc_callbacks_test.go
@@ -0,0 +1,92 @@
+package keeper_test
+
+import (
+	"testing"
+
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+
+	"github.com/stretchr/testify/suite"
+
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	clienttypes "github.com/cosmos/ibc-go/v3/modules/core/02-client/types"
+	channeltypes "github.com/cosmos/ibc-go/v3/modules/core/04-channel/types"
+	ibcgotesting "github.com/cosmos/ibc-go/v3/testing"
+
+	"github.com/tharsis/evmos/ibctesting"
+
+	"github.com/tharsis/evmos/app"
+	"github.com/tharsis/evmos/x/claims/types"
+	inflationtypes "github.com/tharsis/evmos/x/inflation/types"
+)
+
+type IBCTestingSuite struct {
+	suite.Suite
+	coordinator *ibctesting.Coordinator
+
+	// testing chains used for convenience and readability
+	chainA *ibctesting.TestChain
+	chainB *ibctesting.TestChain
+
+	path *ibctesting.Path
+}
+
+var s *IBCTestingSuite
+
+func TestIBCTestingSuite(t *testing.T) {
+	s = new(IBCTestingSuite)
+	suite.Run(t, s)
+
+	// Run Ginkgo integration tests
+	RegisterFailHandler(Fail)
+	RunSpecs(t, "IBC Suite")
+}
+
+func (suite *IBCTestingSuite) SetupTest() {
+	ibcgotesting.DefaultTestingAppInit = app.SetupTestingApp
+
+	ibcgotesting.ChainIDPrefix = "evmos_9000-"
+	suite.coordinator = ibctesting.NewCoordinator(suite.T(), 2)         // initializes 2 test chains
+	suite.chainA = suite.coordinator.GetChain(ibctesting.GetChainID(1)) // convenience and readability
+	suite.chainB = suite.coordinator.GetChain(ibctesting.GetChainID(2)) // convenience and readability
+	suite.coordinator.CommitNBlocks(suite.chainA, 2)
+	suite.coordinator.CommitNBlocks(suite.chainB, 2)
+
+	coins := sdk.NewCoins(sdk.NewCoin("aevmos", sdk.NewInt(10000)))
+	err := suite.chainB.App.(*app.Evmos).BankKeeper.MintCoins(suite.chainB.GetContext(), inflationtypes.ModuleName, coins)
+	suite.Require().NoError(err)
+	err = suite.chainB.App.(*app.Evmos).BankKeeper.SendCoinsFromModuleToModule(suite.chainB.GetContext(), inflationtypes.ModuleName, types.ModuleName, coins)
+	suite.Require().NoError(err)
+
+	err = suite.chainA.App.(*app.Evmos).BankKeeper.MintCoins(suite.chainA.GetContext(), inflationtypes.ModuleName, coins)
+	suite.Require().NoError(err)
+	err = suite.chainA.App.(*app.Evmos).BankKeeper.SendCoinsFromModuleToModule(suite.chainA.GetContext(), inflationtypes.ModuleName, types.ModuleName, coins)
+	suite.Require().NoError(err)
+
+	params := types.DefaultParams()
+	params.AirdropStartTime = suite.chainA.GetContext().BlockTime()
+	params.EnableClaims = true
+	suite.chainA.App.(*app.Evmos).ClaimsKeeper.SetParams(suite.chainA.GetContext(), params)
+	suite.chainB.App.(*app.Evmos).ClaimsKeeper.SetParams(suite.chainB.GetContext(), params)
+
+	suite.path = NewTransferPath(suite.chainA, suite.chainB) // clientID, connectionID, channelID empty
+	suite.coordinator.Setup(suite.path)                      // clientID, connectionID, channelID filled
+	suite.Require().Equal("07-tendermint-0", suite.path.EndpointA.ClientID)
+	suite.Require().Equal("connection-0", suite.path.EndpointA.ConnectionID)
+	suite.Require().Equal("channel-0", suite.path.EndpointA.ChannelID)
+}
+
+var timeoutHeight = clienttypes.NewHeight(1000, 1000)
+
+func NewTransferPath(chainA, chainB *ibctesting.TestChain) *ibctesting.Path {
+	path := ibctesting.NewPath(chainA, chainB)
+	path.EndpointA.ChannelConfig.PortID = ibcgotesting.TransferPort
+	path.EndpointB.ChannelConfig.PortID = ibcgotesting.TransferPort
+
+	path.EndpointA.ChannelConfig.Order = channeltypes.UNORDERED
+	path.EndpointB.ChannelConfig.Order = channeltypes.UNORDERED
+	path.EndpointA.ChannelConfig.Version = "ics20-1"
+	path.EndpointB.ChannelConfig.Version = "ics20-1"
+
+	return path
+}
---
+++ x/withdraw/keeper/integration_test.go
@@ -0,0 +1,110 @@
+package keeper_test
+
+import (
+	"fmt"
+
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+
+	"github.com/cosmos/cosmos-sdk/crypto/keys/secp256k1"
+	"github.com/ethereum/go-ethereum/common"
+
+	transfertypes "github.com/cosmos/ibc-go/v3/modules/apps/transfer/types"
+	channeltypes "github.com/cosmos/ibc-go/v3/modules/core/04-channel/types"
+	host "github.com/cosmos/ibc-go/v3/modules/core/24-host"
+
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	authtypes "github.com/cosmos/cosmos-sdk/x/auth/types"
+
+	"github.com/tharsis/evmos/app"
+	"github.com/tharsis/evmos/x/withdraw/types"
+)
+
+// The IBC Callback transfers all non-evmos balances from the receiver to the
+// sender address for a receiver with a secp256k1 key
+var _ = Describe("Performing a IBC transfer with enabled callback ", Ordered, func() {
+
+	var (
+		sender   sdk.AccAddress
+		receiver sdk.AccAddress
+	)
+
+	BeforeEach(func() {
+		s.SetupTest()
+
+		// Deactivate IBC callback
+		params := types.DefaultParams()
+		params.EnableWithdraw = false
+		s.chainB.App.(*app.Evmos).WithdrawKeeper.SetParams(s.chainB.GetContext(), params)
+
+		// Set sender
+		sender = s.chainA.SenderAccount.GetAddress()
+
+		// Create receiver with secp256k1 address
+		priv := secp256k1.GenPrivKey()
+		receiverHash := common.BytesToAddress(priv.PubKey().Address().Bytes())
+		receiver = sdk.AccAddress(receiverHash.Bytes())
+		baseAcc := authtypes.NewBaseAccountWithAddress(receiver)
+		s.chainB.App.(*app.Evmos).AccountKeeper.SetAccount(s.chainB.GetContext(), baseAcc)
+		fmt.Println(receiver.String())
+		// receiver = s.chainB.SenderAccount.GetAddress()
+
+		path := s.path
+		transfer := transfertypes.NewFungibleTokenPacketData("aevmos", "100", sender.String(), receiver.String())
+		bz := transfertypes.ModuleCdc.MustMarshalJSON(&transfer)
+		packet := channeltypes.NewPacket(bz, 1, path.EndpointA.ChannelConfig.PortID, path.EndpointA.ChannelID, path.EndpointB.ChannelConfig.PortID, path.EndpointB.ChannelID, timeoutHeight, 0)
+
+		// send on endpointA
+		s.path.EndpointA.SendPacket(packet)
+
+		// receive on endpointB
+		err := path.EndpointB.RecvPacket(packet)
+		s.Require().NoError(err)
+
+		balances := s.chainB.App.(*app.Evmos).BankKeeper.GetAllBalances(s.chainB.GetContext(), receiver)
+		coins := sdk.NewCoins(sdk.NewCoin("ibc/8EAC8061F4499F03D2D1419A3E73D346289AE9DB89CAB1486B72539572B1915E", sdk.NewInt(100)))
+		s.Require().Equal(coins, balances)
+
+		// Activate IBC callback
+		params.EnableWithdraw = true
+		s.chainB.App.(*app.Evmos).WithdrawKeeper.SetParams(s.chainB.GetContext(), params)
+	})
+
+	Context("to a secp256k1 receiver address with balance", func() {
+		It("transfers all receiver balances to the respective chains", func() {
+			path := s.path
+
+			// send coin from chainA to chainB
+			coin := sdk.NewCoin("aevmos", sdk.NewInt(100))
+			transferMsg := transfertypes.NewMsgTransfer(path.EndpointA.ChannelConfig.PortID, path.EndpointA.ChannelID, coin, sender.String(), receiver.String(), timeoutHeight, 0)
+			_, err := s.chainA.SendMsgs(transferMsg)
+			s.Require().NoError(err) // message committed
+
+			// receive coin on chainB from chainA
+			fungibleTokenPacket := transfertypes.NewFungibleTokenPacketData(coin.Denom, coin.Amount.String(), sender.String(), receiver.String())
+			packet := channeltypes.NewPacket(fungibleTokenPacket.GetBytes(), 2, path.EndpointA.ChannelConfig.PortID, path.EndpointA.ChannelID, path.EndpointB.ChannelConfig.PortID, path.EndpointB.ChannelID, timeoutHeight, 0)
+
+			// get proof of packet commitment from chainA
+			err = path.EndpointB.UpdateClient()
+			s.Require().NoError(err)
+			packetKey := host.PacketCommitmentKey(packet.GetSourcePort(), packet.GetSourceChannel(), packet.GetSequence())
+			proof, proofHeight := path.EndpointA.QueryProof(packetKey)
+
+			recvMsg := channeltypes.NewMsgRecvPacket(packet, proof, proofHeight, receiver.String())
+			_, err = s.chainB.SendMsgs(recvMsg)
+			s.Require().NoError(err) // message committed
+
+			// receiver balance is 0
+			balances := s.chainB.App.(*app.Evmos).BankKeeper.GetAllBalances(s.chainB.GetContext(), receiver)
+			fmt.Println(balances)
+			Expect(balances.IsZero()).To(BeTrue())
+
+			// sender balance is original receiver balance
+
+		})
+	})
+})
+
+// func transferIBC() {
+
+// }
