ba858dcd230bf13e2f3f73aa95597c7ef54b13ca
Akash <akash@thars.is>
2022-03-05 11:47:01 -0800

    app: set up upgrade handler for v2
    use newer sdk version instructions
    
    run migrations and set modules versions to 1
    
    add TODO
    
    use osmosis style upgrade handlers
    
    remove upgrade handler for olympus mons
    
    make note of inflation module change as well
    
    add store loaders upgrade
    
    set the params for erc20 module
    
    set the params for claims
    
    pass in keeers to upgrade handler
    
    apply upgrade at specified height
    
    remove store loaders upgrade
    
    bump the claims consensus version
    
    add binaries info to upgrade info
    
    add new claims params
    
    add comment
    
    throw upgrade plan at cosmovisor
    
    clean up changes that should be in v2
    
    clean up changes that should be in v2, again
    
    remove extra dependency

---
+++ app/forks.go
@@ -0,0 +1,27 @@
+package app
+
+import (
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/cosmos/cosmos-sdk/x/upgrade"
+	upgradetypes "github.com/cosmos/cosmos-sdk/x/upgrade/types"
+	v2 "github.com/tharsis/evmos/app/upgrades/v2"
+)
+
+// BeginBlockForks executes any necessary fork logic based upon the current block height.
+func BeginBlockForks(ctx sdk.Context, app *Evmos) {
+	switch ctx.BlockHeight() {
+	case v2.UpgradeHeight:
+		upgradePlan := upgradetypes.Plan{
+			Name:   v2.UpgradeName,
+			Info:   v2.UpgradeInfo,
+			Height: v2.UpgradeHeight,
+		}
+		upgradeMsg := upgrade.BuildUpgradeNeededMsg(upgradePlan)
+		ctx.Logger().Error(upgradeMsg)
+
+		panic(upgradeMsg)
+	default:
+		// do nothing
+		return
+	}
+}
