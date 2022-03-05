f796648cf83bfd1e04103379aad6db98ac57f348
Akash <akash@thars.is>
2022-03-05 11:59:58 -0800

    use newer sdk version instructions

--- app/upgrade_handlers.go
+++ app/upgrade_handlers.go
@@ -2,9 +2,10 @@ package app
 
 import (
 	sdk "github.com/cosmos/cosmos-sdk/types"
-	"github.com/cosmos/cosmos-sdk/x/upgrade"
+	"github.com/cosmos/cosmos-sdk/types/module"
+	upgradetypes "github.com/cosmos/cosmos-sdk/x/upgrade/types"
 )
 
 func SetupUpgradeHandlers(app *Evmos) {
-	app.UpgradeKeeper.SetUpgradeHandler("2.0.0", func(ctx sdk.Context, plan upgrade.Plan) {})
+	app.UpgradeKeeper.SetUpgradeHandler("v2.0.0", func(ctx sdk.Context, plan upgradetypes.Plan, _ module.VersionMap) (module.VersionMap, error) {})
 }
