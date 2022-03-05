84286b577f43a652b3eeb48ae1931e113d8d39db
Akash <akash@thars.is>
2022-03-05 12:19:29 -0800

    run migrations and set modules versions to 1

--- app/upgrade_handlers.go
+++ app/upgrade_handlers.go
@@ -7,5 +7,15 @@ import (
 )
 
 func SetupUpgradeHandlers(app *Evmos) {
-	app.UpgradeKeeper.SetUpgradeHandler("v2.0.0", func(ctx sdk.Context, plan upgradetypes.Plan, _ module.VersionMap) (module.VersionMap, error) {})
+	app.UpgradeKeeper.SetUpgradeHandler("v2.0.0", func(ctx sdk.Context, plan upgradetypes.Plan, _ module.VersionMap) (module.VersionMap, error) {
+
+		// Set all modules "old versions" to 1.
+		// Then the run migrations logic will handle running their upgrade logics
+		// This will skip their InitGenesis
+		fromVM := make(map[string]uint64)
+		for moduleName := range app.mm.Modules {
+			fromVM[moduleName] = 1
+		}
+		return app.mm.RunMigrations(ctx, app.configurator, fromVM)
+	})
 }
