c378c2b5006117e310ecfab087191e79661ea957
Akash <akash@thars.is>
2022-03-06 02:33:47 -0800

    add back height gate

--- app/forks.go
+++ app/forks.go
@@ -8,13 +8,18 @@ import (
 
 // BeginBlockForks executes any necessary fork logic based upon the current block height.
 func BeginBlockForks(ctx sdk.Context, app *Evmos) {
-	upgradePlan := upgradetypes.Plan{
-		Name:   v2.UpgradeName,
-		Info:   v2.UpgradeInfo,
-		Height: v2.UpgradeHeight,
-	}
-	err := app.UpgradeKeeper.ScheduleUpgradeNoHeightCheck(ctx, upgradePlan)
-	if err != nil {
-		panic(err)
-	}
+	switch ctx.BlockHeight() {
+	case v2.UpgradeHeight:
+		upgradePlan := upgradetypes.Plan{
+			Name:   v2.UpgradeName,
+			Info:   v2.UpgradeInfo,
+			Height: v2.UpgradeHeight,
+		}
+		err := app.UpgradeKeeper.ScheduleUpgradeNoHeightCheck(ctx, upgradePlan)
+		if err != nil {
+			panic(err)
+		}
+	default:
+		// do nothing
+		return
 }
