fb94f9943b40ffcab8ff8c0d9c9a04e3e9543803
Akash <akash@thars.is>
2022-03-06 01:25:36 -0800

    remove upgrade height check

--- app/forks.go
+++ app/forks.go
@@ -8,16 +8,10 @@ import (
 
 // BeginBlockForks executes any necessary fork logic based upon the current block height.
 func BeginBlockForks(ctx sdk.Context, app *Evmos) {
-	switch ctx.BlockHeight() {
-	case v2.UpgradeHeight:
-		upgradePlan := upgradetypes.Plan{
-			Name:   v2.UpgradeName,
-			Info:   v2.UpgradeInfo,
-			Height: v2.UpgradeHeight,
-		}
-		app.UpgradeKeeper.ScheduleUpgradeNoHeightCheck(ctx, upgradePlan)
-	default:
-		// do nothing
-		return
+	upgradePlan := upgradetypes.Plan{
+		Name:   v2.UpgradeName,
+		Info:   v2.UpgradeInfo,
+		Height: v2.UpgradeHeight,
 	}
+	app.UpgradeKeeper.ScheduleUpgradeNoHeightCheck(ctx, upgradePlan)
 }
