032fc011c0099b3c2a4c6e86c65d99b794e0da54
Federico Kunze Küllmer <federico.kunze94@gmail.com>
2022-03-06 14:02:20 +0100

    update fork.go

--- app/forks.go
+++ app/forks.go
@@ -1,6 +1,8 @@
 package app
 
 import (
+	"strings"
+
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	upgradetypes "github.com/cosmos/cosmos-sdk/x/upgrade/types"
 	v2 "github.com/tharsis/evmos/app/upgrades/v2"
@@ -10,6 +12,11 @@ import (
 func BeginBlockForks(ctx sdk.Context, app *Evmos) {
 	switch ctx.BlockHeight() {
 	case v2.UpgradeHeight:
+		// NOTE: only run for mainnet
+		if !strings.HasPrefix(ctx.ChainID(), "evmos_9001-") {
+			return
+		}
+
 		upgradePlan := upgradetypes.Plan{
 			Name:   v2.UpgradeName,
 			Info:   v2.UpgradeInfo,
