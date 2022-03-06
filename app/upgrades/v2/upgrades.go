a9c40e9e6b3383126c54f8e34e01ef3a51649d1f
Federico Kunze Küllmer <federico.kunze94@gmail.com>
2022-03-06 14:54:34 +0100

    upgrade handler

--- app/upgrades/v2/upgrades.go
+++ app/upgrades/v2/upgrades.go
@@ -4,28 +4,21 @@ import (
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/cosmos/cosmos-sdk/types/module"
 	upgradetypes "github.com/cosmos/cosmos-sdk/x/upgrade/types"
-	claimstypes "github.com/tharsis/evmos/v2/x/claims/types"
 
 	claimskeeper "github.com/tharsis/evmos/v2/x/claims/keeper"
 	erc20keeper "github.com/tharsis/evmos/v2/x/erc20/keeper"
 )
 
 // CreateUpgradeHandler creates an SDK upgrade handler for v2
-func CreateUpgradeHandler(mm *module.Manager, configurator module.Configurator,
-	erc20Keeper *erc20keeper.Keeper,
-	claimsKeeper *claimskeeper.Keeper,
+func CreateUpgradeHandler(
+	mm *module.Manager,
+	configurator module.Configurator,
+	_ *erc20keeper.Keeper,
+	_ *claimskeeper.Keeper,
 ) upgradetypes.UpgradeHandler {
 	return func(ctx sdk.Context, _ upgradetypes.Plan, vm module.VersionMap) (module.VersionMap, error) {
-		// Set the params for the erc20 module here as we are not bumping consensus breaking
-		erc20Params := erc20Keeper.GetParams(ctx)
-		erc20Params.EnableEVMHook = true
-		erc20Params.EnableErc20 = true
-		erc20Keeper.SetParams(ctx, erc20Params)
-
-		// Set the consensus version to the from version for claims so that InitGenesis doesn't run
-		// with a default empty claim's GenesisState and the migration code is executed.
-		vm[claimstypes.ModuleName] = 1
-
+		// migrate claims and ERC20 module and don't perform custom logic here
+		// Ref: https://docs.cosmos.network/master/building-modules/upgrade.html#registering-migrations
 		return mm.RunMigrations(ctx, configurator, vm)
 	}
 }
