8db9f8c57e7e27577602986fe29c01c58b86063a
Federico Kunze Küllmer <federico.kunze94@gmail.com>
2022-03-06 13:27:30 +0100

    claims migration

---
+++ x/claims/migrations/v2/migration.go
@@ -0,0 +1,23 @@
+package v2
+
+import (
+	"time"
+
+	sdk "github.com/cosmos/cosmos-sdk/types"
+
+	"github.com/tharsis/evmos/x/claims/types"
+)
+
+type ClaimsKeeper interface {
+	GetParams(ctx sdk.Context) types.Params
+	SetParams(ctx sdk.Context, params types.Params)
+}
+
+func UpdateParams(ctx sdk.Context, k ClaimsKeeper) error {
+	claimsParams := k.GetParams(ctx)
+	claimsParams.DurationUntilDecay += time.Hour * 24 * 14 // add 2 weeks
+	// claimsParams.AuthorizedChannels = claimstypes.DefaultAuthorizedChannels
+	// claimsParams.EVMChannels = claimstypes.DefaultEVMChannels
+	k.SetParams(ctx, claimsParams)
+	return nil
+}
