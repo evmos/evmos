2be4d7e8812e65bfbcfe21aeca9456b73b6a2c03
Daniel Burckhardt <daniel.m.burckhardt@gmail.com>
2022-03-09 17:52:24 +0100

    integration(erc20): Add more cases

--- x/erc20/keeper/integration_test.go
+++ x/erc20/keeper/integration_test.go
@@ -11,24 +11,50 @@ var _ = Describe("Performing EVM transactions", Ordered, func() {
 		s.SetupTest()
 
 		params := s.app.Erc20Keeper.GetParams(s.ctx)
-		params.EnableEVMHook = false
+		params.EnableEVMHook = true
+		params.EnableErc20 = true
 		s.app.Erc20Keeper.SetParams(s.ctx, params)
 	})
 
 	// Epoch mechanism for triggering allocation and distribution
-	Context("with the ERC20 module disabled", func() {
+	Context("with the ERC20 module and EVM Hook disabled", func() {
+		BeforeEach(func() {
+			params := s.app.Erc20Keeper.GetParams(s.ctx)
+			params.EnableEVMHook = false
+			params.EnableErc20 = false
+			s.app.Erc20Keeper.SetParams(s.ctx, params)
+		})
 		It("should be successful", func() {
 			_, err := s.DeployContract("coin", "token", erc20Decimals)
 			Expect(err).To(BeNil())
 		})
 	})
 
-	Context("with the ERC20 module enabled", func() {
+	Context("with the ERC20 module disabled", func() {
 		BeforeEach(func() {
 			params := s.app.Erc20Keeper.GetParams(s.ctx)
-			params.EnableEVMHook = true
+			params.EnableErc20 = false
 			s.app.Erc20Keeper.SetParams(s.ctx, params)
 		})
+		It("should be successful", func() {
+			_, err := s.DeployContract("coin", "token", erc20Decimals)
+			Expect(err).To(BeNil())
+		})
+	})
+
+	Context("with the EVMHook disabled", func() {
+		BeforeEach(func() {
+			params := s.app.Erc20Keeper.GetParams(s.ctx)
+			params.EnableEVMHook = false
+			s.app.Erc20Keeper.SetParams(s.ctx, params)
+		})
+		It("should be successful", func() {
+			_, err := s.DeployContract("coin", "token", erc20Decimals)
+			Expect(err).To(BeNil())
+		})
+	})
+
+	Context("with the ERC20 module and EVM Hook enabled", func() {
 		It("should be successful", func() {
 			_, err := s.DeployContract("coin", "token", erc20Decimals)
 			Expect(err).To(BeNil())
