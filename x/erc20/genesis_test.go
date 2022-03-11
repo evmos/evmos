5745101ee12e20cc1e78645cc19f7bde4eba7444
Prosper Onogberie <sirpros@gmail.com>
2022-03-11 15:14:21 +0000

    Added genesis test suite

--- x/erc20/genesis_test.go
+++ x/erc20/genesis_test.go
@@ -1,5 +1,25 @@
 package erc20_test
 
-func TestErc20ExportGenesis(t *testing.T){
+import (
+	"github.com/stretchr/testify/suite"
+)
+
+
+type GenesisTestSuite struct {
+	suite.Suite
+	ctx sdk.Context
+	app     *app.Evmos
+	genesis types.GenesisState
+}
+
+func (suite *GenesisTestSuite) SetupTest(){
+	
+}
+
+func (suite *GenesisTestSuite) TestERC20InitGenesis(t *testing.T){
+
+}
+
+func (suite *GenesisTestSuite) TestErc20ExportGenesis(t *testing.T){
 	
 }
