d26cc8608e33d4ff6833eeef36a62d3b7268cb0c
Daniel Burckhardt <daniel.m.burckhardt@gmail.com>
2022-03-14 14:58:45 +0100

    impr(vesting): add events for msgs

---
+++ x/vesting/types/events.go
@@ -0,0 +1,14 @@
+package types
+
+// erc20 events
+const (
+	EventTypeCreateClawbackVestingAccount = "create_clawback_vesting_account"
+	EventTypeClawback                     = "clawback"
+
+	AttributeKeyCoins       = "coins"
+	AttributeKeyStartTime   = "start_time"
+	AttributeKeyMerge       = "merge"
+	AttributeKeyAccount     = "account"
+	AttributeKeyFunder      = "funder"
+	AttributeKeyDestination = "destination"
+)
