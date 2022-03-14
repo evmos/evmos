7b723af02ae79afd67a0fc8a254e93a02ca27b92
Federico Kunze Küllmer <federico.kunze94@gmail.com>
2022-03-14 10:08:52 +0100

    mv funcs

--- x/withdraw/keeper/keeper.go
+++ x/withdraw/keeper/keeper.go
@@ -2,16 +2,19 @@ package keeper
 
 import (
 	"fmt"
+	"strings"
 
 	"github.com/tendermint/tendermint/libs/log"
 
 	sdk "github.com/cosmos/cosmos-sdk/types"
+	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
 	capabilitytypes "github.com/cosmos/cosmos-sdk/x/capability/types"
 	paramtypes "github.com/cosmos/cosmos-sdk/x/params/types"
 
 	transferkeeper "github.com/cosmos/ibc-go/v3/modules/apps/transfer/keeper"
 	transfertypes "github.com/cosmos/ibc-go/v3/modules/apps/transfer/types"
 	channelkeeper "github.com/cosmos/ibc-go/v3/modules/core/04-channel/keeper"
+	channeltypes "github.com/cosmos/ibc-go/v3/modules/core/04-channel/types"
 	"github.com/cosmos/ibc-go/v3/modules/core/exported"
 
 	"github.com/tharsis/evmos/v2/x/withdraw/types"
@@ -67,4 +70,54 @@ func (k Keeper) Logger(ctx sdk.Context) log.Logger {
 // It calls the underlying SendPacket function directly to move down the middleware stack.
 func (k Keeper) SendPacket(ctx sdk.Context, channelCap *capabilitytypes.Capability, packet exported.PacketI) error {
 	return k.ics4Wrapper.SendPacket(ctx, channelCap, packet)
+}
+
+// GetIBCDenomSource returns the source port and channel of the IBC denomination.
+// It returns an error if the the denomination is invalid or if the denom trace or source channel
+// is not found on the store.
+func (k Keeper) GetIBCDenomSource(ctx sdk.Context, denom, sender string) (srcPort, srcChannel string, err error) {
+	ibcDenom := strings.SplitN(denom, "/", 2)
+	if len(ibcDenom) < 2 {
+		return "", "", sdkerrors.Wrap(transfertypes.ErrInvalidDenomForTransfer, denom)
+	}
+
+	hash, err := transfertypes.ParseHexHash(ibcDenom[1])
+	if err != nil {
+		return "", "", sdkerrors.Wrapf(
+			err,
+			"failed to withdraw IBC vouchers back to sender '%s' in the corresponding IBC chain", sender,
+		)
+	}
+
+	denomTrace, found := k.transferKeeper.GetDenomTrace(ctx, hash)
+	if !found {
+		return "", "", sdkerrors.Wrapf(
+			transfertypes.ErrTraceNotFound,
+			"failed to withdraw IBC vouchers back to sender '%s' in the corresponding IBC chain", sender,
+		)
+	}
+
+	path := strings.Split(denomTrace.Path, "/")
+	if len(path)%2 != 0 {
+		return "", "", sdkerrors.Wrapf(
+			transfertypes.ErrInvalidDenomForTransfer,
+			"invalid denom (%s) trace path %s", denomTrace.BaseDenom, denomTrace.Path,
+		)
+	}
+
+	counterpartyPortID := path[0]
+	counterpartyChannelID := path[1]
+
+	channel, found := k.channelKeeper.GetChannel(ctx, counterpartyPortID, counterpartyChannelID)
+	if !found {
+		return "", "", sdkerrors.Wrapf(
+			channeltypes.ErrChannelNotFound,
+			"port ID %s, channel ID %s", counterpartyPortID, counterpartyChannelID,
+		)
+	}
+
+	srcPort = channel.Counterparty.PortId
+	srcChannel = channel.Counterparty.ChannelId
+
+	return srcPort, srcChannel, nil
 }
