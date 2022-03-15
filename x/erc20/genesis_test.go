e2ada8d16961a23e342935fcc6d281181609b074
Prosper <sirpros@gmail.com>
2022-03-15 08:35:15 +0100

    tests: erc20 init/export genesis (#382)
    * Initial commit for erc20 test
    
    * Added genesis test suite
    
    * init genesis
    
    * Temp changes to TestErc20ExportGenesis
    
    * Test for x/RC20 passed, needs review still
    
    * Clean up anic option
    
    * Apply suggestions from code review
    
    Co-authored-by: Federico Kunze Küllmer <federico.kunze94@gmail.com>
    Co-authored-by: Federico Kunze Küllmer <31522760+fedekunze@users.noreply.github.com>

---
+++ x/erc20/genesis_test.go
@@ -0,0 +1,159 @@
+package erc20_test
+
+import (
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/suite"
+
+	sdk "github.com/cosmos/cosmos-sdk/types"
+
+	"github.com/tendermint/tendermint/crypto/tmhash"
+	tmproto "github.com/tendermint/tendermint/proto/tendermint/types"
+	tmversion "github.com/tendermint/tendermint/proto/tendermint/version"
+	"github.com/tendermint/tendermint/version"
+
+	"github.com/tharsis/ethermint/tests"
+	feemarkettypes "github.com/tharsis/ethermint/x/feemarket/types"
+
+	"github.com/tharsis/evmos/v2/app"
+	"github.com/tharsis/evmos/v2/x/erc20"
+	"github.com/tharsis/evmos/v2/x/erc20/types"
+)
+
+type GenesisTestSuite struct {
+	suite.Suite
+	ctx     sdk.Context
+	app     *app.Evmos
+	genesis types.GenesisState
+}
+
+func TestGenesisTestSuite(t *testing.T) {
+	suite.Run(t, new(GenesisTestSuite))
+}
+
+func (suite *GenesisTestSuite) SetupTest() {
+	// consensus key
+	consAddress := sdk.ConsAddress(tests.GenerateAddress().Bytes())
+
+	suite.app = app.Setup(false, feemarkettypes.DefaultGenesisState())
+	suite.ctx = suite.app.BaseApp.NewContext(false, tmproto.Header{
+		Height:          1,
+		ChainID:         "evmos_9000-1",
+		Time:            time.Now().UTC(),
+		ProposerAddress: consAddress.Bytes(),
+
+		Version: tmversion.Consensus{
+			Block: version.BlockProtocol,
+		},
+		LastBlockId: tmproto.BlockID{
+			Hash: tmhash.Sum([]byte("block_id")),
+			PartSetHeader: tmproto.PartSetHeader{
+				Total: 11,
+				Hash:  tmhash.Sum([]byte("partset_header")),
+			},
+		},
+		AppHash:            tmhash.Sum([]byte("app")),
+		DataHash:           tmhash.Sum([]byte("data")),
+		EvidenceHash:       tmhash.Sum([]byte("evidence")),
+		ValidatorsHash:     tmhash.Sum([]byte("validators")),
+		NextValidatorsHash: tmhash.Sum([]byte("next_validators")),
+		ConsensusHash:      tmhash.Sum([]byte("consensus")),
+		LastResultsHash:    tmhash.Sum([]byte("last_result")),
+	})
+
+	suite.genesis = *types.DefaultGenesisState()
+}
+
+func (suite *GenesisTestSuite) TestERC20InitGenesis() {
+	testCases := []struct {
+		name         string
+		genesisState types.GenesisState
+	}{
+		{
+			"empty genesis",
+			types.GenesisState{},
+		},
+		{
+			"default genesis",
+			*types.DefaultGenesisState(),
+		},
+		{
+			"custom genesis",
+			types.NewGenesisState(
+				types.DefaultParams(),
+				[]types.TokenPair{
+					{
+						Erc20Address:  "0x5dCA2483280D9727c80b5518faC4556617fb19ZZ",
+						Denom:         "coin",
+						Enabled:       true,
+						ContractOwner: types.OWNER_MODULE,
+					},
+				}),
+		},
+	}
+
+	for _, tc := range testCases {
+
+		suite.Require().NotPanics(func() {
+			erc20.InitGenesis(suite.ctx, suite.app.Erc20Keeper, suite.app.AccountKeeper, tc.genesisState)
+		})
+		params := suite.app.Erc20Keeper.GetParams(suite.ctx)
+
+		tokenPairs := suite.app.Erc20Keeper.GetAllTokenPairs(suite.ctx)
+		suite.Require().Equal(tc.genesisState.Params, params)
+		if len(tokenPairs) > 0 {
+			suite.Require().Equal(tc.genesisState.TokenPairs, tokenPairs)
+		} else {
+			suite.Require().Len(tc.genesisState.TokenPairs, 0)
+		}
+	}
+}
+
+func (suite *GenesisTestSuite) TestErc20ExportGenesis() {
+
+	testGenCases := []struct {
+		name         string
+		genesisState types.GenesisState
+	}{
+		{
+			"empty genesis",
+			types.GenesisState{},
+		},
+		{
+			"default genesis",
+			*types.DefaultGenesisState(),
+		},
+		{
+			"custom genesis",
+			types.NewGenesisState(
+				types.DefaultParams(),
+				[]types.TokenPair{
+					{
+						Erc20Address:  "0x5dCA2483280D9727c80b5518faC4556617fb19ZZ",
+						Denom:         "coin",
+						Enabled:       true,
+						ContractOwner: types.OWNER_MODULE,
+					},
+				}),
+		},
+	}
+
+	for _, tc := range testGenCases {
+		erc20.InitGenesis(suite.ctx, suite.app.Erc20Keeper, suite.app.AccountKeeper, tc.genesisState)
+		suite.Require().NotPanics(func() {
+
+			genesisExported := erc20.ExportGenesis(suite.ctx, suite.app.Erc20Keeper)
+			params := suite.app.Erc20Keeper.GetParams(suite.ctx)
+			suite.Require().Equal(genesisExported.Params, params)
+
+			tokenPairs := suite.app.Erc20Keeper.GetAllTokenPairs(suite.ctx)
+			if len(tokenPairs) > 0 {
+				suite.Require().Equal(genesisExported.TokenPairs, tokenPairs)
+			} else {
+				suite.Require().Len(genesisExported.TokenPairs, 0)
+			}
+		})
+		// }
+	}
+}
