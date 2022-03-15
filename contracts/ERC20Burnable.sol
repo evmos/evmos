8d8cfd781a6f07cb7339a74124a1dec4696b7a97
Prajjwol Gautam <not@acyb.org>
2022-03-15 07:13:16 -0700

    add solhint step

--- contracts/ERC20Burnable.sol
+++ contracts/ERC20Burnable.sol
@@ -34,7 +34,10 @@ abstract contract ERC20Burnable is Context, ERC20 {
      */
     function burnFrom(address account, uint256 amount) public virtual {
         uint256 currentAllowance = allowance(account, _msgSender());
-        require(currentAllowance >= amount, "ERC20: burn amount exceeds allowance");
+        require(
+            currentAllowance >= amount,
+            "ERC20: burn amount exceeds allowance"
+        );
         unchecked {
             _approve(account, _msgSender(), currentAllowance - amount);
         }
