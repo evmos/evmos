fe3974d21d94db692d675787f90460ca1b9ea3ae
chengwenxi <22697326+chengwenxi@users.noreply.github.com>
2022-04-12 11:15:12 +0800

    use evmos swagger instead of ethermint

--- client/docs/statik/init.go
+++ client/docs/statik/init.go
@@ -1,3 +1,7 @@
 package statik
 
+// unnamed import of statik for swagger UI support
+// override ethermint statik by importing it
+import _ "github.com/tharsis/ethermint/client/docs/statik"
+
 // This just for fixing the error in importing empty github.com/tharsis/ethermint/client/docs/statik
