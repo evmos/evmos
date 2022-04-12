a6a839a66df67bdb2ad36dd6fd8f442b9af03f88
Loredana <loredana.cirstea@gmail.com>
2022-04-13 00:05:31 +0300

    Update CHANGELOG.md

--- x/fees/types/params.go
+++ x/fees/types/params.go
@@ -12,8 +12,8 @@ var (
 	DefaultEnableFees      = false
 	DefaultDeveloperShares = sdk.NewDecWithPrec(50, 2) // 50%
 	DefaultValidatorShares = sdk.NewDecWithPrec(50, 2) // 50%
-	// cost for `crypto.CreateAddress`
-	// keccak256(word) costs 36 gas
+	// Cost for executing `crypto.CreateAddress`
+	// must be at least 36 gas for the contained keccak256(word) operation
 	DefaultAddrDerivationCostCreate       = uint64(50)
 	ParamStoreKeyEnableFees               = []byte("EnableFees")
 	ParamStoreKeyDeveloperShares          = []byte("DeveloperShares")
