c10b74f302cb804f4bbb597f73f20a2b52fe80b2
Jacob Gadikian <jacobgadikian@gmail.com>
2022-05-05 18:42:39 +0700

    feat: Inbound / outbound peers & default seeds (#541)
    * Update init.go
    
    * Update CHANGELOG.md
    
    * Update init.go
    
    * import strings
    
    * tidy
    
    * gofumpt
    
    Co-authored-by: Federico Kunze Küllmer <31522760+fedekunze@users.noreply.github.com>

--- x/recovery/keeper/ibc_callbacks_integration_test.go
+++ x/recovery/keeper/ibc_callbacks_integration_test.go
@@ -52,7 +52,6 @@ var _ = Describe("Recovery: Performing an IBC Transfer", Ordered, func() {
 	})
 
 	Describe("from an authorized, non-EVM chain (e.g. Osmosis)", func() {
-
 		Describe("to a different account on Evmos (sender != recipient)", func() {
 			BeforeEach(func() {
 				sender = s.IBCOsmosisChain.SenderAccount.GetAddress().String()
@@ -174,7 +173,6 @@ var _ = Describe("Recovery: Performing an IBC Transfer", Ordered, func() {
 
 			Context("without a sender's claims record", func() {
 				When("recipient has no ibc vouchers that originated from other chains", func() {
-
 					It("should transfer and recover tokens", func() {
 						// aevmos & ibc tokens that originated from the sender's chain
 						s.SendAndReceiveMessage(s.pathOsmosisEvmos, s.IBCOsmosisChain, coinOsmo.Denom, coinOsmo.Amount.Int64(), sender, receiver, 1)
@@ -218,7 +216,6 @@ var _ = Describe("Recovery: Performing an IBC Transfer", Ordered, func() {
 
 						params.EnableRecovery = true
 						s.EvmosChain.App.(*app.Evmos).RecoveryKeeper.SetParams(s.EvmosChain.GetContext(), params)
-
 					})
 					It("should not recover tokens that originated from other chains", func() {
 						// Send uosmo from Osmosis to Evmos
@@ -295,7 +292,6 @@ var _ = Describe("Recovery: Performing an IBC Transfer", Ordered, func() {
 
 						params.EnableRecovery = true
 						s.EvmosChain.App.(*app.Evmos).RecoveryKeeper.SetParams(s.EvmosChain.GetContext(), params)
-
 					})
 					It("should not recover tokens that originated from other chains", func() {
 						s.SendAndReceiveMessage(s.pathOsmosisEvmos, s.IBCOsmosisChain, "uosmo", 10, sender, receiver, 2)
