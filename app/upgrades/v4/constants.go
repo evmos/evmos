36955c8694b1795c82951330351e916589e25f86
Federico Kunze Küllmer <31522760+fedekunze@users.noreply.github.com>
2022-05-09 21:01:51 +0200

    release: set v4 upgrade heights (#581)
    * release: set v4 upgrade heights
    
    * log instead of error

--- app/upgrades/v4/constants.go
+++ app/upgrades/v4/constants.go
@@ -4,9 +4,9 @@ const (
 	// UpgradeName is the shared upgrade plan name for mainnet and testnet
 	UpgradeName = "v4.0.0"
 	// MainnetUpgradeHeight defines the Evmos mainnet block height on which the upgrade will take place
-	MainnetUpgradeHeight = 581700 // TODO: Update
+	MainnetUpgradeHeight = 257_850
 	// TestnetUpgradeHeight defines the Evmos testnet block height on which the upgrade will take place
-	TestnetUpgradeHeight = 5817000 // TODO: Update
+	TestnetUpgradeHeight = 1_200_000
 	// UpgradeInfo defines the binaries that will be used for the upgrade
 	UpgradeInfo = `'{"binaries":{"darwin/arm64":"https://github.com/tharsis/evmos/releases/download/v4.0.0/evmos_4.0.0_Darwin_arm64.tar.gz","darwin/x86_64":"https://github.com/tharsis/evmos/releases/download/v4.0.0/evmos_4.0.0_Darwin_x86_64.tar.gz","linux/arm64":"https://github.com/tharsis/evmos/releases/download/v4.0.0/evmos_4.0.0_Linux_arm64.tar.gz","linux/x86_64":"https://github.com/tharsis/evmos/releases/download/v4.0.0/evmos_4.0.0_Linux_x86_64.tar.gz","windows/x86_64":"https://github.com/tharsis/evmos/releases/download/v4.0.0/evmos_4.0.0_Windows_x86_64.zip"}}'`
 
