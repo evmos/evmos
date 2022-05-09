6f31f031f2e1648586383f0b1f962a126759992d
Federico Kunze Küllmer <31522760+fedekunze@users.noreply.github.com>
2022-05-09 10:31:13 +0200

    changelog

--- app/forks.go
+++ app/forks.go
@@ -20,34 +20,25 @@ import (
 // 	1) Release a non-breaking patch version so that the chain can set the scheduled upgrade plan at upgrade-height.
 // 	2) Release the software defined in the upgrade-info
 func BeginBlockForks(ctx sdk.Context, app *Evmos) {
+	// NOTE: there are no testnet forks for the existing versions
+	if !strings.HasPrefix(ctx.ChainID(), MainnetChainID) {
+		return
+	}
+
 	upgradePlan := upgradetypes.Plan{
 		Height: ctx.BlockHeight(),
 	}
 
-	if strings.HasPrefix(ctx.ChainID(), MainnetChainID) {
-		// handle mainnet forks
-		switch ctx.BlockHeight() {
-		case v2.MainnetUpgradeHeight:
-			upgradePlan.Name = v2.UpgradeName
-			upgradePlan.Info = v2.UpgradeInfo
-		case v4.MainnetUpgradeHeight:
-			upgradePlan.Name = v4.UpgradeName
-			upgradePlan.Info = v4.UpgradeInfo
-		default:
-			// No-op
-			return
-		}
-	} else if strings.HasPrefix(ctx.ChainID(), TestnetChainID) {
-		// handle testnet forks
-		switch ctx.BlockHeight() {
-		case v4.TestnetUpgradeHeight:
-			upgradePlan.Name = v4.UpgradeName
-			upgradePlan.Info = v4.UpgradeInfo
-		default:
-			// No-op
-			return
-		}
-	} else {
+	// handle mainnet forks
+	switch ctx.BlockHeight() {
+	case v2.MainnetUpgradeHeight:
+		upgradePlan.Name = v2.UpgradeName
+		upgradePlan.Info = v2.UpgradeInfo
+	case v4.MainnetUpgradeHeight:
+		upgradePlan.Name = v4.UpgradeName
+		upgradePlan.Info = v4.UpgradeInfo
+	default:
+		// No-op
 		return
 	}
 
