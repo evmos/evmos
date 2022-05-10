e74d774efc0d1322c559ebbf25dcf4378d1399de
Federico Kunze Küllmer <31522760+fedekunze@users.noreply.github.com>
2022-05-10 09:41:59 +0200

    fix: readd erc20 migration logic

---
+++ x/erc20/migrations/v2/migration_test.go
@@ -0,0 +1,54 @@
+package v2_test
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+
+	"github.com/cosmos/cosmos-sdk/testutil"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	paramtypes "github.com/cosmos/cosmos-sdk/x/params/types"
+
+	"github.com/tharsis/ethermint/encoding"
+
+	"github.com/tharsis/evmos/v4/app"
+	v2 "github.com/tharsis/evmos/v4/x/erc20/migrations/v2"
+	erc20types "github.com/tharsis/evmos/v4/x/erc20/types"
+)
+
+func TestUpdateParams(t *testing.T) {
+	encCfg := encoding.MakeConfig(app.ModuleBasics)
+	erc20Key := sdk.NewKVStoreKey(erc20types.StoreKey)
+	tErc20Key := sdk.NewTransientStoreKey(fmt.Sprintf("%s_test", erc20types.StoreKey))
+	ctx := testutil.DefaultContext(erc20Key, tErc20Key)
+	paramstore := paramtypes.NewSubspace(
+		encCfg.Marshaler, encCfg.Amino, erc20Key, tErc20Key, "erc20",
+	)
+	paramstore = paramstore.WithKeyTable(erc20types.ParamKeyTable())
+	require.True(t, paramstore.HasKeyTable())
+
+	// check no params
+	require.False(t, paramstore.Has(ctx, erc20types.ParamStoreKeyEnableErc20))
+	require.False(t, paramstore.Has(ctx, erc20types.ParamStoreKeyEnableEVMHook))
+
+	// Run migrations
+	err := v2.UpdateParams(ctx, &paramstore)
+	require.NoError(t, err)
+
+	// Make sure the params are set
+	require.True(t, paramstore.Has(ctx, erc20types.ParamStoreKeyEnableErc20))
+	require.True(t, paramstore.Has(ctx, erc20types.ParamStoreKeyEnableEVMHook))
+
+	var enableERC20, enableEVMHook bool
+
+	// Make sure the new params are set
+	require.NotPanics(t, func() {
+		paramstore.Get(ctx, erc20types.ParamStoreKeyEnableErc20, &enableERC20)
+		paramstore.Get(ctx, erc20types.ParamStoreKeyEnableEVMHook, &enableEVMHook)
+	})
+
+	// check the params are updated
+	require.True(t, enableERC20)
+	require.True(t, enableEVMHook)
+}
--- x/erc20/module.go
+++ x/erc20/module.go
@@ -129,7 +129,15 @@ func (am AppModule) RegisterServices(cfg module.Configurator) {
 	types.RegisterMsgServer(cfg.MsgServer(), am.keeper)
 	types.RegisterQueryServer(cfg.QueryServer(), am.keeper)
 
-	_ = keeper.NewMigrator(am.keeper)
+	migrator := keeper.NewMigrator(am.keeper)
+
+	// NOTE: the migrations below will only run if the consensus version has changed
+	// since the last release
+
+	// register v1 -> v2 migration
+	if err := cfg.RegisterMigration(types.ModuleName, 1, migrator.Migrate1to2); err != nil {
+		panic(fmt.Errorf("failed to migrate %s to v2: %w", types.ModuleName, err))
+	}
 }
 
 func (am AppModule) BeginBlock(_ sdk.Context, _ abci.RequestBeginBlock) {
