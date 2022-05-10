256cca85e1f4631f4e482137f693dd5d6397ece9
Loredana <loredana.cirstea@gmail.com>
2022-05-10 19:37:09 +0300

    fees: fix storing registered fees per deployer, add tests
    Each deployer now has a KVStore for storing its registered contracts.

--- x/fees/keeper/fees_test.go
+++ x/fees/keeper/fees_test.go
@@ -1,6 +1,8 @@
 package keeper_test
 
 import (
+	"fmt"
+
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/tharsis/ethermint/tests"
@@ -12,6 +14,63 @@ var (
 	deployer = sdk.AccAddress(tests.GenerateAddress().Bytes())
 	withdraw = sdk.AccAddress(tests.GenerateAddress().Bytes())
 )
+
+func (suite *KeeperTestSuite) TestGetAllFees() {
+	var expRes []types.DevFeeInfo
+
+	testCases := []struct {
+		name     string
+		malleate func()
+	}{
+		{
+			"no fees registered",
+			func() { expRes = []types.DevFeeInfo{} },
+		},
+		{
+			"one fee registered with withdraw address",
+			func() {
+				suite.app.FeesKeeper.SetFee(suite.ctx, contract, deployer, withdraw)
+				expRes = []types.DevFeeInfo{
+					types.NewDevFeeInfo(contract, deployer, withdraw),
+				}
+			},
+		},
+		{
+			"one fee registered with no withdraw address",
+			func() {
+				suite.app.FeesKeeper.SetFee(suite.ctx, contract, deployer, nil)
+				expRes = []types.DevFeeInfo{
+					types.NewDevFeeInfo(contract, deployer, nil),
+				}
+			},
+		},
+		{
+			"multiple fees registered",
+			func() {
+				deployer2 := sdk.AccAddress(tests.GenerateAddress().Bytes())
+				contract2 := tests.GenerateAddress()
+				contract3 := tests.GenerateAddress()
+				suite.app.FeesKeeper.SetFee(suite.ctx, contract, deployer, withdraw)
+				suite.app.FeesKeeper.SetFee(suite.ctx, contract2, deployer, nil)
+				suite.app.FeesKeeper.SetFee(suite.ctx, contract3, deployer2, nil)
+				expRes = []types.DevFeeInfo{
+					types.NewDevFeeInfo(contract, deployer, withdraw),
+					types.NewDevFeeInfo(contract2, deployer, nil),
+					types.NewDevFeeInfo(contract3, deployer2, nil),
+				}
+			},
+		},
+	}
+	for _, tc := range testCases {
+		suite.Run(fmt.Sprintf("Case %s", tc.name), func() {
+			suite.SetupTest() // reset
+			tc.malleate()
+
+			res := suite.app.FeesKeeper.GetAllFees(suite.ctx)
+			suite.Require().ElementsMatch(expRes, res, tc.name)
+		})
+	}
+}
 
 func (suite *KeeperTestSuite) TestGetFeeInfo() {
 	testCases := []struct {
@@ -78,33 +137,35 @@ func (suite *KeeperTestSuite) TestGetFeeInfo() {
 		},
 	}
 	for _, tc := range testCases {
-		suite.SetupTest() // reset
-		tc.malleate(tc.contract, tc.deployer, tc.withdraw)
+		suite.Run(fmt.Sprintf("Case %s", tc.name), func() {
+			suite.SetupTest() // reset
+			tc.malleate(tc.contract, tc.deployer, tc.withdraw)
 
-		fee, found := suite.app.FeesKeeper.GetFeeInfo(suite.ctx, tc.contract)
-		deployer, foundD := suite.app.FeesKeeper.GetDeployer(suite.ctx, tc.contract)
-		withdraw, foundW := suite.app.FeesKeeper.GetWithdrawal(suite.ctx, tc.contract)
+			fee, found := suite.app.FeesKeeper.GetFeeInfo(suite.ctx, tc.contract)
+			deployer, foundD := suite.app.FeesKeeper.GetDeployer(suite.ctx, tc.contract)
+			withdraw, foundW := suite.app.FeesKeeper.GetWithdrawal(suite.ctx, tc.contract)
 
-		if tc.expDeployer {
-			suite.Require().True(found, tc.name)
-			suite.Require().True(foundD, tc.name)
-			suite.Require().Equal(tc.deployer, deployer, tc.name)
-			suite.Require().Equal(tc.deployer.String(), fee.DeployerAddress, tc.name)
-			suite.Require().Equal(tc.contract.Hex(), fee.ContractAddress, tc.name)
+			if tc.expDeployer {
+				suite.Require().True(found, tc.name)
+				suite.Require().True(foundD, tc.name)
+				suite.Require().Equal(tc.deployer, deployer, tc.name)
+				suite.Require().Equal(tc.deployer.String(), fee.DeployerAddress, tc.name)
+				suite.Require().Equal(tc.contract.Hex(), fee.ContractAddress, tc.name)
 
-			if tc.expWithdraw {
-				suite.Require().True(foundW, tc.name)
-				suite.Require().Equal(tc.withdraw, withdraw, tc.name)
-				suite.Require().Equal(tc.withdraw.String(), fee.WithdrawAddress, tc.name)
+				if tc.expWithdraw {
+					suite.Require().True(foundW, tc.name)
+					suite.Require().Equal(tc.withdraw, withdraw, tc.name)
+					suite.Require().Equal(tc.withdraw.String(), fee.WithdrawAddress, tc.name)
+				} else {
+					suite.Require().False(foundW, tc.name)
+					suite.Require().Nil(withdraw, tc.name)
+				}
 			} else {
-				suite.Require().False(foundW, tc.name)
-				suite.Require().Nil(withdraw, tc.name)
+				suite.Require().False(found, tc.name)
+				suite.Require().False(foundD, tc.name)
+				suite.Require().Nil(deployer, tc.name)
 			}
-		} else {
-			suite.Require().False(found, tc.name)
-			suite.Require().False(foundD, tc.name)
-			suite.Require().Nil(deployer, tc.name)
-		}
+		})
 	}
 }
 
@@ -177,54 +238,106 @@ func (suite *KeeperTestSuite) TestIsFeeRegistered() {
 }
 
 func (suite *KeeperTestSuite) TestGetFeesInverse() {
+	suite.app.FeesKeeper.SetFeeInverse(suite.ctx, deployer, contract)
 
+	deployer2 := sdk.AccAddress(tests.GenerateAddress().Bytes())
+	contract2 := tests.GenerateAddress()
+	contract3 := tests.GenerateAddress()
+	suite.app.FeesKeeper.SetFeeInverse(suite.ctx, deployer2, contract2)
+	suite.app.FeesKeeper.SetFeeInverse(suite.ctx, deployer2, contract3)
+
+	testCases := []struct {
+		name        string
+		deployer    sdk.AccAddress
+		exitingFees []common.Address
+	}{
+		{"has registered contracts", deployer, []common.Address{contract}},
+		{"has two registered contracts", deployer2, []common.Address{contract2, contract3}},
+		{"has no registered contracts", sdk.AccAddress(tests.GenerateAddress().Bytes()), []common.Address{}},
+	}
+	for _, tc := range testCases {
+		addresses := suite.app.FeesKeeper.GetFeesInverse(suite.ctx, tc.deployer)
+		suite.Require().ElementsMatch(tc.exitingFees, addresses, tc.name)
+	}
 }
 
 func (suite *KeeperTestSuite) TestDeleteFeeInverse() {
-	// Register inverse fee for deployer
-	suite.app.FeesKeeper.SetFeeInverse(suite.ctx, deployer, contract)
+	contract2 := tests.GenerateAddress()
+	var setup = func() {
+		suite.app.FeesKeeper.SetFeeInverse(suite.ctx, deployer, contract)
+		suite.app.FeesKeeper.SetFeeInverse(suite.ctx, deployer, contract2)
+	}
 
 	testCases := []struct {
-		name     string
-		malleate func()
-		ok       bool
+		name          string
+		malleate      func()
+		deletedFees   []common.Address
+		remainingFees []common.Address
 	}{
-		{"existing inverse fee mapping", func() {}, true},
 		{
-			"no inverse fee mapping",
+			"existing fees, no delete",
+			setup,
+			[]common.Address{},
+			[]common.Address{contract, contract2},
+		},
+		{
+			"existing fees, delete one fee",
 			func() {
-				suite.app.FeesKeeper.DeleteFeeInverse(suite.ctx, deployer)
+				setup()
+				suite.app.FeesKeeper.DeleteFeeInverse(suite.ctx, deployer, contract)
 			},
-			false,
+			[]common.Address{contract},
+			[]common.Address{contract2},
+		},
+		{
+			"existing fees, delete all fees",
+			func() {
+				setup()
+				suite.app.FeesKeeper.DeleteFeeInverse(suite.ctx, deployer, contract)
+				suite.app.FeesKeeper.DeleteFeeInverse(suite.ctx, deployer, contract2)
+			},
+			[]common.Address{contract, contract2},
+			[]common.Address{},
+		},
+		{
+			"delete non existent fee",
+			func() {
+				setup()
+				contract3 := tests.GenerateAddress()
+				suite.app.FeesKeeper.DeleteFeeInverse(suite.ctx, deployer, contract3)
+			},
+			[]common.Address{},
+			[]common.Address{contract, contract2},
 		},
 	}
 	for _, tc := range testCases {
-		tc.malleate()
-		hasFees := suite.app.FeesKeeper.HasFeeInverse(suite.ctx, deployer)
-		addresses := suite.app.FeesKeeper.GetFeesInverse(suite.ctx, deployer)
-		if tc.ok {
-			suite.Require().True(hasFees, tc.name)
-			suite.Require().Equal(len(addresses), 1, tc.name)
-		} else {
-			suite.Require().False(hasFees, tc.name)
-			suite.Require().Equal(len(addresses), 0, tc.name)
-		}
+		suite.Run(fmt.Sprintf("Case %s", tc.name), func() {
+			suite.SetupTest() // reset
+			tc.malleate()
+			for _, deletedFee := range tc.deletedFees {
+				hasFee := suite.app.FeesKeeper.IsFeeInverseRegistered(suite.ctx, deployer, deletedFee)
+				suite.Require().False(hasFee, tc.name)
+			}
+			remainingFees := suite.app.FeesKeeper.GetFeesInverse(suite.ctx, deployer)
+			suite.Require().ElementsMatch(tc.remainingFees, remainingFees, tc.name)
+		})
 	}
 }
 
-func (suite *KeeperTestSuite) TestHasFeeInverse() {
+func (suite *KeeperTestSuite) TestIsFeeInverseRegistered() {
 	suite.app.FeesKeeper.SetFeeInverse(suite.ctx, deployer, contract)
 
 	testCases := []struct {
 		name     string
 		deployer sdk.AccAddress
+		contract common.Address
 		ok       bool
 	}{
-		{"deployer has fees", deployer, true},
-		{"deployer does not have fees", sdk.AccAddress(tests.GenerateAddress().Bytes()), false},
+		{"deployer has contract", deployer, contract, true},
+		{"deployer does not have contract", sdk.AccAddress(tests.GenerateAddress().Bytes()), contract, false},
 	}
 	for _, tc := range testCases {
-		found := suite.app.FeesKeeper.HasFeeInverse(suite.ctx, tc.deployer)
+		found := suite.app.FeesKeeper.IsFeeInverseRegistered(suite.ctx, tc.deployer, tc.contract)
 		if tc.ok {
 			suite.Require().True(found, tc.name)
 		} else {
--- x/fees/types/fees.pb.go
+++ x/fees/types/fees.pb.go
@@ -88,61 +88,14 @@ func (m *DevFeeInfo) GetWithdrawAddress() string {
 	return ""
 }
 
-// DevFeeInfosPerDeployer defines an instance that keeps track of all
-// contracts registered by a deployer
-type DevFeeInfosPerDeployer struct {
-	ContractAddresses []string `protobuf:"bytes,1,rep,name=contract_addresses,json=contractAddresses,proto3" json:"contract_addresses,omitempty"`
-}
-
-func (m *DevFeeInfosPerDeployer) Reset()         { *m = DevFeeInfosPerDeployer{} }
-func (m *DevFeeInfosPerDeployer) String() string { return proto.CompactTextString(m) }
-func (*DevFeeInfosPerDeployer) ProtoMessage()    {}
-func (*DevFeeInfosPerDeployer) Descriptor() ([]byte, []int) {
-	return fileDescriptor_1527b6d4bf16c067, []int{1}
-}
-func (m *DevFeeInfosPerDeployer) XXX_Unmarshal(b []byte) error {
-	return m.Unmarshal(b)
-}
-func (m *DevFeeInfosPerDeployer) XXX_Marshal(b []byte, deterministic bool) ([]byte, error) {
-	if deterministic {
-		return xxx_messageInfo_DevFeeInfosPerDeployer.Marshal(b, m, deterministic)
-	} else {
-		b = b[:cap(b)]
-		n, err := m.MarshalToSizedBuffer(b)
-		if err != nil {
-			return nil, err
-		}
-		return b[:n], nil
-	}
-}
-func (m *DevFeeInfosPerDeployer) XXX_Merge(src proto.Message) {
-	xxx_messageInfo_DevFeeInfosPerDeployer.Merge(m, src)
-}
-func (m *DevFeeInfosPerDeployer) XXX_Size() int {
-	return m.Size()
-}
-func (m *DevFeeInfosPerDeployer) XXX_DiscardUnknown() {
-	xxx_messageInfo_DevFeeInfosPerDeployer.DiscardUnknown(m)
-}
-
-var xxx_messageInfo_DevFeeInfosPerDeployer proto.InternalMessageInfo
-
-func (m *DevFeeInfosPerDeployer) GetContractAddresses() []string {
-	if m != nil {
-		return m.ContractAddresses
-	}
-	return nil
-}
-
 func init() {
 	proto.RegisterType((*DevFeeInfo)(nil), "evmos.fees.v1.DevFeeInfo")
-	proto.RegisterType((*DevFeeInfosPerDeployer)(nil), "evmos.fees.v1.DevFeeInfosPerDeployer")
 }
 
 func init() { proto.RegisterFile("evmos/fees/v1/fees.proto", fileDescriptor_1527b6d4bf16c067) }
 
 var fileDescriptor_1527b6d4bf16c067 = []byte{
-	// 239 bytes of a gzipped FileDescriptorProto
+	// 206 bytes of a gzipped FileDescriptorProto
 	0x1f, 0x8b, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0xff, 0xe2, 0x92, 0x48, 0x2d, 0xcb, 0xcd,
 	0x2f, 0xd6, 0x4f, 0x4b, 0x4d, 0x2d, 0xd6, 0x2f, 0x33, 0x04, 0xd3, 0x7a, 0x05, 0x45, 0xf9, 0x25,
 	0xf9, 0x42, 0xbc, 0x60, 0x19, 0x3d, 0xb0, 0x48, 0x99, 0xa1, 0x52, 0x2f, 0x23, 0x17, 0x97, 0x4b,
@@ -150,14 +103,12 @@ var fileDescriptor_1527b6d4bf16c067 = []byte{
 	0x49, 0x51, 0x62, 0x72, 0x49, 0x7c, 0x62, 0x4a, 0x4a, 0x51, 0x6a, 0x71, 0xb1, 0x04, 0xa3, 0x02,
 	0xa3, 0x06, 0x67, 0x10, 0x3f, 0x4c, 0xdc, 0x11, 0x22, 0x0c, 0x52, 0x9a, 0x92, 0x5a, 0x90, 0x93,
 	0x5f, 0x99, 0x5a, 0x04, 0x57, 0xca, 0x04, 0x51, 0x0a, 0x13, 0x47, 0x52, 0x5a, 0x9e, 0x59, 0x92,
-	0x91, 0x52, 0x94, 0x58, 0x0e, 0x57, 0xca, 0x0c, 0x51, 0x0a, 0x13, 0x87, 0x2a, 0x55, 0x72, 0xe7,
-	0x12, 0x43, 0x38, 0xa7, 0x38, 0x20, 0xb5, 0xc8, 0x05, 0x6a, 0x96, 0x90, 0x2e, 0x97, 0x10, 0xba,
-	0xd3, 0x52, 0x41, 0x8e, 0x63, 0xd6, 0xe0, 0x0c, 0x12, 0x44, 0x73, 0x5c, 0x6a, 0xb1, 0x93, 0xd3,
-	0x89, 0x47, 0x72, 0x8c, 0x17, 0x1e, 0xc9, 0x31, 0x3e, 0x78, 0x24, 0xc7, 0x38, 0xe1, 0xb1, 0x1c,
-	0xc3, 0x85, 0xc7, 0x72, 0x0c, 0x37, 0x1e, 0xcb, 0x31, 0x44, 0x69, 0xa4, 0x67, 0x96, 0x64, 0x94,
-	0x26, 0xe9, 0x25, 0xe7, 0xe7, 0xea, 0x97, 0x64, 0x24, 0x16, 0x15, 0x67, 0x16, 0xeb, 0x43, 0x82,
-	0xab, 0xcc, 0x58, 0xbf, 0x02, 0x12, 0x66, 0x25, 0x95, 0x05, 0xa9, 0xc5, 0x49, 0x6c, 0xe0, 0x20,
-	0x33, 0x06, 0x04, 0x00, 0x00, 0xff, 0xff, 0xb1, 0xff, 0x67, 0x49, 0x4e, 0x01, 0x00, 0x00,
+	0x91, 0x52, 0x94, 0x58, 0x0e, 0x57, 0xca, 0x0c, 0x51, 0x0a, 0x13, 0x87, 0x2a, 0x75, 0x72, 0x3a,
+	0xf1, 0x48, 0x8e, 0xf1, 0xc2, 0x23, 0x39, 0xc6, 0x07, 0x8f, 0xe4, 0x18, 0x27, 0x3c, 0x96, 0x63,
+	0xb8, 0xf0, 0x58, 0x8e, 0xe1, 0xc6, 0x63, 0x39, 0x86, 0x28, 0x8d, 0xf4, 0xcc, 0x92, 0x8c, 0xd2,
+	0x24, 0xbd, 0xe4, 0xfc, 0x5c, 0xfd, 0x92, 0x8c, 0xc4, 0xa2, 0xe2, 0xcc, 0x62, 0x7d, 0x88, 0x2f,
+	0xcb, 0x4c, 0xf4, 0x2b, 0x20, 0x5e, 0x2d, 0xa9, 0x2c, 0x48, 0x2d, 0x4e, 0x62, 0x03, 0xfb, 0xd4,
+	0x18, 0x10, 0x00, 0x00, 0xff, 0xff, 0xee, 0xe9, 0x5a, 0xfc, 0x05, 0x01, 0x00, 0x00,
 }
 
 func (m *DevFeeInfo) Marshal() (dAtA []byte, err error) {
@@ -204,38 +155,6 @@ func (m *DevFeeInfo) MarshalToSizedBuffer(dAtA []byte) (int, error) {
 	return len(dAtA) - i, nil
 }
 
-func (m *DevFeeInfosPerDeployer) Marshal() (dAtA []byte, err error) {
-	size := m.Size()
-	dAtA = make([]byte, size)
-	n, err := m.MarshalToSizedBuffer(dAtA[:size])
-	if err != nil {
-		return nil, err
-	}
-	return dAtA[:n], nil
-}
-
-func (m *DevFeeInfosPerDeployer) MarshalTo(dAtA []byte) (int, error) {
-	size := m.Size()
-	return m.MarshalToSizedBuffer(dAtA[:size])
-}
-
-func (m *DevFeeInfosPerDeployer) MarshalToSizedBuffer(dAtA []byte) (int, error) {
-	i := len(dAtA)
-	_ = i
-	var l int
-	_ = l
-	if len(m.ContractAddresses) > 0 {
-		for iNdEx := len(m.ContractAddresses) - 1; iNdEx >= 0; iNdEx-- {
-			i -= len(m.ContractAddresses[iNdEx])
-			copy(dAtA[i:], m.ContractAddresses[iNdEx])
-			i = encodeVarintFees(dAtA, i, uint64(len(m.ContractAddresses[iNdEx])))
-			i--
-			dAtA[i] = 0xa
-		}
-	}
-	return len(dAtA) - i, nil
-}
-
 func encodeVarintFees(dAtA []byte, offset int, v uint64) int {
 	offset -= sovFees(v)
 	base := offset
@@ -264,21 +183,6 @@ func (m *DevFeeInfo) Size() (n int) {
 	l = len(m.WithdrawAddress)
 	if l > 0 {
 		n += 1 + l + sovFees(uint64(l))
-	}
-	return n
-}
-
-func (m *DevFeeInfosPerDeployer) Size() (n int) {
-	if m == nil {
-		return 0
-	}
-	var l int
-	_ = l
-	if len(m.ContractAddresses) > 0 {
-		for _, s := range m.ContractAddresses {
-			l = len(s)
-			n += 1 + l + sovFees(uint64(l))
-		}
 	}
 	return n
 }
@@ -413,88 +317,6 @@ func (m *DevFeeInfo) Unmarshal(dAtA []byte) error {
 				return io.ErrUnexpectedEOF
 			}
 			m.WithdrawAddress = string(dAtA[iNdEx:postIndex])
-			iNdEx = postIndex
-		default:
-			iNdEx = preIndex
-			skippy, err := skipFees(dAtA[iNdEx:])
-			if err != nil {
-				return err
-			}
-			if (skippy < 0) || (iNdEx+skippy) < 0 {
-				return ErrInvalidLengthFees
-			}
-			if (iNdEx + skippy) > l {
-				return io.ErrUnexpectedEOF
-			}
-			iNdEx += skippy
-		}
-	}
-
-	if iNdEx > l {
-		return io.ErrUnexpectedEOF
-	}
-	return nil
-}
-func (m *DevFeeInfosPerDeployer) Unmarshal(dAtA []byte) error {
-	l := len(dAtA)
-	iNdEx := 0
-	for iNdEx < l {
-		preIndex := iNdEx
-		var wire uint64
-		for shift := uint(0); ; shift += 7 {
-			if shift >= 64 {
-				return ErrIntOverflowFees
-			}
-			if iNdEx >= l {
-				return io.ErrUnexpectedEOF
-			}
-			b := dAtA[iNdEx]
-			iNdEx++
-			wire |= uint64(b&0x7F) << shift
-			if b < 0x80 {
-				break
-			}
-		}
-		fieldNum := int32(wire >> 3)
-		wireType := int(wire & 0x7)
-		if wireType == 4 {
-			return fmt.Errorf("proto: DevFeeInfosPerDeployer: wiretype end group for non-group")
-		}
-		if fieldNum <= 0 {
-			return fmt.Errorf("proto: DevFeeInfosPerDeployer: illegal tag %d (wire type %d)", fieldNum, wire)
-		}
-		switch fieldNum {
-		case 1:
-			if wireType != 2 {
-				return fmt.Errorf("proto: wrong wireType = %d for field ContractAddresses", wireType)
-			}
-			var stringLen uint64
-			for shift := uint(0); ; shift += 7 {
-				if shift >= 64 {
-					return ErrIntOverflowFees
-				}
-				if iNdEx >= l {
-					return io.ErrUnexpectedEOF
-				}
-				b := dAtA[iNdEx]
-				iNdEx++
-				stringLen |= uint64(b&0x7F) << shift
-				if b < 0x80 {
-					break
-				}
-			}
-			intStringLen := int(stringLen)
-			if intStringLen < 0 {
-				return ErrInvalidLengthFees
-			}
-			postIndex := iNdEx + intStringLen
-			if postIndex < 0 {
-				return ErrInvalidLengthFees
-			}
-			if postIndex > l {
-				return io.ErrUnexpectedEOF
-			}
-			m.ContractAddresses = append(m.ContractAddresses, string(dAtA[iNdEx:postIndex]))
 			iNdEx = postIndex
 		default:
 			iNdEx = preIndex
