9bf1ae37cfdf0da0a5415e3db5233111cd703819
Loredana Cirstea <loredana.cirstea@gmail.com>
2022-05-12 11:55:31 +0300

    fees: limit address derivation to 20 iterations at registration (#603)

--- x/fees/types/msg.go
+++ x/fees/types/msg.go
@@ -68,6 +68,10 @@ func (msg MsgRegisterDevFeeInfo) ValidateBasic() error {
 		return sdkerrors.Wrapf(sdkerrors.ErrInvalidRequest, "invalid nonces - empty array")
 	}
 
+	if len(msg.Nonces) > 20 {
+		return sdkerrors.Wrapf(sdkerrors.ErrInvalidRequest, "invalid nonces - array length must be less than 20")
+	}
+
 	return nil
 }
 
