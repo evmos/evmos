54ba0a822125e4601159efcca54c974270be02c4
nivasan1 <nivasan@umich.edu>
2022-05-19 23:31:31 -0400

    New Protocol Buffer Definition

--- x/unigov/keeper/keeper.go
+++ x/unigov/keeper/keeper.go
@@ -20,7 +20,7 @@ type (
 		memKey   	sdk.StoreKey
 		paramstore	paramtypes.Subspace
 
-		mapContractAddr *common.Address
+		mapContractAddr common.Address
 		accKeeper   types.AccountKeeper
 		erc20Keeper types.ERC20Keeper
 	}
@@ -49,7 +49,7 @@ func NewKeeper(
 		cdc:      	cdc,
 		storeKey: 	storeKey,
 		memKey:   	memKey,
-		mapContractAddr: &common.Address{},
+		mapContractAddr: common.Address(0),
 		paramstore:	ps,
 		accKeeper:      ak,
 		erc20Keeper:    ek,
