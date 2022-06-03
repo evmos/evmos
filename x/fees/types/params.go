71a49de2a3f132e0b3970599cd8ee62a20bdb162
hanchon <guillermo.paoletti@gmail.com>
2022-06-03 14:21:54 +0200

    fix: old defaul value removed

--- x/fees/types/params.go
+++ x/fees/types/params.go
@@ -15,7 +15,6 @@ var (
 	// Cost for executing `crypto.CreateAddress`
 	// must be at least 36 gas for the contained keccak256(word) operation
 	DefaultAddrDerivationCostCreate       = uint64(50)
-	DefaultMinGasPrice                    = sdk.ZeroDec()
 	ParamStoreKeyEnableFees               = []byte("EnableFees")
 	ParamStoreKeyDeveloperShares          = []byte("DeveloperShares")
 	ParamStoreKeyValidatorShares          = []byte("ValidatorShares")
