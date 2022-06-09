281da107dc1ad049a2d0b3112a4935a5fdbd5b21
Federico Kunze Küllmer <31522760+fedekunze@users.noreply.github.com>
2022-06-09 15:38:52 +0200

    deps: bump ethermint to v0.16.1 (#663)
    * deps: bump ethermint to v0.16.1
    
    * changelog
    
    * fix test

--- x/incentives/keeper/evm_hooks_test.go
+++ x/incentives/keeper/evm_hooks_test.go
@@ -47,41 +47,27 @@ func (suite *KeeperTestSuite) TestEvmHooksStoreTxGasUsed() {
 			},
 			false,
 		},
-		// {
-		// 	"from address doesn't have an account",
-		// 	func(contractAddr common.Address) {
-		// 		// remove the contract
-		// 		contract := &ethermint.EthAccount{
-		// 			BaseAccount: authtypes.NewBaseAccount(sdk.AccAddress(suite.address.Bytes()), nil, 0, 0),
-		// 			CodeHash:    common.Bytes2Hex(crypto.Keccak256([]byte{0, 1, 2, 2})),
-		// 		}
-		// 		suite.app.AccountKeeper.RemoveAccount(suite.ctx, contract)
-		// 		res := suite.MintERC20Token(contractAddr, suite.address, suite.address, big.NewInt(1000))
-		// 		expGasUsed = res.AsTransaction().Gas()
-		// 	},
-		// 	false,
-		// },
-		// {
-		// 	"from address is not an EOA",
-		// 	func(contractAddr common.Address) {
-		// 		// set a contract account for the address
-		// 		contract := &ethermint.EthAccount{
-		// 			BaseAccount: authtypes.NewBaseAccount(sdk.AccAddress(suite.address.Bytes()), nil, 0, 0),
-		// 			CodeHash:    common.Bytes2Hex(crypto.Keccak256([]byte{0, 1, 2, 2})),
-		// 		}
+		{
+			"from address is not an EOA",
+			func(contractAddr common.Address) {
+				// set a contract account for the address
+				contract := &ethermint.EthAccount{
+					BaseAccount: authtypes.NewBaseAccount(sdk.AccAddress(suite.address.Bytes()), nil, 0, 0),
+					CodeHash:    common.BytesToHash(crypto.Keccak256([]byte{0, 1, 2, 2})).String(),
+				}
 
-		// 		suite.app.AccountKeeper.SetAccount(suite.ctx, contract)
-		// 		res := suite.MintERC20Token(contractAddr, suite.address, suite.address, big.NewInt(1000))
-		// 		expGasUsed = res.AsTransaction().Gas()
-		// 	},
-		// 	false,
-		// },
+				suite.app.AccountKeeper.SetAccount(suite.ctx, contract)
+				res := suite.MintERC20Token(contractAddr, suite.address, suite.address, big.NewInt(1000))
+				expGasUsed = res.AsTransaction().Gas()
+			},
+			false,
+		},
 		{
 			"correct execution - one tx",
 			func(contractAddr common.Address) {
 				acc := &ethermint.EthAccount{
 					BaseAccount: authtypes.NewBaseAccount(sdk.AccAddress(suite.address.Bytes()), nil, 0, 0),
-					CodeHash:    common.Bytes2Hex(crypto.Keccak256(nil)),
+					CodeHash:    common.BytesToHash(crypto.Keccak256(nil)).String(),
 				}
 				suite.app.AccountKeeper.SetAccount(suite.ctx, acc)
 
@@ -121,7 +107,7 @@ func (suite *KeeperTestSuite) TestEvmHooksStoreTxGasUsed() {
 			func(contractAddr common.Address) {
 				acc := &ethermint.EthAccount{
 					BaseAccount: authtypes.NewBaseAccount(sdk.AccAddress(suite.address.Bytes()), nil, 0, 0),
-					CodeHash:    common.Bytes2Hex(crypto.Keccak256(nil)),
+					CodeHash:    common.BytesToHash(crypto.Keccak256(nil)).String(),
 				}
 				suite.app.AccountKeeper.SetAccount(suite.ctx, acc)
 
