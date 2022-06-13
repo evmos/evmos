15e78e7715c91eaf09ded30e7dded325adaee62b
Federico Kunze Küllmer <31522760+fedekunze@users.noreply.github.com>
2022-06-13 16:56:42 +0200

    use amino for GetSignBytes

--- x/erc20/types/codec.go
+++ x/erc20/types/codec.go
@@ -8,12 +8,19 @@ import (
 	govtypes "github.com/cosmos/cosmos-sdk/x/gov/types"
 )
 
-// ModuleCdc references the global erc20 module codec. Note, the codec should
-// ONLY be used in certain instances of tests and for JSON encoding.
-//
-// The actual codec used for serialization should be provided to modules/erc20 and
-// defined at the application level.
-var ModuleCdc = codec.NewProtoCodec(codectypes.NewInterfaceRegistry())
+var (
+	amino = codec.NewLegacyAmino()
+
+	// ModuleCdc references the global erc20 module codec. Note, the codec should
+	// ONLY be used in certain instances of tests and for JSON encoding.
+	//
+	// The actual codec used for serialization should be provided to modules/erc20 and
+	// defined at the application level.
+	ModuleCdc = codec.NewProtoCodec(codectypes.NewInterfaceRegistry())
+
+	// AminoCdc is a amino codec created to support amino JSON compatible msgs.
+	AminoCdc = codec.NewAminoCodec(amino)
+)
 
 // RegisterInterfaces register implementations
 func RegisterInterfaces(registry codectypes.InterfaceRegistry) {
