f77557f007df6c2e14a17a903e9cb8d64432a4eb
Federico Kunze Küllmer <31522760+fedekunze@users.noreply.github.com>
2022-06-27 15:47:54 +0200

    define upgrade heights

--- app/upgrades/v6/constants.go
+++ app/upgrades/v6/constants.go
@@ -4,11 +4,9 @@ const (
 	// UpgradeName is the shared upgrade plan name for mainnet and testnet
 	UpgradeName = "v6.0.0"
 	// MainnetUpgradeHeight defines the Evmos mainnet block height on which the upgrade will take place
-	// TODO: define
-	MainnetUpgradeHeight = 257_850
+	MainnetUpgradeHeight = 1_042_000
 	// TestnetUpgradeHeight defines the Evmos testnet block height on which the upgrade will take place
-	// TODO: define
-	TestnetUpgradeHeight = 1_200_000
+	TestnetUpgradeHeight = 2_145_500
 	// UpgradeInfo defines the binaries that will be used for the upgrade
 	UpgradeInfo = `'{"binaries":{"darwin/arm64":"https://github.com/evmos/evmos/releases/download/v6.0.0/evmos_6.0.0_Darwin_arm64.tar.gz","darwin/x86_64":"https://github.com/evmos/evmos/releases/download/v6.0.0/evmos_6.0.0_Darwin_x86_64.tar.gz","linux/arm64":"https://github.com/evmos/evmos/releases/download/v6.0.0/evmos_6.0.0_Linux_arm64.tar.gz","linux/x86_64":"https://github.com/evmos/evmos/releases/download/v6.0.0/evmos_6.0.0_Linux_x86_64.tar.gz","windows/x86_64":"https://github.com/evmos/evmos/releases/download/v6.0.0/evmos_6.0.0_Windows_x86_64.zip"}}'`
 )
