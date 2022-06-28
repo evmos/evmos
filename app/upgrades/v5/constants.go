e12a42b1752727da7dece10efae9d9165988e09b
Daniel Burckhardt <daniel.m.burckhardt@gmail.com>
2022-06-28 14:12:28 +0200

    upgrade(v6): change MainnetMinGasPrices to 20B aevmos

--- app/upgrades/v5/constants.go
+++ app/upgrades/v5/constants.go
@@ -29,9 +29,9 @@ const (
 )
 
 var (
-	// MainnetMinGasPrices defines 25B aevmos (or atevmos) as the minimum gas price value on the fee market module.
+	// MainnetMinGasPrices defines 20B aevmos (or atevmos) as the minimum gas price value on the fee market module.
 	// See https://commonwealth.im/evmos/discussion/5073-global-min-gas-price-value-for-cosmos-sdk-and-evm-transaction-choosing-a-value for reference
-	MainnetMinGasPrices = sdk.NewDec(25_000_000_000)
+	MainnetMinGasPrices = sdk.NewDec(20_000_000_000)
 	// MainnetMinGasMultiplier defines the min gas multiplier value on the fee market module.
 	// 50% of the leftover gas will be refunded
 	MainnetMinGasMultiplier = sdk.NewDecWithPrec(5, 1)
