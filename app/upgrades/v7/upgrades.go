9c69f1d54ca3ef7f90f86d5c2421ca27a77b2623
adisaran64 <adisaran64@berkeley.edu>
2022-07-11 13:24:29 -0700

    added logic, working on tests

--- app/upgrades/v7/upgrades.go
+++ app/upgrades/v7/upgrades.go
@@ -8,6 +8,8 @@ import (
 	upgradetypes "github.com/cosmos/cosmos-sdk/x/upgrade/types"
 
 	"github.com/evmos/evmos/v6/types"
+	inflationkeeper "github.com/evmos/evmos/v6/x/inflation/keeper"
+	inflationtypes "github.com/evmos/evmos/v6/x/inflation/types"
 )
 
 // CreateUpgradeHandler creates an SDK upgrade handler for v7
@@ -15,6 +17,7 @@ func CreateUpgradeHandler(
 	mm *module.Manager,
 	configurator module.Configurator,
 	bk bankkeeper.Keeper,
+	ik *inflationkeeper.Keeper,
 ) upgradetypes.UpgradeHandler {
 	return func(ctx sdk.Context, _ upgradetypes.Plan, vm module.VersionMap) (module.VersionMap, error) {
 		logger := ctx.Logger().With("upgrade", UpgradeName)
@@ -29,6 +32,9 @@ func CreateUpgradeHandler(
 				// log error instead of aborting the upgrade
 				logger.Error("FAILED TO MIGRATE FAUCET BALANCES", "error", err.Error())
 			}
+
+			logger.Debug("migrating skipped epochs value of inflation module...")
+			MigrateSkippedEpochs(ctx, ik)
 		}
 
 		// Leave modules are as-is to avoid running InitGenesis.
@@ -46,5 +52,14 @@ func MigrateFaucetBalances(ctx sdk.Context, bk bankkeeper.Keeper) error {
 	if err := bk.SendCoins(ctx, from, to, balances); err != nil {
 		return sdkerrors.Wrap(err, "failed to migrate Faucet Balances")
 	}
+	return nil
+}
+
+// MigrateSkippedEpochs migrates the number of skipped epochs to be lower
+// than the previous stored value, due to an overcounting of two epochs pre v6.0.0.
+func MigrateSkippedEpochs(sdk sdk.Context, ik inflationkeeper.Keeper) error {
+	previousValue := ik.GetSkippedEpochs(ctx)
+	newValue := previousValue - uint64(2)
+	ik.SetSkippedEpochs(ctx, newValue)
 	return nil
 }
