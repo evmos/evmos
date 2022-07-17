d3cd109dbdf617b98937477d51d269b711ba7594
faddat <jacobgadikian@gmail.com>
2022-07-17 13:00:21 +0000

    fixes for dev env

--- scripts/protocgen.sh
+++ scripts/protocgen.sh
@@ -2,20 +2,20 @@
 
 set -eo pipefail
 
-protoc_gen_gocosmos() {
+protoc_gen_go_pulsar() {
   if ! grep "github.com/gogo/protobuf => github.com/regen-network/protobuf" go.mod &>/dev/null ; then
-    echo -e "\tPlease run this command from somewhere inside the sommelier folder."
+    echo -e "\tPlease run this command from somewhere inside the evmos folder."
     return 1
   fi
 
-  go get github.com/regen-network/cosmos-proto/protoc-gen-gocosmos 2>/dev/null
+  go install github.com/cosmos/cosmos-proto/cmd/protoc-gen-go-pulsar@v1.0.0-alpha7
 }
 
 protoc_gen_doc() {
-  go get -u github.com/pseudomuto/protoc-gen-doc/cmd/protoc-gen-doc 2>/dev/null
+  go install github.com/pseudomuto/protoc-gen-doc/cmd/protoc-gen-doc@latest 2>/dev/null
 }
 
-protoc_gen_gocosmos
+protoc_gen_go_pulsar
 protoc_gen_doc
 
 proto_dirs=$(find ./proto -path -prune -o -name '*.proto' -print0 | xargs -0 -n1 dirname | sort | uniq)
@@ -24,7 +24,7 @@ for dir in $proto_dirs; do
   buf alpha protoc \
   -I "proto" \
   -I "third_party/proto" \
-  --gocosmos_out=plugins=interfacetype+grpc,\
+  --gopulsar_out=plugins=interfacetype+grpc,\
 Mgoogle/protobuf/any.proto=github.com/cosmos/cosmos-sdk/codec/types:. \
   --grpc-gateway_out=logtostderr=true:. \
   $(find "${dir}" -maxdepth 1 -name '*.proto')
