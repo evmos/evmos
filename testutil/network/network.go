da4e396fe0fe863163ff9791b7299d53ae99d73d
Jacob Gadikian <jacobgadikian@gmail.com>
2022-07-20 02:09:49 +0700

    fix imports

--- testutil/network/network.go
+++ testutil/network/network.go
@@ -370,7 +370,7 @@ func New(l Logger, baseDir string, cfg Config) (*Network, error) {
 		nodeIDs[i] = nodeID
 		valPubKeys[i] = pubKey
 
-		kb, err := keyring.New(sdk.KeyringServiceName(), keyring.BackendTest, clientDir, buf, ctx.Codec, cfg.KeyringOptions)
+		kb, err := keyring.New(sdk.KeyringServiceName(), keyring.BackendTest, clientDir, buf, cfg.Codec, cfg.KeyringOptions...)
 		if err != nil {
 			return nil, err
 		}
