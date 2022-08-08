d833032d58ef4f27d414f909c5d04fc0ec0dcda5
Daniel Burckhardt <daniel.m.burckhardt@gmail.com>
2022-08-08 10:16:05 +0200

    upgrade(testnet): Set v7 upgrade height

--- app/upgrades/v7/constants.go
+++ app/upgrades/v7/constants.go
@@ -6,7 +6,7 @@ const (
 	// MainnetUpgradeHeight defines the Evmos mainnet block height on which the upgrade will take place
 	MainnetUpgradeHeight = 2_476_000
 	// TestnetUpgradeHeight defines the Evmos testnet block height on which the upgrade will take place
-	TestnetUpgradeHeight = 2_176_500 // TODO:
+	TestnetUpgradeHeight = 4_190_000
 	// UpgradeInfo defines the binaries that will be used for the upgrade
 	UpgradeInfo = `'{"binaries":{"darwin/arm64":"https://github.com/evmos/evmos/releases/download/v7.0.0/evmos_7.0.0_Darwin_arm64.tar.gz","darwin/x86_64":"https://github.com/evmos/evmos/releases/download/v7.0.0/evmos_7.0.0_Darwin_x86_64.tar.gz","linux/arm64":"https://github.com/evmos/evmos/releases/download/v7.0.0/evmos_7.0.0_Linux_arm64.tar.gz","linux/x86_64":"https://github.com/evmos/evmos/releases/download/v7.0.0/evmos_7.0.0_Linux_x86_64.tar.gz","windows/x86_64":"https://github.com/evmos/evmos/releases/download/v7.0.0/evmos_7.0.0_Windows_x86_64.zip"}}'`
 
