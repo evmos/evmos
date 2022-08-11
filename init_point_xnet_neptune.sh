1be0bca50cb6a17336c2014df23309b40d57df57
sergevar <86429520+sergevar@users.noreply.github.com>
2022-08-11 21:51:56 +0300

    Update init_point_xnet_neptune.sh

--- init_point_xnet_neptune.sh
+++ init_point_xnet_neptune.sh
@@ -3,7 +3,7 @@
 # 
 # wget https://go.dev/dl/go1.19.linux-amd64.tar.gz
 # rm -rf /usr/local/go && tar -C /usr/local -xzf go1.19.linux-amd64.tar.gz
-# echo 'export PATH=$PATH:/scripts' >> ~/.profile
+# echo 'export PATH=$PATH:/usr/local/go/bin:$HOME/go/bin' >> ~/.profile
 # source ~/.profile
 #
 # # go mod tidy # - needed?
