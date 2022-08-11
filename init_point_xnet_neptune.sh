4c23b00acf2e07a5f2752c511a209dfbdfe99052
sergevar <86429520+sergevar@users.noreply.github.com>
2022-08-11 21:58:32 +0300

    Update init_point_xnet_neptune.sh

--- init_point_xnet_neptune.sh
+++ init_point_xnet_neptune.sh
@@ -2,7 +2,7 @@
 # apt install jq make
 # 
 # wget https://go.dev/dl/go1.19.linux-amd64.tar.gz
-# rm -rf /usr/local/go && tar -C /usr/local -xzf go1.19.linux-amd64.tar.gz
+# rm -rf /usr/local/go && tar -C /usr/local -xzf go1.19.linux-amd64.tar.gz && rm go1.19.linux-amd64.tar.gz
 # echo 'export PATH=$PATH:/usr/local/go/bin:$HOME/go/bin' >> ~/.profile
 # source ~/.profile
 #
