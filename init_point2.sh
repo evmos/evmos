401d6d77daabaf32dce4c8e116648fca529b18a8
Юрий <discours@list.ru>
2022-08-16 09:36:29 +0300

    keyring file init_point2.sh

--- init_point2.sh
+++ init_point2.sh
@@ -12,7 +12,7 @@ KEY="mykey"
 CHAINID="point_10711-1"
 MONIKER="point-xnet-neptune"
 #KEYRING="test" # remember to change to other types of keyring like 'file' in-case exposing to outside world, otherwise your balance will be wiped quickly. The keyring test does not require private key to steal tokens from you
-KEYRING="test"
+KEYRING="file"
 KEYALGO="eth_secp256k1"
 LOGLEVEL="info"
 # to trace evm
