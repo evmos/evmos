1dd91a33bd277f2ec4046db179053339f7f86d94
Iury V <discours@list.ru>
2022-08-19 11:42:32 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,1 +1,1 @@
-ulimit -n 4096 && evmosd start --evm.tracer json --trace-store /root/.evmosd/tracestore.json --pruning=nothing  --log_level info --minimum-gas-prices=0.0001apoint --json-rpc.api eth,txpool,personal,net,debug,web3
+ulimit -n 4096 && evmosd start --evm.tracer json --trace-store ~/.evmosd/tracestore.json --pruning=nothing  --log_level info --minimum-gas-prices=0.0001apoint --json-rpc.api eth,txpool,personal,net,debug,web3
