a00cc4af23e4be641a57107ff1f1c06c2be6fa26
Iury V <discours@list.ru>
2022-08-30 16:21:03 +0300

    Update root.go
    - block speed 1 second

--- cmd/pointd/root.go
+++ cmd/pointd/root.go
@@ -102,7 +102,7 @@ func NewRootCmd() (*cobra.Command, params.EncodingConfig) {
 
 			// TODO: remove the lines below once Cosmos SDK v0.46 is released
 			serverCtx := sdkserver.GetServerContextFromCmd(cmd)
-			serverCtx.Config.Consensus.TimeoutCommit = 2 * time.Second
+			serverCtx.Config.Consensus.TimeoutCommit = 1 * time.Second
 
 			return sdkserver.SetCmdServerContext(cmd, serverCtx)
 		},
