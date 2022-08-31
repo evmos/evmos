834041bcbe0eff368456f400a71926b81056288e
Serge Var <serge@pointnetwork.io>
2022-08-31 22:00:14 +0300

    add_trace

--- init_point_xnet_mainnet.sh
+++ init_point_xnet_mainnet.sh
@@ -29,7 +29,7 @@ KEYALGO="eth_secp256k1"
 LOGLEVEL="info"
 # to trace evm
 #TRACE="--trace"
-TRACE=""
+TRACE="--trace"
 
 # validate dependencies are installed
 #command -v jq > /dev/null 2>&1 || { echo >&2 "jq not installed. More info: https://stedolan.github.io/jq/download/"; exit 1; }
