84efe106e189882b81a7dde8b48037edabebecba
Serge Var <serge@pointnetwork.io>
2022-08-31 23:21:00 +0300

    ulimit

--- start.sh
+++ start.sh
@@ -1,1 +1,1 @@
-ulimit -n 1000000 && pointd start --pruning=nothing  --log_level info --minimum-gas-prices=0.001apoint
+ulimit -n 100000 && pointd start --pruning=nothing  --log_level info --minimum-gas-prices=0.001apoint
