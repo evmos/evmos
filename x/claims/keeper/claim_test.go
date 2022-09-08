5fcb25b1e03012bba936203c6a5deedae4ad6a86
Iury V <discours@list.ru>
2022-09-08 17:56:24 +0300

    Update claim_test.go

--- x/claims/keeper/claim_test.go
+++ x/claims/keeper/claim_test.go
@@ -1047,7 +1047,7 @@ func (suite *KeeperTestSuite) TestClawbackEmptyAccountsAirdrop() {
 	}{
 		{
 			name:           "address active",
-			address:        "evmos1qql8ag4cluz6r4dz28p3w00dnc9w8ueuafmxps",
+			address:        "point1qql8ag4cluz6r4dz28p3w00dnc9w8ueu8w4nuv",
 			sequence:       1,
 			expectClawback: false,
 			claimsRecord: types.ClaimsRecord{
@@ -1057,7 +1057,7 @@ func (suite *KeeperTestSuite) TestClawbackEmptyAccountsAirdrop() {
 		},
 		{
 			name:           "address inactive",
-			address:        "evmos1x2w87cvt5mqjncav4lxy8yfreynn273xn5335v",
+			address:        "point1x2w87cvt5mqjncav4lxy8yfreynn273xfnlyfs",
 			sequence:       0,
 			expectClawback: true,
 			claimsRecord: types.ClaimsRecord{
