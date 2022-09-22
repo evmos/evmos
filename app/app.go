d2e248c69da2c2bd67b71941437390e36362d8a3
Federico Kunze Küllmer <31522760+fedekunze@users.noreply.github.com>
2022-09-22 10:56:37 +0200

    fix(app): implement hard fork logic for v8.2.0

--- app/app.go
+++ app/app.go
@@ -114,7 +114,7 @@ import (
 	v6 "github.com/evmos/evmos/v8/app/upgrades/v6"
 	v7 "github.com/evmos/evmos/v8/app/upgrades/v7"
 	v8 "github.com/evmos/evmos/v8/app/upgrades/v8"
-	"github.com/evmos/evmos/v8/app/upgrades/v8m1"
+	v81 "github.com/evmos/evmos/v8/app/upgrades/v8_1"
 	"github.com/evmos/evmos/v8/x/claims"
 	claimskeeper "github.com/evmos/evmos/v8/x/claims/keeper"
 	claimstypes "github.com/evmos/evmos/v8/x/claims/types"
@@ -1096,8 +1096,8 @@ func (app *Evmos) setupUpgradeHandlers() {
 
 	// v8.1 upgrade handler
 	app.UpgradeKeeper.SetUpgradeHandler(
-		v8m1.UpgradeName,
-		v8m1.CreateUpgradeHandler(
+		v81.UpgradeName,
+		v81.CreateUpgradeHandler(
 			app.mm, app.configurator,
 		),
 	)
