7831a9fe11e8121e0371862c809a74c6f9884e28
Federico Kunze Küllmer <31522760+fedekunze@users.noreply.github.com>
2022-09-23 18:27:16 +0200

    chore(app): update upgrade height for v8.2.0

--- app/upgrades/v8_2/constants.go
+++ app/upgrades/v8_2/constants.go
@@ -4,7 +4,7 @@ const (
 	// UpgradeName is the shared upgrade plan name for mainnet
 	UpgradeName = "v8.2.0"
 	// MainnetUpgradeHeight defines the Evmos mainnet block height on which the upgrade will take place
-	MainnetUpgradeHeight = 4_742_000
+	MainnetUpgradeHeight = 4_888_000
 	// UpgradeInfo defines the binaries that will be used for the upgrade
 	UpgradeInfo = `'{"binaries":{"darwin/amd64":"https://github.com/evmos/evmos/releases/download/v8.2.0/evmos_8.2.0_Darwin_arm64.tar.gz","darwin/x86_64":"https://github.com/evmos/evmos/releases/download/v8.2.0/evmos_8.2.0_Darwin_x86_64.tar.gz","linux/arm64":"https://github.com/evmos/evmos/releases/download/v8.2.0/evmos_8.2.0_Linux_arm64.tar.gz","linux/amd64":"https://github.com/evmos/evmos/releases/download/v8.2.0/evmos_8.2.0_Linux_amd64.tar.gz","windows/x86_64":"https://github.com/evmos/evmos/releases/download/v8.2.0/evmos_8.2.0_Windows_x86_64.zip"}}'`
 )
