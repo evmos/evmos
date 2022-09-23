e75e51b5465e648a3960fa654a558e1b3e6b0a2c
Ramiro Carlucho <ramirocarlucho@gmail.com>
2022-09-23 13:58:27 +0100

    imp(app): delete unnecessary upgrade logic (#932)
    * delete unnecessary upgrade
    
    * delete revenue keeper
    
    * delete revenue keeper
    
    * remove feesplit module from version map
    
    * Update app/upgrades/v8_2/upgrades.go
    
    Co-authored-by: Daniel Burckhardt <daniel.m.burckhardt@gmail.com>
    Co-authored-by: Federico Kunze Küllmer <31522760+fedekunze@users.noreply.github.com>

--- app/upgrades/v8_2/upgrades.go
+++ app/upgrades/v8_2/upgrades.go
@@ -4,21 +4,19 @@ import (
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/cosmos/cosmos-sdk/types/module"
 	upgradetypes "github.com/cosmos/cosmos-sdk/x/upgrade/types"
-	revenuekeeper "github.com/evmos/evmos/v8/x/revenue/keeper"
-	revenuetypes "github.com/evmos/evmos/v8/x/revenue/types"
 )
 
 // CreateUpgradeHandler creates an SDK upgrade handler for v8.2
 func CreateUpgradeHandler(
 	mm *module.Manager,
 	configurator module.Configurator,
-	rk revenuekeeper.Keeper,
 ) upgradetypes.UpgradeHandler {
 	return func(ctx sdk.Context, _ upgradetypes.Plan, vm module.VersionMap) (module.VersionMap, error) {
 		logger := ctx.Logger().With("upgrade", UpgradeName)
 
-		logger.Debug("setting parameters to default parameters in revenue module...")
-		SetRevenueParameters(ctx, rk)
+		// feesplit module is deprecated since it is renamed to "revenue" module
+		logger.Debug("deleting feesplit module from version map...")
+		delete(vm, "feesplit")
 
 		// Refs:
 		// - https://docs.cosmos.network/master/building-modules/upgrade.html#registering-migrations
@@ -28,8 +26,4 @@ func CreateUpgradeHandler(
 		logger.Debug("running module migrations ...")
 		return mm.RunMigrations(ctx, configurator, vm)
 	}
-}
-
-func SetRevenueParameters(ctx sdk.Context, rk revenuekeeper.Keeper) {
-	rk.SetParams(ctx, revenuetypes.DefaultParams())
 }
