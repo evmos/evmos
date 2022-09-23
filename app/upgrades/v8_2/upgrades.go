0b9443130edc6c9fd6b229e01cf60f1b60bc7080
Ramiro Carlucho <ramirocarlucho@gmail.com>
2022-09-23 13:27:57 +0100

    delete revenue keeper

--- app/upgrades/v8_2/upgrades.go
+++ app/upgrades/v8_2/upgrades.go
@@ -4,14 +4,12 @@ import (
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/cosmos/cosmos-sdk/types/module"
 	upgradetypes "github.com/cosmos/cosmos-sdk/x/upgrade/types"
-	revenuekeeper "github.com/evmos/evmos/v8/x/revenue/keeper"
 )
 
 // CreateUpgradeHandler creates an SDK upgrade handler for v8.2
 func CreateUpgradeHandler(
 	mm *module.Manager,
 	configurator module.Configurator,
-	rk revenuekeeper.Keeper,
 ) upgradetypes.UpgradeHandler {
 	return func(ctx sdk.Context, _ upgradetypes.Plan, vm module.VersionMap) (module.VersionMap, error) {
 		logger := ctx.Logger().With("upgrade", UpgradeName)
