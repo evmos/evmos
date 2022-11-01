c130c9bf4a9f6457396aafaebd8bd412de1a7157
Federico Kunze Küllmer <31522760+fedekunze@users.noreply.github.com>
2022-11-01 12:52:52 +0100

    finish ICS4 Wrapper logic

--- x/erc20/keeper/keeper.go
+++ x/erc20/keeper/keeper.go
@@ -6,13 +6,11 @@ import (
 	"github.com/cosmos/cosmos-sdk/codec"
 	storetypes "github.com/cosmos/cosmos-sdk/store/types"
 	sdk "github.com/cosmos/cosmos-sdk/types"
-	capabilitytypes "github.com/cosmos/cosmos-sdk/x/capability/types"
 	paramtypes "github.com/cosmos/cosmos-sdk/x/params/types"
 	"github.com/tendermint/tendermint/libs/log"
 
 	transfertypes "github.com/cosmos/ibc-go/v5/modules/apps/transfer/types"
 	porttypes "github.com/cosmos/ibc-go/v5/modules/core/05-port/types"
-	"github.com/cosmos/ibc-go/v5/modules/core/exported"
 
 	"github.com/evmos/evmos/v9/x/erc20/types"
 )
@@ -71,23 +69,4 @@ func (k *Keeper) SetICS4Wrapper(ics4Wrapper porttypes.ICS4Wrapper) {
 // Logger returns a module-specific logger.
 func (k Keeper) Logger(ctx sdk.Context) log.Logger {
 	return ctx.Logger().With("module", fmt.Sprintf("x/%s", types.ModuleName))
-}
-
-// IBC callbacks and transfer handlers
-
-// SendPacket implements the ICS4Wrapper interface from the transfer module.
-// It calls the underlying SendPacket function directly to move down the middleware stack.
-func (k Keeper) SendPacket(ctx sdk.Context, channelCap *capabilitytypes.Capability, packet exported.PacketI) error {
-	return k.ics4Wrapper.SendPacket(ctx, channelCap, packet)
-}
-
-// WriteAcknowledgement implements the ICS4Wrapper interface from the transfer module.
-// It calls the underlying WriteAcknowledgement function directly to move down the middleware stack.
-func (k Keeper) WriteAcknowledgement(ctx sdk.Context, channelCap *capabilitytypes.Capability, packet exported.PacketI, ack exported.Acknowledgement) error {
-	return k.ics4Wrapper.WriteAcknowledgement(ctx, channelCap, packet, ack)
-}
-
-// GetAppVersion returns the underlying application version.
-func (k Keeper) GetAppVersion(ctx sdk.Context, portID, channelID string) (string, bool) {
-	return k.ics4Wrapper.GetAppVersion(ctx, portID, channelID)
 }
