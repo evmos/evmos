69515732c11a55b15863690f0b9cebf6c4441f22
Ramiro Carlucho <ramirocarlucho@gmail.com>
2022-11-02 23:00:58 +0000

    imp(erc20): ibc integration tests setup (#1036)
    initial setup

--- x/erc20/keeper/keeper_test.go
+++ x/erc20/keeper/keeper_test.go
@@ -5,6 +5,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"math/big"
+	"strconv"
 	"testing"
 	"time"
 
@@ -12,9 +13,11 @@ import (
 	. "github.com/onsi/gomega"
 
 	"github.com/cosmos/cosmos-sdk/baseapp"
+	"github.com/cosmos/cosmos-sdk/client"
 	"github.com/cosmos/cosmos-sdk/crypto/keyring"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	authtypes "github.com/cosmos/cosmos-sdk/x/auth/types"
+	minttypes "github.com/cosmos/cosmos-sdk/x/mint/types"
 	stakingtypes "github.com/cosmos/cosmos-sdk/x/staking/types"
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/common/hexutil"
@@ -43,10 +46,17 @@ import (
 
 	"github.com/evmos/evmos/v9/app"
 	"github.com/evmos/evmos/v9/contracts"
+	ibctesting "github.com/evmos/evmos/v9/ibc/testing"
 	claimstypes "github.com/evmos/evmos/v9/x/claims/types"
+	claimtypes "github.com/evmos/evmos/v9/x/claims/types"
 	"github.com/evmos/evmos/v9/x/erc20/types"
+	inflationtypes "github.com/evmos/evmos/v9/x/inflation/types"
 
+	transfertypes "github.com/cosmos/ibc-go/v5/modules/apps/transfer/types"
+	clienttypes "github.com/cosmos/ibc-go/v5/modules/core/02-client/types"
+	channeltypes "github.com/cosmos/ibc-go/v5/modules/core/04-channel/types"
 	ibcgotesting "github.com/cosmos/ibc-go/v5/testing"
+	ibcgotestinghelpers "github.com/cosmos/ibc-go/v5/testing/simapp/helpers"
 )
 
 type KeeperTestSuite struct {
@@ -58,6 +68,7 @@ type KeeperTestSuite struct {
 	queryClient      types.QueryClient
 	address          common.Address
 	consAddress      sdk.ConsAddress
+	clientCtx        client.Context
 	ethSigner        ethtypes.Signer
 	validator        stakingtypes.Validator
 	signer           keyring.Signer
@@ -166,6 +177,144 @@ func (suite *KeeperTestSuite) DoSetupTest(t require.TestingT) {
 	}
 
 	suite.ethSigner = ethtypes.LatestSignerForChainID(s.app.EvmKeeper.ChainID())
+
+	if suite.suiteIBCTesting {
+		suite.SetupIBCTest()
+	}
+}
+
+var (
+	timeoutHeight = clienttypes.NewHeight(1000, 1000)
+
+	uosmoDenomtrace = transfertypes.DenomTrace{
+		Path:      "transfer/channel-0",
+		BaseDenom: "uosmo",
+	}
+
+	uosmoIbcdenom = uosmoDenomtrace.IBCDenom()
+
+	uatomDenomtrace = transfertypes.DenomTrace{
+		Path:      "transfer/channel-1",
+		BaseDenom: "uatom",
+	}
+	uatomIbcdenom = uatomDenomtrace.IBCDenom()
+
+	aevmosDenomtrace = transfertypes.DenomTrace{
+		Path:      "transfer/channel-0",
+		BaseDenom: "aevmos",
+	}
+	aevmosIbcdenom = aevmosDenomtrace.IBCDenom()
+
+	uatomOsmoDenomtrace = transfertypes.DenomTrace{
+		Path:      "transfer/channel-0/transfer/channel-1",
+		BaseDenom: "uatom",
+	}
+	uatomOsmoIbcdenom = uatomOsmoDenomtrace.IBCDenom()
+)
+
+func (suite *KeeperTestSuite) SendAndReceiveMessage(path *ibcgotesting.Path, origin *ibcgotesting.TestChain, coin string, amount int64, sender string, receiver string, seq uint64) {
+	// Send coin from A to B
+	transferMsg := transfertypes.NewMsgTransfer(path.EndpointA.ChannelConfig.PortID, path.EndpointA.ChannelID, sdk.NewCoin(coin, sdk.NewInt(amount)), sender, receiver, timeoutHeight, 0)
+	_, err := origin.SendMsgs(transferMsg)
+	suite.Require().NoError(err) // message committed
+	// Recreate the packet that was sent
+	transfer := transfertypes.NewFungibleTokenPacketData(coin, strconv.Itoa(int(amount)), sender, receiver)
+	packet := channeltypes.NewPacket(transfer.GetBytes(), seq, path.EndpointA.ChannelConfig.PortID, path.EndpointA.ChannelID, path.EndpointB.ChannelConfig.PortID, path.EndpointB.ChannelID, timeoutHeight, 0)
+	// Receive message on the counterparty side, and send ack
+	err = path.RelayPacket(packet)
+}
+
+func CreatePacket(amount, denom, sender, receiver, srcPort, srcChannel, dstPort, dstChannel string, seq, timeout uint64) channeltypes.Packet {
+	transfer := transfertypes.FungibleTokenPacketData{
+		Amount:   amount,
+		Denom:    denom,
+		Receiver: sender,
+		Sender:   receiver,
+	}
+	return channeltypes.NewPacket(
+		transfer.GetBytes(),
+		seq,
+		srcPort,
+		srcChannel,
+		dstPort,
+		dstChannel,
+		clienttypes.ZeroHeight(), // timeout height disabled
+		timeout,
+	)
+}
+
+func (suite *KeeperTestSuite) SetupIBCTest() {
+	// initializes 3 test chains
+	suite.coordinator = ibctesting.NewCoordinator(suite.T(), 1, 2)
+	suite.EvmosChain = suite.coordinator.GetChain(ibcgotesting.GetChainID(1))
+	suite.IBCOsmosisChain = suite.coordinator.GetChain(ibcgotesting.GetChainID(2))
+	suite.IBCCosmosChain = suite.coordinator.GetChain(ibcgotesting.GetChainID(3))
+	suite.coordinator.CommitNBlocks(suite.EvmosChain, 2)
+	suite.coordinator.CommitNBlocks(suite.IBCOsmosisChain, 2)
+	suite.coordinator.CommitNBlocks(suite.IBCCosmosChain, 2)
+
+	// Increase max gas
+	ibcgotestinghelpers.DefaultGenTxGas = uint64(1000000000)
+
+	// Set block propooser once, so its carried over on the ibc-go-testing suite
+	validators := suite.EvmosChain.App.(*app.Evmos).StakingKeeper.GetValidators(suite.EvmosChain.GetContext(), 2)
+	cons, err := validators[0].GetConsAddr()
+	suite.EvmosChain.CurrentHeader.ProposerAddress = cons.Bytes()
+
+	err = suite.EvmosChain.App.(*app.Evmos).StakingKeeper.SetValidatorByConsAddr(suite.EvmosChain.GetContext(), validators[0])
+
+	_, err = suite.EvmosChain.App.(*app.Evmos).EvmKeeper.GetCoinbaseAddress(suite.EvmosChain.GetContext())
+	suite.Require().NoError(err)
+	// Mint coins locked on the evmos account generated with secp.
+	coinEvmos := sdk.NewCoin("aevmos", sdk.NewInt(10000))
+	coins := sdk.NewCoins(coinEvmos)
+	err = suite.EvmosChain.App.(*app.Evmos).BankKeeper.MintCoins(suite.EvmosChain.GetContext(), inflationtypes.ModuleName, coins)
+	suite.Require().NoError(err)
+	err = suite.EvmosChain.App.(*app.Evmos).BankKeeper.SendCoinsFromModuleToAccount(suite.EvmosChain.GetContext(), inflationtypes.ModuleName, suite.IBCOsmosisChain.SenderAccount.GetAddress(), coins)
+	suite.Require().NoError(err)
+
+	// we need some coins in the bankkeeper to be able to register the coins later
+	coins = sdk.NewCoins(sdk.NewCoin(uosmoIbcdenom, sdk.NewInt(100)))
+	err = s.EvmosChain.App.(*app.Evmos).BankKeeper.MintCoins(s.EvmosChain.GetContext(), types.ModuleName, coins)
+	s.Require().NoError(err)
+	coins = sdk.NewCoins(sdk.NewCoin(uatomIbcdenom, sdk.NewInt(100)))
+	err = s.EvmosChain.App.(*app.Evmos).BankKeeper.MintCoins(s.EvmosChain.GetContext(), types.ModuleName, coins)
+	s.Require().NoError(err)
+
+	// Mint coins on the osmosis side which we'll use to unlock our aevmos
+	coinOsmo := sdk.NewCoin("uosmo", sdk.NewInt(10))
+	coins = sdk.NewCoins(coinOsmo)
+	err = suite.IBCOsmosisChain.GetSimApp().BankKeeper.MintCoins(suite.IBCOsmosisChain.GetContext(), minttypes.ModuleName, coins)
+	suite.Require().NoError(err)
+	err = suite.IBCOsmosisChain.GetSimApp().BankKeeper.SendCoinsFromModuleToAccount(suite.IBCOsmosisChain.GetContext(), minttypes.ModuleName, suite.IBCOsmosisChain.SenderAccount.GetAddress(), coins)
+	suite.Require().NoError(err)
+
+	// Mint coins on the cosmos side which we'll use to unlock our aevmos
+	coinAtom := sdk.NewCoin("uatom", sdk.NewInt(10))
+	coins = sdk.NewCoins(coinAtom)
+	err = suite.IBCCosmosChain.GetSimApp().BankKeeper.MintCoins(suite.IBCCosmosChain.GetContext(), minttypes.ModuleName, coins)
+	suite.Require().NoError(err)
+	err = suite.IBCCosmosChain.GetSimApp().BankKeeper.SendCoinsFromModuleToAccount(suite.IBCCosmosChain.GetContext(), minttypes.ModuleName, suite.IBCCosmosChain.SenderAccount.GetAddress(), coins)
+	suite.Require().NoError(err)
+
+	claimparams := claimtypes.DefaultParams()
+	claimparams.AirdropStartTime = suite.EvmosChain.GetContext().BlockTime()
+	claimparams.EnableClaims = true
+	suite.EvmosChain.App.(*app.Evmos).ClaimsKeeper.SetParams(suite.EvmosChain.GetContext(), claimparams)
+
+	params := types.DefaultParams()
+	params.EnableErc20 = true
+	suite.EvmosChain.App.(*app.Evmos).Erc20Keeper.SetParams(suite.EvmosChain.GetContext(), params)
+
+	suite.pathOsmosisEvmos = ibctesting.NewTransferPath(suite.IBCOsmosisChain, suite.EvmosChain) // clientID, connectionID, channelID empty
+	suite.pathCosmosEvmos = ibctesting.NewTransferPath(suite.IBCCosmosChain, suite.EvmosChain)
+	suite.pathOsmosisCosmos = ibctesting.NewTransferPath(suite.IBCCosmosChain, suite.IBCOsmosisChain)
+	suite.coordinator.Setup(suite.pathOsmosisEvmos) // clientID, connectionID, channelID filled
+	suite.coordinator.Setup(suite.pathCosmosEvmos)
+	suite.coordinator.Setup(suite.pathOsmosisCosmos)
+	suite.Require().Equal("07-tendermint-0", suite.pathOsmosisEvmos.EndpointA.ClientID)
+	suite.Require().Equal("connection-0", suite.pathOsmosisEvmos.EndpointA.ConnectionID)
+	suite.Require().Equal("channel-0", suite.pathOsmosisEvmos.EndpointA.ChannelID)
 }
 
 func (suite *KeeperTestSuite) StateDB() *statedb.StateDB {
