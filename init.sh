998bb46bea1ab473da47819463e95c2ba18da1e6
MalteHerrmann <malteherrmann.mail@web.de>
2022-11-02 15:57:31 +0100

    Use --home flag for evmosd commands to store config in temp folder

--- init.sh
+++ init.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-KEYS[0]="mykey"
-KEYS[1]="mykey1"
-KEYS[2]="mykey2"
+KEYS[0]="dev0"
+KEYS[1]="dev1"
+KEYS[2]="dev2"
 CHAINID="evmos_9000-1"
 MONIKER="localtestnet"
 KEYRING="test" # remember to change to other types of keyring like 'file' in-case exposing to outside world, otherwise your balance will be wiped quickly. The keyring test does not require private key to steal tokens from you
@@ -10,8 +10,6 @@ LOGLEVEL="info"
 # to trace evm
 #TRACE="--trace"
 TRACE=""
-# To remove previous installation folders (-> ~/.evmosd*)
-REMOVE_FOLDERS=true
 
 # validate dependencies are installed
 command -v jq > /dev/null 2>&1 || { echo >&2 "jq not installed. More info: https://stedolan.github.io/jq/download/"; exit 1; }
@@ -19,16 +17,8 @@ command -v jq > /dev/null 2>&1 || { echo >&2 "jq not installed. More info: https
 # used to exit on first error (any non-zero exit code)
 set -e
 
-# Clear everything of previous installation
-if [ "$REMOVE_FOLDERS" = true ]; then
-  rm -rf ~/.evmosd*
-else
-  if [ -d ~/.evmosd ]; then
-    echo "Folder ~/.evmosd exists already and REMOVE_FOLDERS is set to false in the init.sh script."
-    echo "Please either set REMOVE_FOLDERS=true or clean up your installation by manually deleting or moving the folder."
-    exit 1
-  fi
-fi
+# Set dedicated home directory for the evmosd instance
+homedir=~/.temp-evmosd
 
 # Reinstall daemon
 make install
@@ -40,85 +30,88 @@ evmosd config chain-id $CHAINID
 # If keys exist they should be deleted
 for KEY in "${KEYS[@]}"
 do
-  evmosd keys add $KEY --keyring-backend $KEYRING --algo $KEYALGO
+  evmosd keys add $KEY --keyring-backend $KEYRING --algo $KEYALGO --home $homedir
 done
 
 # Set moniker and chain-id for Evmos (Moniker can be anything, chain-id must be an integer)
-evmosd init $MONIKER --chain-id $CHAINID
+evmosd init $MONIKER -o --chain-id $CHAINID --home $homedir
 
 # Change parameter token denominations to aevmos
-cat $HOME/.evmosd/config/genesis.json | jq '.app_state["staking"]["params"]["bond_denom"]="aevmos"' > $HOME/.evmosd/config/tmp_genesis.json && mv $HOME/.evmosd/config/tmp_genesis.json $HOME/.evmosd/config/genesis.json
-cat $HOME/.evmosd/config/genesis.json | jq '.app_state["crisis"]["constant_fee"]["denom"]="aevmos"' > $HOME/.evmosd/config/tmp_genesis.json && mv $HOME/.evmosd/config/tmp_genesis.json $HOME/.evmosd/config/genesis.json
-cat $HOME/.evmosd/config/genesis.json | jq '.app_state["gov"]["deposit_params"]["min_deposit"][0]["denom"]="aevmos"' > $HOME/.evmosd/config/tmp_genesis.json && mv $HOME/.evmosd/config/tmp_genesis.json $HOME/.evmosd/config/genesis.json
-cat $HOME/.evmosd/config/genesis.json | jq '.app_state["evm"]["params"]["evm_denom"]="aevmos"' > $HOME/.evmosd/config/tmp_genesis.json && mv $HOME/.evmosd/config/tmp_genesis.json $HOME/.evmosd/config/genesis.json
-cat $HOME/.evmosd/config/genesis.json | jq '.app_state["inflation"]["params"]["mint_denom"]="aevmos"' > $HOME/.evmosd/config/tmp_genesis.json && mv $HOME/.evmosd/config/tmp_genesis.json $HOME/.evmosd/config/genesis.json
+cat $homedir/config/genesis.json | jq '.app_state["staking"]["params"]["bond_denom"]="aevmos"' > $homedir/config/tmp_genesis.json && mv $homedir/config/tmp_genesis.json $homedir/config/genesis.json
+cat $homedir/config/genesis.json | jq '.app_state["crisis"]["constant_fee"]["denom"]="aevmos"' > $homedir/config/tmp_genesis.json && mv $homedir/config/tmp_genesis.json $homedir/config/genesis.json
+cat $homedir/config/genesis.json | jq '.app_state["gov"]["deposit_params"]["min_deposit"][0]["denom"]="aevmos"' > $homedir/config/tmp_genesis.json && mv $homedir/config/tmp_genesis.json $homedir/config/genesis.json
+cat $homedir/config/genesis.json | jq '.app_state["evm"]["params"]["evm_denom"]="aevmos"' > $homedir/config/tmp_genesis.json && mv $homedir/config/tmp_genesis.json $homedir/config/genesis.json
+cat $homedir/config/genesis.json | jq '.app_state["inflation"]["params"]["mint_denom"]="aevmos"' > $homedir/config/tmp_genesis.json && mv $homedir/config/tmp_genesis.json $homedir/config/genesis.json
 
 # Set gas limit in genesis
-cat $HOME/.evmosd/config/genesis.json | jq '.consensus_params["block"]["max_gas"]="10000000"' > $HOME/.evmosd/config/tmp_genesis.json && mv $HOME/.evmosd/config/tmp_genesis.json $HOME/.evmosd/config/genesis.json
+cat $homedir/config/genesis.json | jq '.consensus_params["block"]["max_gas"]="10000000"' > $homedir/config/tmp_genesis.json && mv $homedir/config/tmp_genesis.json $homedir/config/genesis.json
 
 # Set claims start time
 current_date=$(date -u +"%Y-%m-%dT%TZ")
-cat $HOME/.evmosd/config/genesis.json | jq -r --arg current_date "$current_date" '.app_state["claims"]["params"]["airdrop_start_time"]=$current_date' > $HOME/.evmosd/config/tmp_genesis.json && mv $HOME/.evmosd/config/tmp_genesis.json $HOME/.evmosd/config/genesis.json
+cat $homedir/config/genesis.json | jq -r --arg current_date "$current_date" '.app_state["claims"]["params"]["airdrop_start_time"]=$current_date' > $homedir/config/tmp_genesis.json && mv $homedir/config/tmp_genesis.json $homedir/config/genesis.json
 
 # Set claims records for validator account
 amount_to_claim=10000
 claims_key=${KEYS[0]}
-node_address=$(evmosd keys show $claims_key | grep "address" | cut -c12-)
-cat $HOME/.evmosd/config/genesis.json | jq -r --arg node_address "$node_address" --arg amount_to_claim "$amount_to_claim" '.app_state["claims"]["claims_records"]=[{"initial_claimable_amount":$amount_to_claim, "actions_completed":[false, false, false, false],"address":$node_address}]' > $HOME/.evmosd/config/tmp_genesis.json && mv $HOME/.evmosd/config/tmp_genesis.json $HOME/.evmosd/config/genesis.json
+node_address=$(evmosd keys show $claims_key --keyring-backend $KEYRING --home $homedir | grep "address" | cut -c12-)
+cat $homedir/config/genesis.json | jq -r --arg node_address "$node_address" --arg amount_to_claim "$amount_to_claim" '.app_state["claims"]["claims_records"]=[{"initial_claimable_amount":$amount_to_claim, "actions_completed":[false, false, false, false],"address":$node_address}]' > $homedir/config/tmp_genesis.json && mv $homedir/config/tmp_genesis.json $homedir/config/genesis.json
 
 # Set claims decay
-cat $HOME/.evmosd/config/genesis.json | jq '.app_state["claims"]["params"]["duration_of_decay"]="1000000s"' > $HOME/.evmosd/config/tmp_genesis.json && mv $HOME/.evmosd/config/tmp_genesis.json $HOME/.evmosd/config/genesis.json
-cat $HOME/.evmosd/config/genesis.json | jq '.app_state["claims"]["params"]["duration_until_decay"]="100000s"' > $HOME/.evmosd/config/tmp_genesis.json && mv $HOME/.evmosd/config/tmp_genesis.json $HOME/.evmosd/config/genesis.json
+cat $homedir/config/genesis.json | jq '.app_state["claims"]["params"]["duration_of_decay"]="1000000s"' > $homedir/config/tmp_genesis.json && mv $homedir/config/tmp_genesis.json $homedir/config/genesis.json
+cat $homedir/config/genesis.json | jq '.app_state["claims"]["params"]["duration_until_decay"]="100000s"' > $homedir/config/tmp_genesis.json && mv $homedir/config/tmp_genesis.json $homedir/config/genesis.json
 
 # Claim module account:
 # 0xA61808Fe40fEb8B3433778BBC2ecECCAA47c8c47 || evmos15cvq3ljql6utxseh0zau9m8ve2j8erz89m5wkz
-cat $HOME/.evmosd/config/genesis.json | jq -r --arg amount_to_claim "$amount_to_claim" '.app_state["bank"]["balances"] += [{"address":"evmos15cvq3ljql6utxseh0zau9m8ve2j8erz89m5wkz","coins":[{"denom":"aevmos", "amount":$amount_to_claim}]}]' > $HOME/.evmosd/config/tmp_genesis.json && mv $HOME/.evmosd/config/tmp_genesis.json $HOME/.evmosd/config/genesis.json
+cat $homedir/config/genesis.json | jq -r --arg amount_to_claim "$amount_to_claim" '.app_state["bank"]["balances"] += [{"address":"evmos15cvq3ljql6utxseh0zau9m8ve2j8erz89m5wkz","coins":[{"denom":"aevmos", "amount":$amount_to_claim}]}]' > $homedir/config/tmp_genesis.json && mv $homedir/config/tmp_genesis.json $homedir/config/genesis.json
 
 # disable produce empty block
 if [[ "$OSTYPE" == "darwin"* ]]; then
-    sed -i '' 's/create_empty_blocks = true/create_empty_blocks = false/g' $HOME/.evmosd/config/config.toml
+    sed -i '' 's/create_empty_blocks = true/create_empty_blocks = false/g' $homedir/config/config.toml
   else
-    sed -i 's/create_empty_blocks = true/create_empty_blocks = false/g' $HOME/.evmosd/config/config.toml
+    sed -i 's/create_empty_blocks = true/create_empty_blocks = false/g' $homedir/config/config.toml
 fi
 
 if [[ $1 == "pending" ]]; then
   if [[ "$OSTYPE" == "darwin"* ]]; then
-      sed -i '' 's/create_empty_blocks_interval = "0s"/create_empty_blocks_interval = "30s"/g' $HOME/.evmosd/config/config.toml
-      sed -i '' 's/timeout_propose = "3s"/timeout_propose = "30s"/g' $HOME/.evmosd/config/config.toml
-      sed -i '' 's/timeout_propose_delta = "500ms"/timeout_propose_delta = "5s"/g' $HOME/.evmosd/config/config.toml
-      sed -i '' 's/timeout_prevote = "1s"/timeout_prevote = "10s"/g' $HOME/.evmosd/config/config.toml
-      sed -i '' 's/timeout_prevote_delta = "500ms"/timeout_prevote_delta = "5s"/g' $HOME/.evmosd/config/config.toml
-      sed -i '' 's/timeout_precommit = "1s"/timeout_precommit = "10s"/g' $HOME/.evmosd/config/config.toml
-      sed -i '' 's/timeout_precommit_delta = "500ms"/timeout_precommit_delta = "5s"/g' $HOME/.evmosd/config/config.toml
-      sed -i '' 's/timeout_commit = "5s"/timeout_commit = "150s"/g' $HOME/.evmosd/config/config.toml
-      sed -i '' 's/timeout_broadcast_tx_commit = "10s"/timeout_broadcast_tx_commit = "150s"/g' $HOME/.evmosd/config/config.toml
+      sed -i '' 's/create_empty_blocks_interval = "0s"/create_empty_blocks_interval = "30s"/g' $homedir/config/config.toml
+      sed -i '' 's/timeout_propose = "3s"/timeout_propose = "30s"/g' $homedir/config/config.toml
+      sed -i '' 's/timeout_propose_delta = "500ms"/timeout_propose_delta = "5s"/g' $homedir/config/config.toml
+      sed -i '' 's/timeout_prevote = "1s"/timeout_prevote = "10s"/g' $homedir/config/config.toml
+      sed -i '' 's/timeout_prevote_delta = "500ms"/timeout_prevote_delta = "5s"/g' $homedir/config/config.toml
+      sed -i '' 's/timeout_precommit = "1s"/timeout_precommit = "10s"/g' $homedir/config/config.toml
+      sed -i '' 's/timeout_precommit_delta = "500ms"/timeout_precommit_delta = "5s"/g' $homedir/config/config.toml
+      sed -i '' 's/timeout_commit = "5s"/timeout_commit = "150s"/g' $homedir/config/config.toml
+      sed -i '' 's/timeout_broadcast_tx_commit = "10s"/timeout_broadcast_tx_commit = "150s"/g' $homedir/config/config.toml
   else
-      sed -i 's/create_empty_blocks_interval = "0s"/create_empty_blocks_interval = "30s"/g' $HOME/.evmosd/config/config.toml
-      sed -i 's/timeout_propose = "3s"/timeout_propose = "30s"/g' $HOME/.evmosd/config/config.toml
-      sed -i 's/timeout_propose_delta = "500ms"/timeout_propose_delta = "5s"/g' $HOME/.evmosd/config/config.toml
-      sed -i 's/timeout_prevote = "1s"/timeout_prevote = "10s"/g' $HOME/.evmosd/config/config.toml
-      sed -i 's/timeout_prevote_delta = "500ms"/timeout_prevote_delta = "5s"/g' $HOME/.evmosd/config/config.toml
-      sed -i 's/timeout_precommit = "1s"/timeout_precommit = "10s"/g' $HOME/.evmosd/config/config.toml
-      sed -i 's/timeout_precommit_delta = "500ms"/timeout_precommit_delta = "5s"/g' $HOME/.evmosd/config/config.toml
-      sed -i 's/timeout_commit = "5s"/timeout_commit = "150s"/g' $HOME/.evmosd/config/config.toml
-      sed -i 's/timeout_broadcast_tx_commit = "10s"/timeout_broadcast_tx_commit = "150s"/g' $HOME/.evmosd/config/config.toml
+      sed -i 's/create_empty_blocks_interval = "0s"/create_empty_blocks_interval = "30s"/g' $homedir/config/config.toml
+      sed -i 's/timeout_propose = "3s"/timeout_propose = "30s"/g' $homedir/config/config.toml
+      sed -i 's/timeout_propose_delta = "500ms"/timeout_propose_delta = "5s"/g' $homedir/config/config.toml
+      sed -i 's/timeout_prevote = "1s"/timeout_prevote = "10s"/g' $homedir/config/config.toml
+      sed -i 's/timeout_prevote_delta = "500ms"/timeout_prevote_delta = "5s"/g' $homedir/config/config.toml
+      sed -i 's/timeout_precommit = "1s"/timeout_precommit = "10s"/g' $homedir/config/config.toml
+      sed -i 's/timeout_precommit_delta = "500ms"/timeout_precommit_delta = "5s"/g' $homedir/config/config.toml
+      sed -i 's/timeout_commit = "5s"/timeout_commit = "150s"/g' $homedir/config/config.toml
+      sed -i 's/timeout_broadcast_tx_commit = "10s"/timeout_broadcast_tx_commit = "150s"/g' $homedir/config/config.toml
   fi
 fi
 
 # Allocate genesis accounts (cosmos formatted addresses)
 for KEY in "${KEYS[@]}"
 do
-  evmosd add-genesis-account $KEY 100000000000000000000000000aevmos --keyring-backend $KEYRING
+  evmosd add-genesis-account $KEY 100000000000000000000000000aevmos --keyring-backend $KEYRING --home $homedir
 done 
 
 # Update total supply with claim values
-validators_supply=$(cat $HOME/.evmosd/config/genesis.json | jq -r '.app_state["bank"]["supply"][0]["amount"]')
+validators_supply=$(cat $homedir/config/genesis.json | jq -r '.app_state["bank"]["supply"][0]["amount"]')
 # Bc is required to add these big numbers
 total_supply=$(echo "${#KEYS[@]} * 100000000000000000000000000 + $amount_to_claim" | bc)
-cat $HOME/.evmosd/config/genesis.json | jq -r --arg total_supply "$total_supply" '.app_state["bank"]["supply"][0]["amount"]=$total_supply' > $HOME/.evmosd/config/tmp_genesis.json && mv $HOME/.evmosd/config/tmp_genesis.json $HOME/.evmosd/config/genesis.json
+cat $homedir/config/genesis.json | jq -r --arg total_supply "$total_supply" '.app_state["bank"]["supply"][0]["amount"]=$total_supply' > $homedir/config/tmp_genesis.json && mv $homedir/config/tmp_genesis.json $homedir/config/genesis.json
+
+# Remove genesis transaction if it exists already
+rm -rf $homedir/config/gentx
 
 # Sign genesis transaction
-evmosd gentx ${KEYS[0]} 1000000000000000000000aevmos --keyring-backend $KEYRING --chain-id $CHAINID
+evmosd gentx ${KEYS[0]} 1000000000000000000000aevmos --keyring-backend $KEYRING --chain-id $CHAINID --home $homedir
 ## In case you want to create multiple validators at genesis
 ## 1. Back to `evmosd keys add` step, init more keys
 ## 2. Back to `evmosd add-genesis-account` step, add balance for those
@@ -127,14 +120,14 @@ evmosd gentx ${KEYS[0]} 1000000000000000000000aevmos --keyring-backend $KEYRING 
 ## 5. Copy the `gentx-*` folders under `~/.clonedEvmosd/config/gentx/` folders into the original `~/.evmosd/config/gentx`
 
 # Collect genesis tx
-evmosd collect-gentxs
+evmosd collect-gentxs --home $homedir
 
 # Run this to ensure everything worked and that the genesis file is setup correctly
-evmosd validate-genesis
+evmosd validate-genesis --home $homedir
 
 if [[ $1 == "pending" ]]; then
   echo "pending mode is on, please wait for the first block committed."
 fi
 
 # Start the node (remove the --pruning=nothing flag if historical queries are not needed)
-evmosd start --pruning=nothing $TRACE --log_level $LOGLEVEL --minimum-gas-prices=0.0001aevmos --json-rpc.api eth,txpool,personal,net,debug,web3
+evmosd start --pruning=nothing $TRACE --log_level $LOGLEVEL --minimum-gas-prices=0.0001aevmos --json-rpc.api eth,txpool,personal,net,debug,web3 --home $homedir
