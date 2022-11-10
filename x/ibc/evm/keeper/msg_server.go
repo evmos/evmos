38668f11c036231c287b4919b8ce9ba6d1f4727f
Vladislav Varadinov <vlad@evmos.org>
2022-11-10 18:38:19 +0100

    boilerplate

--- x/ibc/evm/keeper/msg_server.go
+++ x/ibc/evm/keeper/msg_server.go
@@ -1,1 +1,15 @@
 package keeper
+
+import (
+	"context"
+	"github.com/evmos/evmos/x/ibc/evm/types"
+)
+
+var _ types.MsgServer = &Keeper{}
+
+func (k Keeper) CallEVM(ctx context.Context, msg *types.MsgCallEVM) (*types.MsgCallEVMResponse, error) {
+
+	res := *types.MsgCallEVMResponse{}
+	return res, nil
+
+}
