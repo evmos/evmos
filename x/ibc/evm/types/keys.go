8934b30b553d8a0928358ec17d3a8493029b3ca2
Tom <tomasguerraalda@hotmail.com>
2022-11-10 11:54:48 -0300

    ibcEvmTx (dev) add boilerplate code - module, keeper and types

--- x/ibc/evm/types/keys.go
+++ x/ibc/evm/types/keys.go
@@ -1,1 +1,22 @@
 package types
+
+const (
+	// ModuleName defines the IBC EVM transaction name
+	ModuleName = "ibc-evm-tx"
+
+	// Version defines the current version the IBC tranfer
+	// module supports
+	Version = "ibc-evm-tx-1"
+
+	// PortID is the default port id that transfer module binds to
+	PortID = "evm-tx"
+
+	// StoreKey is the store key string for IBC transfer
+	StoreKey = ModuleName
+
+	// RouterKey is the message route for IBC transfer
+	RouterKey = ModuleName
+
+	// QuerierRoute is the querier route for IBC transfer
+	QuerierRoute = ModuleName
+)
