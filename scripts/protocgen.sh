d543ced401258d2dd52e89b07d470fd49881a416
MalteHerrmann <malteherrmann.mail@web.de>
2022-11-18 12:31:41 +0100

    address more linter comments

--- scripts/protocgen.sh
+++ scripts/protocgen.sh
@@ -12,8 +12,8 @@ for dir in $proto_dirs; do
   proto_files=$(find "${dir}" -maxdepth 1 -name '*.proto')
   for file in $proto_files; do
     # Check if the go_package in the file is pointing to evmos
-    if grep -q "option go_package.*evmos" $file; then
-      buf generate --template proto/buf.gen.gogo.yaml $file
+    if grep -q "option go_package.*evmos" "$file"; then
+      buf generate --template proto/buf.gen.gogo.yaml "$file"
     fi
   done
 done
