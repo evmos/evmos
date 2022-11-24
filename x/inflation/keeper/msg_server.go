4e32abd53e1b0ff6c28b893d550d8deb52dd95eb
Vladislav Varadinov <vladislav.varadinov@gmail.com>
2022-11-24 12:45:49 +0200

    (refactor): added tx processing in the msg and msg_server and handler for the new message

---
+++ x/inflation/keeper/msg_server.go
@@ -0,0 +1,24 @@
+package keeper
+
+import (
+	"context"
+	errorsmod "cosmossdk.io/errors"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	govtypes "github.com/cosmos/cosmos-sdk/x/gov/types"
+	"github.com/evmos/evmos/v10/x/inflation/types"
+)
+
+var _ types.MsgServer = &Keeper{}
+
+func (k Keeper) UpdateParams(goCtx context.Context, req *types.MsgUpdateParams) (*types.MsgUpdateParamsResponse, error) {
+	if k.authority != req.Authority {
+		return nil, errorsmod.Wrapf(govtypes.ErrInvalidSigner, "invalid authority; expected %s, got %s", k.authority, req.Authority)
+	}
+
+	ctx := sdk.UnwrapSDKContext(goCtx)
+	if err := k.SetParams(ctx, req.Params); err != nil {
+		return nil, err
+	}
+
+	return &types.MsgUpdateParamsResponse{}, nil
+}
