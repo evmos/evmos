b783ab63e19f7a7d92e1d1e32dd7d218194eeae4
Vladislav Varadinov <vladislav.varadinov@gmail.com>
2022-12-01 15:45:08 +0200

    (fix): Ran linter

--- x/claims/types/params_test.go
+++ x/claims/types/params_test.go
@@ -2,9 +2,10 @@ package types
 
 import (
 	"fmt"
-	sdk "github.com/cosmos/cosmos-sdk/types"
 	"testing"
 	"time"
+
+	sdk "github.com/cosmos/cosmos-sdk/types"
 
 	"github.com/stretchr/testify/require"
 )
