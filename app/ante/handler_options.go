ba605119f4d8e82975d45c9c7381e8cd7ef82965
Federico Kunze Küllmer <31522760+fedekunze@users.noreply.github.com>
2022-12-02 18:33:12 +0100

    lint

--- app/ante/handler_options.go
+++ app/ante/handler_options.go
@@ -118,6 +118,7 @@ func newLegacyCosmosAnteHandlerEip712(options HandlerOptions) sdk.AnteHandler {
 		ante.NewValidateSigCountDecorator(options.AccountKeeper),
 		ante.NewSigGasConsumeDecorator(options.AccountKeeper, options.SigGasConsumer),
 		// Note: signature verification uses EIP instead of the cosmos signature validator
+		// nolint: staticcheck
 		ethante.NewLegacyEip712SigVerificationDecorator(options.AccountKeeper, options.SignModeHandler),
 		ante.NewIncrementSequenceDecorator(options.AccountKeeper),
 		ibcante.NewRedundantRelayDecorator(options.IBCKeeper),
