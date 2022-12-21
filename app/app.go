f7424b66c634975aa40edd8e030c37dcafc212b2
Freddy Caceres <facs95@gmail.com>
2022-12-21 06:50:49 -0500

    Release `v10.0.0-rc3` (#1179)
    * debug(app): add flag for remote debugging compile (#1160)
    
    * build(deps): bump github.com/onsi/ginkgo/v2 from 2.6.0 to 2.6.1 (#1168)
    
    Bumps [github.com/onsi/ginkgo/v2](https://github.com/onsi/ginkgo) from 2.6.0 to 2.6.1.
    - [Release notes](https://github.com/onsi/ginkgo/releases)
    - [Changelog](https://github.com/onsi/ginkgo/blob/master/CHANGELOG.md)
    - [Commits](https://github.com/onsi/ginkgo/compare/v2.6.0...v2.6.1)
    
    ---
    updated-dependencies:
    - dependency-name: github.com/onsi/ginkgo/v2
      dependency-type: direct:production
      update-type: version-update:semver-patch
    ...
    
    Signed-off-by: dependabot[bot] <support@github.com>
    
    Signed-off-by: dependabot[bot] <support@github.com>
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>
    
    * build(deps): bump github.com/onsi/gomega from 1.24.1 to 1.24.2 (#1167)
    
    Bumps [github.com/onsi/gomega](https://github.com/onsi/gomega) from 1.24.1 to 1.24.2.
    - [Release notes](https://github.com/onsi/gomega/releases)
    - [Changelog](https://github.com/onsi/gomega/blob/master/CHANGELOG.md)
    - [Commits](https://github.com/onsi/gomega/compare/v1.24.1...v1.24.2)
    
    ---
    updated-dependencies:
    - dependency-name: github.com/onsi/gomega
      dependency-type: direct:production
      update-type: version-update:semver-patch
    ...
    
    Signed-off-by: dependabot[bot] <support@github.com>
    
    Signed-off-by: dependabot[bot] <support@github.com>
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>
    
    * chore(cmd): Set default mempool to 'v1' and update default snapshot interval to `5000` (#1172)
    
    * chore(cmd): Set default mempool to 'v1'
    
    * changelog
    
    * update interval
    
    * build(deps): bump github.com/cosmos/ibc-go/v5 from 5.1.0 to 5.2.0 (#1176)
    
    Bumps [github.com/cosmos/ibc-go/v5](https://github.com/cosmos/ibc-go) from 5.1.0 to 5.2.0.
    - [Release notes](https://github.com/cosmos/ibc-go/releases)
    - [Changelog](https://github.com/cosmos/ibc-go/blob/v5.2.0/CHANGELOG.md)
    - [Commits](https://github.com/cosmos/ibc-go/compare/v5.1.0...v5.2.0)
    
    ---
    updated-dependencies:
    - dependency-name: github.com/cosmos/ibc-go/v5
      dependency-type: direct:production
      update-type: version-update:semver-minor
    ...
    
    Signed-off-by: dependabot[bot] <support@github.com>
    
    Signed-off-by: dependabot[bot] <support@github.com>
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>
    
    * update receiving module accounts
    
    * update changelog
    
    * fix linter and changelog
    
    Signed-off-by: dependabot[bot] <support@github.com>
    Co-authored-by: Tomas Guerra <54514587+GAtom22@users.noreply.github.com>
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>
    Co-authored-by: Federico Kunze Küllmer <31522760+fedekunze@users.noreply.github.com>

--- app/app.go
+++ app/app.go
@@ -233,7 +233,6 @@ var (
 
 	// module accounts that are allowed to receive tokens
 	allowedReceivingModAcc = map[string]bool{
-		distrtypes.ModuleName:      true,
 		incentivestypes.ModuleName: true,
 	}
 )
