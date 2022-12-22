eaad5aa7318f6fe7e255f55e2bed5769ff1d3dc7
MalteHerrmann <malte@evmos.org>
2022-12-22 18:15:54 +0100

    update upgrade name

--- app/upgrades/v10/constants.go
+++ app/upgrades/v10/constants.go
@@ -2,7 +2,7 @@ package v10
 
 const (
 	// UpgradeName is the shared upgrade plan name for mainnet
-	UpgradeName = "v10.0.0-rc2"
+	UpgradeName = "v10.0.0-rc3"
 	// UpgradeInfo defines the binaries that will be used for the upgrade
-	UpgradeInfo = `'{"binaries":{"darwin/arm64":"https://github.com/evmos/evmos/releases/download/v10.0.0-rc2/evmos_10.0.0-rc2_Darwin_arm64.tar.gz","darwin/amd64":"https://github.com/evmos/evmos/releases/download/v10.0.0-rc2/evmos_10.0.0-rc2_Darwin_amd64.tar.gz","linux/arm64":"https://github.com/evmos/evmos/releases/download/v10.0.0-rc2/evmos_10.0.0-rc2_Linux_arm64.tar.gz","linux/amd64":"https://github.com/evmos/evmos/releases/download/v10.0.0-rc2/evmos_10.0.0-rc2_Linux_amd64.tar.gz","windows/x86_64":"https://github.com/evmos/evmos/releases/download/v10.0.0-rc2/evmos_10.0.0-rc2_Windows_x86_64.zip"}}'`
+	UpgradeInfo = `'{"binaries":{"darwin/arm64":"https://github.com/evmos/evmos/releases/download/v10.0.0-rc3/evmos_10.0.0-rc3_Darwin_arm64.tar.gz","darwin/amd64":"https://github.com/evmos/evmos/releases/download/v10.0.0-rc3/evmos_10.0.0-rc3_Darwin_amd64.tar.gz","linux/arm64":"https://github.com/evmos/evmos/releases/download/v10.0.0-rc3/evmos_10.0.0-rc3_Linux_arm64.tar.gz","linux/amd64":"https://github.com/evmos/evmos/releases/download/v10.0.0-rc3/evmos_10.0.0-rc3_Linux_amd64.tar.gz","windows/x86_64":"https://github.com/evmos/evmos/releases/download/v10.0.0-rc3/evmos_10.0.0-rc3_Windows_x86_64.zip"}}'`
 )
