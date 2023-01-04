1d58d71eb9e5c419c120d2fb5bebbdd04a5407ca
adisaran64 <adi@evmos.org>
2023-01-04 07:48:09 -0800

    finish upgrade logic pre-testing

--- app/upgrades/v11/upgrades.go
+++ app/upgrades/v11/upgrades.go
@@ -2,6 +2,9 @@ package v11
 
 import (
 	"fmt"
+	"strconv"
+
+	"github.com/tendermint/tendermint/libs/log"
 
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/cosmos/cosmos-sdk/types/module"
@@ -12,6 +15,8 @@ import (
 	stakingkeeper "github.com/cosmos/cosmos-sdk/x/staking/keeper"
 	upgradetypes "github.com/cosmos/cosmos-sdk/x/upgrade/types"
 	ibctypes "github.com/cosmos/ibc-go/v5/modules/apps/transfer/types"
+
+	"github.com/evmos/evmos/v10/types"
 )
 
 // CreateUpgradeHandler creates an SDK upgrade handler for v11
@@ -24,6 +29,11 @@ func CreateUpgradeHandler(
 ) upgradetypes.UpgradeHandler {
 	return func(ctx sdk.Context, _ upgradetypes.Plan, vm module.VersionMap) (module.VersionMap, error) {
 		logger := ctx.Logger().With("upgrade", UpgradeName)
+
+		if types.IsMainnet(ctx.ChainID()) {
+			logger.Debug("distributing incentivized testnet rewards...")
+			HandleRewardDistribution(ctx, bk, sk, logger)
+		}
 
 		MigrateEscrowAccounts(ctx, ak)
 
@@ -64,23 +74,80 @@ func MigrateEscrowAccounts(ctx sdk.Context, ak authkeeper.AccountKeeper) {
 	}
 }
 
+// HandleMainnetUpgrade handles the logic for the reward distribution, it only commits to the db if successful
+func HandleRewardDistribution(ctx sdk.Context, bk bankkeeper.Keeper, sk stakingkeeper.Keeper, logger log.Logger) {
+	// use a cache context as a rollback mechanism in case
+	// the distrbution fails
+	cacheCtx, writeFn := ctx.CacheContext()
+	err := DistributeRewards(cacheCtx, bk, sk)
+	if err != nil {
+		// log error instead of aborting the upgrade
+		logger.Error("failed to distribute rewards", "error", err.Error())
+	} else {
+		writeFn()
+	}
+}
+
 // DistributeRewards distributes the token allocations from the Olympus Mons incentivized testnet
-func DistributeRewards(ctx sdk.Context, bk authtypes.BankKeeper, sk stakingkeeper.Keeper) error {
+func DistributeRewards(ctx sdk.Context, bk bankkeeper.Keeper, sk stakingkeeper.Keeper) error {
 	for _, currentDistribute := range Accounts {
 
 		// move rewards to the recieving account
 		receivingAccount := sdk.MustAccAddressFromBech32(currentDistribute[0])
-		receivingAmount, err := sdk.NewIntFromString(currentDistribute[1])
+		receivingAmount, err := strconv.ParseInt(currentDistribute[1], 10, 64)
 		if err != nil {
 			return fmt.Errorf(
-				"reward distribution to address %s failed due to invalid allocation parsing",
+				"reward distribution to address %s failed due to invalid parsing",
 				currentDistribute[0],
 			)
 		}
-		currentRewards := sdk.Coin{
-			Denom:  "aevmos",
-			Amount: receivingAmount,
+		currentRewards := sdk.Coins{
+			sdk.NewInt64Coin("aevmos", receivingAmount),
 		}
-		//bk.SendCoins(ctx, sdk.AccAddress(FundingAccount), receivingAccount, currentRewards)
+		err = bk.SendCoins(ctx, sdk.AccAddress(FundingAccount), receivingAccount, currentRewards)
+		if err != nil {
+			return fmt.Errorf(
+				"unable to send coins from fund account to participant account",
+			)
+		}
+
+		// stake from the recieving account to all validators equally
+		numValidators := len(Validators)
+		currentStakeAmount := (currentRewards.QuoInt(sdk.NewInt(int64(numValidators)))).AmountOf("aevmos")
+		for _, validatorBech32 := range Validators {
+			validatorAddress, err := sdk.ValAddressFromBech32(validatorBech32)
+			if err != nil {
+				return fmt.Errorf(
+					"unable to convert validator address %s",
+					validatorBech32,
+				)
+			}
+			validator, found := sk.GetValidator(ctx, validatorAddress)
+			if !found {
+				return fmt.Errorf(
+					"unable to find validator corresponding to address %s",
+					validatorBech32,
+				)
+			}
+			// 1 signifies unbonded tokens, subtractAccount being true means delegation, not redelegation
+			_, err = sk.Delegate(ctx, receivingAccount, currentStakeAmount, 1, validator, true)
+			if err != nil {
+				return fmt.Errorf(
+					"unable to delegate to validator with address %s",
+					validatorBech32,
+				)
+			}
+		}
 	}
+
+	// transfer all remaining tokens after distribution to the community pool
+	remainingFunds := bk.GetAllBalances(ctx, sdk.AccAddress(FundingAccount))
+	err := bk.SendCoins(ctx, sdk.AccAddress(FundingAccount), sdk.AccAddress(CommunityPoolAccount), remainingFunds)
+	if err != nil {
+		return fmt.Errorf(
+			"unable to send coins from fund account to community pool",
+		)
+	}
+
+	return nil
 }
