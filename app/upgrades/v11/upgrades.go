031f35bd6296cc5c0b5a04bdd03380c14d649ee9
MalteHerrmann <malte@evmos.org>
2023-01-04 17:53:47 +0100

    add function to v11 upgrade handler that initializes the ICA module with the desired params

--- app/upgrades/v11/upgrades.go
+++ app/upgrades/v11/upgrades.go
@@ -2,13 +2,21 @@ package v11
 
 import (
 	"fmt"
+	banktypes "github.com/cosmos/cosmos-sdk/x/bank/types"
+	distrtypes "github.com/cosmos/cosmos-sdk/x/distribution/types"
+	govtypes "github.com/cosmos/cosmos-sdk/x/gov/types/v1beta1"
+	stakingtypes "github.com/cosmos/cosmos-sdk/x/staking/types"
+	ica "github.com/cosmos/ibc-go/v5/modules/apps/27-interchain-accounts"
+	icacontrollertypes "github.com/cosmos/ibc-go/v5/modules/apps/27-interchain-accounts/controller/types"
+	icahosttypes "github.com/cosmos/ibc-go/v5/modules/apps/27-interchain-accounts/host/types"
+	icatypes "github.com/cosmos/ibc-go/v5/modules/apps/27-interchain-accounts/types"
+	transfertypes "github.com/cosmos/ibc-go/v5/modules/apps/transfer/types"
 
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/cosmos/cosmos-sdk/types/module"
 	authkeeper "github.com/cosmos/cosmos-sdk/x/auth/keeper"
 	authtypes "github.com/cosmos/cosmos-sdk/x/auth/types"
 	upgradetypes "github.com/cosmos/cosmos-sdk/x/upgrade/types"
-	ibctypes "github.com/cosmos/ibc-go/v5/modules/apps/transfer/types"
 )
 
 // CreateUpgradeHandler creates an SDK upgrade handler for v11
@@ -21,6 +29,7 @@ func CreateUpgradeHandler(
 		logger := ctx.Logger().With("upgrade", UpgradeName)
 
 		MigrateEscrowAccounts(ctx, ak)
+		InitICAModule(ctx, mm)
 
 		// Leave modules are as-is to avoid running InitGenesis.
 		logger.Debug("running module migrations ...")
@@ -32,7 +41,7 @@ func CreateUpgradeHandler(
 func MigrateEscrowAccounts(ctx sdk.Context, ak authkeeper.AccountKeeper) {
 	for i := 0; i <= openChannels; i++ {
 		channelID := fmt.Sprintf("channel-%d", i)
-		address := ibctypes.GetEscrowAddress(ibctypes.PortID, channelID)
+		address := transfertypes.GetEscrowAddress(transfertypes.PortID, channelID)
 
 		// check if account exists
 		existingAcc := ak.GetAccount(ctx, address)
@@ -47,14 +56,46 @@ func MigrateEscrowAccounts(ctx sdk.Context, ak authkeeper.AccountKeeper) {
 			continue
 		}
 
-		// account name based on the address derived by the ibctypes.GetEscrowAddress
+		// account name based on the address derived by the transfertypes.GetEscrowAddress
 		// this function appends the current IBC transfer module version to the provided port and channel IDs
 		// To pass account validation, need to have address derived from account name
-		accountName := fmt.Sprintf("%s\x00%s/%s", ibctypes.Version, ibctypes.PortID, channelID)
+		accountName := fmt.Sprintf("%s\x00%s/%s", transfertypes.Version, transfertypes.PortID, channelID)
 		baseAcc := authtypes.NewBaseAccountWithAddress(address)
 
 		// no special permissions defined for the module account
 		acc := authtypes.NewModuleAccount(baseAcc, accountName)
 		ak.SetModuleAccount(ctx, acc)
 	}
+}
+
+// InitICAModule will initialize the ICA module with the parameters defined in this function.
+func InitICAModule(ctx sdk.Context, mm *module.Manager) {
+	// create ICS27 Controller submodule params, controller module not enabled
+	controllerParams := icacontrollertypes.Params{}
+
+	// create ICS27 Host submodule params
+	hostParams := icahosttypes.Params{
+		HostEnabled: true,
+		AllowMessages: []string{
+			sdk.MsgTypeURL(&banktypes.MsgSend{}),
+			sdk.MsgTypeURL(&banktypes.MsgMultiSend{}),
+			sdk.MsgTypeURL(&distrtypes.MsgSetWithdrawAddress{}),
+			sdk.MsgTypeURL(&distrtypes.MsgWithdrawDelegatorReward{}),
+			sdk.MsgTypeURL(&govtypes.MsgVote{}),
+			sdk.MsgTypeURL(&govtypes.MsgVoteWeighted{}),
+			sdk.MsgTypeURL(&stakingtypes.MsgDelegate{}),
+			sdk.MsgTypeURL(&stakingtypes.MsgUndelegate{}),
+			sdk.MsgTypeURL(&stakingtypes.MsgCancelUnbondingDelegation{}),
+			sdk.MsgTypeURL(&stakingtypes.MsgBeginRedelegate{}),
+			sdk.MsgTypeURL(&transfertypes.MsgTransfer{}),
+		},
+	}
+
+	// initialize ICS27 module
+	icaModule, correctTypecast := mm.Modules[icatypes.ModuleName].(ica.AppModule)
+	if !correctTypecast {
+		panic("mm.Modules[icatypes.ModuleName] is not of type ica.AppModule")
+	}
+
+	icaModule.InitModule(ctx, controllerParams, hostParams)
 }
