611134b0dc0e0acea85e0901651754b19510eade
Federico Kunze Küllmer <31522760+fedekunze@users.noreply.github.com>
2023-01-20 11:25:13 +0100

    conflict

--- x/inflation/migrations/v3/migrate_test.go
+++ x/inflation/migrations/v3/migrate_test.go
@@ -1,12 +1,13 @@
 package v3_test
 
 import (
+	"testing"
+
 	"github.com/cosmos/cosmos-sdk/testutil"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	v3 "github.com/evmos/evmos/v11/x/inflation/migrations/v3"
 	"github.com/evmos/evmos/v11/x/inflation/types"
 	"github.com/stretchr/testify/require"
-	"testing"
 )
 
 func TestMigrate(t *testing.T) {
@@ -20,12 +21,5 @@ func TestMigrate(t *testing.T) {
 	require.Equal(t, epochMintProvision, []byte{0x01})
 
 	require.NoError(t, v3.MigrateStore(store))
-<<<<<<< HEAD
-
-	epochMintProvision = store.Get(v3.KeyPrefixEpochMintProvision)
-	require.Equal(t, len(epochMintProvision), 0)
-
-=======
 	require.False(t, store.Has(v3.KeyPrefixEpochMintProvision))
->>>>>>> 76dcaf8 (imp(inflation): remove dup store calls and improve perf (#1258))
 }
