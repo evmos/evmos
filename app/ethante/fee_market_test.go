15686cbc09922a2005609fa58d27e055c93a04b7
Freddy Caceres <facs95@gmail.com>
2023-01-24 18:42:47 -0500

    fix fee_market_test

--- app/ethante/fee_market_test.go
+++ app/ethante/fee_market_test.go
@@ -12,7 +12,7 @@ import (
 	evmtypes "github.com/evmos/evmos/v11/x/evm/types"
 )
 
-func (suite AnteTestSuite) TestGasWantedDecorator() {
+func (suite *AnteTestSuite) TestGasWantedDecorator() {
 	suite.enableFeemarket = true
 	suite.SetupTest()
 	dec := ethante.NewGasWantedDecorator(suite.app.EvmKeeper, suite.app.FeeMarketKeeper)
