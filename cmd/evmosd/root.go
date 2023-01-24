5ab97b18aa9c2dc638aa62311c09615b304fd799
Tomas Guerra <54514587+GAtom22@users.noreply.github.com>
2023-01-24 06:05:49 -0300

    chore (deps): bump Ethermint to v0.21.0-rc2 (backport #1269) (#1270)
    * chore (deps): bump Ethermint to v0.21.0-rc2 (#1269)
    
    * bump Ethermint to v0.21.0-rc2
    
    * update ethermintserver syntax in root.go
    
    * lint
    
    * update based on review comments
    
    Co-authored-by: Federico Kunze Küllmer <31522760+fedekunze@users.noreply.github.com>
    Co-authored-by: tom <tomasguerraalda@hotmail.com>
    
    * update changelog entry
    
    * update PR number
    
    * fix md link
    
    Co-authored-by: MalteHerrmann <42640438+MalteHerrmann@users.noreply.github.com>
    Co-authored-by: Federico Kunze Küllmer <31522760+fedekunze@users.noreply.github.com>

--- cmd/evmosd/root.go
+++ cmd/evmosd/root.go
@@ -137,7 +137,12 @@ func NewRootCmd() (*cobra.Command, params.EncodingConfig) {
 		pruning.PruningCmd(a.newApp),
 	)
 
-	ethermintserver.AddCommands(rootCmd, app.DefaultNodeHome, a.newApp, a.appExport, addModuleInitFlags)
+	ethermintserver.AddCommands(
+		rootCmd,
+		ethermintserver.NewDefaultStartOptions(a.newApp, app.DefaultNodeHome),
+		a.appExport,
+		addModuleInitFlags,
+	)
 
 	// add keybase, auxiliary RPC, query, and tx child commands
 	rootCmd.AddCommand(
