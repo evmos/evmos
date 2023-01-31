71cba9c1edeb0c49f760117e79d081a09ca44390
MalteHerrmann <malte@evmos.org>
2023-01-31 15:22:55 +0100

    add evm types that are not yet on main

---
+++ x/evm/types/chain_config_test.go
@@ -0,0 +1,381 @@
+package types
+
+import (
+	"testing"
+
+	sdkmath "cosmossdk.io/math"
+	"github.com/stretchr/testify/require"
+
+	"github.com/ethereum/go-ethereum/common"
+)
+
+var defaultEIP150Hash = common.Hash{}.String()
+
+func newIntPtr(i int64) *sdkmath.Int {
+	v := sdkmath.NewInt(i)
+	return &v
+}
+
+func TestChainConfigValidate(t *testing.T) {
+	testCases := []struct {
+		name     string
+		config   ChainConfig
+		expError bool
+	}{
+		{"default", DefaultChainConfig(), false},
+		{
+			"valid",
+			ChainConfig{
+				HomesteadBlock:      newIntPtr(0),
+				DAOForkBlock:        newIntPtr(0),
+				EIP150Block:         newIntPtr(0),
+				EIP150Hash:          defaultEIP150Hash,
+				EIP155Block:         newIntPtr(0),
+				EIP158Block:         newIntPtr(0),
+				ByzantiumBlock:      newIntPtr(0),
+				ConstantinopleBlock: newIntPtr(0),
+				PetersburgBlock:     newIntPtr(0),
+				IstanbulBlock:       newIntPtr(0),
+				MuirGlacierBlock:    newIntPtr(0),
+				BerlinBlock:         newIntPtr(0),
+				LondonBlock:         newIntPtr(0),
+				CancunBlock:         newIntPtr(0),
+				ShanghaiBlock:       newIntPtr(0),
+			},
+			false,
+		},
+		{
+			"valid with nil values",
+			ChainConfig{
+				HomesteadBlock:      nil,
+				DAOForkBlock:        nil,
+				EIP150Block:         nil,
+				EIP150Hash:          defaultEIP150Hash,
+				EIP155Block:         nil,
+				EIP158Block:         nil,
+				ByzantiumBlock:      nil,
+				ConstantinopleBlock: nil,
+				PetersburgBlock:     nil,
+				IstanbulBlock:       nil,
+				MuirGlacierBlock:    nil,
+				BerlinBlock:         nil,
+				LondonBlock:         nil,
+				CancunBlock:         nil,
+				ShanghaiBlock:       nil,
+			},
+			false,
+		},
+		{
+			"empty",
+			ChainConfig{},
+			false,
+		},
+		{
+			"invalid HomesteadBlock",
+			ChainConfig{
+				HomesteadBlock: newIntPtr(-1),
+			},
+			true,
+		},
+		{
+			"invalid DAOForkBlock",
+			ChainConfig{
+				HomesteadBlock: newIntPtr(0),
+				DAOForkBlock:   newIntPtr(-1),
+			},
+			true,
+		},
+		{
+			"invalid EIP150Block",
+			ChainConfig{
+				HomesteadBlock: newIntPtr(0),
+				DAOForkBlock:   newIntPtr(0),
+				EIP150Block:    newIntPtr(-1),
+			},
+			true,
+		},
+		{
+			"invalid EIP150Hash",
+			ChainConfig{
+				HomesteadBlock: newIntPtr(0),
+				DAOForkBlock:   newIntPtr(0),
+				EIP150Block:    newIntPtr(0),
+				EIP150Hash:     "  ",
+			},
+			true,
+		},
+		{
+			"invalid EIP155Block",
+			ChainConfig{
+				HomesteadBlock: newIntPtr(0),
+				DAOForkBlock:   newIntPtr(0),
+				EIP150Block:    newIntPtr(0),
+				EIP150Hash:     defaultEIP150Hash,
+				EIP155Block:    newIntPtr(-1),
+			},
+			true,
+		},
+		{
+			"invalid EIP158Block",
+			ChainConfig{
+				HomesteadBlock: newIntPtr(0),
+				DAOForkBlock:   newIntPtr(0),
+				EIP150Block:    newIntPtr(0),
+				EIP150Hash:     defaultEIP150Hash,
+				EIP155Block:    newIntPtr(0),
+				EIP158Block:    newIntPtr(-1),
+			},
+			true,
+		},
+		{
+			"invalid ByzantiumBlock",
+			ChainConfig{
+				HomesteadBlock: newIntPtr(0),
+				DAOForkBlock:   newIntPtr(0),
+				EIP150Block:    newIntPtr(0),
+				EIP150Hash:     defaultEIP150Hash,
+				EIP155Block:    newIntPtr(0),
+				EIP158Block:    newIntPtr(0),
+				ByzantiumBlock: newIntPtr(-1),
+			},
+			true,
+		},
+		{
+			"invalid ConstantinopleBlock",
+			ChainConfig{
+				HomesteadBlock:      newIntPtr(0),
+				DAOForkBlock:        newIntPtr(0),
+				EIP150Block:         newIntPtr(0),
+				EIP150Hash:          defaultEIP150Hash,
+				EIP155Block:         newIntPtr(0),
+				EIP158Block:         newIntPtr(0),
+				ByzantiumBlock:      newIntPtr(0),
+				ConstantinopleBlock: newIntPtr(-1),
+			},
+			true,
+		},
+		{
+			"invalid PetersburgBlock",
+			ChainConfig{
+				HomesteadBlock:      newIntPtr(0),
+				DAOForkBlock:        newIntPtr(0),
+				EIP150Block:         newIntPtr(0),
+				EIP150Hash:          defaultEIP150Hash,
+				EIP155Block:         newIntPtr(0),
+				EIP158Block:         newIntPtr(0),
+				ByzantiumBlock:      newIntPtr(0),
+				ConstantinopleBlock: newIntPtr(0),
+				PetersburgBlock:     newIntPtr(-1),
+			},
+			true,
+		},
+		{
+			"invalid IstanbulBlock",
+			ChainConfig{
+				HomesteadBlock:      newIntPtr(0),
+				DAOForkBlock:        newIntPtr(0),
+				EIP150Block:         newIntPtr(0),
+				EIP150Hash:          defaultEIP150Hash,
+				EIP155Block:         newIntPtr(0),
+				EIP158Block:         newIntPtr(0),
+				ByzantiumBlock:      newIntPtr(0),
+				ConstantinopleBlock: newIntPtr(0),
+				PetersburgBlock:     newIntPtr(0),
+				IstanbulBlock:       newIntPtr(-1),
+			},
+			true,
+		},
+		{
+			"invalid MuirGlacierBlock",
+			ChainConfig{
+				HomesteadBlock:      newIntPtr(0),
+				DAOForkBlock:        newIntPtr(0),
+				EIP150Block:         newIntPtr(0),
+				EIP150Hash:          defaultEIP150Hash,
+				EIP155Block:         newIntPtr(0),
+				EIP158Block:         newIntPtr(0),
+				ByzantiumBlock:      newIntPtr(0),
+				ConstantinopleBlock: newIntPtr(0),
+				PetersburgBlock:     newIntPtr(0),
+				IstanbulBlock:       newIntPtr(0),
+				MuirGlacierBlock:    newIntPtr(-1),
+			},
+			true,
+		},
+		{
+			"invalid BerlinBlock",
+			ChainConfig{
+				HomesteadBlock:      newIntPtr(0),
+				DAOForkBlock:        newIntPtr(0),
+				EIP150Block:         newIntPtr(0),
+				EIP150Hash:          defaultEIP150Hash,
+				EIP155Block:         newIntPtr(0),
+				EIP158Block:         newIntPtr(0),
+				ByzantiumBlock:      newIntPtr(0),
+				ConstantinopleBlock: newIntPtr(0),
+				PetersburgBlock:     newIntPtr(0),
+				IstanbulBlock:       newIntPtr(0),
+				MuirGlacierBlock:    newIntPtr(0),
+				BerlinBlock:         newIntPtr(-1),
+			},
+			true,
+		},
+		{
+			"invalid LondonBlock",
+			ChainConfig{
+				HomesteadBlock:      newIntPtr(0),
+				DAOForkBlock:        newIntPtr(0),
+				EIP150Block:         newIntPtr(0),
+				EIP150Hash:          defaultEIP150Hash,
+				EIP155Block:         newIntPtr(0),
+				EIP158Block:         newIntPtr(0),
+				ByzantiumBlock:      newIntPtr(0),
+				ConstantinopleBlock: newIntPtr(0),
+				PetersburgBlock:     newIntPtr(0),
+				IstanbulBlock:       newIntPtr(0),
+				MuirGlacierBlock:    newIntPtr(0),
+				BerlinBlock:         newIntPtr(0),
+				LondonBlock:         newIntPtr(-1),
+			},
+			true,
+		},
+		{
+			"invalid ArrowGlacierBlock",
+			ChainConfig{
+				HomesteadBlock:      newIntPtr(0),
+				DAOForkBlock:        newIntPtr(0),
+				EIP150Block:         newIntPtr(0),
+				EIP150Hash:          defaultEIP150Hash,
+				EIP155Block:         newIntPtr(0),
+				EIP158Block:         newIntPtr(0),
+				ByzantiumBlock:      newIntPtr(0),
+				ConstantinopleBlock: newIntPtr(0),
+				PetersburgBlock:     newIntPtr(0),
+				IstanbulBlock:       newIntPtr(0),
+				MuirGlacierBlock:    newIntPtr(0),
+				BerlinBlock:         newIntPtr(0),
+				LondonBlock:         newIntPtr(0),
+				ArrowGlacierBlock:   newIntPtr(-1),
+			},
+			true,
+		},
+		{
+			"invalid GrayGlacierBlock",
+			ChainConfig{
+				HomesteadBlock:      newIntPtr(0),
+				DAOForkBlock:        newIntPtr(0),
+				EIP150Block:         newIntPtr(0),
+				EIP150Hash:          defaultEIP150Hash,
+				EIP155Block:         newIntPtr(0),
+				EIP158Block:         newIntPtr(0),
+				ByzantiumBlock:      newIntPtr(0),
+				ConstantinopleBlock: newIntPtr(0),
+				PetersburgBlock:     newIntPtr(0),
+				IstanbulBlock:       newIntPtr(0),
+				MuirGlacierBlock:    newIntPtr(0),
+				BerlinBlock:         newIntPtr(0),
+				LondonBlock:         newIntPtr(0),
+				ArrowGlacierBlock:   newIntPtr(0),
+				GrayGlacierBlock:    newIntPtr(-1),
+			},
+			true,
+		},
+		{
+			"invalid MergeNetsplitBlock",
+			ChainConfig{
+				HomesteadBlock:      newIntPtr(0),
+				DAOForkBlock:        newIntPtr(0),
+				EIP150Block:         newIntPtr(0),
+				EIP150Hash:          defaultEIP150Hash,
+				EIP155Block:         newIntPtr(0),
+				EIP158Block:         newIntPtr(0),
+				ByzantiumBlock:      newIntPtr(0),
+				ConstantinopleBlock: newIntPtr(0),
+				PetersburgBlock:     newIntPtr(0),
+				IstanbulBlock:       newIntPtr(0),
+				MuirGlacierBlock:    newIntPtr(0),
+				BerlinBlock:         newIntPtr(0),
+				LondonBlock:         newIntPtr(0),
+				ArrowGlacierBlock:   newIntPtr(0),
+				GrayGlacierBlock:    newIntPtr(0),
+				MergeNetsplitBlock:  newIntPtr(-1),
+			},
+			true,
+		},
+		{
+			"invalid fork order - skip HomesteadBlock",
+			ChainConfig{
+				DAOForkBlock:        newIntPtr(0),
+				EIP150Block:         newIntPtr(0),
+				EIP150Hash:          defaultEIP150Hash,
+				EIP155Block:         newIntPtr(0),
+				EIP158Block:         newIntPtr(0),
+				ByzantiumBlock:      newIntPtr(0),
+				ConstantinopleBlock: newIntPtr(0),
+				PetersburgBlock:     newIntPtr(0),
+				IstanbulBlock:       newIntPtr(0),
+				MuirGlacierBlock:    newIntPtr(0),
+				BerlinBlock:         newIntPtr(0),
+				LondonBlock:         newIntPtr(0),
+			},
+			true,
+		},
+		{
+			"invalid ShanghaiBlock",
+			ChainConfig{
+				HomesteadBlock:      newIntPtr(0),
+				DAOForkBlock:        newIntPtr(0),
+				EIP150Block:         newIntPtr(0),
+				EIP150Hash:          defaultEIP150Hash,
+				EIP155Block:         newIntPtr(0),
+				EIP158Block:         newIntPtr(0),
+				ByzantiumBlock:      newIntPtr(0),
+				ConstantinopleBlock: newIntPtr(0),
+				PetersburgBlock:     newIntPtr(0),
+				IstanbulBlock:       newIntPtr(0),
+				MuirGlacierBlock:    newIntPtr(0),
+				BerlinBlock:         newIntPtr(0),
+				LondonBlock:         newIntPtr(0),
+				ArrowGlacierBlock:   newIntPtr(0),
+				GrayGlacierBlock:    newIntPtr(0),
+				MergeNetsplitBlock:  newIntPtr(0),
+				ShanghaiBlock:       newIntPtr(-1),
+			},
+			true,
+		},
+		{
+			"invalid CancunBlock",
+			ChainConfig{
+				HomesteadBlock:      newIntPtr(0),
+				DAOForkBlock:        newIntPtr(0),
+				EIP150Block:         newIntPtr(0),
+				EIP150Hash:          defaultEIP150Hash,
+				EIP155Block:         newIntPtr(0),
+				EIP158Block:         newIntPtr(0),
+				ByzantiumBlock:      newIntPtr(0),
+				ConstantinopleBlock: newIntPtr(0),
+				PetersburgBlock:     newIntPtr(0),
+				IstanbulBlock:       newIntPtr(0),
+				MuirGlacierBlock:    newIntPtr(0),
+				BerlinBlock:         newIntPtr(0),
+				LondonBlock:         newIntPtr(0),
+				ArrowGlacierBlock:   newIntPtr(0),
+				GrayGlacierBlock:    newIntPtr(0),
+				MergeNetsplitBlock:  newIntPtr(0),
+				ShanghaiBlock:       newIntPtr(0),
+				CancunBlock:         newIntPtr(-1),
+			},
+			true,
+		},
+	}
+
+	for _, tc := range testCases {
+		err := tc.config.Validate()
+
+		if tc.expError {
+			require.Error(t, err, tc.name)
+		} else {
+			require.NoError(t, err, tc.name)
+		}
+	}
+}
---
+++ x/evm/types/tx_data.go
@@ -0,0 +1,138 @@
+// Copyright 2021 Evmos Foundation
+// This file is part of Evmos' Evmos packages.
+//
+// The Evmos packages is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// The Evmos packages is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with the Evmos packages. If not, see https://github.com/evmos/ethermint/blob/main/LICENSE
+package types
+
+import (
+	"math/big"
+
+	"github.com/ethereum/go-ethereum/common"
+	ethtypes "github.com/ethereum/go-ethereum/core/types"
+)
+
+var (
+	_ TxData = &LegacyTx{}
+	_ TxData = &AccessListTx{}
+	_ TxData = &DynamicFeeTx{}
+)
+
+// TxData implements the Ethereum transaction tx structure. It is used
+// solely as intended in Ethereum abiding by the protocol.
+type TxData interface {
+	// TODO: embed ethtypes.TxData. See https://github.com/ethereum/go-ethereum/issues/23154
+
+	TxType() byte
+	Copy() TxData
+	GetChainID() *big.Int
+	GetAccessList() ethtypes.AccessList
+	GetData() []byte
+	GetNonce() uint64
+	GetGas() uint64
+	GetGasPrice() *big.Int
+	GetGasTipCap() *big.Int
+	GetGasFeeCap() *big.Int
+	GetValue() *big.Int
+	GetTo() *common.Address
+
+	GetRawSignatureValues() (v, r, s *big.Int)
+	SetSignatureValues(chainID, v, r, s *big.Int)
+
+	AsEthereumData() ethtypes.TxData
+	Validate() error
+
+	// static fee
+	Fee() *big.Int
+	Cost() *big.Int
+
+	// effective gasPrice/fee/cost according to current base fee
+	EffectiveGasPrice(baseFee *big.Int) *big.Int
+	EffectiveFee(baseFee *big.Int) *big.Int
+	EffectiveCost(baseFee *big.Int) *big.Int
+}
+
+// NOTE: All non-protected transactions (i.e non EIP155 signed) will fail if the
+// AllowUnprotectedTxs parameter is disabled.
+func NewTxDataFromTx(tx *ethtypes.Transaction) (TxData, error) {
+	var txData TxData
+	var err error
+	switch tx.Type() {
+	case ethtypes.DynamicFeeTxType:
+		txData, err = newDynamicFeeTx(tx)
+	case ethtypes.AccessListTxType:
+		txData, err = newAccessListTx(tx)
+	default:
+		txData, err = newLegacyTx(tx)
+	}
+	if err != nil {
+		return nil, err
+	}
+
+	return txData, nil
+}
+
+// DeriveChainID derives the chain id from the given v parameter.
+//
+// CONTRACT: v value is either:
+//
+//   - {0,1} + CHAIN_ID * 2 + 35, if EIP155 is used
+//   - {0,1} + 27, otherwise
+//
+// Ref: https://github.com/ethereum/EIPs/blob/master/EIPS/eip-155.md
+func DeriveChainID(v *big.Int) *big.Int {
+	if v == nil || v.Sign() < 1 {
+		return nil
+	}
+
+	if v.BitLen() <= 64 {
+		v := v.Uint64()
+		if v == 27 || v == 28 {
+			return new(big.Int)
+		}
+
+		if v < 35 {
+			return nil
+		}
+
+		// V MUST be of the form {0,1} + CHAIN_ID * 2 + 35
+		return new(big.Int).SetUint64((v - 35) / 2)
+	}
+	v = new(big.Int).Sub(v, big.NewInt(35))
+	return v.Div(v, big.NewInt(2))
+}
+
+func rawSignatureValues(vBz, rBz, sBz []byte) (v, r, s *big.Int) {
+	if len(vBz) > 0 {
+		v = new(big.Int).SetBytes(vBz)
+	}
+	if len(rBz) > 0 {
+		r = new(big.Int).SetBytes(rBz)
+	}
+	if len(sBz) > 0 {
+		s = new(big.Int).SetBytes(sBz)
+	}
+	return v, r, s
+}
+
+func fee(gasPrice *big.Int, gas uint64) *big.Int {
+	gasLimit := new(big.Int).SetUint64(gas)
+	return new(big.Int).Mul(gasPrice, gasLimit)
+}
+
+func cost(fee, value *big.Int) *big.Int {
+	if value != nil {
+		return new(big.Int).Add(fee, value)
+	}
+	return fee
+}
