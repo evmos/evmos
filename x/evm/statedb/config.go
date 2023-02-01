93a098662d290c4199fdc87325289ddd063ba568
Freddy Caceres <facs95@gmail.com>
2023-02-01 11:53:57 -0500

    chore: Migrate `x/evm/statedb` and `x/evm/vm` folders from Ethermint (#1314)
    * add evm statedb
    
    * add evm folder
    
    * fix gosec issues
    
    * Update x/evm/statedb/config.go
    
    Co-authored-by: Tomas Guerra <54514587+GAtom22@users.noreply.github.com>
    
    * fix nil pointer issue
    
    * Update x/evm/statedb/config.go
    
    Co-authored-by: Tomas Guerra <54514587+GAtom22@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Tomas Guerra <54514587+GAtom22@users.noreply.github.com>

---
+++ x/evm/statedb/config.go
@@ -0,0 +1,62 @@
+// Copyright 2022 Evmos Foundation
+// This file is part of Evmos' Evmos packages.
+//
+// The Evmos packages is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// The Evmos packages is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with the Evmos packages. If not, see https://github.com/evmos/evmos/blob/main/LICENSE
+package statedb
+
+import (
+	"math/big"
+
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/params"
+	"github.com/evmos/evmos/v11/x/evm/types"
+)
+
+// TxConfig encapulates the readonly information of current tx for `StateDB`.
+type TxConfig struct {
+	BlockHash common.Hash // hash of current block
+	TxHash    common.Hash // hash of current tx
+	TxIndex   uint        // the index of current transaction
+	LogIndex  uint        // the index of next log within current block
+}
+
+// NewTxConfig returns a TxConfig
+func NewTxConfig(bhash, thash common.Hash, txIndex, logIndex uint) TxConfig {
+	return TxConfig{
+		BlockHash: bhash,
+		TxHash:    thash,
+		TxIndex:   txIndex,
+		LogIndex:  logIndex,
+	}
+}
+
+// NewEmptyTxConfig construct an empty TxConfig,
+// used in context where there's no transaction, e.g. `eth_call`/`eth_estimateGas`.
+func NewEmptyTxConfig(bhash common.Hash) TxConfig {
+	return TxConfig{
+		BlockHash: bhash,
+		TxHash:    common.Hash{},
+		TxIndex:   0,
+		LogIndex:  0,
+	}
+}
+
+// EVMConfig encapsulates common parameters needed to create an EVM to execute a message
+// It's mainly to reduce the number of method parameters
+type EVMConfig struct {
+	Params      types.Params
+	ChainConfig *params.ChainConfig
+	CoinBase    common.Address
+	BaseFee     *big.Int
+}
