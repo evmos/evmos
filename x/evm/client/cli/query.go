9b4ab8c092800c067e93675f672b100141474ea5
Freddy Caceres <facs95@gmail.com>
2023-02-01 12:53:51 -0500

    Ethermint migration of EVM and FeeMarket Cli  (#1316)
    * migrate evm cli
    
    * migrate feemarket cli

---
+++ x/evm/client/cli/query.go
@@ -0,0 +1,149 @@
+// Copyright 2021 Evmos Foundation
+// This file is part of Evmos' Evmos packages.
+//
+// The Evmos packages is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// The Evmos packages is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with the Evmos packages. If not, see https://github.com/evmos/ethermint/blob/main/LICENSE
+package cli
+
+import (
+	rpctypes "github.com/evmos/ethermint/rpc/types"
+	"github.com/spf13/cobra"
+
+	"github.com/cosmos/cosmos-sdk/client"
+	"github.com/cosmos/cosmos-sdk/client/flags"
+
+	"github.com/evmos/evmos/v11/x/evm/types"
+)
+
+// GetQueryCmd returns the parent command for all x/bank CLi query commands.
+func GetQueryCmd() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:                        types.ModuleName,
+		Short:                      "Querying commands for the evm module",
+		DisableFlagParsing:         true,
+		SuggestionsMinimumDistance: 2,
+		RunE:                       client.ValidateCmd,
+	}
+
+	cmd.AddCommand(
+		GetStorageCmd(),
+		GetCodeCmd(),
+		GetParamsCmd(),
+	)
+	return cmd
+}
+
+// GetStorageCmd queries a key in an accounts storage
+func GetStorageCmd() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "storage ADDRESS KEY",
+		Short: "Gets storage for an account with a given key and height",
+		Long:  "Gets storage for an account with a given key and height. If the height is not provided, it will use the latest height from context.", //nolint:lll
+		Args:  cobra.ExactArgs(2),
+		RunE: func(cmd *cobra.Command, args []string) error {
+			clientCtx, err := client.GetClientQueryContext(cmd)
+			if err != nil {
+				return err
+			}
+
+			queryClient := types.NewQueryClient(clientCtx)
+
+			address, err := accountToHex(args[0])
+			if err != nil {
+				return err
+			}
+
+			key := formatKeyToHash(args[1])
+
+			req := &types.QueryStorageRequest{
+				Address: address,
+				Key:     key,
+			}
+
+			res, err := queryClient.Storage(rpctypes.ContextWithHeight(clientCtx.Height), req)
+			if err != nil {
+				return err
+			}
+
+			return clientCtx.PrintProto(res)
+		},
+	}
+
+	flags.AddQueryFlagsToCmd(cmd)
+	return cmd
+}
+
+// GetCodeCmd queries the code field of a given address
+func GetCodeCmd() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "code ADDRESS",
+		Short: "Gets code from an account",
+		Long:  "Gets code from an account. If the height is not provided, it will use the latest height from context.",
+		Args:  cobra.ExactArgs(1),
+		RunE: func(cmd *cobra.Command, args []string) error {
+			clientCtx, err := client.GetClientQueryContext(cmd)
+			if err != nil {
+				return err
+			}
+
+			queryClient := types.NewQueryClient(clientCtx)
+
+			address, err := accountToHex(args[0])
+			if err != nil {
+				return err
+			}
+
+			req := &types.QueryCodeRequest{
+				Address: address,
+			}
+
+			res, err := queryClient.Code(rpctypes.ContextWithHeight(clientCtx.Height), req)
+			if err != nil {
+				return err
+			}
+
+			return clientCtx.PrintProto(res)
+		},
+	}
+
+	flags.AddQueryFlagsToCmd(cmd)
+	return cmd
+}
+
+// GetParamsCmd queries the fee market params
+func GetParamsCmd() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "params",
+		Short: "Get the evm params",
+		Long:  "Get the evm parameter values.",
+		Args:  cobra.NoArgs,
+		RunE: func(cmd *cobra.Command, _ []string) error {
+			clientCtx, err := client.GetClientQueryContext(cmd)
+			if err != nil {
+				return err
+			}
+
+			queryClient := types.NewQueryClient(clientCtx)
+
+			res, err := queryClient.Params(cmd.Context(), &types.QueryParamsRequest{})
+			if err != nil {
+				return err
+			}
+
+			return clientCtx.PrintProto(res)
+		},
+	}
+
+	flags.AddQueryFlagsToCmd(cmd)
+	return cmd
+}
---
+++ x/feemarket/client/cli/query.go
@@ -0,0 +1,131 @@
+// Copyright 2021 Evmos Foundation
+// This file is part of Evmos' Evmos packages.
+//
+// The Evmos packages is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// The Evmos packages is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with the Evmos packages. If not, see https://github.com/evmos/ethermint/blob/main/LICENSE
+package cli
+
+import (
+	"github.com/spf13/cobra"
+
+	"github.com/cosmos/cosmos-sdk/client"
+	"github.com/cosmos/cosmos-sdk/client/flags"
+
+	"github.com/evmos/evmos/v11/x/feemarket/types"
+)
+
+// GetQueryCmd returns the parent command for all x/feemarket CLI query commands.
+func GetQueryCmd() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:                        types.ModuleName,
+		Short:                      "Querying commands for the fee market module",
+		DisableFlagParsing:         true,
+		SuggestionsMinimumDistance: 2,
+		RunE:                       client.ValidateCmd,
+	}
+
+	cmd.AddCommand(
+		GetBlockGasCmd(),
+		GetBaseFeeCmd(),
+		GetParamsCmd(),
+	)
+	return cmd
+}
+
+// GetBlockGasCmd queries the gas used in a block
+func GetBlockGasCmd() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "block-gas",
+		Short: "Get the block gas used at a given block height",
+		Long: `Get the block gas used at a given block height.
+If the height is not provided, it will use the latest height from context`,
+		Args: cobra.NoArgs,
+		RunE: func(cmd *cobra.Command, args []string) error {
+			clientCtx, err := client.GetClientQueryContext(cmd)
+			if err != nil {
+				return err
+			}
+
+			queryClient := types.NewQueryClient(clientCtx)
+
+			ctx := cmd.Context()
+			res, err := queryClient.BlockGas(ctx, &types.QueryBlockGasRequest{})
+			if err != nil {
+				return err
+			}
+
+			return clientCtx.PrintProto(res)
+		},
+	}
+
+	flags.AddQueryFlagsToCmd(cmd)
+	return cmd
+}
+
+// GetParamsCmd queries the fee market params
+func GetParamsCmd() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "params",
+		Short: "Get the fee market params",
+		Long:  "Get the fee market parameter values.",
+		Args:  cobra.NoArgs,
+		RunE: func(cmd *cobra.Command, _ []string) error {
+			clientCtx, err := client.GetClientQueryContext(cmd)
+			if err != nil {
+				return err
+			}
+
+			queryClient := types.NewQueryClient(clientCtx)
+
+			res, err := queryClient.Params(cmd.Context(), &types.QueryParamsRequest{})
+			if err != nil {
+				return err
+			}
+
+			return clientCtx.PrintProto(res)
+		},
+	}
+
+	flags.AddQueryFlagsToCmd(cmd)
+	return cmd
+}
+
+// GetBaseFeeCmd queries the base fee at a given height
+func GetBaseFeeCmd() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "base-fee",
+		Short: "Get the base fee amount at a given block height",
+		Long: `Get the base fee amount at a given block height.
+If the height is not provided, it will use the latest height from context.`,
+		Args: cobra.NoArgs,
+		RunE: func(cmd *cobra.Command, args []string) error {
+			clientCtx, err := client.GetClientQueryContext(cmd)
+			if err != nil {
+				return err
+			}
+
+			queryClient := types.NewQueryClient(clientCtx)
+
+			ctx := cmd.Context()
+			res, err := queryClient.BaseFee(ctx, &types.QueryBaseFeeRequest{})
+			if err != nil {
+				return err
+			}
+
+			return clientCtx.PrintProto(res)
+		},
+	}
+
+	flags.AddQueryFlagsToCmd(cmd)
+	return cmd
+}
