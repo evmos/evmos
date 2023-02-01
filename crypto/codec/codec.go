20b606c03adaa7473c7d0d3498dc2d00267d8cfb
Tomas Guerra <54514587+GAtom22@users.noreply.github.com>
2023-02-01 19:21:01 -0300

    chore(migration) add crypto and encoding pkgs (#1317)
    * chore(migration) add cripto and encoding pkgs
    
    * chore(migration) fix licenses
    
    * chore(migration) fix tests
    
    * Apply suggestions from code review
    
    Co-authored-by: MalteHerrmann <42640438+MalteHerrmann@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: MalteHerrmann <42640438+MalteHerrmann@users.noreply.github.com>

---
+++ crypto/codec/codec.go
@@ -0,0 +1,29 @@
+// Copyright 2022 Evmos Foundation
+// This file is part of the Evmos Network packages.
+//
+// Evmos is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// The Evmos packages are distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with the Evmos packages. If not, see https://github.com/evmos/evmos/blob/main/LICENSE
+package codec
+
+import (
+	codectypes "github.com/cosmos/cosmos-sdk/codec/types"
+	cryptotypes "github.com/cosmos/cosmos-sdk/crypto/types"
+
+	"github.com/evmos/evmos/v11/crypto/ethsecp256k1"
+)
+
+// RegisterInterfaces register the Evmos key concrete types.
+func RegisterInterfaces(registry codectypes.InterfaceRegistry) {
+	registry.RegisterImplementations((*cryptotypes.PubKey)(nil), &ethsecp256k1.PubKey{})
+	registry.RegisterImplementations((*cryptotypes.PrivKey)(nil), &ethsecp256k1.PrivKey{})
+}
---
+++ encoding/config_test.go
@@ -0,0 +1,39 @@
+package encoding_test
+
+import (
+	"math/big"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+
+	ethtypes "github.com/ethereum/go-ethereum/core/types"
+
+	"github.com/evmos/ethermint/tests"
+	"github.com/evmos/evmos/v11/app"
+	"github.com/evmos/evmos/v11/encoding"
+	evmtypes "github.com/evmos/evmos/v11/x/evm/types"
+)
+
+func TestTxEncoding(t *testing.T) {
+	addr, key := tests.NewAddrKey()
+	signer := tests.NewSigner(key)
+
+	msg := evmtypes.NewTxContract(big.NewInt(1), 1, big.NewInt(10), 100000, nil, big.NewInt(1), big.NewInt(1), []byte{}, nil)
+	msg.From = addr.Hex()
+
+	ethSigner := ethtypes.LatestSignerForChainID(big.NewInt(1))
+	err := msg.Sign(ethSigner, signer)
+	require.NoError(t, err)
+
+	cfg := encoding.MakeConfig(app.ModuleBasics)
+
+	_, err = cfg.TxConfig.TxEncoder()(msg)
+	require.Error(t, err, "encoding failed")
+
+	// FIXME: transaction hashing is hardcoded on Tendermint:
+	// See https://github.com/tendermint/tendermint/issues/6539 for reference
+	// txHash := msg.AsTransaction().Hash()
+	// tmTx := tmtypes.Tx(bz)
+
+	// require.Equal(t, txHash.Bytes(), tmTx.Hash())
+}
