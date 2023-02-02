aa419056a4143c8210254251f07b3f975b5cae96
MalteHerrmann <42640438+MalteHerrmann@users.noreply.github.com>
2023-02-02 11:05:13 +0100

    chore (proto): fix swagger generation and update swagger.yaml (#1318)
    * remove unused plugins from buf.gen.swagger.yaml
    
    * update protoc-swagger-gen script
    
    * update makefile commands
    
    * add gov v1 queries to swagger config
    
    * update swagger.yaml
    
    * remove unused variable
    
    ---------
    
    Co-authored-by: Federico Kunze Küllmer <31522760+fedekunze@users.noreply.github.com>

--- scripts/protoc-swagger-gen.sh
+++ scripts/protoc-swagger-gen.sh
@@ -1,15 +1,26 @@
 #!/usr/bin/env bash
+SWAGGER_DIR=./swagger-proto
 
 set -eo pipefail
 
-# need to install statik on the docker image
-go install github.com/rakyll/statik
+# prepare swagger generation
+mkdir -p "$SWAGGER_DIR/proto"
+printf "version: v1\ndirectories:\n  - proto\n  - third_party" > "$SWAGGER_DIR/buf.work.yaml"
+printf "version: v1\nname: buf.build/evmos/evmos\n" > "$SWAGGER_DIR/proto/buf.yaml"
+cp ./proto/buf.gen.swagger.yaml "$SWAGGER_DIR/proto/buf.gen.swagger.yaml"
 
-# create temporary folder to store intermediate results from `buf build` + `buf generate`
+# copy existing proto files
+cp -r ./proto/evmos "$SWAGGER_DIR/proto"
+cp -r ./proto/ethermint "$SWAGGER_DIR/proto"
+
+# create temporary folder to store intermediate results from `buf generate`
 mkdir -p ./tmp-swagger-gen
 
+# step into swagger folder
+cd "$SWAGGER_DIR"
+
 # create swagger files on an individual basis  w/ `buf build` and `buf generate` (needed for `swagger-combine`)
-proto_dirs=$(find ./proto -path -prune -o -name '*.proto' -print0 | xargs -0 -n1 dirname | sort | uniq)
+proto_dirs=$(find ./proto ./third_party -path -prune -o -name '*.proto' -print0 | xargs -0 -n1 dirname | sort | uniq)
 for dir in $proto_dirs; do
   # generate swagger files (filter query files)
   query_file=$(find "${dir}" -maxdepth 1 \( -name 'query.proto' -o -name 'service.proto' \))
@@ -18,6 +29,8 @@ for dir in $proto_dirs; do
   fi
 done
 
+cd ..
+
 # combine swagger files
 # uses nodejs package `swagger-combine`.
 # all the individual swagger files need to be configured in `config.json` for merging
@@ -25,6 +38,4 @@ swagger-combine ./client/docs/config.json -o ./client/docs/swagger-ui/swagger.ya
 
 # clean swagger files
 rm -rf ./tmp-swagger-gen
-
-# generate binary for static server (use -f flag to replace current binary)
-statik -f -src=./client/docs/swagger-ui -dest=./client/docs
+rm -rf "$SWAGGER_DIR"
