393f4684012f077f0d820fcb6992fecf5632b532
Freddy Caceres <facs95@gmail.com>
2023-02-03 08:02:45 -0500

    migrate cosmos specific tests to cosmos package

---
+++ app/ante/cosmos/utils_test.go
@@ -0,0 +1,139 @@
+package cosmos_test
+
+import (
+	"math"
+	"testing"
+	"time"
+
+	sdkmath "cosmossdk.io/math"
+	"github.com/stretchr/testify/suite"
+
+	"github.com/ethereum/go-ethereum/common"
+	ethtypes "github.com/ethereum/go-ethereum/core/types"
+	"github.com/evmos/ethermint/ethereum/eip712"
+
+	"github.com/cosmos/cosmos-sdk/client"
+	"github.com/cosmos/cosmos-sdk/simapp"
+	"github.com/cosmos/cosmos-sdk/testutil/testdata"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	authtypes "github.com/cosmos/cosmos-sdk/x/auth/types"
+	"github.com/evmos/ethermint/app"
+	ante "github.com/evmos/ethermint/app/ante"
+	"github.com/evmos/ethermint/encoding"
+	"github.com/evmos/ethermint/x/evm/statedb"
+	evmtypes "github.com/evmos/ethermint/x/evm/types"
+	feemarkettypes "github.com/evmos/ethermint/x/feemarket/types"
+
+	tmproto "github.com/tendermint/tendermint/proto/tendermint/types"
+)
+
+type AnteTestSuite struct {
+	suite.Suite
+
+	ctx             sdk.Context
+	app             *app.EthermintApp
+	clientCtx       client.Context
+	anteHandler     sdk.AnteHandler
+	ethSigner       ethtypes.Signer
+	enableFeemarket bool
+	enableLondonHF  bool
+	evmParamsOption func(*evmtypes.Params)
+}
+
+const TestGasLimit uint64 = 100000
+
+func (suite *AnteTestSuite) StateDB() *statedb.StateDB {
+	return statedb.New(suite.ctx, suite.app.EvmKeeper, statedb.NewEmptyTxConfig(common.BytesToHash(suite.ctx.HeaderHash().Bytes())))
+}
+
+func (suite *AnteTestSuite) SetupTest() {
+	checkTx := false
+
+	suite.app = app.Setup(checkTx, func(app *app.EthermintApp, genesis simapp.GenesisState) simapp.GenesisState {
+		if suite.enableFeemarket {
+			// setup feemarketGenesis params
+			feemarketGenesis := feemarkettypes.DefaultGenesisState()
+			feemarketGenesis.Params.EnableHeight = 1
+			feemarketGenesis.Params.NoBaseFee = false
+			// Verify feeMarket genesis
+			err := feemarketGenesis.Validate()
+			suite.Require().NoError(err)
+			genesis[feemarkettypes.ModuleName] = app.AppCodec().MustMarshalJSON(feemarketGenesis)
+		}
+		evmGenesis := evmtypes.DefaultGenesisState()
+		evmGenesis.Params.AllowUnprotectedTxs = false
+		if !suite.enableLondonHF {
+			maxInt := sdkmath.NewInt(math.MaxInt64)
+			evmGenesis.Params.ChainConfig.LondonBlock = &maxInt
+			evmGenesis.Params.ChainConfig.ArrowGlacierBlock = &maxInt
+			evmGenesis.Params.ChainConfig.GrayGlacierBlock = &maxInt
+			evmGenesis.Params.ChainConfig.MergeNetsplitBlock = &maxInt
+			evmGenesis.Params.ChainConfig.ShanghaiBlock = &maxInt
+			evmGenesis.Params.ChainConfig.CancunBlock = &maxInt
+		}
+		if suite.evmParamsOption != nil {
+			suite.evmParamsOption(&evmGenesis.Params)
+		}
+		genesis[evmtypes.ModuleName] = app.AppCodec().MustMarshalJSON(evmGenesis)
+		return genesis
+	})
+
+	suite.ctx = suite.app.BaseApp.NewContext(checkTx, tmproto.Header{Height: 2, ChainID: "ethermint_9000-1", Time: time.Now().UTC()})
+	suite.ctx = suite.ctx.WithMinGasPrices(sdk.NewDecCoins(sdk.NewDecCoin(evmtypes.DefaultEVMDenom, sdk.OneInt())))
+	suite.ctx = suite.ctx.WithBlockGasMeter(sdk.NewGasMeter(1000000000000000000))
+	suite.app.EvmKeeper.WithChainID(suite.ctx)
+
+	infCtx := suite.ctx.WithGasMeter(sdk.NewInfiniteGasMeter())
+	suite.app.AccountKeeper.SetParams(infCtx, authtypes.DefaultParams())
+
+	encodingConfig := encoding.MakeConfig(app.ModuleBasics)
+	// We're using TestMsg amino encoding in some tests, so register it here.
+	encodingConfig.Amino.RegisterConcrete(&testdata.TestMsg{}, "testdata.TestMsg", nil)
+	eip712.SetEncodingConfig(encodingConfig)
+
+	suite.clientCtx = client.Context{}.WithTxConfig(encodingConfig.TxConfig)
+
+	anteHandler, err := ante.NewAnteHandler(ante.HandlerOptions{
+		AccountKeeper:   suite.app.AccountKeeper,
+		BankKeeper:      suite.app.BankKeeper,
+		EvmKeeper:       suite.app.EvmKeeper,
+		FeegrantKeeper:  suite.app.FeeGrantKeeper,
+		IBCKeeper:       suite.app.IBCKeeper,
+		FeeMarketKeeper: suite.app.FeeMarketKeeper,
+		SignModeHandler: encodingConfig.TxConfig.SignModeHandler(),
+		SigGasConsumer:  ante.DefaultSigVerificationGasConsumer,
+	})
+	suite.Require().NoError(err)
+
+	suite.anteHandler = anteHandler
+	suite.ethSigner = ethtypes.LatestSignerForChainID(suite.app.EvmKeeper.ChainID())
+}
+
+func TestAnteTestSuite(t *testing.T) {
+	suite.Run(t, &AnteTestSuite{
+		enableLondonHF: true,
+	})
+}
+
+func (suite *AnteTestSuite) CreateTestCosmosTxBuilder(gasPrice sdkmath.Int, denom string, msgs ...sdk.Msg) client.TxBuilder {
+	txBuilder := suite.clientCtx.TxConfig.NewTxBuilder()
+
+	txBuilder.SetGasLimit(TestGasLimit)
+	fees := &sdk.Coins{{Denom: denom, Amount: gasPrice.MulRaw(int64(TestGasLimit))}}
+	txBuilder.SetFeeAmount(*fees)
+	err := txBuilder.SetMsgs(msgs...)
+	suite.Require().NoError(err)
+	return txBuilder
+}
+
+func NextFn(ctx sdk.Context, _ sdk.Tx, _ bool) (sdk.Context, error) {
+	return ctx, nil
+}
+
+var _ sdk.Tx = &invalidTx{}
+
+type invalidTx struct{}
+
+func (invalidTx) GetMsgs() []sdk.Msg { return []sdk.Msg{nil} }
+
+func (invalidTx) ValidateBasic() error { return nil }
--- app/ante/evm/fees_test.go
+++ app/ante/evm/fees_test.go
@@ -1,9 +1,8 @@
 package evm_test
 
 import (
+	evmante "github.com/evmos/evmos/v11/app/ante/evm"
 	"math/big"
-
-	"github.com/evmos/evmos/v11/app/ethante"
 
 	sdkmath "cosmossdk.io/math"
 	sdk "github.com/cosmos/cosmos-sdk/types"
@@ -20,126 +19,6 @@ var execTypes = []struct {
 }{
 	{"deliverTx", false, false},
 	{"deliverTxSimulate", false, true},
-}
-
-func (suite *AnteTestSuite) TestMinGasPriceDecorator() {
-	denom := evmtypes.DefaultEVMDenom
-	testMsg := banktypes.MsgSend{
-		FromAddress: "evmos1x8fhpj9nmhqk8z9kpgjt95ck2xwyue0ptzkucp",
-		ToAddress:   "evmos1dx67l23hz9l0k9hcher8xz04uj7wf3yu26l2yn",
-		Amount:      sdk.Coins{sdk.Coin{Amount: sdkmath.NewInt(10), Denom: denom}},
-	}
-
-	testCases := []struct {
-		name                string
-		malleate            func() sdk.Tx
-		expPass             bool
-		errMsg              string
-		allowPassOnSimulate bool
-	}{
-		{
-			"invalid cosmos tx type",
-			func() sdk.Tx {
-				return &invalidTx{}
-			},
-			false,
-			"invalid transaction type",
-			false,
-		},
-		{
-			"valid cosmos tx with MinGasPrices = 0, gasPrice = 0",
-			func() sdk.Tx {
-				params := suite.app.FeeMarketKeeper.GetParams(suite.ctx)
-				params.MinGasPrice = sdk.ZeroDec()
-				err := suite.app.FeeMarketKeeper.SetParams(suite.ctx, params)
-				suite.Require().NoError(err)
-
-				txBuilder := suite.CreateTestCosmosTxBuilder(sdkmath.NewInt(0), denom, &testMsg)
-				return txBuilder.GetTx()
-			},
-			true,
-			"",
-			false,
-		},
-		{
-			"valid cosmos tx with MinGasPrices = 0, gasPrice > 0",
-			func() sdk.Tx {
-				params := suite.app.FeeMarketKeeper.GetParams(suite.ctx)
-				params.MinGasPrice = sdk.ZeroDec()
-				err := suite.app.FeeMarketKeeper.SetParams(suite.ctx, params)
-				suite.Require().NoError(err)
-
-				txBuilder := suite.CreateTestCosmosTxBuilder(sdkmath.NewInt(10), denom, &testMsg)
-				return txBuilder.GetTx()
-			},
-			true,
-			"",
-			false,
-		},
-		{
-			"valid cosmos tx with MinGasPrices = 10, gasPrice = 10",
-			func() sdk.Tx {
-				params := suite.app.FeeMarketKeeper.GetParams(suite.ctx)
-				params.MinGasPrice = sdk.NewDec(10)
-				err := suite.app.FeeMarketKeeper.SetParams(suite.ctx, params)
-				suite.Require().NoError(err)
-
-				txBuilder := suite.CreateTestCosmosTxBuilder(sdkmath.NewInt(10), denom, &testMsg)
-				return txBuilder.GetTx()
-			},
-			true,
-			"",
-			false,
-		},
-		{
-			"invalid cosmos tx with MinGasPrices = 10, gasPrice = 0",
-			func() sdk.Tx {
-				params := suite.app.FeeMarketKeeper.GetParams(suite.ctx)
-				params.MinGasPrice = sdk.NewDec(10)
-				err := suite.app.FeeMarketKeeper.SetParams(suite.ctx, params)
-				suite.Require().NoError(err)
-
-				txBuilder := suite.CreateTestCosmosTxBuilder(sdkmath.NewInt(0), denom, &testMsg)
-				return txBuilder.GetTx()
-			},
-			false,
-			"provided fee < minimum global fee",
-			true,
-		},
-		{
-			"invalid cosmos tx with wrong denom",
-			func() sdk.Tx {
-				params := suite.app.FeeMarketKeeper.GetParams(suite.ctx)
-				params.MinGasPrice = sdk.NewDec(10)
-				err := suite.app.FeeMarketKeeper.SetParams(suite.ctx, params)
-				suite.Require().NoError(err)
-
-				txBuilder := suite.CreateTestCosmosTxBuilder(sdkmath.NewInt(10), "stake", &testMsg)
-				return txBuilder.GetTx()
-			},
-			false,
-			"provided fee < minimum global fee",
-			true,
-		},
-	}
-
-	for _, et := range execTypes {
-		for _, tc := range testCases {
-			suite.Run(et.name+"_"+tc.name, func() {
-				// s.SetupTest(et.isCheckTx)
-				ctx := suite.ctx.WithIsReCheckTx(et.isCheckTx)
-				dec := ethante.NewMinGasPriceDecorator(suite.app.FeeMarketKeeper, suite.app.EvmKeeper)
-				_, err := dec.AnteHandle(ctx, tc.malleate(), et.simulate, NextFn)
-
-				if tc.expPass || (et.simulate && tc.allowPassOnSimulate) {
-					suite.Require().NoError(err, tc.name)
-				} else {
-					suite.Require().Error(err, tc.name)
-					suite.Require().Contains(err.Error(), tc.errMsg, tc.name)
-				}
-			})
-		}
-	}
 }
 
 func (suite *AnteTestSuite) TestEthMinGasPriceDecorator() {
@@ -354,7 +233,7 @@ func (suite *AnteTestSuite) TestEthMinGasPriceDecorator() {
 			suite.Run(et.name+"_"+tc.name, func() {
 				// s.SetupTest(et.isCheckTx)
 				suite.SetupTest()
-				dec := ethante.NewEthMinGasPriceDecorator(suite.app.FeeMarketKeeper, suite.app.EvmKeeper)
+				dec := evmante.NewEthMinGasPriceDecorator(suite.app.FeeMarketKeeper, suite.app.EvmKeeper)
 				_, err := dec.AnteHandle(suite.ctx, tc.malleate(), et.simulate, NextFn)
 
 				if tc.expPass {
