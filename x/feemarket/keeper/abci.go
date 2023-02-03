b23c3b1014661c0e94598111a74fa68f925490be
Freddy Caceres <facs95@gmail.com>
2023-02-02 19:08:11 -0500

    update licenses URL

--- x/feemarket/keeper/abci.go
+++ x/feemarket/keeper/abci.go
@@ -12,7 +12,7 @@
 // GNU Lesser General Public License for more details.
 //
 // You should have received a copy of the GNU Lesser General Public License
-// along with the Ethermint library. If not, see https://github.com/evmos/ethermint/blob/main/LICENSE
+// along with the Ethermint library. If not, see https://github.com/evmos/evmos/blob/main/LICENSE
 package keeper
 
 import (
