7ea2899822e66e37266b1f7207524ca512b28d0d
Freddy Caceres <facs95@gmail.com>
2023-02-02 19:06:29 -0500

    update licenses

--- x/feemarket/keeper/params.go
+++ x/feemarket/keeper/params.go
@@ -1,12 +1,12 @@
 // Copyright 2021 Evmos Foundation
 // This file is part of Evmos' Ethermint library.
 //
-// The Ethermint library is free software: you can redistribute it and/or modify
+// Evmos is free software: you can redistribute it and/or modify
 // it under the terms of the GNU Lesser General Public License as published by
 // the Free Software Foundation, either version 3 of the License, or
 // (at your option) any later version.
 //
-// The Ethermint library is distributed in the hope that it will be useful,
+// The Evmos packages are distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
 // GNU Lesser General Public License for more details.
