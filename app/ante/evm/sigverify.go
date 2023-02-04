1a87410ad17260690749602f14c8e7590bb45e07
Freddy Caceres <facs95@gmail.com>
2023-02-03 22:41:10 -0500

    fix license

--- app/ante/evm/sigverify.go
+++ app/ante/evm/sigverify.go
@@ -1,4 +1,4 @@
-// Copyright 2021 Evmos Foundation
+// Copyright 2022 Evmos Foundation
 // This file is part of the Evmos Network packages.
 //
 // Evmos is free software: you can redistribute it and/or modify
