587599c88a8d48ea37cefd9354e1871746ec4cad
tom <tomasguerraalda@hotmail.com>
2023-02-07 18:06:47 -0300

    chore(tests) fix lint issues

--- cmd/config/config.go
+++ cmd/config/config.go
@@ -57,7 +57,7 @@ func SetBech32Prefixes(config *sdk.Config) {
 // SetBip44CoinType sets the global coin type to be used in hierarchical deterministic wallets.
 func SetBip44CoinType(config *sdk.Config) {
 	config.SetCoinType(types.Bip44CoinType)
-	config.SetPurpose(sdk.Purpose)                      // Shared
+	config.SetPurpose(sdk.Purpose)                  // Shared
 	config.SetFullFundraiserPath(types.BIP44HDPath) //nolint: staticcheck
 }
 
