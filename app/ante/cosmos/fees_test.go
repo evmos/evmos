9b1ac059842f18a86cc536f8235a0177c4ce9669
MalteHerrmann <malte@evmos.org>
2023-02-17 11:15:49 +0100

    restructure cosmos/fees_test to table-driven approach and add test cases

--- app/ante/cosmos/fees_test.go
+++ app/ante/cosmos/fees_test.go
@@ -3,44 +3,129 @@ package cosmos_test
 import (
 	sdktestutil "github.com/cosmos/cosmos-sdk/testutil/testdata"
 	sdk "github.com/cosmos/cosmos-sdk/types"
-	"github.com/cosmos/cosmos-sdk/x/auth/ante"
+	"github.com/cosmos/cosmos-sdk/x/auth/signing"
+	cosmosante "github.com/evmos/evmos/v11/app/ante/cosmos"
 	"github.com/evmos/evmos/v11/testutil"
 	"github.com/evmos/evmos/v11/utils"
 )
 
-func (suite *AnteTestSuite) TestDeductFeeDecorator_ZeroGas() {
-	suite.SetupTest()
-
+func (suite *AnteTestSuite) TestDeductFeeDecorator() {
 	txBuilder := suite.clientCtx.TxConfig.NewTxBuilder()
 
-	dfd := ante.NewDeductFeeDecorator(suite.app.AccountKeeper, suite.app.BankKeeper, suite.app.FeeGrantKeeper, nil)
-	antehandler := sdk.ChainAnteDecorators(dfd)
+	testcases := []struct {
+		name        string
+		malleate    func() signing.Tx
+		checkTx     bool
+		simulate    bool
+		expPass     bool
+		errContains string
+	}{
+		{
+			name: "pass - zero gas limit in simulation mode",
+			malleate: func() signing.Tx {
+				// Generate new account
+				addr, priv := testutil.NewAccAddressAndKey()
+				coins := sdk.NewCoins(sdk.NewCoin(utils.BaseDenom, sdk.NewInt(300)))
+				err := testutil.FundAccount(suite.ctx, suite.app.BankKeeper, addr, coins)
+				suite.Require().NoError(err, "failed to fund account")
 
-	// Generate new account
-	addr, priv := testutil.NewAccAddressAndKey()
-	coins := sdk.NewCoins(sdk.NewCoin(utils.BaseDenom, sdk.NewInt(300)))
-	err := testutil.FundAccount(suite.ctx, suite.app.BankKeeper, addr, coins)
-	suite.Require().NoError(err, "failed to fund account")
+				// Create an arbitrary message for testing purposes
+				msg := sdktestutil.NewTestMsg(addr)
+				err = txBuilder.SetMsgs(msg)
+				suite.Require().NoError(err, "failed to set message")
 
-	// Create an arbitrary message for testing purposes
-	msg := sdktestutil.NewTestMsg(addr)
-	err = txBuilder.SetMsgs(msg)
-	suite.Require().NoError(err, "failed to set message")
+				// Set gas limit to zero for testing purposes
+				txBuilder.SetGasLimit(0)
 
-	// Set gas limit to zero for testing purposes
-	txBuilder.SetGasLimit(0)
+				// Create a transaction out of the message
+				txBuilder, err = testutil.CreateTxInTxBuilder(suite.ctx, suite.app, txBuilder, priv, msg)
+				suite.Require().NoError(err, "failed to create transaction")
 
-	// Create a transaction out of the message
-	txBuilder, err = testutil.CreateTxInTxBuilder(suite.ctx, suite.app, txBuilder, priv, msg)
-	suite.Require().NoError(err, "failed to create transaction")
+				return txBuilder.GetTx()
+			},
+			checkTx:     false,
+			simulate:    true,
+			expPass:     true,
+			errContains: "",
+		},
+		{
+			name: "fail - zero gas limit in check tx mode",
+			malleate: func() signing.Tx {
+				// TODO: refactor this
+				// Generate new account
+				addr, priv := testutil.NewAccAddressAndKey()
+				coins := sdk.NewCoins(sdk.NewCoin(utils.BaseDenom, sdk.NewInt(300)))
+				err := testutil.FundAccount(suite.ctx, suite.app.BankKeeper, addr, coins)
+				suite.Require().NoError(err, "failed to fund account")
 
-	// Set IsCheckTx to true and check if the antehandler returns an error
-	// because a gas limit of zero should not be accepted
-	suite.ctx = suite.ctx.WithIsCheckTx(true)
-	_, err = antehandler(suite.ctx, txBuilder.GetTx(), false)
-	suite.Require().Error(err, "expected error when gas limit is zero")
+				// Create an arbitrary message for testing purposes
+				msg := sdktestutil.NewTestMsg(addr)
+				err = txBuilder.SetMsgs(msg)
+				suite.Require().NoError(err, "failed to set message")
 
-	// Zero gas is accepted in simulation mode
-	_, err = antehandler(suite.ctx, txBuilder.GetTx(), true)
-	suite.Require().NoError(err, "expected no error when gas limit is zero in simulation mode")
+				// Set gas limit to zero for testing purposes
+				txBuilder.SetGasLimit(0)
+
+				// Create a transaction out of the message
+				txBuilder, err = testutil.CreateTxInTxBuilder(suite.ctx, suite.app, txBuilder, priv, msg)
+				suite.Require().NoError(err, "failed to create transaction")
+
+				return txBuilder.GetTx()
+			},
+			checkTx:     true,
+			simulate:    false,
+			expPass:     false,
+			errContains: "must provide positive gas",
+		},
+		{
+			name: "fail - checkTx - insufficient funds and no staking rewards",
+			malleate: func() signing.Tx {
+				// Generate new account
+				addr, priv := testutil.NewAccAddressAndKey()
+				suite.app.AccountKeeper.SetAccount(suite.ctx, suite.app.AccountKeeper.NewAccountWithAddress(suite.ctx, addr))
+
+				// Create an arbitrary message for testing purposes
+				msg := sdktestutil.NewTestMsg(addr)
+				err := txBuilder.SetMsgs(msg)
+				suite.Require().NoError(err, "failed to set message")
+
+				// Set gas limit to zero for testing purposes
+				txBuilder.SetGasLimit(100)
+
+				// Create a transaction out of the message
+				txBuilder, err = testutil.CreateTxInTxBuilder(suite.ctx, suite.app, txBuilder, priv, msg)
+				suite.Require().NoError(err, "failed to create transaction")
+
+				return txBuilder.GetTx()
+			},
+			checkTx:     true,
+			simulate:    false,
+			expPass:     false,
+			errContains: "insufficient funds",
+		},
+	}
+
+	for _, tc := range testcases {
+		suite.Run(tc.name, func() {
+			suite.SetupTest()
+			dfd := cosmosante.NewDeductFeeDecorator(
+				suite.app.AccountKeeper, suite.app.BankKeeper, suite.app.DistrKeeper, suite.app.FeeGrantKeeper, suite.app.StakingKeeper, nil,
+			)
+
+			// set up the testcase
+			tx := tc.malleate()
+			suite.ctx = suite.ctx.WithIsCheckTx(tc.checkTx)
+
+			// run the ante handler
+			_, err := dfd.AnteHandle(suite.ctx, tx, tc.simulate, testutil.NextFn)
+
+			// assert the resulting error
+			if tc.expPass {
+				suite.Require().NoError(err, "expected no error")
+			} else {
+				suite.Require().Error(err, "expected error")
+				suite.Require().ErrorContains(err, tc.errContains)
+			}
+		})
+	}
 }
