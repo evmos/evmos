c864dc65820d15dfb231f692da54621aed8c3897
MalteHerrmann <malte@evmos.org>
2023-02-22 16:47:20 +0100

    remove logging

--- app/ante/evm/claim_rewards.go
+++ app/ante/evm/claim_rewards.go
@@ -18,7 +18,6 @@ package evm
 
 import (
 	"fmt"
-	"log"
 
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	stakingtypes "github.com/cosmos/cosmos-sdk/x/staking/types"
@@ -41,17 +40,12 @@ func ClaimSufficientStakingRewards(ctx sdk.Context, stakingKeeper StakingKeeper,
 	cacheCtx, writeFn := ctx.CacheContext()
 
 	// iterate through all delegations and get the rewards if any are unclaimed.
-	// TODO: remove logging
-	log.Print("iterating through delegations...")
 	stakingKeeper.IterateDelegations(
 		cacheCtx, addr, func(_ int64, delegation stakingtypes.DelegationI) (stop bool) {
-			log.Printf("delegation: %s -> %s", delegation.GetDelegatorAddr().String(), delegation.GetValidatorAddr().String())
 			reward, err = distributionKeeper.WithdrawDelegationRewards(cacheCtx, addr, delegation.GetValidatorAddr())
 			if err != nil {
-				log.Printf("error while withdrawing delegation rewards: %s", err)
 				return true
 			}
-			log.Printf("reward: %s", reward.String())
 			rewards = rewards.Add(reward...)
 
 			return rewards.AmountOf(utils.BaseDenom).GTE(baseAmount)
