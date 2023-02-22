53a855bfd1c914b99a2d8371797790ca2b0d1b54
Federico Kunze Küllmer <31522760+fedekunze@users.noreply.github.com>
2023-02-22 09:08:32 +0100

    tests: header testutil (#1412)

---
+++ testutil/setup.go
@@ -0,0 +1,46 @@
+package testutil
+
+import (
+	"time"
+
+	"github.com/tendermint/tendermint/crypto/tmhash"
+	tmproto "github.com/tendermint/tendermint/proto/tendermint/types"
+	tmversion "github.com/tendermint/tendermint/proto/tendermint/version"
+	"github.com/tendermint/tendermint/version"
+
+	sdk "github.com/cosmos/cosmos-sdk/types"
+)
+
+// NewHeader creates a new Tendermint header for testing purposes.
+func NewHeader(
+	height int64,
+	blockTime time.Time,
+	chainID string,
+	proposer sdk.ConsAddress,
+	appHash,
+	validatorHash []byte,
+) tmproto.Header {
+	return tmproto.Header{
+		ChainID:         chainID,
+		Height:          height,
+		Time:            blockTime,
+		ValidatorsHash:  validatorHash,
+		AppHash:         appHash,
+		ProposerAddress: proposer.Bytes(),
+		Version: tmversion.Consensus{
+			Block: version.BlockProtocol,
+		},
+		LastBlockId: tmproto.BlockID{
+			Hash: tmhash.Sum([]byte("block_id")),
+			PartSetHeader: tmproto.PartSetHeader{
+				Total: 11,
+				Hash:  tmhash.Sum([]byte("partset_header")),
+			},
+		},
+		DataHash:           tmhash.Sum([]byte("data")),
+		NextValidatorsHash: tmhash.Sum([]byte("next_validators")),
+		ConsensusHash:      tmhash.Sum([]byte("consensus")),
+		LastResultsHash:    tmhash.Sum([]byte("last_result")),
+		EvidenceHash:       tmhash.Sum([]byte("evidence")),
+	}
+}
