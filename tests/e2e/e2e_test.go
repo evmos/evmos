7ff48f4bbd0f2a92f9ff5588841a81501adb9c17
tom <tomasguerraalda@hotmail.com>
2023-02-22 13:19:08 -0300

    chore(tests) update e2e test exp err

--- tests/e2e/e2e_test.go
+++ tests/e2e/e2e_test.go
@@ -53,7 +53,7 @@ func (s *IntegrationTestSuite) TestCLITxs() {
 				)
 			},
 			expPass:   false,
-			expErrMsg: "insufficient fee",
+			expErrMsg: "out of gas",
 			// when the PR https://github.com/evmos/cosmos-sdk/pull/8 on cosmos-sdk and included on this repo, will get an error that cannot define gas flag when using fees=auto (which is the default)
 		},
 		{
