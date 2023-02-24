146f8ee9b18f8a58312e0460693d24cc0b570f7f
MalteHerrmann <malte@evmos.org>
2023-02-24 09:12:08 +0100

    add test case that withdraws multiple rewards

--- app/ante/evm/claim_rewards_test.go
+++ app/ante/evm/claim_rewards_test.go
@@ -2,6 +2,7 @@ package evm_test
 
 import (
 	sdk "github.com/cosmos/cosmos-sdk/types"
+	distributiontypes "github.com/cosmos/cosmos-sdk/x/distribution/types"
 	"github.com/evmos/evmos/v11/app/ante/evm"
 	"github.com/evmos/evmos/v11/testutil"
 	testutiltx "github.com/evmos/evmos/v11/testutil/tx"
@@ -14,30 +15,72 @@ func (suite *AnteTestSuite) TestClaimSufficientStakingRewards() {
 	// Define testcases
 	//
 	testcases := []struct {
-		name        string
-		malleate    func(addr sdk.AccAddress)
-		amount      int64
-		expErr      bool
+		// testcase name
+		name string
+		// malleate sets up the test case specific state, i.e. delegations and assigning rewards
+		malleate func(addr sdk.AccAddress)
+		// amount specifies the necessary amount of rewards to be withdrawn
+		amount int64
+		// expErr defines whether the test case is expected to return an error
+		expErr bool
+		// expErrContains defines the error message that is expected to be returned
 		errContains string
+		// postCheck contains assertions that check the state after the test case has been executed
+		// to further ensure that no false positives are reported
+		postCheck func(addr sdk.AccAddress)
 	}{
 		{
-			name: "pass - sufficient rewards can be claimed",
+			name: "pass - sufficient rewards can be withdrawn",
 			malleate: func(addr sdk.AccAddress) {
 				ctx, err := testutil.PrepareAccountsForDelegationRewards(
-					suite.T(), suite.ctx, suite.app, addr, sdk.NewInt(1e18), sdk.NewInt(1e18),
+					suite.T(), suite.ctx, suite.app, addr, sdk.ZeroInt(), sdk.NewInt(1e18),
 				)
 				suite.Require().NoError(err, "failed to prepare accounts for delegation rewards")
 				suite.ctx = ctx
-
-				// check that the validator historical reference count is 3 (initial creation, delegation + reward allocation)
-				historicalCount := suite.app.DistrKeeper.GetValidatorHistoricalReferenceCount(suite.ctx)
-				suite.Require().Equal(uint64(3), historicalCount, "historical count should be 3; got %d", historicalCount)
 			},
 			amount: 1000,
 			expErr: false,
+			postCheck: func(addr sdk.AccAddress) {
+				// Check that the necessary rewards are withdrawn, which means that there are no outstanding
+				// rewards left
+				resp, err := suite.app.DistrKeeper.DelegationTotalRewards(
+					suite.ctx,
+					&distributiontypes.QueryDelegationTotalRewardsRequest{DelegatorAddress: addr.String()},
+				)
+				suite.Require().NoError(err, "failed to query delegation total rewards")
+				suite.Require().Empty(resp.Total, "expected no total rewards to be left")
+			},
 		},
 		{
-			name:        "fail - insufficient staking rewards to claim",
+			name: "pass - Out of multiple outstanding rewards only those necessary are withdrawn",
+			malleate: func(addr sdk.AccAddress) {
+				ctx, err := testutil.PrepareAccountsForDelegationRewards(
+					suite.T(), suite.ctx, suite.app, addr, sdk.ZeroInt(), sdk.NewInt(1e14), sdk.NewInt(2e14), sdk.NewInt(2e13),
+				)
+				suite.Require().NoError(err, "failed to prepare accounts for delegation rewards")
+				suite.ctx = ctx
+			},
+			amount: 2e14,
+			expErr: false,
+			postCheck: func(addr sdk.AccAddress) {
+				// Check that only the necessary rewards are withdrawn (=1e14+2e14), which means that there is an outstanding
+				// reward of 2e13
+				resp, err := suite.app.DistrKeeper.DelegationTotalRewards(
+					suite.ctx,
+					&distributiontypes.QueryDelegationTotalRewardsRequest{DelegatorAddress: addr.String()},
+				)
+				suite.Require().NoError(err, "failed to query delegation total rewards")
+				suite.Require().NotNil(resp.Total, "expected rewards in one denomination yet to be withdrawn")
+				suite.Require().Equal(1, len(resp.Total), "expected rewards in one denomination yet to be withdrawn")
+				suite.Require().Equal(
+					sdk.NewDecCoin(utils.BaseDenom, sdk.NewInt(2e13)),
+					resp.Total[0],
+					"expected total rewards with an amount of 2e15 yet to be withdrawn",
+				)
+			},
+		},
+		{
+			name:        "fail - insufficient staking rewards to withdraw",
 			malleate:    func(addr sdk.AccAddress) {},
 			amount:      1000,
 			expErr:      true,
@@ -67,6 +110,9 @@ func (suite *AnteTestSuite) TestClaimSufficientStakingRewards() {
 				suite.Require().ErrorContains(err, tc.errContains)
 			} else {
 				suite.Require().NoError(err)
+			}
+			if tc.postCheck != nil {
+				tc.postCheck(addr)
 			}
 		})
 	}
