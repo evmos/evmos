a03c223e9d75d92e4b2176446c375f0d872a26bb
tom <tomasguerraalda@hotmail.com>
2023-02-27 19:01:14 -0300

    fix lint issues

--- app/ante/integration_test.go
+++ app/ante/integration_test.go
@@ -39,7 +39,6 @@ var _ = Describe("when sending a Cosmos transaction", func() {
 			ToAddress:   "evmos1dx67l23hz9l0k9hcher8xz04uj7wf3yu26l2yn",
 			Amount:      sdk.Coins{sdk.Coin{Amount: sdkmath.NewInt(1e14), Denom: utils.BaseDenom}},
 		}
-
 	})
 
 	Context("and the sender account has enough balance to pay for the transaction cost", Ordered, func() {
