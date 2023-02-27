252a4bb1e23fab670da2660277fc7f92ec78436c
Federico Kunze Küllmer <31522760+fedekunze@users.noreply.github.com>
2023-02-27 15:36:50 +0100

    fix(ante): add additional check to FeeChecker (#1433)
    * add check on dynamic fee
    
    * update check
    
    * changelog
    
    ---------
    
    Co-authored-by: Freddy Caceres <facs95@gmail.com>

--- app/ante/evm/fee_checker.go
+++ app/ante/evm/fee_checker.go
@@ -72,6 +72,11 @@ func NewDynamicFeeChecker(k DynamicFeeEVMKeeper) authante.TxFeeChecker {
 			}
 		}
 
+		// priority fee cannot be negative
+		if maxPriorityPrice.IsNegative() {
+			return nil, 0, errorsmod.Wrapf(errortypes.ErrInsufficientFee, "max priority price cannot be negative")
+		}
+
 		gas := feeTx.GetGas()
 		feeCoins := feeTx.GetFee()
 		fee := feeCoins.AmountOfNoDenomValidation(denom)
