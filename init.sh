dcbd6cd12723f15cef97039a7c88209aec237f97
chen <aaa445366516@gmail.com>
2023-06-24 10:01:58 +0800

    Update init.sh

--- init.sh
+++ init.sh
@@ -8,7 +8,7 @@ LOGLEVEL="info"
 # to trace evm
 #TRACE="--trace"
 TRACE=""
-
+======================
 # validate dependencies are installed
 command -v jq > /dev/null 2>&1 || { echo >&2 "jq not installed. More info: https://stedolan.github.io/jq/download/"; exit 1; }
 
