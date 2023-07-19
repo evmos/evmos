562dbf1967e9e1ca99a1ce44fc32cd5425579667
Freddy Caceres <facs95@gmail.com>
2023-07-19 15:24:56 -0400

    update commment

--- testutil/abci.go
+++ testutil/abci.go
@@ -109,9 +109,10 @@ func DeliverEthTx(
 	return res, nil
 }
 
-// DeliverEthTx generates and broadcasts a Cosmos Tx populated with MsgEthereumTx messages.
+// DeliverEthTxWithoutCheck generates and broadcasts a Cosmos Tx populated with MsgEthereumTx messages.
 // If a private key is provided, it will attempt to sign all messages with the given private key,
-// otherwise, it will assume the messages have already been signed.
+// otherwise, it will assume the messages have already been signed. It does not check if the Eth tx is
+// successful or not.
 func DeliverEthTxWithoutCheck(
 	appEvmos *app.Evmos,
 	priv cryptotypes.PrivKey,
