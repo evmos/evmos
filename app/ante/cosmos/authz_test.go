c64c37ea6859c11a13a1a75b9bbb5d1624d5deea
tom <tomasguerraalda@hotmail.com>
2023-07-27 18:49:33 -0300

    fix TestRejectMsgsInAuthz in ante tests

--- app/ante/cosmos/authz_test.go
+++ app/ante/cosmos/authz_test.go
@@ -2,6 +2,7 @@ package cosmos_test
 
 import (
 	"fmt"
+	"math/big"
 	"testing"
 	"time"
 
@@ -15,6 +16,7 @@ import (
 	banktypes "github.com/cosmos/cosmos-sdk/x/bank/types"
 	stakingtypes "github.com/cosmos/cosmos-sdk/x/staking/types"
 
+	ethtypes "github.com/ethereum/go-ethereum/core/types"
 	cosmosante "github.com/evmos/evmos/v13/app/ante/cosmos"
 	testutil "github.com/evmos/evmos/v13/testutil"
 	utiltx "github.com/evmos/evmos/v13/testutil/tx"
@@ -280,6 +282,18 @@ func (suite *AnteTestSuite) TestRejectMsgsInAuthz() {
 
 	distantFuture := time.Date(9000, 1, 1, 0, 0, 0, 0, time.UTC)
 
+	// create a dummy MsgEthereumTx for the test
+	// otherwise throws error that cannot unpack tx data
+	msgEthereumTx := evmtypes.NewTx(&evmtypes.EvmTxArgs{
+		ChainID:   big.NewInt(9000),
+		Nonce:     0,
+		GasLimit:  1000000,
+		GasFeeCap: suite.app.FeeMarketKeeper.GetBaseFee(suite.ctx),
+		GasTipCap: big.NewInt(1),
+		Input:     nil,
+		Accesses:  &ethtypes.AccessList{},
+	})
+
 	newMsgGrant := func(msgTypeUrl string) *authz.MsgGrant {
 		msg, err := authz.NewMsgGrant(
 			testAddresses[0],
@@ -326,7 +340,7 @@ func (suite *AnteTestSuite) TestRejectMsgsInAuthz() {
 							testAddresses[3],
 							sdk.NewCoins(sdk.NewInt64Coin(evmtypes.DefaultEVMDenom, 100e6)),
 						),
-						&evmtypes.MsgEthereumTx{},
+						msgEthereumTx,
 					},
 				),
 			},
@@ -339,7 +353,7 @@ func (suite *AnteTestSuite) TestRejectMsgsInAuthz() {
 					testAddresses[1],
 					2,
 					[]sdk.Msg{
-						&evmtypes.MsgEthereumTx{},
+						msgEthereumTx,
 					},
 				),
 			},
