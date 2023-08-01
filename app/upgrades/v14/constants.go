417fc963c3f6056611963b0c16824b90c107c82a
MalteHerrmann <42640438+MalteHerrmann@users.noreply.github.com>
2023-08-01 18:00:09 +0200

    chore: prepare v14.0.0-rc1 release (#1673)
    * adjust upgrade constants
    
    * adjust changelog
    
    * disable markdown link check for sec advisories

--- app/upgrades/v14/constants.go
+++ app/upgrades/v14/constants.go
@@ -5,7 +5,7 @@ package v14
 
 const (
 	// UpgradeName is the shared upgrade plan name for mainnet
-	UpgradeName = "v14.0.0"
+	UpgradeName = "v14.0.0-rc1"
 	// UpgradeInfo defines the binaries that will be used for the upgrade
-	UpgradeInfo = `'{"binaries":{"darwin/arm64":"https://github.com/evmos/evmos/releases/download/v14.0.0/evmos_14.0.0_Darwin_arm64.tar.gz","darwin/amd64":"https://github.com/evmos/evmos/releases/download/v14.0.0/evmos_14.0.0_Darwin_amd64.tar.gz","linux/arm64":"https://github.com/evmos/evmos/releases/download/v14.0.0/evmos_14.0.0_Linux_arm64.tar.gz","linux/amd64":"https://github.com/evmos/evmos/releases/download/v14.0.0/evmos_14.0.0_Linux_amd64.tar.gz","windows/x86_64":"https://github.com/evmos/evmos/releases/download/v14.0.0/evmos_14.0.0_Windows_x86_64.zip"}}'`
+	UpgradeInfo = `'{"binaries":{"darwin/arm64":"https://github.com/evmos/evmos/releases/download/v14.0.0-rc1/evmos_14.0.0-rc1_Darwin_arm64.tar.gz","darwin/amd64":"https://github.com/evmos/evmos/releases/download/v14.0.0-rc1/evmos_14.0.0-rc1_Darwin_amd64.tar.gz","linux/arm64":"https://github.com/evmos/evmos/releases/download/v14.0.0-rc1/evmos_14.0.0-rc1_Linux_arm64.tar.gz","linux/amd64":"https://github.com/evmos/evmos/releases/download/v14.0.0-rc1/evmos_14.0.0-rc1_Linux_amd64.tar.gz","windows/x86_64":"https://github.com/evmos/evmos/releases/download/v14.0.0-rc1/evmos_14.0.0-rc1_Windows_x86_64.zip"}}'`
 )
