361a950d45c5fd57dd80228cc50c800983289dec
MalteHerrmann <malte@evmos.org>
2023-08-09 11:56:24 +0200

    move tests

--- app/upgrades/v14rc2/migrations_test.go
+++ app/upgrades/v14rc2/migrations_test.go
@@ -8,55 +8,12 @@ import (
 	"github.com/evmos/evmos/v13/crypto/ethsecp256k1"
 	"github.com/evmos/evmos/v13/testutil"
 	testutiltx "github.com/evmos/evmos/v13/testutil/tx"
-	"github.com/evmos/evmos/v13/x/vesting/types"
 )
 
 var (
 	// zeroDec is a zero decimal value
 	zeroDec = sdk.ZeroDec()
 )
-
-func (s *UpgradesTestSuite) TestUpdateVestingFunders() {
-	s.SetupTest()
-
-	// Fund the affected accounts to initialize them and then create vesting accounts
-	s.prepareVestingAccount(v14rc2.VestingAddrByFunder1, v14rc2.OldFunder1)
-	for _, address := range v14rc2.VestingAddrsByFunder2 {
-		s.prepareVestingAccount(address, v14rc2.OldFunder2)
-	}
-
-	// Run the upgrade function
-	err := v14rc2.UpdateVestingFunders(s.ctx, s.app.VestingKeeper, v14rc2.NewTeamPremintWalletAcc)
-	s.Require().NoError(err, "failed to update vesting funders")
-
-	// Check that the vesting accounts have been updated
-	affectedAddrs := append(v14rc2.VestingAddrsByFunder2, v14rc2.VestingAddrByFunder1)
-	for _, address := range affectedAddrs {
-		accAddr := sdk.MustAccAddressFromBech32(address)
-		acc := s.app.AccountKeeper.GetAccount(s.ctx, accAddr)
-		s.Require().NotNil(acc, "account not found for %s", address)
-		vestingAcc, ok := acc.(*types.ClawbackVestingAccount)
-		s.Require().True(ok, "account is not a vesting account for %s", address)
-		s.Require().Equal(address, vestingAcc.Address, "expected different address in vesting account for %s", address)
-
-		// Check that the funder has been updated
-		s.Require().Equal(v14rc2.NewTeamPremintWalletAcc.String(), vestingAcc.FunderAddress, "expected different funder address for %s", address)
-	}
-}
-
-func (s *UpgradesTestSuite) prepareVestingAccount(address string, funder string) {
-	accAddr := sdk.MustAccAddressFromBech32(address)
-	err := testutil.FundAccountWithBaseDenom(s.ctx, s.app.BankKeeper, accAddr, 1000)
-	s.Require().NoError(err, "failed to fund account %s", address)
-
-	// Create vesting account
-	createMsg := &types.MsgCreateClawbackVestingAccount{
-		FunderAddress:  funder,
-		VestingAddress: address,
-	}
-	_, err = s.app.VestingKeeper.CreateClawbackVestingAccount(sdk.UnwrapSDKContext(s.ctx), createMsg)
-	s.Require().NoError(err, "failed to create vesting account for %s", address)
-}
 
 func (s *UpgradesTestSuite) TestUpdateMigrateNativeMultisigs() {
 	s.SetupTest()
