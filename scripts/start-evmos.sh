31a440bd0e02a26361489e46d2248873a701e76a
tom <tomasguerraalda@hotmail.com>
2023-08-24 09:31:50 -0300

    fix lint issues

--- scripts/start-evmos.sh
+++ scripts/start-evmos.sh
@@ -2,21 +2,21 @@
 set -e
 
 CONFIG=$1
-if [ -z ""$CONFIG"" ]; then
+if [ -z "$CONFIG" ]; then
     echo "No config file supplied"
     exit 1
 fi
 shift
 
 DOTENV=$1
-if [ -z ""$DOTENV"" ]; then
+if [ -z "$DOTENV" ]; then
     echo "No dotenv file supplied"
     exit 1
 fi
 shift
 
 DATA=$1
-if [ -z ""$DATA"" ]; then
+if [ -z "$DATA" ]; then
     echo "No data directory supplied"
     exit 1
 fi
