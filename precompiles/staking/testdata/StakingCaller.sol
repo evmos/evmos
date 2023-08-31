9f35715029f19a905871f2276a2d7ef4d50f198f
tom <tomasguerraalda@hotmail.com>
2023-08-31 19:35:04 -0300

    chore: update precompiles file name to match the interface naming convention

--- precompiles/staking/testdata/StakingCaller.sol
+++ precompiles/staking/testdata/StakingCaller.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: LGPL-3.0-only
 pragma solidity >=0.8.17;
 
-import "../Staking.sol" as staking;
+import "../StakingI.sol" as staking;
 
 /// @title StakingCaller
 /// @author Evmos Core Team
