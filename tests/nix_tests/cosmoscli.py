6970876052c44d9107fe40ff6d46f248e0c22224
tom <tomasguerraalda@hotmail.com>
2023-09-06 11:05:27 -0300

    update cosmos cli

--- tests/nix_tests/cosmoscli.py
+++ tests/nix_tests/cosmoscli.py
@@ -519,7 +519,7 @@ class CosmosCLI:
         return r.decode("utf-8")
 
     def broadcast_tx(self, tx_file, **kwargs):
-        kwargs.setdefault("broadcast_mode", "block")
+        kwargs.setdefault("broadcast_mode", "sync")
         kwargs.setdefault("output", "json")
         return json.loads(
             self.raw("tx", "broadcast", tx_file, node=self.node_rpc, **kwargs)
@@ -637,7 +637,7 @@ class CosmosCLI:
         )
 
     def gov_propose(self, proposer, kind, proposal, **kwargs):
-        method = "submit-proposal"
+        method = "submit-legacy-proposal"
         kwargs.setdefault("gas_prices", DEFAULT_GAS_PRICE)
         if kind == "software-upgrade":
             return json.loads(
