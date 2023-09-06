101011ab15ce48aca3d17051a139178a869c7d29
MalteHerrmann <malte@evmos.org>
2023-09-06 19:31:03 +0200

    add store logic

--- x/vesting/keeper/gov.go
+++ x/vesting/keeper/gov.go
@@ -33,3 +33,36 @@ func (k Keeper) DeleteGovClawbackDisabled(ctx sdk.Context, addr sdk.AccAddress) 
 	key := append(types.KeyPrefixGovClawbackDisabledKey, addr.Bytes()...)
 	ctx.KVStore(k.storeKey).Delete(key)
 }
+
+// HasActiveClawbackProposal checks if there is an active clawback proposal for the given
+// combination of account address and vesting funder.
+func (k Keeper) HasActiveClawbackProposal(ctx sdk.Context, addr, funder sdk.AccAddress) bool {
+	key := buildActiveAccountClawbackProposalKey(addr, funder)
+
+	return ctx.KVStore(k.storeKey).Has(key)
+}
+
+// SetActiveClawbackProposal sets the given combination of account address and vesting funder
+// as subject to an active governance clawback proposal by storing it in the store.
+func (k Keeper) SetActiveClawbackProposal(ctx sdk.Context, addr, funder sdk.AccAddress) {
+	key := buildActiveAccountClawbackProposalKey(addr, funder)
+	ctx.KVStore(k.storeKey).Set(key, []byte{0x01})
+}
+
+// DeleteActiveClawbackProposal deletes the given combination of account address and vesting funder
+// from the store, indicating that there is no active governance clawback proposal for it.
+func (k Keeper) DeleteActiveClawbackProposal(ctx sdk.Context, addr, funder sdk.AccAddress) {
+	key := buildActiveAccountClawbackProposalKey(addr, funder)
+	ctx.KVStore(k.storeKey).Delete(key)
+}
+
+// buildActiveAccountClawbackProposalKey builds the key for the given combination of account address
+// and vesting funder.
+func buildActiveAccountClawbackProposalKey(addr, funder sdk.AccAddress) []byte {
+	key := make([]byte, 0, len(types.KeyPrefixGovClawbackProposalKey)+len(addr.Bytes())+len(funder.Bytes()))
+	key = append(key, types.KeyPrefixGovClawbackProposalKey...)
+	key = append(key, addr.Bytes()...)
+	key = append(key, funder.Bytes()...)
+
+	return key
+}
--- x/vesting/types/keys.go
+++ x/vesting/types/keys.go
@@ -7,10 +7,18 @@ const (
 	// prefixGovClawbackDisabledKey to be used in the KVStore to track vesting accounts that are not subject
 	// to clawback from governance.
 	prefixGovClawbackDisabledKey = iota + 1
+	// prefixGovClawbackProposalKey to be used in the KVStore to track vesting accounts that are subject
+	// to active governance clawback proposals.
+	prefixGovClawbackProposalKey
 )
 
-// KeyPrefixGovClawbackDisabledKey is the slice of prefix bytes for storing the governance clawback enabled/disabled flag.
-var KeyPrefixGovClawbackDisabledKey = []byte{prefixGovClawbackDisabledKey}
+var (
+	// KeyPrefixGovClawbackDisabledKey is the slice of prefix bytes for storing the governance clawback enabled/disabled flag.
+	KeyPrefixGovClawbackDisabledKey = []byte{prefixGovClawbackDisabledKey}
+	// KeyPrefixGovClawbackProposalKey is the slice of prefix bytes for storing the vesting account and funder combination
+	// of governance clawback proposals.
+	KeyPrefixGovClawbackProposalKey = []byte{prefixGovClawbackProposalKey}
+)
 
 const (
 	// ModuleName defines the module's name.
