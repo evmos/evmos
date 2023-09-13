9c17c6cfe09af3c12174bee7b3de46c3bca4aa8e
tom <tomasguerraalda@hotmail.com>
2023-09-13 17:30:24 -0300

    fix descr

--- client/block/block.go
+++ client/block/block.go
@@ -17,7 +17,7 @@ func Cmd() *cobra.Command {
 	cmd := &cobra.Command{
 		Use:   "block",
 		Short: "Get a specific block persisted in the db. If height is not specified, defaults to the latest.",
-		Long:  "Get the last block persisted in the db. If height is not specified, defaults to the latest.\nThis command works only if no other process is using the db. Before using it, make sure to stop your node.\nIf you're using a custom home directory, specify it with the '--home' flag",
+		Long:  "Get a specific block persisted in the db. If height is not specified, defaults to the latest.\nThis command works only if no other process is using the db. Before using it, make sure to stop your node.\nIf you're using a custom home directory, specify it with the '--home' flag",
 		PreRunE: func(cmd *cobra.Command, _ []string) error {
 			// Bind flags to the Context's Viper so the app construction can set
 			// options accordingly.
@@ -63,7 +63,6 @@ func Cmd() *cobra.Command {
 			}
 
 			cmd.Println(string(bz))
-
 			return nil
 		},
 	}
