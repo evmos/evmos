aeae145c664665478f1a62f479a8bfcbaea5f065
MalteHerrmann <malte@evmos.org>
2023-09-14 12:17:30 +0200

    add test to rc5 upgrade logic as best practice

--- app/upgrades/v14rc5/upgrades.go
+++ app/upgrades/v14rc5/upgrades.go
@@ -7,9 +7,9 @@ import (
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/cosmos/cosmos-sdk/types/module"
 	upgradetypes "github.com/cosmos/cosmos-sdk/x/upgrade/types"
-	distprecompile "github.com/evmos/evmos/v14/precompiles/distribution"
 	ics20precompile "github.com/evmos/evmos/v14/precompiles/ics20"
 	stakingprecompile "github.com/evmos/evmos/v14/precompiles/staking"
+	vestingprecompile "github.com/evmos/evmos/v14/precompiles/vesting"
 	evmkeeper "github.com/evmos/evmos/v14/x/evm/keeper"
 )
 
@@ -22,18 +22,12 @@ func CreateUpgradeHandler(
 	return func(ctx sdk.Context, _ upgradetypes.Plan, vm module.VersionMap) (module.VersionMap, error) {
 		logger := ctx.Logger().With("upgrade", UpgradeName)
 
-		// Disable vesting precompile in the EVM module
+		// Disable distribution precompile in the EVM module
 		//
 		// NOTE: This only serves testing purposes and should NOT be used in the mainnet handler.
 		// In order to test the changed behavior of calling non-active EVM extensions, it is necessary
 		// to disable one to test this.
-		evmParams := ek.GetParams(ctx)
-		evmParams.ActivePrecompiles = []string{
-			stakingprecompile.Precompile{}.Address().String(),
-			distprecompile.Precompile{}.Address().String(),
-			ics20precompile.Precompile{}.Address().String(),
-		}
-		err := ek.SetParams(ctx, evmParams)
+		err := DisableDistributionPrecompile(ctx, ek)
 		if err != nil {
 			// log error instead of aborting the upgrade
 			logger.Error("failed to set EVM params", "err", err)
@@ -43,4 +37,17 @@ func CreateUpgradeHandler(
 		logger.Debug("running module migrations ...")
 		return mm.RunMigrations(ctx, configurator, vm)
 	}
+}
+
+// DisableDistributionPrecompile updates the EVM parameters to include all available precompiles except
+// the distribution precompile.
+func DisableDistributionPrecompile(ctx sdk.Context, ek *evmkeeper.Keeper) error {
+	evmParams := ek.GetParams(ctx)
+	evmParams.ActivePrecompiles = []string{
+		stakingprecompile.Precompile{}.Address().String(),
+		ics20precompile.Precompile{}.Address().String(),
+		vestingprecompile.Precompile{}.Address().String(),
+	}
+
+	return ek.SetParams(ctx, evmParams)
 }
