3636b39a8c7910e7160bc3735c4e71621c78e627
Freddy Caceres <facs95@gmail.com>
2023-09-28 08:49:03 -0400

    some test structure

--- x/evm/keeper/integration_test.go
+++ x/evm/keeper/integration_test.go
@@ -1,6 +1,7 @@
 package keeper_test
 
 import (
+	"fmt"
 	"math/big"
 
 	. "github.com/onsi/ginkgo/v2"
@@ -48,6 +49,8 @@ var _ = Describe("Handling a MsgEthereumTx message", Label("EVM"), Ordered, func
 			defaultParams := evmtypes.DefaultParams()
 			err := s.network.UpdateEvmParams(defaultParams)
 			Expect(err).To(BeNil())
+			err = s.network.NextBlock()
+			Expect(err).To(BeNil())
 		})
 
 		It("performs a transfer call", func() {
@@ -86,6 +89,108 @@ var _ = Describe("Handling a MsgEthereumTx message", Label("EVM"), Ordered, func
 			res, err := s.factory.ExecuteContractCall(senderPriv, txArgs, callArgs)
 			Expect(err).To(BeNil())
 			Expect(res.IsOK()).To(Equal(true), "transaction should have succeeded", res.GetLog())
+		})
+
+		It("performs a precompile call", func() {
+		})
+
+		It("performs a legacyTx", func() {
+		})
+
+		It("performs an AccessListTx", func() {
+		})
+
+		It("performs an DynamicFeeTx", func() {
+		})
+	})
+
+	When("EnableCreate param is set to false", Ordered, func() {
+		BeforeAll(func() {
+			// Set params to default values
+			defaultParams := evmtypes.DefaultParams()
+			defaultParams.EnableCreate = false
+			err := s.network.UpdateEvmParams(defaultParams)
+			Expect(err).To(BeNil())
+			err = s.network.NextBlock()
+			Expect(err).To(BeNil())
+		})
+
+		It("performs a transfer call", func() {
+			senderPriv := s.keyring.GetPrivKey(0)
+			receiver := s.keyring.GetKey(1)
+			txArgs := evmtypes.EvmTxArgs{
+				To:     &receiver.Addr,
+				Amount: big.NewInt(1000),
+			}
+
+			res, err := s.factory.ExecuteEthTx(senderPriv, txArgs)
+			Expect(err).To(BeNil())
+			Expect(res.IsOK()).To(Equal(true), "transaction should have succeeded", res.GetLog())
+		})
+
+		It("fails when trying to perform contract deployment", func() {
+			senderPriv := s.keyring.GetPrivKey(0)
+			constructorArgs := []interface{}{"coin", "token", uint8(18)}
+			compiledContract := contracts.ERC20MinterBurnerDecimalsContract
+			contractAddr, err := s.factory.DeployContract(
+				senderPriv,
+				compiledContract,
+				constructorArgs...,
+			)
+			Expect(err).NotTo(BeNil())
+			Expect(contractAddr).To(Equal(common.Address{}))
+		})
+	})
+
+	When("EnableCall param is set to false", Ordered, func() {
+		BeforeAll(func() {
+			// Set params to default values
+			defaultParams := evmtypes.DefaultParams()
+			defaultParams.EnableCall = false
+			err := s.network.UpdateEvmParams(defaultParams)
+			Expect(err).To(BeNil())
+			err = s.network.NextBlock()
+			Expect(err).To(BeNil())
+		})
+
+		It("fails when performing a transfer call", func() {
+			senderPriv := s.keyring.GetPrivKey(0)
+			receiver := s.keyring.GetKey(1)
+			txArgs := evmtypes.EvmTxArgs{
+				To:     &receiver.Addr,
+				Amount: big.NewInt(1000),
+			}
+
+			res, err := s.factory.ExecuteEthTx(senderPriv, txArgs)
+			Expect(err).NotTo(BeNil())
+			// Note that response deliver shows this as a succesful transaction
+			Expect(res.IsOK()).To(Equal(true))
+		})
+
+		It("performs a contract deployment and fails to perform a contract call", func() {
+			senderPriv := s.keyring.GetPrivKey(0)
+			constructorArgs := []interface{}{"coin", "token", uint8(18)}
+			compiledContract := contracts.ERC20MinterBurnerDecimalsContract
+			contractAddr, err := s.factory.DeployContract(
+				senderPriv,
+				compiledContract,
+				constructorArgs...,
+			)
+			Expect(err).To(BeNil())
+			Expect(contractAddr).ToNot(Equal(common.Address{}))
+
+			txArgs := evmtypes.EvmTxArgs{
+				To: &contractAddr,
+			}
+			callArgs := factory.CallArgs{
+				ContractABI: compiledContract.ABI,
+				MethodName:  "mint",
+				Args:        []interface{}{s.keyring.GetAddr(1), big.NewInt(1e18)},
+			}
+			res, err := s.factory.ExecuteContractCall(senderPriv, txArgs, callArgs)
+			Expect(err).NotTo(BeNil())
+			// Note that response deliver shows this as a succesful transaction
+			Expect(res.IsOK()).To(Equal(true))
 		})
 	})
 })
