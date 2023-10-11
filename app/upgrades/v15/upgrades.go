9c1be85453fc934a470f9e5f50c97d785930d198
Freddy Caceres <facs95@gmail.com>
2023-10-11 18:20:48 -0400

    remove unnecessary comments

--- app/upgrades/v15/upgrades.go
+++ app/upgrades/v15/upgrades.go
@@ -20,10 +20,6 @@ func CreateUpgradeHandler(
 		logger.Debug("deleting feesplit module from version map...")
 		delete(vm, "crisis")
 
-		// Refs:
-		// - https://docs.cosmos.network/master/building-modules/upgrade.html#registering-migrations
-		// - https://docs.cosmos.network/master/migrations/chain-upgrade-guide-044.html#chain-upgrade
-
 		// Leave modules are as-is to avoid running InitGenesis.
 		logger.Debug("running module migrations ...")
 		return mm.RunMigrations(ctx, configurator, vm)
