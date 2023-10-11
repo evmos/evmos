fc2c9a2acb93af8cc373e2ab58870f4a6d26af76
MalteHerrmann <42640438+MalteHerrmann@users.noreply.github.com>
2023-10-11 19:08:13 +0200

    chore(upgrade): add migration to upgrade logic (#1845)
    * add migration to upgrade logic
    
    * adjust changelog

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -37,10 +37,15 @@ Ref: https://keepachangelog.com/en/1.0.0/
 
 ## Unreleased
 
+### State Machine Breaking
+
+- (upgrade) [#1845](https://github.com/evmos/evmos/pull/1845) Include remaining strategic reserve migrations.
+
 ### Improvements
 
 - (upgrade) [#1834](https://github.com/evmos/evmos/pull/1834) Improve v14 migration tests and utilities.
 - (app) [#1835](https://github.com/evmos/evmos/pull/1835) Remove migration logic from the app's `BeginBlocker`.
+- (tests) [#1805](https://github.com/evmos/evmos/pull/1805) Improve local node script by using predefined keys and adding configuration flags.
 - (docker) [#1743](https://github.com/evmos/evmos/pull/1743) Add rclone binary to Docker image.
 
 ## [v14.1.0] - 2023-09-25
--- app/upgrades/v14_2/upgrades.go
+++ app/upgrades/v14_2/upgrades.go
@@ -6,16 +6,32 @@ package v142
 import (
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/cosmos/cosmos-sdk/types/module"
+	bankkeeper "github.com/cosmos/cosmos-sdk/x/bank/keeper"
+	stakingkeeper "github.com/cosmos/cosmos-sdk/x/staking/keeper"
 	upgradetypes "github.com/cosmos/cosmos-sdk/x/upgrade/types"
+	v14 "github.com/evmos/evmos/v14/app/upgrades/v14"
+	"github.com/evmos/evmos/v14/utils"
 )
 
 // CreateUpgradeHandler creates an SDK upgrade handler for v14_2
 func CreateUpgradeHandler(
 	mm *module.Manager,
 	configurator module.Configurator,
+	bk bankkeeper.Keeper,
+	sk stakingkeeper.Keeper,
 ) upgradetypes.UpgradeHandler {
 	return func(ctx sdk.Context, _ upgradetypes.Plan, vm module.VersionMap) (module.VersionMap, error) {
 		logger := ctx.Logger().With("upgrade", UpgradeName)
+
+		if utils.IsMainnet(ctx.ChainID()) {
+			logger.Info("migrating strategic reserves")
+			if err := v14.MigrateNativeMultisigs(
+				ctx, bk, sk, v14.NewTeamStrategicReserveAcc, v14.OldStrategicReserves...,
+			); err != nil {
+				// NOTE: log error instead of aborting the upgrade
+				logger.Error("error while migrating native multisigs", "error", err)
+			}
+		}
 
 		// Leave modules are as-is to avoid running InitGenesis.
 		logger.Debug("running module migrations ...")
