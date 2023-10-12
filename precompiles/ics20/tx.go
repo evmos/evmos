9a77fb701a7a8046df2c560599595c2dca80821d
Vlad <vladislav.varadinov@gmail.com>
2023-10-12 12:34:09 +0300

    fix: extract check and update grant functions for case when smart contracts are calling

--- precompiles/ics20/tx.go
+++ precompiles/ics20/tx.go
@@ -4,17 +4,12 @@
 package ics20
 
 import (
-	"fmt"
-	"time"
-
 	errorsmod "cosmossdk.io/errors"
 	sdk "github.com/cosmos/cosmos-sdk/types"
-	"github.com/cosmos/cosmos-sdk/x/authz"
 	channeltypes "github.com/cosmos/ibc-go/v7/modules/core/04-channel/types"
 	"github.com/ethereum/go-ethereum/accounts/abi"
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/core/vm"
-	"github.com/evmos/evmos/v14/precompiles/authorization"
 )
 
 const (
@@ -47,32 +42,16 @@ func (p Precompile) Transfer(
 	// update the sender address to be equal to the origin address.
 	// Otherwise, if the provided delegator address is different from the origin address,
 	// return an error because is a forbidden operation
-	if contract.CallerAddress == sender {
-		sender = origin
-	} else if origin != sender {
-		return nil, fmt.Errorf(ErrDifferentOriginFromSender, origin.String(), sender.String())
+	sender, err = CheckOriginAndSender(contract, origin, sender)
+	if err != nil {
+		return nil, err
 	}
 
 	// no need to have authorization when the contract caller is the same as origin (owner of funds)
 	// and the sender is the origin
-	var (
-		expiration *time.Time
-		auth       authz.Authorization
-		resp       *authz.AcceptResponse
-	)
-
-	if contract.CallerAddress != origin {
-		// check if authorization exists
-		auth, expiration, err = authorization.CheckAuthzExists(ctx, p.AuthzKeeper, contract.CallerAddress, origin, TransferMsgURL)
-		if err != nil {
-			return nil, fmt.Errorf(authorization.ErrAuthzDoesNotExistOrExpired, contract.CallerAddress, origin)
-		}
-
-		// Accept the grant and return an error if the grant is not accepted
-		resp, err = AcceptGrant(ctx, contract.CallerAddress, origin, msg, auth)
-		if err != nil {
-			return nil, err
-		}
+	resp, expiration, err := CheckAndAcceptAuthorizationIfNeeded(ctx, contract, origin, p.AuthzKeeper, msg)
+	if err != nil {
+		return nil, err
 	}
 
 	res, err := p.transferKeeper.Transfer(sdk.WrapSDKContext(ctx), msg)
@@ -80,12 +59,8 @@ func (p Precompile) Transfer(
 		return nil, err
 	}
 
-	// Update grant only if is needed
-	if contract.CallerAddress != origin {
-		// accepts and updates the grant adjusting the spending limit
-		if err = UpdateGrant(ctx, p.AuthzKeeper, contract.CallerAddress, origin, expiration, resp); err != nil {
-			return nil, err
-		}
+	if err := UpdateGrantIfNeeded(ctx, contract, p.AuthzKeeper, origin, expiration, resp); err != nil {
+		return nil, err
 	}
 
 	if err = p.EmitIBCTransferEvent(
