6189c6eb203088014864b3d6fbe14c06c2a548ae
tom <tomasguerraalda@hotmail.com>
2023-10-18 10:38:22 -0300

    fix(test): seq error

--- tests/e2e/e2e_test.go
+++ tests/e2e/e2e_test.go
@@ -29,11 +29,11 @@ func (s *IntegrationTestSuite) TestUpgrade() {
 		s.T().Logf("(upgrade %d): UPGRADING TO %s WITH PROPOSAL NAME %s", idx, version.ImageTag, version.UpgradeName)
 		s.proposeUpgrade(version.UpgradeName, version.ImageTag)
 
-		_, err = s.upgradeManager.WaitForHeight(ctx, currentHeight+2)
+		_, err = s.upgradeManager.WaitForHeight(ctx, currentHeight+5)
 		s.Require().NoError(err)
 		s.voteForProposal(idx)
 
-		_, err = s.upgradeManager.WaitForHeight(ctx, currentHeight+3)
+		_, err = s.upgradeManager.WaitForHeight(ctx, currentHeight+8)
 		s.Require().NoError(err)
 		s.upgrade(version.ImageName, version.ImageTag)
 	}
