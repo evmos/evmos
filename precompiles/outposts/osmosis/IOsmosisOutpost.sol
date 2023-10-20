973dfb4facec2e58c67aca155e9be5848f28de87
stepit <stefanofrancesco.pitton@gmail.com>
2023-10-20 10:41:36 +0200

    formatting

--- precompiles/outposts/osmosis/IOsmosisOutpost.sol
+++ precompiles/outposts/osmosis/IOsmosisOutpost.sol
@@ -5,8 +5,9 @@ pragma solidity >=0.8.18;
 address constant OSMOSIS_OUTPOST_ADDRESS = 0x0000000000000000000000000000000000000901;
 
 /// @dev The Osmosis Outpost contract's instance.
-IOsmosisOutpost constant OSMOSIS_OUTPOST_CONTRACT = IOsmosisOutpost(OSMOSIS_OUTPOST_ADDRESS);
-
+IOsmosisOutpost constant OSMOSIS_OUTPOST_CONTRACT = IOsmosisOutpost(
+    OSMOSIS_OUTPOST_ADDRESS
+);
 
 /// @dev The default value used for the slippage_percentage in the swap
 string constant DEFAULT_TWAP_SLIPPAGE_PERCENTAGE = "5";
@@ -16,7 +17,7 @@ uint64 constant DEFAULT_TWAP_WINDOW_SECONDS = 10;
 /// @author Evmos Core Team
 /// @dev Interface for directly interacting with Osmosis Outpost
 interface IOsmosisOutpost {
-	/// @dev Emitted when an ICS-20 transfer is executed.
+    /// @dev Emitted when an ICS-20 transfer is executed.
     /// @param sender The address of the sender.
     /// @param receiver The address of the receiver.
     /// @param sourcePort The source port of the IBC transaction.
@@ -34,38 +35,38 @@ interface IOsmosisOutpost {
         string memo
     );
 
-	/// @dev Emitted when a user executes a swap
+    /// @dev Emitted when a user executes a swap
     /// @param sender The address of the sender
     /// @param input The ERC-20 token contract address to be swapped for
     /// @param output The ERC-20 token contract address to be swapped to (received)
     /// @param amount The amount of input tokens to be swapped
-    /// @param receiver The bech32-formatted address of the receiver of the newly swapped 
+    /// @param receiver The bech32-formatted address of the receiver of the newly swapped
     /// tokens, can be any chain connected to Osmosis e.g. evmosAddr, cosmosAddr, ...
     event Swap(
         address indexed sender,
-        address indexed input, 
+        address indexed input,
         address indexed output,
-        uint256 amount, 
+        uint256 amount,
         string receiver
     );
 
-	/// @dev This function is used to swap tokens on Osmosis
+    /// @dev This function is used to swap tokens on Osmosis
     /// @param sender The address of the sender
     /// @param input The ERC-20 token contract address to be swapped for
     /// @param output The ERC-20 token contract address to be swapped to (received)
     /// @param amount The amount of input tokens to be swapped
-	/// @param receiver The bech32-formatted address of the receiver of the newly swapped 
+    /// @param receiver The bech32-formatted address of the receiver of the newly swapped
     /// tokens. It can be any chain connected to Osmosis e.g. evmos1..., cosmos1..., etc.
-	/// @param slippage_percentage The percentage of slippage accepted for the swap.
-	/// @param window_seconds The amount of seconds considered to compute TWAP price
+    /// @param slippage_percentage The percentage of slippage accepted for the swap.
+    /// @param window_seconds The amount of seconds considered to compute TWAP price
     /// @return success The boolean value indicating whether the operation succeeded
     function swap(
         address sender,
         address input,
         address output,
         uint256 amount,
-		string calldata slippage_percentage,
-		uint64 window_seconds,
+        string calldata slippage_percentage,
+        uint64 window_seconds,
         string calldata receiver
     ) external returns (bool success);
 }
