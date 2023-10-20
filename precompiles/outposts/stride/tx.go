f4e2065a3f00ff1e1e97edb698ec3935ab0362b8
Vlad <vladislav.varadinov@gmail.com>
2023-10-20 12:21:46 +0300

    fix: add tx logic

--- precompiles/outposts/stride/tx.go
+++ precompiles/outposts/stride/tx.go
@@ -4,10 +4,14 @@
 package stride
 
 import (
+	"fmt"
+
 	sdk "github.com/cosmos/cosmos-sdk/types"
+	ibctransfertypes "github.com/cosmos/ibc-go/v7/modules/apps/transfer/types"
 	"github.com/ethereum/go-ethereum/accounts/abi"
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/core/vm"
+	"github.com/evmos/evmos/v15/precompiles/ics20"
 )
 
 const (
@@ -24,26 +28,192 @@ const (
 // LiquidStake is a transaction that liquid stakes tokens using
 // a ICS20 transfer with a custom memo field that will trigger Stride's Autopilot middleware
 func (p Precompile) LiquidStake(
-	_ sdk.Context,
-	_ common.Address,
-	_ vm.StateDB,
-	_ *vm.Contract,
-	_ *abi.Method,
-	_ []interface{},
+	ctx sdk.Context,
+	origin common.Address,
+	stateDB vm.StateDB,
+	contract *vm.Contract,
+	method *abi.Method,
+	args []interface{},
 ) ([]byte, error) {
-	return nil, nil
+	sender, token, amount, receiver, err := parseLiquidStakeArgs(args)
+	if err != nil {
+		return nil, err
+	}
+
+	// The provided sender address should always be equal to the origin address.
+	// In case the contract caller address is the same as the sender address provided,
+	// update the sender address to be equal to the origin address.
+	// Otherwise, if the provided sender address is different from the origin address,
+	// return an error because is a forbidden operation
+	sender, err = ics20.CheckOriginAndSender(contract, origin, sender)
+	if err != nil {
+		return nil, err
+	}
+
+	bondDenom := p.stakingKeeper.BondDenom(ctx)
+
+	tokenPairID := p.erc20Keeper.GetDenomMap(ctx, bondDenom)
+	tokenPair, found := p.erc20Keeper.GetTokenPair(ctx, tokenPairID)
+	// NOTE this should always exist
+	if !found {
+		return nil, fmt.Errorf("token pair not found")
+	}
+
+	// NOTE: for v1 we only support the native EVM (and staking) denomination (WEVMOS/WTEVMOS).
+	if token != tokenPair.GetERC20Contract() {
+		return nil, fmt.Errorf("unsupported token %s. The only supported token contract for Stride Outpost v1 is %s", token, tokenPair.Erc20Address)
+	}
+
+	coin := sdk.Coin{Denom: tokenPair.Denom, Amount: sdk.NewIntFromBigInt(amount)}
+
+	// Create the memo for the ICS20 transfer packet
+	memo, err := CreateMemo(LiquidStakeAction, receiver)
+	if err != nil {
+		return nil, err
+	}
+
+	// Build the MsgTransfer with the memo and coin
+	msg, err := ics20.CreateAndValidateMsgTransfer(
+		p.portID,
+		p.channelID,
+		coin,
+		sdk.AccAddress(sender.Bytes()).String(),
+		receiver,
+		p.timeoutHeight,
+		ibctransfertypes.DefaultRelativePacketTimeoutTimestamp,
+		memo,
+	)
+	if err != nil {
+		return nil, err
+	}
+
+	// no need to have authorization when the contract caller is the same as origin (owner of funds)
+	// and the sender is the origin
+	accept, expiration, err := ics20.CheckAndAcceptAuthorizationIfNeeded(ctx, contract, origin, p.AuthzKeeper, msg)
+	if err != nil {
+		return nil, err
+	}
+
+	// Execute the ICS20 Transfer
+	res, err := p.transferKeeper.Transfer(sdk.WrapSDKContext(ctx), msg)
+	if err != nil {
+		return nil, err
+	}
+
+	// Update grant only if is needed
+	if err := ics20.UpdateGrantIfNeeded(ctx, contract, p.AuthzKeeper, origin, expiration, accept); err != nil {
+		return nil, err
+	}
+
+	// Emit the IBC transfer Event
+	if err = ics20.EmitIBCTransferEvent(ctx, stateDB, p.ABI.Events, sender, p.Address(), msg); err != nil {
+		return nil, err
+	}
+
+	// Emit the custom LiquidStake Event
+	if err = p.EmitLiquidStakeEvent(ctx, stateDB, sender, token, amount); err != nil {
+		return nil, err
+	}
+
+	return method.Outputs.Pack(res.Sequence, true)
 }
 
 // Redeem is a transaction that redeems the native tokens using the liquid stake
 // tokens. It executes a ICS20 transfer with a custom memo field that will
 // trigger Stride's Autopilot middleware
 func (p Precompile) Redeem(
-	_ sdk.Context,
-	_ common.Address,
-	_ vm.StateDB,
-	_ *vm.Contract,
-	_ *abi.Method,
-	_ []interface{},
+	ctx sdk.Context,
+	origin common.Address,
+	stateDB vm.StateDB,
+	contract *vm.Contract,
+	method *abi.Method,
+	args []interface{},
 ) ([]byte, error) {
-	return nil, nil
+	sender, token, amount, receiver, err := parseLiquidStakeArgs(args)
+	if err != nil {
+		return nil, err
+	}
+
+	// The provided sender address should always be equal to the origin address.
+	// In case the contract caller address is the same as the sender address provided,
+	// update the sender address to be equal to the origin address.
+	// Otherwise, if the provided sender address is different from the origin address,
+	// return an error because is a forbidden operation
+	sender, err = ics20.CheckOriginAndSender(contract, origin, sender)
+	if err != nil {
+		return nil, err
+	}
+
+	bondDenom := p.stakingKeeper.BondDenom(ctx)
+	stToken := "st" + bondDenom
+
+	ibcDenom := computeIBCDenom(p.portID, p.channelID, stToken)
+
+	tokenPairID := p.erc20Keeper.GetDenomMap(ctx, ibcDenom)
+	tokenPair, found := p.erc20Keeper.GetTokenPair(ctx, tokenPairID)
+	if !found {
+		return nil, fmt.Errorf("token pair not found for %s", ibcDenom)
+	}
+
+	if token != tokenPair.GetERC20Contract() {
+		return nil, fmt.Errorf("unsupported token %s. The only supported token contract for Stride Outpost v1 is %s", token, tokenPair.Erc20Address)
+	}
+
+	coin := sdk.Coin{Denom: tokenPair.Denom, Amount: sdk.NewIntFromBigInt(amount)}
+
+	// Create the memo for the ICS20 transfer
+	memo, err := CreateMemo(RedeemAction, receiver)
+	if err != nil {
+		return nil, err
+	}
+
+	// Build the MsgTransfer with the memo and coin
+	msg, err := NewMsgTransfer(p.portID, p.channelID, sdk.AccAddress(sender.Bytes()).String(), receiver, memo, p.timeoutHeight, coin)
+	if err != nil {
+		return nil, err
+	}
+
+	// no need to have authorization when the contract caller is the same as origin (owner of funds)
+	// and the sender is the origin
+	accept, expiration, err := ics20.CheckAndAcceptAuthorizationIfNeeded(ctx, contract, origin, p.AuthzKeeper, msg)
+	if err != nil {
+		return nil, err
+	}
+
+	// Execute the ICS20 Transfer
+	res, err := p.transferKeeper.Transfer(sdk.WrapSDKContext(ctx), msg)
+	if err != nil {
+		return nil, err
+	}
+
+	// Update grant only if is needed
+	if err := ics20.UpdateGrantIfNeeded(ctx, contract, p.AuthzKeeper, origin, expiration, accept); err != nil {
+		return nil, err
+	}
+
+	// Emit the IBC transfer Event
+	if err = ics20.EmitIBCTransferEvent(ctx, stateDB, p.ABI.Events, sender, p.Address(), msg); err != nil {
+		return nil, err
+	}
+
+	// Emit the custom Redeem Event
+	if err = p.EmitRedeemEvent(ctx, stateDB, sender, token, receiver, amount); err != nil {
+		return nil, err
+	}
+
+	return method.Outputs.Pack(res.Sequence, true)
+}
+
+// computeIBCDenom compute the ibc voucher denom associated to
+// the portID and channelID of the precompile given a token denomination.
+func computeIBCDenom(
+	portID, channelID,
+	denom string,
+) string {
+	denomTrace := ibctransfertypes.DenomTrace{
+		Path:      fmt.Sprintf("%s/%s", portID, channelID),
+		BaseDenom: denom,
+	}
+
+	return denomTrace.IBCDenom()
 }
