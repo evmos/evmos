fa43a10c832c7c70ecfa44790455748d3b537f3a
Vladislav Varadinov <vladislav.varadinov@gmail.com>
2023-10-24 15:33:30 +0300

    chore(stride): Add stride outpost unit testing setup (#1931)
    * chore(stride): Add stride outpost unit testing setup
    
    * fix: add the registering in the setup
    
    * CHANGELOG

---
+++ precompiles/outposts/stride/setup_test.go
@@ -0,0 +1,71 @@
+// Copyright Tharsis Labs Ltd.(Evmos)
+// SPDX-License-Identifier:ENCL-1.0(https://github.com/evmos/evmos/blob/main/LICENSE)
+
+package stride_test
+
+import (
+	"testing"
+	"time"
+
+	"github.com/evmos/evmos/v15/precompiles/outposts/stride"
+
+	tmtypes "github.com/cometbft/cometbft/types"
+	"github.com/cosmos/cosmos-sdk/crypto/keyring"
+	cryptotypes "github.com/cosmos/cosmos-sdk/crypto/types"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	stakingtypes "github.com/cosmos/cosmos-sdk/x/staking/types"
+	ibctesting "github.com/cosmos/ibc-go/v7/testing"
+	"github.com/ethereum/go-ethereum/common"
+	ethtypes "github.com/ethereum/go-ethereum/core/types"
+	evmosapp "github.com/evmos/evmos/v15/app"
+	evmosibc "github.com/evmos/evmos/v15/ibc/testing"
+	"github.com/evmos/evmos/v15/x/evm/statedb"
+	evmtypes "github.com/evmos/evmos/v15/x/evm/types"
+
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+	"github.com/stretchr/testify/suite"
+)
+
+var s *PrecompileTestSuite
+
+type PrecompileTestSuite struct {
+	suite.Suite
+
+	ctx           sdk.Context
+	app           *evmosapp.Evmos
+	address       common.Address
+	differentAddr common.Address
+	validators    []stakingtypes.Validator
+	valSet        *tmtypes.ValidatorSet
+	ethSigner     ethtypes.Signer
+	privKey       cryptotypes.PrivKey
+	signer        keyring.Signer
+	bondDenom     string
+
+	precompile *stride.Precompile
+	stateDB    *statedb.StateDB
+
+	coordinator    *ibctesting.Coordinator
+	chainA         *ibctesting.TestChain
+	chainB         *ibctesting.TestChain
+	transferPath   *evmosibc.Path
+	queryClientEVM evmtypes.QueryClient
+
+	defaultExpirationDuration time.Time
+
+	suiteIBCTesting bool
+}
+
+func TestPrecompileTestSuite(t *testing.T) {
+	s = new(PrecompileTestSuite)
+	suite.Run(t, s)
+
+	// Run Ginkgo integration tests
+	RegisterFailHandler(Fail)
+	RunSpecs(t, "ICS20 Precompile Suite")
+}
+
+func (s *PrecompileTestSuite) SetupTest() {
+	s.DoSetupTest()
+}
