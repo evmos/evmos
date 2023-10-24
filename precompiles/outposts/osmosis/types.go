2dc692668834b173ef0c778ad8224940909e9322
0xstepit <0xstepit@users.noreply.github.com>
2023-10-24 09:44:22 +0000

    run make format

--- precompiles/outposts/osmosis/types.go
+++ precompiles/outposts/osmosis/types.go
@@ -6,9 +6,10 @@ package osmosis
 import (
 	"encoding/json"
 	"fmt"
+	"math/big"
+
 	"github.com/cosmos/cosmos-sdk/types/address"
 	"golang.org/x/exp/slices"
-	"math/big"
 
 	"github.com/cosmos/btcutil/bech32"
 	transfertypes "github.com/cosmos/ibc-go/v7/modules/apps/transfer/types"
