7b6ec5e2d669d7eaf8e7bf743109aed81ecdcb75
tom <tomasguerraalda@hotmail.com>
2023-10-26 14:24:05 -0300

    remove unnecessary file

--- nix/default.nix
+++ nix/default.nix
@@ -18,7 +18,7 @@ import sources.nixpkgs {
       };
       rocksdb = pkgs.callPackage ./rocksdb.nix {};
       # evmos with rocksdb build
-      evmosd-rocksdb = pkgs.callPackage ./evmos-rocksdb.nix {};
+      evmosd-rocksdb = pkgs.callPackage ../default.nix { dbBackend = "rocksdb"; };
       # other chains to use in IBC tests
       chain-maind = pkgs.callPackage sources.chain-main { rocksdb = null; };
       strided = pkgs.callPackage ./cosmos-chain.nix { 
