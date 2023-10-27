39e6a18f98ccab931114888963d77a3613749b7e
MalteHerrmann <malte@evmos.org>
2023-10-27 12:02:00 +0200

    add tests

---
+++ app/upgrades/v15rc2/setup_test.go
@@ -0,0 +1,37 @@
+// Copyright Tharsis Labs Ltd.(Evmos)
+// SPDX-License-Identifier:ENCL-1.0(https://github.com/evmos/evmos/blob/main/LICENSE)
+
+package v15rc2_test
+
+import (
+	"testing"
+
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	stakingtypes "github.com/cosmos/cosmos-sdk/x/staking/types"
+	"github.com/ethereum/go-ethereum/common"
+	ethtypes "github.com/ethereum/go-ethereum/core/types"
+	evmosapp "github.com/evmos/evmos/v15/app"
+	"github.com/stretchr/testify/suite"
+)
+
+var s *UpgradesTestSuite
+
+type UpgradesTestSuite struct {
+	suite.Suite
+
+	ctx        sdk.Context
+	app        *evmosapp.Evmos
+	address    common.Address
+	validators []stakingtypes.Validator
+	ethSigner  ethtypes.Signer
+	bondDenom  string
+}
+
+func TestUpgradeTestSuite(t *testing.T) {
+	s = new(UpgradesTestSuite)
+	suite.Run(t, s)
+}
+
+func (s *UpgradesTestSuite) SetupTest() {
+	s.DoSetupTest()
+}
