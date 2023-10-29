6f2b4371007f4917bf87066a2a4d2b262678ba67
Freddy Caceres <facs95@gmail.com>
2023-10-29 17:42:52 -0400

    initial refractor

---
+++ testutil/integration/common/grpc/grpc.go
@@ -0,0 +1,38 @@
+package grpc
+
+import (
+	sdktypes "github.com/cosmos/cosmos-sdk/types"
+	authtypes "github.com/cosmos/cosmos-sdk/x/auth/types"
+	banktypes "github.com/cosmos/cosmos-sdk/x/bank/types"
+	stakingtypes "github.com/cosmos/cosmos-sdk/x/staking/types"
+	"github.com/evmos/evmos/v15/testutil/integration/common/network"
+)
+
+// Handler is an interface that defines the common methods that are used to query
+// the network's modules via gRPC.
+type Handler interface {
+	// Bank methods
+	GetBalance(address sdktypes.AccAddress, denom string) (*banktypes.QueryBalanceResponse, error)
+
+	// Account methods
+	GetAccount(address string) (authtypes.AccountI, error)
+
+	// Staking methods
+	GetDelegation(delegatorAddress string, validatorAddress string) (*stakingtypes.QueryDelegationResponse, error)
+}
+
+var _ Handler = (*IntegrationHandler)(nil)
+
+// IntegrationHandler is a helper struct to query the network's modules
+// via gRPC. This is to simulate the behavior of a real user and avoid querying
+// the modules directly.
+type IntegrationHandler struct {
+	network network.Network
+}
+
+// NewIntegrationHandler creates a new IntegrationHandler instance.
+func NewIntegrationHandler(network network.Network) *IntegrationHandler {
+	return &IntegrationHandler{
+		network: network,
+	}
+}
--- testutil/integration/evmos/grpc/grpc.go
+++ testutil/integration/evmos/grpc/grpc.go
@@ -3,12 +3,9 @@
 package grpc
 
 import (
-	sdktypes "github.com/cosmos/cosmos-sdk/types"
-	authtypes "github.com/cosmos/cosmos-sdk/x/auth/types"
-	banktypes "github.com/cosmos/cosmos-sdk/x/bank/types"
-	stakingtypes "github.com/cosmos/cosmos-sdk/x/staking/types"
 	"github.com/ethereum/go-ethereum/common"
-	"github.com/evmos/evmos/v15/testutil/integration/network"
+	commongrpc "github.com/evmos/evmos/v15/testutil/integration/common/grpc"
+	"github.com/evmos/evmos/v15/testutil/integration/evmos/network"
 	evmtypes "github.com/evmos/evmos/v15/x/evm/types"
 	feemarkettypes "github.com/evmos/evmos/v15/x/feemarket/types"
 	revtypes "github.com/evmos/evmos/v15/x/revenue/v1/types"
@@ -17,22 +14,15 @@ import (
 // Handler is an interface that defines the methods that are used to query
 // the network's modules via gRPC.
 type Handler interface {
+	commongrpc.Handler
+
 	// EVM methods
 	GetEvmAccount(address common.Address) (*evmtypes.QueryAccountResponse, error)
 	EstimateGas(args []byte, GasCap uint64) (*evmtypes.EstimateGasResponse, error)
 	GetEvmParams() (*evmtypes.QueryParamsResponse, error)
 
-	// Bank methods
-	GetBalance(address sdktypes.AccAddress, denom string) (*banktypes.QueryBalanceResponse, error)
-
-	// Account methods
-	GetAccount(address string) (authtypes.AccountI, error)
-
 	// FeeMarket methods
 	GetBaseFee() (*feemarkettypes.QueryBaseFeeResponse, error)
-
-	// Staking methods
-	GetDelegation(delegatorAddress string, validatorAddress string) (*stakingtypes.QueryDelegationResponse, error)
 
 	// Revenue methods
 	GetRevenue(address common.Address) (*revtypes.QueryRevenueResponse, error)
@@ -45,12 +35,16 @@ var _ Handler = (*IntegrationHandler)(nil)
 // via gRPC. This is to simulate the behavior of a real user and avoid querying
 // the modules directly.
 type IntegrationHandler struct {
+	// We take the IntegrationHandler from common/grpc to get the common methods.
+	*commongrpc.IntegrationHandler
 	network network.Network
 }
 
 // NewIntegrationHandler creates a new IntegrationHandler instance.
 func NewIntegrationHandler(network network.Network) Handler {
 	return &IntegrationHandler{
+		// Is there a better way to do this?
+		IntegrationHandler: commongrpc.NewIntegrationHandler(network),
 		network: network,
 	}
 }
