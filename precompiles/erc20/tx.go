3046da4b9c8d168a8603137f5f3baf82ab3a77bc
Federico Kunze Küllmer <31522760+fedekunze@users.noreply.github.com>
2023-11-01 06:51:07 +0100

    feat(erc20): precompile setup

---
+++ precompiles/erc20/tx.go
@@ -0,0 +1,12 @@
+// Copyright Tharsis Labs Ltd.(Evmos)
+// SPDX-License-Identifier:ENCL-1.0(https://github.com/evmos/evmos/blob/main/LICENSE)
+package erc20
+
+const (
+	// TransferMethod defines the ABI method name for the IERC20 transfer
+	// transaction.
+	TransferMethod = "transfer"
+	// TransferFromMethod defines the ABI method name for the IERC20 transferFrom
+	// transaction.
+	TransferFromMethod = "transferFrom"
+)
