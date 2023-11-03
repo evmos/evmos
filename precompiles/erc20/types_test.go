a23c9ded5def988bf3d661021d5a5baa2d62e86c
MalteHerrmann <malte@evmos.org>
2023-11-03 11:23:39 +0100

    add parse transfer from args tests

--- precompiles/erc20/types_test.go
+++ precompiles/erc20/types_test.go
@@ -58,3 +58,69 @@ func (s *PrecompileTestSuite) TestParseTransferArgs() {
 		})
 	}
 }
+
+func (s *PrecompileTestSuite) TestParseTransferFromArgs() {
+	from := utiltx.GenerateAddress()
+	to := utiltx.GenerateAddress()
+	amount := big.NewInt(100)
+
+	testcases := []struct {
+		name        string
+		args        []interface{}
+		expPass     bool
+		errContains string
+	}{
+		{
+			name: "pass - correct arguments",
+			args: []interface{}{
+				from,
+				to,
+				amount,
+			},
+			expPass: true,
+		},
+		{
+			name: "fail - invalid from address",
+			args: []interface{}{
+				"invalid address",
+				to,
+				amount,
+			},
+			errContains: "invalid from address",
+		},
+		{
+			name: "fail - invalid to address",
+			args: []interface{}{
+				from,
+				"invalid address",
+				amount,
+			},
+			errContains: "invalid to address",
+		},
+		{
+			name: "fail - invalid amount",
+			args: []interface{}{
+				from,
+				to,
+				"invalid amount",
+			},
+			errContains: "invalid amount",
+		},
+	}
+
+	for _, tc := range testcases {
+		tc := tc
+		s.Run(tc.name, func() {
+			from, to, amount, err := erc20.ParseTransferFromArgs(tc.args)
+			if tc.expPass {
+				s.Require().NoError(err, "unexpected error parsing the transferFrom arguments")
+				s.Require().Equal(from, tc.args[0], "expected different from address")
+				s.Require().Equal(to, tc.args[1], "expected different to address")
+				s.Require().Equal(amount, tc.args[2], "expected different amount")
+			} else {
+				s.Require().Error(err, "expected an error parsing the transferFrom arguments")
+				s.Require().ErrorContains(err, tc.errContains, "expected different error message")
+			}
+		})
+	}
+}
