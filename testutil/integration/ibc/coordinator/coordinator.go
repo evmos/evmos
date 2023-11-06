ce69b4b51e5a652b2c2527b41a09747865ecc609
Freddy Caceres <facs95@gmail.com>
2023-11-06 10:43:00 -0500

    testutil(feat): IBC integration test suit helper functions (#2003)
    * working setup
    
    * working setup
    
    * simplify PR
    
    * fix lint
    
    * Update testutil/integration/ibc/coordinator/coordinator.go
    
    Co-authored-by: stepit <48993133+0xstepit@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: stepit <48993133+0xstepit@users.noreply.github.com>

--- testutil/integration/ibc/coordinator/coordinator.go
+++ testutil/integration/ibc/coordinator/coordinator.go
@@ -6,7 +6,10 @@ import (
 	"testing"
 	"time"
 
+	cryptotypes "github.com/cosmos/cosmos-sdk/crypto/types"
+	authtypes "github.com/cosmos/cosmos-sdk/x/auth/types"
 	ibctesting "github.com/cosmos/ibc-go/v7/testing"
+	evmosibc "github.com/evmos/evmos/v15/ibc/testing"
 	"github.com/evmos/evmos/v15/testutil/integration/common/network"
 	ibcchain "github.com/evmos/evmos/v15/testutil/integration/ibc/chain"
 )
@@ -23,10 +26,14 @@ type Coordinator interface {
 	UpdateTimeForChain(chainID string)
 	// GetChain returns the TestChain for a given chainID.
 	GetChain(chainID string) ibcchain.Chain
+	// GetDummyChainsIds returns the chainIDs for all dummy chains.
+	GetDummyChainsIds() []string
+	// SetDefaultSignerForChain sets the default signer for the chain with the given chainID.
+	SetDefaultSignerForChain(chainID string, priv cryptotypes.PrivKey, acc authtypes.AccountI)
 	// Setup constructs a TM client, connection, and channel on both chains provided. It will
 	// fail if any error occurs. The clientID's, TestConnections, and TestChannels are returned
 	// for both chains. The channels created are connected to the ibc-transfer application.
-	Setup(path *ibctesting.Path)
+	Setup(src, dst string) IBCConnection
 	// CommitNBlocks commits n blocks on the chain with the given chainID.
 	CommitNBlocks(chainID string, n uint64) error
 }
@@ -41,8 +48,12 @@ var _ Coordinator = (*IntegrationCoordinator)(nil)
 
 // IntegrationCoordinator is a testing struct which contains N TestChain's. It handles keeping all chains
 // in sync with regards to time.
+// NOTE: When using the coordinator, it is important to commit blocks through the coordinator and not
+// through the network interface directly. This is because the coordinator does not keep the context in
+// sync with the network interface.
 type IntegrationCoordinator struct {
-	coord *ibctesting.Coordinator
+	coord          *ibctesting.Coordinator
+	dummyChainsIds []string
 }
 
 // NewIntegrationCoordinator returns a new IntegrationCoordinator with N TestChain's.
@@ -52,44 +63,23 @@ func NewIntegrationCoordinator(t *testing.T, preConfiguredChains []network.Netwo
 		CurrentTime: GlobalTime,
 	}
 	ibcChains := getIBCChains(t, coord, preConfiguredChains)
-	dummyChains := generateDummyChains(t, coord, AmountOfDummyChains)
+	dummyChains, dummyChainsIds := generateDummyChains(t, coord, AmountOfDummyChains)
 	totalChains := mergeMaps(ibcChains, dummyChains)
 	coord.Chains = totalChains
 	return &IntegrationCoordinator{
-		coord: coord,
+		coord:          coord,
+		dummyChainsIds: dummyChainsIds,
 	}
-}
-
-// getIBCChains returns a map of TestChain's for the given network interface.
-func getIBCChains(t *testing.T, coord *ibctesting.Coordinator, chains []network.Network) map[string]*ibctesting.TestChain {
-	ibcChains := make(map[string]*ibctesting.TestChain)
-	for _, chain := range chains {
-		ibcChains[chain.GetChainID()] = chain.GetIBCChain(t, coord)
-	}
-	return ibcChains
-}
-
-// generateDummyChains returns a map of dummy chains to complement IBC connections for integration tests.
-func generateDummyChains(t *testing.T, coord *ibctesting.Coordinator, numberOfChains int) map[string]*ibctesting.TestChain {
-	ibcChains := make(map[string]*ibctesting.TestChain)
-	for i := 1; i <= numberOfChains; i++ {
-		chainID := ibctesting.GetChainID(i)
-		ibcChains[chainID] = ibctesting.NewTestChain(t, coord, chainID)
-	}
-	return ibcChains
-}
-
-// mergeMaps merges two maps of TestChain's.
-func mergeMaps(m1, m2 map[string]*ibctesting.TestChain) map[string]*ibctesting.TestChain {
-	for k, v := range m2 {
-		m1[k] = v
-	}
-	return m1
 }
 
 // GetChain returns the TestChain for a given chainID.
 func (c *IntegrationCoordinator) GetChain(chainID string) ibcchain.Chain {
 	return c.coord.Chains[chainID]
+}
+
+// GetDummyChainsIds returns the chainIDs for all dummy chains.
+func (c *IntegrationCoordinator) GetDummyChainsIds() []string {
+	return c.dummyChainsIds
 }
 
 // IncrementTime iterates through all the TestChain's and increments their current header time
@@ -109,11 +99,40 @@ func (c *IntegrationCoordinator) UpdateTimeForChain(chainID string) {
 	c.coord.UpdateTimeForChain(chain)
 }
 
+// SetDefaultSignerForChain sets the default signer for the chain with the given chainID.
+func (c *IntegrationCoordinator) SetDefaultSignerForChain(chainID string, priv cryptotypes.PrivKey, acc authtypes.AccountI) {
+	chain := c.coord.GetChain(chainID)
+	chain.SenderPrivKey = priv
+	chain.SenderAccount = acc
+	chain.SenderAccounts = []ibctesting.SenderAccount{{SenderPrivKey: priv, SenderAccount: acc}}
+}
+
 // Setup constructs a TM client, connection, and channel on both chains provided. It will
 // fail if any error occurs. The clientID's, TestConnections, and TestChannels are returned
 // for both chains. The channels created are connected to the ibc-transfer application.
-func (c *IntegrationCoordinator) Setup(path *ibctesting.Path) {
-	c.coord.Setup(path)
+func (c *IntegrationCoordinator) Setup(a, b string) IBCConnection {
+	chainA := c.coord.GetChain(a)
+	chainB := c.coord.GetChain(b)
+
+	path := evmosibc.NewTransferPath(chainA, chainB)
+	evmosibc.SetupPath(c.coord, path)
+
+	return IBCConnection{
+		EndpointA: Endpoint{
+			ChainID:      a,
+			ClientID:     path.EndpointA.ClientID,
+			ConnectionID: path.EndpointA.ConnectionID,
+			ChannelID:    path.EndpointA.ChannelID,
+			PortID:       path.EndpointA.ChannelConfig.PortID,
+		},
+		EndpointB: Endpoint{
+			ChainID:      b,
+			ClientID:     path.EndpointB.ClientID,
+			ConnectionID: path.EndpointB.ConnectionID,
+			ChannelID:    path.EndpointB.ChannelID,
+			PortID:       path.EndpointB.ChannelConfig.PortID,
+		},
+	}
 }
 
 // CommitNBlocks commits n blocks on the chain with the given chainID.
