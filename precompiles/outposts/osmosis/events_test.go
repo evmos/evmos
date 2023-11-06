156fb5b22d75d9cd5bc36b265007c3e4785c1e15
Freddy Caceres <facs95@gmail.com>
2023-11-06 11:39:18 -0500

    fix event_test

--- precompiles/outposts/osmosis/events_test.go
+++ precompiles/outposts/osmosis/events_test.go
@@ -8,15 +8,33 @@ import (
 	"github.com/evmos/evmos/v15/precompiles/outposts/osmosis"
 	evmosutiltx "github.com/evmos/evmos/v15/testutil/tx"
 	"github.com/evmos/evmos/v15/x/evm/statedb"
+
+	testkeyring "github.com/evmos/evmos/v15/testutil/integration/evmos/keyring"
+	"github.com/evmos/evmos/v15/testutil/integration/evmos/network"
 )
 
 func (s *PrecompileTestSuite) TestSwapEvent() {
+	keyring := testkeyring.New(1)
+	unitNetwork := network.NewUnitTestNetwork(
+		network.WithPreFundedAccounts(keyring.GetAllAccAddrs()...),
+	)
+
+	precompile, err := osmosis.NewPrecompile(
+		portID,
+		channelID,
+		osmosis.XCSContract,
+		unitNetwork.App.BankKeeper,
+		unitNetwork.App.TransferKeeper,
+		unitNetwork.App.StakingKeeper,
+		unitNetwork.App.Erc20Keeper,
+	)
+	s.Require().NoError(err)
 	// random common.Address that represents the evmos ERC20 token address and
 	// the IBC OSMO ERC20 token address.
 	evmosAddress := evmosutiltx.GenerateAddress()
 	osmoAddress := evmosutiltx.GenerateAddress()
 
-	sender := s.keyring.GetAddr(0)
+	sender := keyring.GetAddr(0)
 	receiver := "osmo1qql8ag4cluz6r4dz28p3w00dnc9w8ueuhnecd2"
 	transferAmount := int64(10)
 
@@ -40,10 +58,10 @@ func (s *PrecompileTestSuite) TestSwapEvent() {
 				swapLog := stateDB.Logs()[0]
 				s.Require().Equal(
 					swapLog.Address,
-					s.precompile.Address(),
+					precompile.Address(),
 					"expected first log address equal to osmosis outpost precompile",
 				)
-				event := s.precompile.ABI.Events[osmosis.EventTypeSwap]
+				event := precompile.ABI.Events[osmosis.EventTypeSwap]
 				s.Require().Equal(
 					event.ID,
 					common.HexToHash(swapLog.Topics[0].Hex()),
@@ -51,13 +69,13 @@ func (s *PrecompileTestSuite) TestSwapEvent() {
 				)
 				s.Require().Equal(
 					swapLog.BlockNumber,
-					uint64(s.network.GetContext().BlockHeight()),
+					uint64(unitNetwork.GetContext().BlockHeight()),
 					"require event block height equal to context block height",
 				)
 
 				// Check for swap specific information in the event
 				var swapEvent osmosis.EventSwap
-				err := cmn.UnpackLog(s.precompile.ABI, &swapEvent, osmosis.EventTypeSwap, *swapLog)
+				err := cmn.UnpackLog(precompile.ABI, &swapEvent, osmosis.EventTypeSwap, *swapLog)
 				s.Require().NoError(err)
 				s.Require().Equal(
 					sender,
@@ -90,13 +108,13 @@ func (s *PrecompileTestSuite) TestSwapEvent() {
 
 	for _, tc := range testCases {
 		s.Run(tc.name, func() {
-			err := s.network.NextBlock()
+			err := unitNetwork.NextBlock()
 			s.Require().NoError(err)
 
-			stateDB := s.network.GetStateDB()
+			stateDB := unitNetwork.GetStateDB()
 
-			err = s.precompile.EmitSwapEvent(
-				s.network.GetContext(),
+			err = precompile.EmitSwapEvent(
+				unitNetwork.GetContext(),
 				stateDB,
 				sender,
 				tc.input,
