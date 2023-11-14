79e0fdf2f24a261aa32abe597f35919bb11adbf0
Vlad <vladislav.varadinov@gmail.com>
2023-11-14 10:45:47 +0200

    chore(bech32): Port bech32 precompile to Evmos repo

---
+++ precompiles/bech32/setup_test.go
@@ -0,0 +1,53 @@
+package bech32_test
+
+import (
+	"testing"
+
+	//nolint:revive // dot imports are fine for Ginkgo
+	. "github.com/onsi/ginkgo/v2"
+	//nolint:revive // dot imports are fine for Ginkgo
+	. "github.com/onsi/gomega"
+
+	"github.com/cosmos/cosmos-sdk/crypto/keyring"
+	cryptotypes "github.com/cosmos/cosmos-sdk/crypto/types"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	stakingtypes "github.com/cosmos/cosmos-sdk/x/staking/types"
+	"github.com/ethereum/go-ethereum/common"
+	ethtypes "github.com/ethereum/go-ethereum/core/types"
+	"github.com/ethereum/go-ethereum/core/vm"
+	evmosapp "github.com/evmos/evmos/v15/app"
+	"github.com/evmos/evmos/v15/precompiles/bech32"
+	"github.com/stretchr/testify/suite"
+)
+
+var s *PrecompileTestSuite
+
+type PrecompileTestSuite struct {
+	suite.Suite
+
+	ctx        sdk.Context
+	app        *evmosapp.Evmos
+	address    common.Address
+	validators []stakingtypes.Validator
+	ethSigner  ethtypes.Signer
+	privKey    cryptotypes.PrivKey
+	signer     keyring.Signer
+	bondDenom  string
+
+	precompile        *bech32.Precompile
+	activePrecompiles []common.Address
+	stateDB           vm.StateDB
+}
+
+func TestPrecompileTestSuite(t *testing.T) {
+	s = new(PrecompileTestSuite)
+	suite.Run(t, s)
+
+	// Run Ginkgo integration tests
+	RegisterFailHandler(Fail)
+	RunSpecs(t, "Keeper Suite")
+}
+
+func (s *PrecompileTestSuite) SetupTest() {
+	s.DoSetupTest()
+}
