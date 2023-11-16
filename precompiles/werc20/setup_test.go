46ab9790b0ff780ed6e1386c8f4a318d0808a315
Vlad <vladislav.varadinov@gmail.com>
2023-11-16 11:48:06 +0200

    fix: add setup test for WEVMOS

---
+++ precompiles/werc20/setup_test.go
@@ -0,0 +1,77 @@
+package werc20_test
+
+import (
+	"github.com/evmos/evmos/v15/precompiles/werc20"
+	utiltx "github.com/evmos/evmos/v15/testutil/tx"
+	erc20types "github.com/evmos/evmos/v15/x/erc20/types"
+	"testing"
+
+	"github.com/evmos/evmos/v15/testutil/integration/evmos/factory"
+	"github.com/evmos/evmos/v15/testutil/integration/evmos/grpc"
+	testkeyring "github.com/evmos/evmos/v15/testutil/integration/evmos/keyring"
+	"github.com/evmos/evmos/v15/testutil/integration/evmos/network"
+	"github.com/stretchr/testify/suite"
+
+	//nolint:revive // dot imports are fine for Ginkgo
+	. "github.com/onsi/ginkgo/v2"
+	//nolint:revive // dot imports are fine for Ginkgo
+	. "github.com/onsi/gomega"
+)
+
+var s *PrecompileTestSuite
+
+// PrecompileTestSuite is the implementation of the TestSuite interface for ERC20 precompile
+// unit tests.
+type PrecompileTestSuite struct {
+	suite.Suite
+
+	bondDenom   string
+	network     *network.UnitTestNetwork
+	factory     factory.TxFactory
+	grpcHandler grpc.Handler
+	keyring     testkeyring.Keyring
+
+	precompile *werc20.Precompile
+}
+
+func TestPrecompileTestSuite(t *testing.T) {
+	s = new(PrecompileTestSuite)
+	suite.Run(t, s)
+
+	// Run Ginkgo integration tests
+	RegisterFailHandler(Fail)
+	RunSpecs(t, "WEVMOS Extension Suite")
+}
+
+func (s *PrecompileTestSuite) SetupTest() {
+	keyring := testkeyring.New(2)
+	integrationNetwork := network.NewUnitTestNetwork(
+		network.WithPreFundedAccounts(keyring.GetAllAccAddrs()...),
+	)
+	grpcHandler := grpc.NewIntegrationHandler(integrationNetwork)
+	txFactory := factory.New(integrationNetwork, grpcHandler)
+
+	ctx := integrationNetwork.GetContext()
+	sk := integrationNetwork.App.StakingKeeper
+	bondDenom := sk.BondDenom(ctx)
+	s.Require().NotEmpty(bondDenom, "bond denom cannot be empty")
+
+	s.bondDenom = bondDenom
+	s.factory = txFactory
+	s.grpcHandler = grpcHandler
+	s.keyring = keyring
+	s.network = integrationNetwork
+
+	// Create the token pair for WEVMOS <-> EVMOS.
+	tokenPair := erc20types.NewTokenPair(utiltx.GenerateAddress(), bondDenom, erc20types.OWNER_MODULE)
+
+	precompile, err := werc20.NewPrecompile(
+		tokenPair,
+		s.network.App.BankKeeper,
+		s.network.App.AuthzKeeper,
+		s.network.App.TransferKeeper,
+	)
+	s.Require().NoError(err, "failed to create wevmos precompile")
+
+	s.precompile = precompile
+}
