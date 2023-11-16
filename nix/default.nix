eb0bc156f43f079364add5ec9b5b94993fa9b37f
tom <tomasguerraalda@hotmail.com>
2023-11-16 20:35:53 -0300

    add comment

--- nix/default.nix
+++ nix/default.nix
@@ -28,7 +28,7 @@ import sources.nixpkgs {
         version = "v16.0.0";
         rev = "e0c02910e036f4f2894a96c5222aebacc3ce0a4a";
         vendorSha256 = "sha256-vktJQOnnr/QcxiReMnCrlKEFqarMMFzfMjoB3LQ27vk=";
-        patches = [ ../tests/nix_tests/configs/stride-admins.patch ];
+        patches = [ ../tests/nix_tests/configs/stride-admins.patch ]; # patch stride to allow tests addresses perform transactions that would need a gov proposal instead
       };
       # In case of osmosis & gaia, they provide the compiled binary. We'll use this
       # cause it is faster than building from source
