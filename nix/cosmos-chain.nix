a805fb175346607775de2e30003abab8cf813c20
tom <tomasguerraalda@hotmail.com>
2023-11-16 20:18:04 -0300

    add stride patch

--- nix/cosmos-chain.nix
+++ nix/cosmos-chain.nix
@@ -6,11 +6,12 @@
 , appName
 , rev
 , vendorSha256
+, patches ? []
 }:
 buildGo119Module rec {
   # Use this nix file to build any cosmos chain you need,
   # e.g. Stride, Osmosis, etc.
-  inherit src version name appName rev vendorSha256;
+  inherit src version name appName rev vendorSha256 patches;
   tags = [ "netgo" ];
   ldflags = lib.concatStringsSep "\n" ([
     "-X github.com/cosmos/cosmos-sdk/version.Name=${name}"
@@ -19,6 +20,12 @@ buildGo119Module rec {
     "-X github.com/cosmos/cosmos-sdk/version.BuildTags=${lib.concatStringsSep "," tags}"
     "-X github.com/cosmos/cosmos-sdk/version.Commit=${rev}"
   ]);
+
+  # allow to edit the source code files
+  # in case patches are provided
+  prePatch = ''
+    chmod -R +w $src
+  '';
 
   doCheck = false;
   subPackages = [ "cmd/${appName}" ];
