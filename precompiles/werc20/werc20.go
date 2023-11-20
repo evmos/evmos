589d87cfdf556402ff03cb7cdcd3840e48100ac2
Vladislav Varadinov <vladislav.varadinov@gmail.com>
2023-11-20 18:37:06 +0200

    fix(werc20): remove references to fallback and receive as they do not have names (#2062)
    * fix: remove references to fallback and receive as they do not have names
    
    * run make format
    
    * CHANGELOG
    
    ---------
    
    Co-authored-by: Vvaradinov <Vvaradinov@users.noreply.github.com>

--- precompiles/werc20/werc20.go
+++ precompiles/werc20/werc20.go
@@ -88,7 +88,7 @@ func (p Precompile) RequiredGas(input []byte) uint64 {
 	}
 
 	switch method.Name {
-	case DepositMethod, cmn.FallbackMethod, cmn.ReceiveMethod:
+	case DepositMethod:
 		return DepositRequiredGas
 	case WithdrawMethod:
 		return WithdrawRequiredGas
@@ -110,8 +110,6 @@ func (p Precompile) Run(evm *vm.EVM, contract *vm.Contract, readOnly bool) (bz [
 
 	switch {
 	case method == nil,
-		method.Name == cmn.FallbackMethod,
-		method.Name == cmn.ReceiveMethod,
 		method.Name == DepositMethod:
 		// WERC20 transactions
 		bz, err = p.Deposit(ctx, contract, stateDB, method, args)
@@ -137,14 +135,11 @@ func (p Precompile) Run(evm *vm.EVM, contract *vm.Contract, readOnly bool) (bz [
 }
 
 // IsTransaction checks if the given methodID corresponds to a transaction or query.
-func (p Precompile) IsTransaction(methodID string) bool {
-	switch methodID {
-	case cmn.FallbackMethod,
-		cmn.ReceiveMethod,
-		DepositMethod,
-		WithdrawMethod:
+func (p Precompile) IsTransaction(methodName string) bool {
+	switch methodName {
+	case DepositMethod, WithdrawMethod:
 		return true
 	default:
-		return p.Precompile.IsTransaction(methodID)
+		return p.Precompile.IsTransaction(methodName)
 	}
 }
