63c92f23251e46ccafcb8c34a46e0da4b6d04f5f
tom <tomasguerraalda@hotmail.com>
2023-11-28 17:24:41 -0300

    update upgrade logic and permissions

--- app/upgrades/v16/incentives.go
+++ app/upgrades/v16/incentives.go
@@ -12,9 +12,15 @@ import (
 
 // BurnUsageIncentivesPool burns the entirety of the usage incentives pool
 func BurnUsageIncentivesPool(ctx sdk.Context, bk bankkeeper.Keeper) error {
-	incentivesPoolBalance := bk.GetAllBalances(ctx, authtypes.NewModuleAddress("incentives"))
+	incentivesAddr := authtypes.NewModuleAddress("incentives")
+	incentivesPoolBalance := bk.GetAllBalances(ctx, incentivesAddr)
 	if !incentivesPoolBalance.IsAllPositive() {
 		return nil
+	}
+
+	// transfer funds to the fee collector account and then burn it
+	if err := bk.SendCoinsFromAccountToModule(ctx, incentivesAddr, authtypes.FeeCollectorName, incentivesPoolBalance); err != nil {
+		return err
 	}
 
 	return bk.BurnCoins(ctx, authtypes.FeeCollectorName, incentivesPoolBalance)
@@ -25,7 +31,7 @@ func BurnUsageIncentivesPool(ctx sdk.Context, bk bankkeeper.Keeper) error {
 func UpdateInflationParams(ctx sdk.Context, ik inflationkeeper.Keeper) error {
 	params := ik.GetParams(ctx)
 	params.InflationDistribution.CommunityPool = sdkmath.LegacyOneDec().Sub(params.InflationDistribution.StakingRewards)
-	params.InflationDistribution.UsageIncentives = sdkmath.LegacyZeroDec() //nolint:staticcheck
+	params.InflationDistribution.UsageIncentives = sdkmath.LegacyZeroDec()
 
 	if err := params.Validate(); err != nil {
 		return err
