786b279cec0a9c3e7014a59caf6854dcfaf1486a
MalteHerrmann <malte@evmos.org>
2023-11-29 12:22:24 +0100

    fix linter

--- x/inflation/v1/keeper/integration_test.go
+++ x/inflation/v1/keeper/integration_test.go
@@ -61,7 +61,7 @@ var _ = Describe("Inflation", Ordered, func() {
 				})
 			})
 
-			Context("after an epoch ends", func() {
+			Context("after an epoch ends", func() { //nolint:dupl // these tests are not duplicates
 				BeforeEach(func() {
 					s.CommitAfter(time.Minute)    // Start Epoch
 					s.CommitAfter(time.Hour * 25) // End Epoch
