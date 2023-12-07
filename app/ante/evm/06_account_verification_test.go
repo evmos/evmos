aeca7c3e10f63080104b67ecd637cd6d8aa1a2e9
Freddy Caceres <facs95@gmail.com>
2023-12-07 15:47:35 +0800

    fix lint

--- app/ante/evm/06_account_verification_test.go
+++ app/ante/evm/06_account_verification_test.go
@@ -15,8 +15,6 @@ import (
 	evmtypes "github.com/evmos/evmos/v16/x/evm/types"
 )
 
-const SENDER_KEYRING_KEY = 1
-
 func (suite *EvmAnteTestSuite) TestVerifyAccountBalance() {
 	// Setup
 	keyring := testkeyring.New(2)
@@ -25,6 +23,7 @@ func (suite *EvmAnteTestSuite) TestVerifyAccountBalance() {
 	)
 	grpcHandler := grpc.NewIntegrationHandler(unitNetwork)
 	txFactory := factory.New(unitNetwork, grpcHandler)
+	senderKey := keyring.GetKey(1)
 
 	testCases := []struct {
 		name          string
@@ -36,21 +35,20 @@ func (suite *EvmAnteTestSuite) TestVerifyAccountBalance() {
 			expectedError: errortypes.ErrInvalidType,
 			malleate: func(statedbAccount *statedb.Account, _ *evmtypes.EvmTxArgs) {
 				statedbAccount.CodeHash = []byte("test")
-				err := unitNetwork.App.EvmKeeper.SetAccount(unitNetwork.GetContext(), keyring.GetAddr(SENDER_KEYRING_KEY), *statedbAccount)
+				err := unitNetwork.App.EvmKeeper.SetAccount(unitNetwork.GetContext(), senderKey.Addr, *statedbAccount)
 				suite.Require().NoError(err)
 			},
 		}, {
 			name:          "fail: sender balance is lower than the transaction cost",
 			expectedError: errortypes.ErrInsufficientFunds,
 			malleate: func(statedbAccount *statedb.Account, args *evmtypes.EvmTxArgs) {
-				sender := keyring.GetKey(SENDER_KEYRING_KEY)
 				// Make sure the account has no code hash
 				statedbAccount.CodeHash = evmtypes.EmptyCodeHash
-				err := unitNetwork.App.EvmKeeper.SetAccount(unitNetwork.GetContext(), sender.Addr, *statedbAccount)
+				err := unitNetwork.App.EvmKeeper.SetAccount(unitNetwork.GetContext(), senderKey.Addr, *statedbAccount)
 				suite.Require().NoError(err)
 
 				// Make tx cost greater than balance
-				balanceResp, err := grpcHandler.GetBalance(sender.AccAddr, unitNetwork.GetDenom())
+				balanceResp, err := grpcHandler.GetBalance(senderKey.AccAddr, unitNetwork.GetDenom())
 				suite.Require().NoError(err)
 
 				invalidaAmount := balanceResp.Balance.Amount.Add(math.NewInt(100))
@@ -60,10 +58,9 @@ func (suite *EvmAnteTestSuite) TestVerifyAccountBalance() {
 			name:          "fail: tx cost is negative",
 			expectedError: errortypes.ErrInvalidCoins,
 			malleate: func(statedbAccount *statedb.Account, args *evmtypes.EvmTxArgs) {
-				sender := keyring.GetKey(SENDER_KEYRING_KEY)
 				// Make sure the account has no code hash
 				statedbAccount.CodeHash = evmtypes.EmptyCodeHash
-				err := unitNetwork.App.EvmKeeper.SetAccount(unitNetwork.GetContext(), sender.Addr, *statedbAccount)
+				err := unitNetwork.App.EvmKeeper.SetAccount(unitNetwork.GetContext(), senderKey.Addr, *statedbAccount)
 				suite.Require().NoError(err)
 
 				// Make tx cost is negative. This has to be a big value because the
@@ -74,17 +71,16 @@ func (suite *EvmAnteTestSuite) TestVerifyAccountBalance() {
 		}, {
 			name:          "success: tx is succesfull and account is created if its nil",
 			expectedError: nil,
-			malleate: func(statedbAccount *statedb.Account, args *evmtypes.EvmTxArgs) {
+			malleate: func(statedbAccount *statedb.Account, _ *evmtypes.EvmTxArgs) {
 				statedbAccount = nil
 			},
 		}, {
 			name:          "success: tx is succesfull if account is EOA and exists",
 			expectedError: nil,
-			malleate: func(statedbAccount *statedb.Account, args *evmtypes.EvmTxArgs) {
-				sender := keyring.GetKey(SENDER_KEYRING_KEY)
+			malleate: func(statedbAccount *statedb.Account, _ *evmtypes.EvmTxArgs) {
 				// Make sure the account has no code hash
 				statedbAccount.CodeHash = evmtypes.EmptyCodeHash
-				err := unitNetwork.App.EvmKeeper.SetAccount(unitNetwork.GetContext(), sender.Addr, *statedbAccount)
+				err := unitNetwork.App.EvmKeeper.SetAccount(unitNetwork.GetContext(), senderKey.Addr, *statedbAccount)
 				suite.Require().NoError(err)
 			},
 		},
@@ -93,10 +89,9 @@ func (suite *EvmAnteTestSuite) TestVerifyAccountBalance() {
 	for _, tc := range testCases {
 		suite.Run(fmt.Sprintf("%v_%v", suite.getTxTypeTestName(), tc.name), func() {
 			// Variable data
-			sender := keyring.GetKey(SENDER_KEYRING_KEY)
 			statedb := unitNetwork.GetStateDB()
-			statedbAccount := statedb.Keeper().GetAccount(unitNetwork.GetContext(), sender.Addr)
-			txArgs, err := txFactory.GenerateDefaultTxTypeArgs(sender.Addr, suite.ethTxType)
+			statedbAccount := statedb.Keeper().GetAccount(unitNetwork.GetContext(), senderKey.Addr)
+			txArgs, err := txFactory.GenerateDefaultTxTypeArgs(senderKey.Addr, suite.ethTxType)
 			suite.Require().NoError(err)
 
 			// Perform test logic
@@ -110,7 +105,7 @@ func (suite *EvmAnteTestSuite) TestVerifyAccountBalance() {
 				unitNetwork.GetContext(),
 				unitNetwork.App.AccountKeeper,
 				statedbAccount,
-				keyring.GetAddr(SENDER_KEYRING_KEY),
+				senderKey.Addr,
 				txData,
 			)
 
@@ -121,7 +116,7 @@ func (suite *EvmAnteTestSuite) TestVerifyAccountBalance() {
 				suite.Require().NoError(err)
 
 				// Make sure the account is created
-				acc, err := grpcHandler.GetAccount(sender.AccAddr.String())
+				acc, err := grpcHandler.GetAccount(senderKey.AccAddr.String())
 				suite.Require().NoError(err)
 				suite.Require().NotEmpty(acc)
 			}
