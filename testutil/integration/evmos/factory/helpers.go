bc66981d6c4ad9dbf1e48b0ef2882285f9afe800
Freddy Caceres <facs95@gmail.com>
2023-12-07 15:32:53 +0800

    fix lint

--- testutil/integration/evmos/factory/helpers.go
+++ testutil/integration/evmos/factory/helpers.go
@@ -6,20 +6,16 @@ import (
 	"strings"
 
 	"github.com/ethereum/go-ethereum/common"
-	"github.com/evmos/evmos/v16/precompiles/testutil"
 	evmtypes "github.com/evmos/evmos/v16/x/evm/types"
 
 	errorsmod "cosmossdk.io/errors"
 	amino "github.com/cosmos/cosmos-sdk/codec"
 	codectypes "github.com/cosmos/cosmos-sdk/codec/types"
-	cryptotypes "github.com/cosmos/cosmos-sdk/crypto/types"
 	"github.com/cosmos/cosmos-sdk/types/module"
 	testutiltypes "github.com/cosmos/cosmos-sdk/types/module/testutil"
 	authtx "github.com/cosmos/cosmos-sdk/x/auth/tx"
-	ethtypes "github.com/ethereum/go-ethereum/core/types"
 	enccodec "github.com/evmos/evmos/v16/encoding/codec"
-	"github.com/evmos/evmos/v16/testutil/tx"
-	evmostypes "github.com/evmos/evmos/v16/types"
+	"github.com/evmos/evmos/v16/precompiles/testutil"
 )
 
 // buildMsgEthereumTx builds an Ethereum transaction from the given arguments and populates the From field.
@@ -27,26 +23,6 @@ func buildMsgEthereumTx(txArgs evmtypes.EvmTxArgs, fromAddr common.Address) evmt
 	msgEthereumTx := evmtypes.NewTx(&txArgs)
 	msgEthereumTx.From = fromAddr.String()
 	return *msgEthereumTx
-}
-
-// signMsgEthereumTx signs a MsgEthereumTx with the provided private key and chainID.
-func signMsgEthereumTx(msgEthereumTx evmtypes.MsgEthereumTx, privKey cryptotypes.PrivKey, chainID string) (evmtypes.MsgEthereumTx, error) {
-	ethChainID, err := evmostypes.ParseChainID(chainID)
-	if err != nil {
-		return evmtypes.MsgEthereumTx{}, errorsmod.Wrapf(err, "failed to parse chainID: %v", chainID)
-	}
-
-	signer := ethtypes.LatestSignerForChainID(ethChainID)
-	err = msgEthereumTx.Sign(signer, tx.NewSigner(privKey))
-	if err != nil {
-		return evmtypes.MsgEthereumTx{}, errorsmod.Wrap(err, "failed to sign transaction")
-	}
-
-	// Validate the transaction to avoid unrealistic behavior
-	if err = msgEthereumTx.ValidateBasic(); err != nil {
-		return evmtypes.MsgEthereumTx{}, errorsmod.Wrap(err, "failed to validate transaction")
-	}
-	return msgEthereumTx, nil
 }
 
 // makeConfig creates an EncodingConfig for testing
