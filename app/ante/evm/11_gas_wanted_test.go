55081ea68beb22db94928c87a9b038faf0c1ba2b
Freddy Caceres <facs95@gmail.com>
2023-12-12 16:35:39 +0800

    imp(ante): Remove legacy GasWanted decorator and refractor tests

---
+++ app/ante/evm/11_gas_wanted_test.go
@@ -0,0 +1,90 @@
+// Copyright Tharsis Labs Ltd.(Evmos)
+// SPDX-License-Identifier:ENCL-1.0(https://github.com/evmos/evmos/blob/main/LICENSE)
+package evm_test
+
+import (
+	// errortypes "github.com/cosmos/cosmos-sdk/types/errors"
+	sdktypes "github.com/cosmos/cosmos-sdk/types"
+	"github.com/evmos/evmos/v16/app/ante/evm"
+	"github.com/evmos/evmos/v16/testutil/integration/evmos/factory"
+	"github.com/evmos/evmos/v16/testutil/integration/evmos/grpc"
+	testkeyring "github.com/evmos/evmos/v16/testutil/integration/evmos/keyring"
+	"github.com/evmos/evmos/v16/testutil/integration/evmos/network"
+)
+
+func (suite *EvmAnteTestSuite) TestCheckGasWanted() {
+	keyring := testkeyring.New(1)
+	unitNetwork := network.NewUnitTestNetwork(
+		network.WithPreFundedAccounts(keyring.GetAllAccAddrs()...),
+	)
+	grpcHandler := grpc.NewIntegrationHandler(unitNetwork)
+	txFactory := factory.New(unitNetwork, grpcHandler)
+	commonGasLimit := uint64(100000)
+
+	testCases := []struct {
+		name                       string
+		expectedError              error
+		getCtx                     func() sdktypes.Context
+		isLondon                   bool
+		expectedTransientGasWanted uint64
+	}{
+		{
+			name:          "success: if isLondon false it should not error",
+			expectedError: nil,
+			getCtx: func() sdktypes.Context {
+				return unitNetwork.GetContext()
+			},
+			isLondon:                   false,
+			expectedTransientGasWanted: 0,
+		},
+		{
+			name:          "success: gasWanted is less than blockGasLimit",
+			expectedError: nil,
+			getCtx: func() sdktypes.Context {
+				blockMeter := sdktypes.NewGasMeter(commonGasLimit + 10000)
+				return unitNetwork.GetContext().WithBlockGasMeter(blockMeter)
+			},
+			isLondon:                   false,
+			expectedTransientGasWanted: commonGasLimit,
+		},
+	}
+
+	for _, tc := range testCases {
+		suite.Run(tc.name, func() {
+			sender := keyring.GetKey(0)
+			txArgs, err := txFactory.GenerateDefaultTxTypeArgs(
+				sender.Addr,
+				suite.ethTxType,
+			)
+			suite.Require().NoError(err)
+			txArgs.GasLimit = commonGasLimit
+			tx, err := txFactory.GenerateSignedEthTx(sender.Priv, txArgs)
+			suite.Require().NoError(err)
+
+			ctx := tc.getCtx()
+
+			// Function under test
+			err = evm.CheckGasWanted(
+				ctx,
+				unitNetwork.App.FeeMarketKeeper,
+				tx,
+				tc.isLondon,
+			)
+
+			if tc.expectedError != nil {
+				suite.Require().Error(err)
+				suite.Contains(err.Error(), tc.expectedError.Error())
+			} else {
+				suite.Require().NoError(err)
+				transientGasWanted := unitNetwork.App.FeeMarketKeeper.GetTransientGasWanted(
+					ctx,
+				)
+				suite.Require().Equal(tc.expectedTransientGasWanted, transientGasWanted)
+			}
+
+			// Start from a fresh block and ctx
+			err = unitNetwork.NextBlock()
+			suite.Require().NoError(err)
+		})
+	}
+}
