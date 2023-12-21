002ac770a813563d3223d24f1f61e5ca7abe2e01
MalteHerrmann <malte@evmos.org>
2023-12-21 18:06:11 +0100

    add WEVMOS contract address as constants in ERC20 module

---
+++ x/erc20/types/constants.go
@@ -0,0 +1,8 @@
+package types
+
+const (
+	// WEVMOSContractMainnet is the address of the WEVMOS contract on mainnet.
+	WEVMOSContractMainnet = "0xD4949664cD82660AaE99bEdc034a0deA8A0bd517"
+	// WEVMOSContractTestnet is the address of the WEVMOS contract on testnet.
+	WEVMOSContractTestnet = "0xcc491f589b45d4a3c679016195b3fb87d7848210"
+)
