5981aa744ec33622608d170e431192628b18f49f
lukema95 <867273263@qq.com>
2024-01-02 22:21:49 +0800

    remove ValidateBasic in erc20 cli

--- x/erc20/client/cli/tx.go
+++ x/erc20/client/cli/tx.go
@@ -192,10 +192,6 @@ func NewToggleTokenConversionProposalCmd() *cobra.Command {
 				return err
 			}
 
-			if err := msg.ValidateBasic(); err != nil {
-				return err
-			}
-
 			return tx.GenerateOrBroadcastTxCLI(clientCtx, cmd.Flags(), msg)
 		},
 	}
