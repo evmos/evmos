927a368d1eb70b6c3884f47163187de044f80fb7
MalteHerrmann <malte@evmos.org>
2024-01-03 18:34:28 +0100

    Revert "adjust upgrade name"
    This reverts commit 6a11af4817f86914baadf27adf7e8f5fe6c4799b.

--- app/upgrades/v16/constants.go
+++ app/upgrades/v16/constants.go
@@ -5,7 +5,7 @@ package v16
 
 const (
 	// UpgradeName is the shared upgrade plan name for mainnet
-	UpgradeName = "v16.0.0-rc5"
+	UpgradeName = "v16.0.0"
 	// UpgradeNameTestnetRC2 is the shared upgrade plan name for testnet rc2 upgrade
 	UpgradeNameTestnetRC2 = "v16.0.0-rc2"
 	// UpgradeNameTestnetRC3 is the shared upgrade plan name for testnet rc3 patch upgrade
@@ -17,5 +17,5 @@ const (
 	// UpgradeNameTestnetRC5 is the shared upgrade plan name for testnet rc5 upgrade
 	UpgradeNameTestnetRC5 = "v16.0.0-rc5"
 	// UpgradeInfo defines the binaries that will be used for the upgrade
-	UpgradeInfo = `'{"binaries":{"darwin/amd64":"https://github.com/evmos/evmos/releases/download/v16.0.0-rc5/evmos_16.0.0-rc5_Darwin_arm64.tar.gz","darwin/x86_64":"https://github.com/evmos/evmos/releases/download/v16.0.0-rc5/evmos_16.0.0-rc5_Darwin_x86_64.tar.gz","linux/arm64":"https://github.com/evmos/evmos/releases/download/v16.0.0-rc5/evmos_16.0.0-rc5_Linux_arm64.tar.gz","linux/amd64":"https://github.com/evmos/evmos/releases/download/v16.0.0-rc5/evmos_16.0.0-rc5_Linux_amd64.tar.gz","windows/x86_64":"https://github.com/evmos/evmos/releases/download/v16.0.0-rc5/evmos_16.0.0-rc5_Windows_x86_64.zip"}}'`
+	UpgradeInfo = `'{"binaries":{"darwin/amd64":"https://github.com/evmos/evmos/releases/download/v16.0.0-rc4/evmos_16.0.0-rc4_Darwin_arm64.tar.gz","darwin/x86_64":"https://github.com/evmos/evmos/releases/download/v16.0.0-rc4/evmos_16.0.0-rc4_Darwin_x86_64.tar.gz","linux/arm64":"https://github.com/evmos/evmos/releases/download/v16.0.0-rc4/evmos_16.0.0-rc4_Linux_arm64.tar.gz","linux/amd64":"https://github.com/evmos/evmos/releases/download/v16.0.0-rc4/evmos_16.0.0-rc4_Linux_amd64.tar.gz","windows/x86_64":"https://github.com/evmos/evmos/releases/download/v16.0.0-rc4/evmos_16.0.0-rc4_Windows_x86_64.zip"}}'`
 )
