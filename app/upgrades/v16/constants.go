6a11af4817f86914baadf27adf7e8f5fe6c4799b
MalteHerrmann <malte@evmos.org>
2024-01-03 18:16:22 +0100

    adjust upgrade name

--- app/upgrades/v16/constants.go
+++ app/upgrades/v16/constants.go
@@ -5,7 +5,7 @@ package v16
 
 const (
 	// UpgradeName is the shared upgrade plan name for mainnet
-	UpgradeName = "v16.0.0"
+	UpgradeName = "v16.0.0-rc5"
 	// UpgradeNameTestnetRC2 is the shared upgrade plan name for testnet rc2 upgrade
 	UpgradeNameTestnetRC2 = "v16.0.0-rc2"
 	// UpgradeNameTestnetRC3 is the shared upgrade plan name for testnet rc3 patch upgrade
@@ -15,5 +15,5 @@ const (
 	// TestnetUpgradeHeight defines the Evmos testnet block height on which the rc3 upgrade will take place
 	TestnetUpgradeHeight = 19465900
 	// UpgradeInfo defines the binaries that will be used for the upgrade
-	UpgradeInfo = `'{"binaries":{"darwin/amd64":"https://github.com/evmos/evmos/releases/download/v16.0.0-rc4/evmos_16.0.0-rc4_Darwin_arm64.tar.gz","darwin/x86_64":"https://github.com/evmos/evmos/releases/download/v16.0.0-rc4/evmos_16.0.0-rc4_Darwin_x86_64.tar.gz","linux/arm64":"https://github.com/evmos/evmos/releases/download/v16.0.0-rc4/evmos_16.0.0-rc4_Linux_arm64.tar.gz","linux/amd64":"https://github.com/evmos/evmos/releases/download/v16.0.0-rc4/evmos_16.0.0-rc4_Linux_amd64.tar.gz","windows/x86_64":"https://github.com/evmos/evmos/releases/download/v16.0.0-rc4/evmos_16.0.0-rc4_Windows_x86_64.zip"}}'`
+	UpgradeInfo = `'{"binaries":{"darwin/amd64":"https://github.com/evmos/evmos/releases/download/v16.0.0-rc5/evmos_16.0.0-rc5_Darwin_arm64.tar.gz","darwin/x86_64":"https://github.com/evmos/evmos/releases/download/v16.0.0-rc5/evmos_16.0.0-rc5_Darwin_x86_64.tar.gz","linux/arm64":"https://github.com/evmos/evmos/releases/download/v16.0.0-rc5/evmos_16.0.0-rc5_Linux_arm64.tar.gz","linux/amd64":"https://github.com/evmos/evmos/releases/download/v16.0.0-rc5/evmos_16.0.0-rc5_Linux_amd64.tar.gz","windows/x86_64":"https://github.com/evmos/evmos/releases/download/v16.0.0-rc5/evmos_16.0.0-rc5_Windows_x86_64.zip"}}'`
 )
