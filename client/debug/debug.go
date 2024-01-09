eeda5235d53cecbb4dc4cc73e80debc9089d3c58
Luke <luchenqun@qq.com>
2024-01-09 20:16:40 +0800

    imp(debug): update convert an address between hex and bech32

--- client/debug/debug.go
+++ client/debug/debug.go
@@ -10,11 +10,11 @@ import (
 	"strings"
 
 	authclient "github.com/cosmos/cosmos-sdk/x/auth/client"
+	cfg "github.com/evmos/evmos/v16/cmd/config"
 	"github.com/evmos/evmos/v16/ethereum/eip712"
 	evmos "github.com/evmos/evmos/v16/types"
 	"github.com/pkg/errors"
 
-	"github.com/cometbft/cometbft/libs/bytes"
 	"github.com/spf13/cobra"
 
 	"github.com/ethereum/go-ethereum/common"
@@ -23,6 +23,10 @@ import (
 	cryptotypes "github.com/cosmos/cosmos-sdk/crypto/types"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/cosmos/cosmos-sdk/version"
+)
+
+var (
+	flagPrefix = "prefix"
 )
 
 // Cmd creates a main CLI command
@@ -75,38 +79,38 @@ func PubkeyCmd() *cobra.Command {
 }
 
 func AddrCmd() *cobra.Command {
-	return &cobra.Command{
+	cmd := &cobra.Command{
 		Use:   "addr [address]",
 		Short: "Convert an address between hex and bech32",
 		Long:  "Convert an address between hex encoding and bech32.",
 		Example: fmt.Sprintf(
-			`$ %s debug addr ethm10jmp6sgh4cc6zt3e8gw05wavvejgr5pw2unfju
-$ %s debug addr 0xA588C66983a81e800Db4dF74564F09f91c026351`, version.AppName, version.AppName),
+			`$ %s debug addr evmos1qqqqhe5pnaq5qq39wqkn957aydnrm45sdn8583
+$ %s debug addr 0x00000Be6819f41400225702D32d3dd23663Dd690 --prefix evmos`, version.AppName, version.AppName),
 		Args: cobra.ExactArgs(1),
 		RunE: func(cmd *cobra.Command, args []string) error {
 			addrString := args[0]
-			cfg := sdk.GetConfig()
-
-			var addr []byte
 			switch {
 			case common.IsHexAddress(addrString):
-				addr = common.HexToAddress(addrString).Bytes()
-			case strings.HasPrefix(addrString, cfg.GetBech32ValidatorAddrPrefix()):
-				addr, _ = sdk.ValAddressFromBech32(addrString)
-			case strings.HasPrefix(addrString, cfg.GetBech32AccountAddrPrefix()):
-				addr, _ = sdk.AccAddressFromBech32(addrString)
-			default:
-				return fmt.Errorf("expected a valid hex or bech32 address (acc prefix %s), got '%s'", cfg.GetBech32AccountAddrPrefix(), addrString)
-			}
+				hexAddr := common.HexToAddress(addrString).Bytes()
+				prefix, _ := cmd.Flags().GetString(flagPrefix)
+				bech32Address, _ := sdk.Bech32ifyAddressBytes(prefix, hexAddr)
 
-			cmd.Println("Address bytes:", addr)
-			cmd.Printf("Address (hex): %s\n", bytes.HexBytes(addr))
-			cmd.Printf("Address (EIP-55): %s\n", common.BytesToAddress(addr))
-			cmd.Printf("Bech32 Acc: %s\n", sdk.AccAddress(addr))
-			cmd.Printf("Bech32 Val: %s\n", sdk.ValAddress(addr))
+				cmd.Println("Address bytes:", hexAddr)
+				cmd.Printf("Bech32 %s\n", bech32Address)
+			default:
+				prefix := strings.SplitN(addrString, "1", 2)[0]
+				hexAddr, _ := sdk.GetFromBech32(addrString, prefix)
+				hexAddrString := common.BytesToAddress(hexAddr).String()
+
+				cmd.Println("Address bytes:", hexAddr)
+				cmd.Printf("Address hex: %s\n", hexAddrString)
+			}
 			return nil
 		},
 	}
+
+	cmd.Flags().String(flagPrefix, cfg.Bech32Prefix, "Bech32 encoded account prefix, for example evmos, evmosvaloper")
+	return cmd
 }
 
 func RawBytesCmd() *cobra.Command {
