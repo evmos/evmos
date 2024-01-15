bf6b806d46fb49cedde88c9a49cad5e60abda07e
MalteHerrmann <malte@evmos.org>
2024-01-15 17:17:18 +0100

    commit refactor WIP

--- scripts/check_changelog.py
+++ scripts/check_changelog.py
@@ -5,12 +5,12 @@ from typing import Dict, List, Tuple
 
 # Allowed entry pattern: `- (module) [#PR](link) - description`
 ENTRY_PATTERN = re.compile(
-    r'^-\s+\([a-zA-Z0-9\-]+\) \[\\?#(?P<pr>\d+)\]\((?P<link>.+)\) (?P<desc>.+)$',
+    r'^-\s+\([a-zA-Z0-9\-]+\) \[\\?#(?P<pr>\d+)]\((?P<link>.+)\) (?P<desc>.+)$',
 )
 
 # Allowed release pattern: vX.Y.Z(-rcN) (YYYY-MM-DD)
 RELEASE_PATTERN = re.compile(
-    r'^## (Unreleased|\[(?P<version>v\d+\.\d+\.\d+(-rc\d+)?)\] - \d{4}-\d{2}-\d{2})$',
+    r'^## (Unreleased|\[(?P<version>v\d+\.\d+\.\d+(-rc\d+)?)] - \d{4}-\d{2}-\d{2})$',
 )
 
 ALLOWED_CATEGORIES = [
@@ -21,67 +21,127 @@ ALLOWED_CATEGORIES = [
 ]
 
 
-def parse_changelog(file_path) -> Tuple[Dict[str, Dict[str, Dict[int, str]]], List[str]]:
+class Changelog:
     """
-    This function parses the changelog and checks if the structure is as expected.
+    This class represents the contents of the changelog and provides methods to parse it.
     """
 
-    with open(file_path, 'r') as file:
-        content = file.read()
+    def __init__(self, filename: str):
+        self.contents: List[str]
+        self.filename: str = filename
 
-    releases = {}
-    failed_entries = []
-    current_release = None
-    current_category = None
+        self.failed_entries: List[str] = []
+        # TODO: extract releases type
+        self.releases: Dict[str, Dict[str, Dict[int, Dict[str, str]]]] = {}
 
-    for line in content.split('\n'):
-        # Check for Header 2 (##) to identify releases
-        stripped_line = line.strip()
-        if stripped_line [:3] == '## ':
-            release_match = RELEASE_PATTERN.match(line)
-            if not release_match:
-                raise ValueError("Header 2 should be used for releases - invalid release pattern: " + line)
+        with open(self.filename, 'r') as file:
+            self.contents = file.read()
 
-            current_release = release_match.group("version") if release_match.group("version") is not None else "Unreleased"
-            releases[current_release] = {}
-            continue
+    def parse(self) -> bool:
+        """
+        This function parses the changelog and checks if the structure is as expected.
+        """
 
-        # Check for Header 3 (###) to identify categories
-        category_match = re.match(r'^###\s+(.+)$', line)
-        if category_match:
-            current_category = category_match.group(1)
-            if current_category not in ALLOWED_CATEGORIES:
-                failed_entries.append(f'Invalid change category in {current_release}: "{current_category}"')
-            releases[current_release][current_category] = {}
-            continue
+        current_release = None
+        current_category = None
 
-        # Check for individual entries
-        if stripped_line[:2] != '- ':
-            continue
+        for line in self.contents.split('\n'):
+            # Check for Header 2 (##) to identify releases
+            stripped_line = line.strip()
+            if stripped_line[:3] == '## ':
+                release_match = RELEASE_PATTERN.match(line)
+                if not release_match:
+                    raise ValueError("Header 2 should be used for releases - invalid release pattern: " + line)
 
-        entry_match = ENTRY_PATTERN.match(line)
-        if not entry_match:
-            failed_entries.append(f'Invalid entry in {current_release} - {current_category}: "{line}"')
-            continue
+                version_match = release_match.group("version")
+                current_release = version_match if version_match is not None else "Unreleased"
+                self.releases[current_release] = {}
+                continue
 
-        pr_number = entry_match.group("pr")
-        pr_link = entry_match.group("link")
-        pr_description = entry_match.group("desc")
+            # Check for Header 3 (###) to identify categories
+            category_match = re.match(r'^###\s+(.+)$', line)
+            if category_match:
+                current_category = category_match.group(1)
+                if current_category not in ALLOWED_CATEGORIES:
+                    self.failed_entries.append(f'Invalid change category in {current_release}: "{current_category}"')
+                self.releases[current_release][current_category] = {}
+                continue
 
-        if pr_number not in pr_link:
-            failed_entries.append(f'PR link is not matching PR number in {current_release} - {current_category}: "{line}"')
+            # Check for individual entries
+            if stripped_line[:2] != '- ':
+                continue
 
-        if not pr_description[0].isupper():
-            failed_entries.append(f'PR description should start with capital letter in {current_release} - {current_category}: "{line}"')
+            entry_match = ENTRY_PATTERN.match(line)
+            if not entry_match:
+                self.failed_entries.append(f'Invalid entry in {current_release} - {current_category}: "{line}"')
+                continue
 
-        if pr_description[-1] != '.':
-            failed_entries.append(f'PR description should end with a dot in {current_release} - {current_category}: "{line}"')
+            pr_number = entry_match.group("pr")
+            pr_link = entry_match.group("link")
+            pr_description = entry_match.group("desc")
 
-        releases[current_release][current_category][int(pr_number)] = {
-            'description': pr_description
-        }
+            if pr_number not in pr_link:
+                self.failed_entries.append(
+                    f'PR link is not matching PR number in {current_release} - {current_category}: "{line}"'
+                )
 
-    return releases, failed_entries
+            if not pr_description[0].isupper():
+                self.failed_entries.append(
+                    f'PR description should start with capital letter in {current_release} - {current_category}: "{line}"'
+                )
+
+            if pr_description[-1] != '.':
+                self.failed_entries.append(
+                    f'PR description should end with a dot in {current_release} - {current_category}: "{line}"'
+                )
+
+            self.releases[current_release][current_category][int(pr_number)] = {
+                "description": pr_description
+            }
+
+        return self.failed_entries == []
+
+
+class Release:
+    """
+    This class represents a release in the changelog.
+    """
+
+    def __init__(self, contents: List[str]):
+        self.contents = contents
+        self.version = None
+        self.date = None
+        self.entries: List[Entry] = []
+
+
+class Entry:
+    """
+    This class represents an individual changelog entry that is describing the changes on one specific PR.
+    """
+
+    def __init__(self, line: str):
+        self.line: str = line
+        self.pr_number: int = 0
+        self.category: str = ""
+        self.description: str = ""
+
+    def parse(self) -> tuple[bool, str]:
+        """
+        Parses a changelog entry from a line of text.
+
+        :param line: the line of text to parse
+        :return: a tuple indicating whether the parsing was successful and an error message in case of failure
+        """
+
+        match = ENTRY_PATTERN.match(self.line)
+        if not match:
+            return False, f'Invalid entry: "{self.line}"'
+
+        self.pr_number = int(match.group("pr"))
+        self.category = match.group("category")
+        self.description = match.group("desc")
+
+        return True, ""
 
 
 if __name__ == "__main__":
@@ -90,8 +150,9 @@ if __name__ == "__main__":
         print('Changelog file not found')
         sys.exit(1)
 
-    _, fails = parse_changelog(changelog_file_path)
-    if len(fails) > 0:
-        print(f'Changelog file is not valid - check the following {len(fails)} problems:\n')
-        print('\n'.join(fails))
+    changelog = Changelog(sys.argv[1])
+    failed = changelog.parse()
+    if failed:
+        print(f'Changelog file is not valid - check the following {len(changelog.failed_entries)} problems:\n')
+        print('\n'.join(changelog.failed_entries))
         sys.exit(1)
--- scripts/test_check_changelog.py
+++ scripts/test_check_changelog.py
@@ -1,6 +1,6 @@
 import os
 import pytest
-from check_changelog import parse_changelog
+from check_changelog import Changelog, Entry
 
 # Get the directory of this script
 SCRIPT_DIR = os.path.dirname(os.path.realpath(__file__))
@@ -41,68 +41,95 @@ class TestParseChangelog:
             },
         }
 
-        releases, failed_entries = parse_changelog(os.path.join(SCRIPT_DIR, "testdata", "changelog_ok.md"))
-        assert failed_entries == [], "expected no failed entries"
-        assert releases == expected_result, "expected different parsed result"
+        changelog = Changelog(os.path.join(SCRIPT_DIR, "testdata", "changelog_ok.md"))
+        ok = changelog.parse()
+        assert ok
+        assert changelog.failed_entries == [], "expected no failed entries"
+        assert changelog.releases == expected_result, "expected different parsed result"
 
-    def test_parse_changelog_invalid_pr_link(self):
-        expected_result = {
-            'Unreleased': {
-                'State Machine Breaking': {
-                    1948: {'description': 'Add `ClaimRewards` custom transaction.'},
-                },
-            },
-        }
-        releases, failed_entries = parse_changelog(
-            os.path.join(SCRIPT_DIR, "testdata", "changelog_invalid_entry_pr_not_in_link.md"),
+    # TODO: uncomment when done with refactor
+    # def test_parse_changelog_invalid_pr_link(self):
+    #     expected_result = {
+    #         'Unreleased': {
+    #             'State Machine Breaking': {
+    #                 1948: {'description': 'Add `ClaimRewards` custom transaction.'},
+    #             },
+    #         },
+    #     }
+    #     changelog = Changelog(os.path.join(SCRIPT_DIR, "testdata", "changelog_invalid_entry_pr_not_in_link.md"))
+    #     ok = changelog.parse()
+    #     assert not ok
+    #     assert changelog.failed_entries == [
+    #         'PR link is not matching PR number in Unreleased - State Machine Breaking: "- (distribution-precompile)' +
+    #         ' [#1948](https://github.com/evmos/evmos/pull/1949) Add `ClaimRewards` custom transaction."',
+    #     ]
+    #     assert changelog.releases == expected_result
+    #
+    # def test_parse_changelog_malformed_description(self):
+    #     expected_result = {
+    #        'Unreleased': {
+    #            'State Machine Breaking': {
+    #                1949: {'description': 'add `ClaimRewards` custom transaction'},
+    #            },
+    #        },
+    #     }
+    #     releases, failed_entries = parse_changelog(
+    #         os.path.join(SCRIPT_DIR, "testdata", "changelog_invalid_entry_misformatted_description.md"),
+    #     )
+    #     assert failed_entries == [
+    #         'PR description should start with capital letter in Unreleased - State Machine Breaking: "- (distribution' +
+    #         '-precompile) [#1949](https://github.com/evmos/evmos/pull/1949) add `ClaimRewards` custom transaction"',
+    #         'PR description should end with a dot in Unreleased - State Machine Breaking: "- (distribution' +
+    #         '-precompile) [#1949](https://github.com/evmos/evmos/pull/1949) add `ClaimRewards` custom transaction"',
+    #     ]
+    #     assert releases == expected_result
+    #
+    # def test_parse_changelog_invalid_category(self):
+    #     expected_result = {
+    #        'Unreleased': {
+    #            'Invalid Category': {
+    #                1949: {'description': 'Add `ClaimRewards` custom transaction.'},
+    #            },
+    #        },
+    #     }
+    #     releases, failed_entries = parse_changelog(
+    #         os.path.join(SCRIPT_DIR, "testdata", "changelog_invalid_category.md")
+    #     )
+    #     assert failed_entries == ["Invalid change category in Unreleased: \"Invalid Category\""]
+    #     assert releases == expected_result
+    #
+    # def test_parse_changelog_invalid_header(self):
+    #     with pytest.raises(ValueError):
+    #         parse_changelog(os.path.join(SCRIPT_DIR, "testdata", "changelog_invalid_version.md"))
+    #
+    # def test_parse_changelog_invalid_date(self):
+    #     with pytest.raises(ValueError):
+    #         parse_changelog(os.path.join(SCRIPT_DIR, "testdata", "changelog_invalid_date.md"))
+    #
+    # def test_parse_changelog_nonexistent_file(self):
+    #     with pytest.raises(FileNotFoundError):
+    #         parse_changelog("nonexistent_file.md")
+
+
+class TestEntry:
+    """
+    This class collects all tests that are checking individual changelog entries.
+    """
+
+    def test_entry_ok(self):
+        entry = Entry(
+            "- (distribution-precompile) [#1949](https://github.com/evmos/evmos/pull/1949)" +
+            "Add `ClaimRewards` custom transaction."
         )
-        assert failed_entries == [
-            'PR link is not matching PR number in Unreleased - State Machine Breaking: "- (distribution-precompile)' +
-            ' [#1948](https://github.com/evmos/evmos/pull/1949) Add `ClaimRewards` custom transaction."',
-        ]
-        assert releases == expected_result
+        ok, reason = entry.parse()
+        assert ok
+        assert reason == ""
 
-    def test_parse_changelog_malformed_description(self):
-        expected_result = {
-           'Unreleased': {
-               'State Machine Breaking': {
-                   1949: {'description': 'add `ClaimRewards` custom transaction'},
-               },
-           },
-        }
-        releases, failed_entries = parse_changelog(
-            os.path.join(SCRIPT_DIR, "testdata", "changelog_invalid_entry_misformatted_description.md"),
+    def test_entry_wrong_pr_link(self):
+        entry = Entry(
+            "- (distribution-precompile) [#1949](https://github.com/evmos/evmos/pull/1948)" +
+            "Add `ClaimRewards` custom transaction."
         )
-        assert failed_entries == [
-            'PR description should start with capital letter in Unreleased - State Machine Breaking: "- (distribution' +
-            '-precompile) [#1949](https://github.com/evmos/evmos/pull/1949) add `ClaimRewards` custom transaction"',
-            'PR description should end with a dot in Unreleased - State Machine Breaking: "- (distribution' +
-            '-precompile) [#1949](https://github.com/evmos/evmos/pull/1949) add `ClaimRewards` custom transaction"',
-        ]
-        assert releases == expected_result
-
-    def test_parse_changelog_invalid_category(self):
-        expected_result = {
-           'Unreleased': {
-               'Invalid Category': {
-                   1949: {'description': 'Add `ClaimRewards` custom transaction.'},
-               },
-           },
-        }
-        releases, failed_entries = parse_changelog(
-            os.path.join(SCRIPT_DIR, "testdata", "changelog_invalid_category.md")
-        )
-        assert failed_entries == ["Invalid change category in Unreleased: \"Invalid Category\""]
-        assert releases == expected_result
-
-    def test_parse_changelog_invalid_header(self):
-        with pytest.raises(ValueError):
-            parse_changelog(os.path.join(SCRIPT_DIR, "testdata", "changelog_invalid_version.md"))
-
-    def test_parse_changelog_invalid_date(self):
-        with pytest.raises(ValueError):
-            parse_changelog(os.path.join(SCRIPT_DIR, "testdata", "changelog_invalid_date.md"))
-
-    def test_parse_changelog_nonexistent_file(self):
-        with pytest.raises(FileNotFoundError):
-            parse_changelog("nonexistent_file.md")
+        ok, reason = entry.parse()
+        assert not ok
+        assert reason == "PR link is not matching PR number"
