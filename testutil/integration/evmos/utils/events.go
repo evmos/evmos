12e466470327b6193bd60d88223df453c9039ff9
Freddy Caceres <facs95@gmail.com>
2024-01-18 12:05:09 -0500

    add events utils

---
+++ testutil/integration/evmos/utils/events.go
@@ -0,0 +1,14 @@
+package utils
+
+import sdktypes "github.com/cosmos/cosmos-sdk/types"
+
+// ContainsEventType returns true if the given events contain the given eventType
+func ContainsEventType(events sdktypes.Events, eventType string) bool {
+	for _, event := range events {
+		if event.Type == eventType {
+			return true
+		}
+	}
+	return false
+}
+
