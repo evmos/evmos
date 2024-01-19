011439f1a3c447fa6b9cde3ef62885e2caf5f3c7
MalteHerrmann <malte@evmos.org>
2024-01-19 15:53:09 +0100

    address more linters

--- scripts/changelog_checker/test_check_changelog.py
+++ scripts/changelog_checker/test_check_changelog.py
@@ -52,8 +52,7 @@ class TestParseChangelog:
                     },
                     1912: {"description": "Add Stride outpost interface and ABI."},
                     2104: {
-                        "description":
-                            "Refactor code to use `sdkmath.Int` and `sdkmath.LegacyDec` instead of the SDK types."
+                        "description": "Refactor to use `sdkmath.Int` and `sdkmath.LegacyDec` instead of SDK types."
                     },
                     701: {"description": "Rename Go module to `evmos/evmos`."},
                 },
