3e7d870ef243b25644dcaade0a401183a2fea2a9
MalteHerrmann <malte@evmos.org>
2024-01-24 12:36:54 +0100

    add utility to check balances to testutils

---
+++ testutil/integration/evmos/utils/bank.go
@@ -0,0 +1,31 @@
+package utils
+
+import (
+	"fmt"
+
+	banktypes "github.com/cosmos/cosmos-sdk/x/bank/types"
+	"github.com/evmos/evmos/v16/testutil/integration/common/grpc"
+)
+
+// CheckBalances checks that the given accounts have the expected balances and
+// returns an error if that is not the case.
+func CheckBalances(handler grpc.Handler, balances []banktypes.Balance) error {
+	for _, balance := range balances {
+		addr := balance.GetAddress()
+		for _, coin := range balance.GetCoins() {
+			balance, err := handler.GetBalance(addr, coin.Denom)
+			if err != nil {
+				return err
+			}
+
+			if !balance.Balance.IsEqual(coin) {
+				return fmt.Errorf(
+					"expected balance %s, got %s for address %s",
+					coin, balance.Balance, addr,
+				)
+			}
+		}
+	}
+
+	return nil
+}
---
+++ testutil/integration/evmos/utils/bank_test.go
@@ -0,0 +1,65 @@
+package utils_test
+
+import (
+	"testing"
+
+	"cosmossdk.io/math"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	banktypes "github.com/cosmos/cosmos-sdk/x/bank/types"
+	grpchandler "github.com/evmos/evmos/v16/testutil/integration/evmos/grpc"
+	testkeyring "github.com/evmos/evmos/v16/testutil/integration/evmos/keyring"
+	"github.com/evmos/evmos/v16/testutil/integration/evmos/network"
+	"github.com/evmos/evmos/v16/testutil/integration/evmos/utils"
+	"github.com/stretchr/testify/require"
+)
+
+func TestCheckBalances(t *testing.T) {
+	testDenom := "atest"
+	keyring := testkeyring.New(1)
+	nw := network.New(
+		network.WithDenom(testDenom),
+		network.WithPreFundedAccounts(keyring.GetAllAccAddrs()...),
+	)
+	handler := grpchandler.NewIntegrationHandler(nw)
+
+	testcases := []struct {
+		name        string
+		address     string
+		expAmount   math.Int
+		expPass     bool
+		errContains string
+	}{
+		{
+			name:      "pass",
+			address:   keyring.GetAccAddr(0).String(),
+			expAmount: network.PrefundedAccountInitialBalance,
+			expPass:   true,
+		},
+		{
+			name:        "fail - wrong amount",
+			address:     keyring.GetAccAddr(0).String(),
+			expAmount:   sdk.NewInt(1),
+			errContains: "expected balance",
+		},
+	}
+
+	for _, tc := range testcases {
+		tc := tc
+		t.Run(tc.name, func(t *testing.T) {
+			balances := []banktypes.Balance{{
+				Address: tc.address,
+				Coins: sdk.NewCoins(
+					sdk.NewCoin(testDenom, tc.expAmount),
+				),
+			}}
+
+			err := utils.CheckBalances(handler, balances)
+			if tc.expPass {
+				require.NoError(t, err, "unexpected error checking balances")
+			} else {
+				require.Error(t, err, "expected error checking balances")
+				require.ErrorContains(t, err, tc.errContains, "expected different error checking balances")
+			}
+		})
+	}
+}
