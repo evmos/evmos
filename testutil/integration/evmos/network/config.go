936c43af65e7b61bfc1a4c2f90f8bf926041031f
Freddy Caceres <facs95@gmail.com>
2024-01-27 04:02:56 -0500

    imp(test): Add custom genesis config functionality to integration setup (#2296)
    * first working version
    
    * remove unnecessary genesisState
    
    * update changelog
    
    * fix changelog
    
    ---------
    
    Co-authored-by: Tom <54514587+GAtom22@users.noreply.github.com>

--- testutil/integration/evmos/network/config.go
+++ testutil/integration/evmos/network/config.go
@@ -1,15 +1,17 @@
 // Copyright Tharsis Labs Ltd.(Evmos)
 // SPDX-License-Identifier:ENCL-1.0(https://github.com/evmos/evmos/blob/main/LICENSE)
+
 package network
 
 import (
 	"math/big"
 
-	testtx "github.com/evmos/evmos/v16/testutil/tx"
-	"github.com/evmos/evmos/v16/utils"
-
 	sdktypes "github.com/cosmos/cosmos-sdk/types"
+	authtypes "github.com/cosmos/cosmos-sdk/x/auth/types"
+	banktypes "github.com/cosmos/cosmos-sdk/x/bank/types"
+	testtx "github.com/evmos/evmos/v16/testutil/tx"
 	evmostypes "github.com/evmos/evmos/v16/types"
+	"github.com/evmos/evmos/v16/utils"
 )
 
 // Config defines the configuration for a chain.
@@ -20,8 +22,12 @@ type Config struct {
 	eip155ChainID      *big.Int
 	amountOfValidators int
 	preFundedAccounts  []sdktypes.AccAddress
+	balances           []banktypes.Balance
 	denom              string
+	customGenesisState CustomGenesisState
 }
+
+type CustomGenesisState map[string]interface{}
 
 // DefaultConfig returns the default configuration for a chain.
 func DefaultConfig() Config {
@@ -32,8 +38,29 @@ func DefaultConfig() Config {
 		amountOfValidators: 3,
 		// No funded accounts besides the validators by default
 		preFundedAccounts: []sdktypes.AccAddress{account},
-		denom:             utils.BaseDenom,
+		// NOTE: Per default, the balances are left empty, and the pre-funded accounts are used.
+		balances:           nil,
+		denom:              utils.BaseDenom,
+		customGenesisState: nil,
 	}
+}
+
+// getGenAccountsAndBalances takes the network configuration and returns the used
+// genesis accounts and balances.
+//
+// NOTE: If the balances are set, the pre-funded accounts are ignored.
+func getGenAccountsAndBalances(cfg Config) (genAccounts []authtypes.GenesisAccount, balances []banktypes.Balance) {
+	if len(cfg.balances) > 0 {
+		balances = cfg.balances
+		accounts := getAccAddrsFromBalances(balances)
+		genAccounts = createGenesisAccounts(accounts)
+	} else {
+		coin := sdktypes.NewCoin(cfg.denom, PrefundedAccountInitialBalance)
+		genAccounts = createGenesisAccounts(cfg.preFundedAccounts)
+		balances = createBalances(cfg.preFundedAccounts, coin)
+	}
+
+	return
 }
 
 // ConfigOption defines a function that can modify the NetworkConfig.
@@ -67,9 +94,24 @@ func WithPreFundedAccounts(accounts ...sdktypes.AccAddress) ConfigOption {
 	}
 }
 
+// WithBalances sets the specific balances for the pre-funded accounts, that
+// are being set up for the network.
+func WithBalances(balances ...banktypes.Balance) ConfigOption {
+	return func(cfg *Config) {
+		cfg.balances = append(cfg.balances, balances...)
+	}
+}
+
 // WithDenom sets the denom for the network.
 func WithDenom(denom string) ConfigOption {
 	return func(cfg *Config) {
 		cfg.denom = denom
+	}
+}
+
+// WithCustomGenesis sets the custom genesis of the network for specific modules.
+func WithCustomGenesis(customGenesis CustomGenesisState) ConfigOption {
+	return func(cfg *Config) {
+		cfg.customGenesisState = customGenesis
 	}
 }
