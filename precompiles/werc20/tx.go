a31b8dd990b306f06ebe6fa1eee5fe7abebff2d8
MalteHerrmann <malte@evmos.org>
2024-02-12 22:27:49 +0100

    revert adding args to deposit and withdraw functions

--- precompiles/werc20/tx.go
+++ precompiles/werc20/tx.go
@@ -3,12 +3,6 @@
 
 package werc20
 
-import (
-	sdk "github.com/cosmos/cosmos-sdk/types"
-	"github.com/ethereum/go-ethereum/accounts/abi"
-	"github.com/ethereum/go-ethereum/core/vm"
-)
-
 const (
 	// DepositMethod defines the ABI method name for the IWERC20 deposit
 	// transaction.
@@ -21,25 +15,13 @@ const (
 // Deposit is a no-op and mock function that provides the same interface as the
 // WETH contract to support equality between the native coin and its wrapped
 // ERC-20 (e.g. EVMOS and WEVMOS). It only emits the Deposit event.
-func (p Precompile) Deposit(
-	_ sdk.Context,
-	_ *vm.Contract,
-	_ vm.StateDB,
-	_ *abi.Method,
-	_ []interface{},
-) ([]byte, error) {
+func (p Precompile) Deposit() ([]byte, error) {
 	return nil, nil
 }
 
 // Withdraw is a no-op and mock function that provides the same interface as the
 // WETH contract to support equality between the native coin and its wrapped
 // ERC-20 (e.g. EVMOS and WEVMOS). It only emits the Withdraw event.
-func (p Precompile) Withdraw(
-	_ sdk.Context,
-	_ *vm.Contract,
-	_ vm.StateDB,
-	_ *abi.Method,
-	_ []interface{},
-) ([]byte, error) {
+func (p Precompile) Withdraw() ([]byte, error) {
 	return nil, nil
 }
