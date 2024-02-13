c21017f0785d6ec382595f63ce1d44a2df022d42
tom <tomasguerraalda@hotmail.com>
2024-02-13 17:40:05 -0300

    fix lint issue

--- testutil/staking-rewards.go
+++ testutil/staking-rewards.go
@@ -136,7 +136,9 @@ func PrepareAccountsForDelegationRewards(t *testing.T, ctx sdk.Context, app *app
 			return sdk.Context{}, fmt.Errorf("failed to get validator: %s", err.Error())
 		}
 		allocatedRewards := sdk.NewDecCoins(sdk.NewDecCoin(utils.BaseDenom, reward.Mul(math.NewInt(2))))
-		app.DistrKeeper.AllocateTokensToValidator(ctx, validator, allocatedRewards)
+		if err = app.DistrKeeper.AllocateTokensToValidator(ctx, validator, allocatedRewards); err != nil {
+			return sdk.Context{}, fmt.Errorf("failed to allocate tokens to validator: %s", err.Error())
+		}
 	}
 
 	// Increase block height in ctx for the rewards calculation
