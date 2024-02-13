978d1d52c7154b11e95f314874e12b736cd9a08b
Vlad <vladislav.varadinov@gmail.com>
2024-02-13 10:28:20 +0200

    fix: error code

--- x/erc20/types/errors.go
+++ x/erc20/types/errors.go
@@ -12,7 +12,6 @@ var (
 	ErrERC20Disabled          = errorsmod.Register(ModuleName, 2, "erc20 module is disabled")
 	ErrInternalTokenPair      = errorsmod.Register(ModuleName, 3, "internal ethereum token mapping error")
 	ErrTokenPairNotFound      = errorsmod.Register(ModuleName, 4, "token pair not found")
-	ErrTokenPairOwnedByModule = errorsmod.Register(ModuleName, 5, "token pair owned by module")
 	ErrTokenPairAlreadyExists = errorsmod.Register(ModuleName, 5, "token pair already exists")
 	ErrUndefinedOwner         = errorsmod.Register(ModuleName, 6, "undefined owner of contract pair")
 	ErrBalanceInvariance      = errorsmod.Register(ModuleName, 7, "post transfer balance invariant failed")
@@ -23,4 +22,5 @@ var (
 	ErrEVMCall                = errorsmod.Register(ModuleName, 12, "EVM call unexpected error")
 	ErrERC20TokenPairDisabled = errorsmod.Register(ModuleName, 13, "erc20 token pair is disabled")
 	ErrInvalidIBC             = errorsmod.Register(ModuleName, 14, "invalid IBC transaction")
+	ErrTokenPairOwnedByModule = errorsmod.Register(ModuleName, 15, "token pair owned by module")
 )
