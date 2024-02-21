ffa6a6a43a8aaf89d21fc774173b218fe15454d9
Freddy Caceres <facs95@gmail.com>
2024-02-21 10:32:04 -0500

    fix review comments

--- x/evm/ante/ctx_test.go
+++ x/evm/ante/ctx_test.go
@@ -1,19 +1,16 @@
 // Copyright Tharsis Labs Ltd.(Evmos)
 // SPDX-License-Identifier:ENCL-1.0(https://github.com/evmos/evmos/blob/main/LICENSE)
+
 package ante_test
 
 import (
 	storetypes "github.com/cosmos/cosmos-sdk/store/types"
-	testkeyring "github.com/evmos/evmos/v16/testutil/integration/evmos/keyring"
 	"github.com/evmos/evmos/v16/testutil/integration/evmos/network"
 	evmante "github.com/evmos/evmos/v16/x/evm/ante"
 )
 
 func (suite *EvmAnteTestSuite) TestBuildEvmExecutionCtx() {
-	keyring := testkeyring.New(1)
-	network := network.New(
-		network.WithPreFundedAccounts(keyring.GetAllAccAddrs()...),
-	)
+	network := network.New()
 
 	ctx := evmante.BuildEvmExecutionCtx(network.GetContext())
 
