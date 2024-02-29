9755115ba330b0e402d972adfaed37c1f38fd116
stepit <stefanofrancesco.pitton@gmail.com>
2024-02-29 18:13:44 +0100

    remove references to evm hook and update tests

--- x/evm/types/interfaces.go
+++ x/evm/types/interfaces.go
@@ -11,8 +11,6 @@ import (
 	authtypes "github.com/cosmos/cosmos-sdk/x/auth/types"
 	stakingtypes "github.com/cosmos/cosmos-sdk/x/staking/types"
 
-	"github.com/ethereum/go-ethereum/core"
-	ethtypes "github.com/ethereum/go-ethereum/core/types"
 	feemarkettypes "github.com/evmos/evmos/v16/x/feemarket/types"
 )
 
@@ -50,15 +48,6 @@ type FeeMarketKeeper interface {
 	GetParams(ctx sdk.Context) feemarkettypes.Params
 	AddTransientGasWanted(ctx sdk.Context, gasWanted uint64) (uint64, error)
 	CalculateBaseFee(ctx sdk.Context) *big.Int
-}
-
-// Event Hooks
-// These can be utilized to customize evm transaction processing.
-
-// EvmHooks event hooks for evm tx processing
-type EvmHooks interface {
-	// Must be called after tx is processed successfully, if return an error, the whole transaction is reverted.
-	PostTxProcessing(ctx sdk.Context, msg core.Message, receipt *ethtypes.Receipt) error
 }
 
 type (
