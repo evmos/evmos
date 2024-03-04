c410c6ea44fb93aac7eb864888db12499e0c360b
tom <tomasguerraalda@hotmail.com>
2024-03-04 20:13:02 -0300

    fix height error for evmos chain on ibc setup

--- testutil/integration/evmos/network/ibc.go
+++ testutil/integration/evmos/network/ibc.go
@@ -12,12 +12,14 @@ import (
 // Note: the sender accounts are not populated. Do not use this accounts to send transactions during tests.
 // The keyring should be used instead.
 func (n *IntegrationNetwork) GetIBCChain(t *testing.T, coord *ibctesting.Coordinator) *ibctesting.TestChain {
+	// current header should have height = LastBlockHeight + 1
+	currentHeader := n.ctx.WithBlockHeight(n.app.LastBlockHeight() + 1).BlockHeader()
 	return &ibctesting.TestChain{
 		TB:            t,
 		Coordinator:   coord,
 		ChainID:       n.GetChainID(),
 		App:           n.app,
-		CurrentHeader: n.ctx.BlockHeader(),
+		CurrentHeader: currentHeader,
 		QueryServer:   n.app.GetIBCKeeper(),
 		TxConfig:      n.app.GetTxConfig(),
 		Codec:         n.app.AppCodec(),
