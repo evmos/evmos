78cf4ce25feed4223eecc864ca6bcad139a75b19
tom <tomasguerraalda@hotmail.com>
2024-03-06 10:59:11 -0300

    fix abci tests

--- x/feemarket/keeper/abci_test.go
+++ x/feemarket/keeper/abci_test.go
@@ -1,12 +1,20 @@
 package keeper_test
 
 import (
-	"fmt"
+	"testing"
 
 	storetypes "cosmossdk.io/store/types"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/evmos/evmos/v16/testutil/integration/evmos/network"
+	"github.com/stretchr/testify/require"
 )
 
-func (suite *KeeperTestSuite) TestEndBlock() {
+func TestEndBlock(t *testing.T) {
+	var (
+		nw  *network.UnitTestNetwork
+		ctx sdk.Context
+	)
+
 	testCases := []struct {
 		name         string
 		NoBaseFee    bool
@@ -24,25 +32,31 @@ func (suite *KeeperTestSuite) TestEndBlock() {
 			false,
 			func() {
 				meter := storetypes.NewGasMeter(uint64(1000000000))
-				suite.ctx = suite.ctx.WithBlockGasMeter(meter)
-				suite.app.FeeMarketKeeper.SetTransientBlockGasWanted(suite.ctx, 5000000)
+				ctx = ctx.WithBlockGasMeter(meter)
+				nw.App.FeeMarketKeeper.SetTransientBlockGasWanted(ctx, 5000000)
 			},
 			uint64(2500000),
 		},
 	}
 	for _, tc := range testCases {
-		suite.Run(fmt.Sprintf("Case %s", tc.name), func() {
-			suite.SetupTest() // reset
-			params := suite.app.FeeMarketKeeper.GetParams(suite.ctx)
+		t.Run(tc.name, func(t *testing.T) {
+			// reset network and context
+			nw = network.NewUnitTestNetwork()
+			ctx = nw.GetContext()
+	
+			params := nw.App.FeeMarketKeeper.GetParams(ctx)
 			params.NoBaseFee = tc.NoBaseFee
-			err := suite.app.FeeMarketKeeper.SetParams(suite.ctx, params)
-			suite.Require().NoError(err)
-
+	
+			err := nw.App.FeeMarketKeeper.SetParams(ctx, params)
+			require.NoError(t, err)
+	
 			tc.malleate()
-			err = suite.app.FeeMarketKeeper.EndBlock(suite.ctx)
-			suite.Require().NoError(err)
-			gasWanted := suite.app.FeeMarketKeeper.GetBlockGasWanted(suite.ctx)
-			suite.Require().Equal(tc.expGasWanted, gasWanted, tc.name)
+	
+			err = nw.App.FeeMarketKeeper.EndBlock(ctx)
+			require.NoError(t, err)
+	
+			gasWanted := nw.App.FeeMarketKeeper.GetBlockGasWanted(ctx)
+			require.Equal(t, tc.expGasWanted, gasWanted, tc.name)
 		})
 	}
 }
