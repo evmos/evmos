7249181acf71e107758fc0b3d6cc74b17a2a6114
tom <tomasguerraalda@hotmail.com>
2024-03-07 10:30:10 -0300

    migrate gen tests

--- x/inflation/v1/keeper/genesis_test.go
+++ x/inflation/v1/keeper/genesis_test.go
@@ -2,12 +2,15 @@ package keeper_test
 
 import (
 	"cosmossdk.io/math"
+	"github.com/evmos/evmos/v16/testutil/integration/evmos/network"
 	inflationkeeper "github.com/evmos/evmos/v16/x/inflation/v1/keeper"
 )
 
 func (suite *KeeperTestSuite) TestInitGenesis() {
+	nw := network.NewUnitTestNetwork()
+	ctx := nw.GetContext()
 	// check calculated epochMintProvision at genesis
-	epochMintProvision := suite.app.InflationKeeper.GetEpochMintProvision(suite.ctx)
+	epochMintProvision := nw.App.InflationKeeper.GetEpochMintProvision(ctx)
 	expMintProvision := math.LegacyMustNewDecFromStr("847602739726027397260274.000000000000000000").Quo(math.LegacyNewDec(inflationkeeper.ReductionFactor))
 	suite.Require().Equal(expMintProvision, epochMintProvision)
 }
