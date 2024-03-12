df818800683dc368877a5720c44a9df951915d69
MalteHerrmann <malte@evmos.org>
2024-03-12 22:43:10 +0100

    adjust setup to include IBC coins on genesis

--- x/erc20/keeper/strv2_integration_utils_test.go
+++ x/erc20/keeper/strv2_integration_utils_test.go
@@ -5,19 +5,40 @@ package keeper_test
 
 import (
 	errorsmod "cosmossdk.io/errors"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	banktypes "github.com/cosmos/cosmos-sdk/x/bank/types"
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/evmos/evmos/v16/contracts"
 	testfactory "github.com/evmos/evmos/v16/testutil/integration/evmos/factory"
 	"github.com/evmos/evmos/v16/testutil/integration/evmos/grpc"
 	testkeyring "github.com/evmos/evmos/v16/testutil/integration/evmos/keyring"
 	testnetwork "github.com/evmos/evmos/v16/testutil/integration/evmos/network"
+	"github.com/evmos/evmos/v16/utils"
 	evmtypes "github.com/evmos/evmos/v16/x/evm/types"
 )
 
-func SetupTest() *STRv2TrackingSuite {
-	keyring := testkeyring.New(2)
+func SetupTestWithIBCCoinsInGenesis() *STRv2TrackingSuite {
+	keyring := testkeyring.New(3)
+
+	genesisBalances := []banktypes.Balance{
+		{
+			Address: keyring.GetAccAddr(0).String(),
+			Coins: sdk.NewCoins(
+				sdk.Coin{Denom: utils.BaseDenom, Amount: testnetwork.PrefundedAccountInitialBalance},
+				sdk.Coin{Denom: nativeIBCCoinDenom, Amount: testnetwork.PrefundedAccountInitialBalance},
+			),
+		},
+		{
+			Address: keyring.GetAccAddr(1).String(),
+			Coins: sdk.NewCoins(
+				sdk.Coin{Denom: utils.BaseDenom, Amount: testnetwork.PrefundedAccountInitialBalance},
+				sdk.Coin{Denom: nativeIBCCoinDenom, Amount: testnetwork.PrefundedAccountInitialBalance.QuoRaw(2)},
+			),
+		},
+	}
+
 	network := testnetwork.NewUnitTestNetwork(
-		testnetwork.WithPreFundedAccounts(keyring.GetAllAccAddrs()...),
+		testnetwork.WithBalances(genesisBalances...),
 	)
 	handler := grpc.NewIntegrationHandler(network)
 	factory := testfactory.New(network, handler)
