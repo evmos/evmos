5bf006129ee0e96e3ebdcc3f0672140c1e43239e
one230six <723682061@qq.com>
2024-03-13 16:00:22 +0800

    chore: remove repetitive words
    Signed-off-by: one230six <723682061@qq.com>

--- x/erc20/keeper/ibc_callbacks.go
+++ x/erc20/keeper/ibc_callbacks.go
@@ -132,7 +132,7 @@ func (k Keeper) OnRecvPacket(
 	return ack
 }
 
-// OnAcknowledgementPacket responds to the the success or failure of a packet
+// OnAcknowledgementPacket responds to the success or failure of a packet
 // acknowledgement written on the receiving chain. If the acknowledgement was a
 // success then nothing occurs. If the acknowledgement failed, then the sender
 // is refunded and then the IBC Coins are converted to ERC20.
