79b7e76f847e7a9f0a9ded63b1b733f5a7935928
MalteHerrmann <malte@evmos.org>
2024-03-19 10:07:49 +0100

    address linter

--- local_node.sh
+++ local_node.sh
@@ -109,10 +109,6 @@ if [[ $overwrite == "y" || $overwrite == "Y" ]]; then
 
 	# Import keys from mnemonics
 	echo "$VAL_MNEMONIC" | evmosd keys add "$VAL_KEY" --recover --keyring-backend "$KEYRING" --algo "$KEYALGO" --home "$HOMEDIR"
-
-	# Store the validator address in a variable to use it later
-	node_address=$(evmosd keys show -a "$VAL_KEY" --keyring-backend "$KEYRING" --home "$HOMEDIR")
-
 	echo "$USER1_MNEMONIC" | evmosd keys add "$USER1_KEY" --recover --keyring-backend "$KEYRING" --algo "$KEYALGO" --home "$HOMEDIR"
 	echo "$USER2_MNEMONIC" | evmosd keys add "$USER2_KEY" --recover --keyring-backend "$KEYRING" --algo "$KEYALGO" --home "$HOMEDIR"
 	echo "$USER3_MNEMONIC" | evmosd keys add "$USER3_KEY" --recover --keyring-backend "$KEYRING" --algo "$KEYALGO" --home "$HOMEDIR"
