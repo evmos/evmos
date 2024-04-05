2fe345040cb4989cd05a270d653dcb630ad6942b
Ramiro Carlucho <ramirocarlucho@gmail.com>
2024-04-05 16:32:05 -0300

    Update release branch with the latest version v16.0.4 (#2466)
    * Prepare for v16.0.4 (#2460)
    
    * initial upgrade logic
    
    * gofumpt
    
    * Apply suggestions from code review
    
    Co-authored-by: stepit <48993133+0xstepit@users.noreply.github.com>
    
    * run make format
    
    * delete unused logger
    
    ---------
    
    Co-authored-by: stepit <48993133+0xstepit@users.noreply.github.com>
    Co-authored-by: ramacarlucho <ramacarlucho@users.noreply.github.com>
    
    * chore(docker): update docker deps (#2463)
    
    * update docker deps
    
    * ignore link in failing ci
    
    * use fixed golangci-lint version for v17
    
    ---------
    
    Co-authored-by: stepit <48993133+0xstepit@users.noreply.github.com>
    Co-authored-by: ramacarlucho <ramacarlucho@users.noreply.github.com>
    Co-authored-by: Tom <54514587+GAtom22@users.noreply.github.com>
    Co-authored-by: tom <tomasguerraalda@hotmail.com>

--- app/app.go
+++ app/app.go
@@ -893,9 +893,8 @@ func (app *Evmos) setPostHandler() {
 // BeginBlocker will schedule the upgrade plan and perform the state migration (if any).
 func (app *Evmos) BeginBlocker(ctx sdk.Context, req abci.RequestBeginBlock) abci.ResponseBeginBlock {
 	// Perform any scheduled forks before executing the modules logic
-	//
-	// NOTE: there are no scheduled forks currently, uncomment when needed
-	// app.ScheduleForkUpgrade(ctx)
+
+	app.ScheduleForkUpgrade(ctx)
 
 	return app.mm.BeginBlock(ctx, req)
 }
