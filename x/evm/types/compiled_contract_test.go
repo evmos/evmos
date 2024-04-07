17efe4ee15a47d4fbd09db5a3c5ff5f1718826b6
MalteHerrmann <malte@evmos.org>
2024-04-08 00:45:58 +0200

    add hardhat compiled contract type and corresponding test - move contracts into testdata subfolder

---
+++ x/evm/types/compiled_contract_test.go
@@ -0,0 +1,31 @@
+package types
+
+import (
+	"encoding/json"
+	"os"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestHardhatCompiledContract(t *testing.T) {
+	contents, err := os.ReadFile("testdata/SimpleContractHardhat.json")
+	require.NoError(t, err, "failed to read file")
+	require.NotEmpty(t, contents, "expected contents not to be empty")
+
+	var hardhatContract HardhatCompiledContract
+	err = json.Unmarshal(contents, &hardhatContract)
+	require.NoError(t, err, "failed to unmarshal contract")
+
+	require.Equal(t, hardhatContract.ContractName, "SimpleContract")
+	require.Contains(t,
+		hardhatContract.ABI.Methods,
+		"setValue",
+		"missing setValue method in contract ABI methods",
+	)
+
+	compiledContract, err := hardhatContract.ToCompiledContract()
+	require.NoError(t, err, "failed to convert hardhat contract to compiled contract type")
+	require.Equal(t, compiledContract.ABI, hardhatContract.ABI, "expected ABIs to be equal")
+	require.NotEmpty(t, compiledContract.Bin, "expected bin data  not to be empty")
+}
