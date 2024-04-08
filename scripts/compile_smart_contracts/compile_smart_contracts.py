9f2051e31fd560c15ba839cebaf872ef284743bc
MalteHerrmann <malte@evmos.org>
2024-04-08 18:23:42 +0200

    commit wip scanning the repo for Solidity files

--- scripts/compile_smart_contracts/compile_smart_contracts.py
+++ scripts/compile_smart_contracts/compile_smart_contracts.py
@@ -9,22 +9,71 @@ Usage:
 
 import os
 import re
+from dataclasses import dataclass
 from pathlib import Path
 from typing import List
 
 
-def find_solidity_files(path: Path) -> List[Path]:
+# This list contains all files that should be ignored when scanning the repository
+# for Solidity files.
+IGNORED_FILES: List[str] = [
+    "ERC20Minter_OpenZeppelinV5.sol", # Ignored because it uses a different OpenZeppelin contracts version to compile
+]
+
+
+# This list contains all folders that should be ignored when scanning the repository
+# for Solidity files.
+IGNORED_FOLDERS: List[str] = [
+    "scripts",
+]
+
+
+@dataclass
+class Contract:
+    """
+    Dataclass to store the name and path of a Solidity contract
+    as well as the path to where the compiled JSON data is stored.
+    """
+
+    filename: str
+    path: Path
+    compiledJSONPath: Path
+
+
+def find_solidity_contracts(path: Path) -> List[Contract]:
     """
     Finds all Solidity files in the given Path.
     """
 
-    solidity_files: List[Path] = []
+    solidity_files: List[Contract] = []
 
     for root, _, files in os.walk(path):
-        print("root: ", root)
+        if is_ignored_folder(root):
+            continue
+
         for file in files:
-            print(file)
             if re.search(r"(?!\.dbg)\.sol$", file):
-                solidity_files.append(Path(root) / file)
+                filename = os.path.splitext(file)[0]
+                compiledJSONPath = os.path.join(root, f"{filename}.json")
+                if not os.path.exists(compiledJSONPath):
+                    # TODO: collect failed compilations
+                    print("failed to find compiled JSON file for contract: ", file)
+                    continue
+
+                solidity_files.append(
+                    Contract(
+                        filename=file,
+                        path=Path(root),
+                        compiledJSONPath=Path(root) / f"{file}.json"
+                    )
+                )
 
     return solidity_files
+
+
+def is_ignored_folder(path: str) -> bool:
+    """
+    Check if the folder is in the list of ignored folders.
+    """
+
+    return any([re.search(folder, path) for folder in IGNORED_FOLDERS])
--- scripts/compile_smart_contracts/test_compile_smart_contracts.py
+++ scripts/compile_smart_contracts/test_compile_smart_contracts.py
@@ -9,18 +9,29 @@ def setup_contracts_folder(tmp_path):
     """
 
     (tmp_path / "Contract1.sol").touch()
+    (tmp_path / "Contract1.json").touch()
     (tmp_path / "Contract2.sol").touch()
+    # NOTE: we're not adding the JSON file for Contract2
 
     (tmp_path / "contracts").mkdir()
     (tmp_path / "contracts" / "Contract3.sol").touch()
+    (tmp_path / "contracts" / "Contract3.json").touch()
 
     (tmp_path / "precompiles").mkdir()
     (tmp_path / "precompiles" / "Contract4.sol").touch()
+    (tmp_path / "precompiles" / "Contract4.json").touch()
 
     return tmp_path
 
 
 def test_find_solidity_files(setup_contracts_folder):
     tmp_path = setup_contracts_folder
-    found_solidity_files = find_solidity_files(tmp_path)
-    assert len(found_solidity_files) == 4
+    found_solidity_contracts = find_solidity_contracts(tmp_path)
+    assert len(found_solidity_contracts) == 3
+
+    assert found_solidity_contracts[0].filename == "Contract1.sol"
+    assert found_solidity_contracts[0].path == tmp_path
+    assert found_solidity_contracts[0].compiledJSONPath == tmp_path / "Contract1.json"
+    assert found_solidity_contracts[1].filename == "Contract2"
+    assert found_solidity_contracts[2].filename == "Contract3"
+    assert found_solidity_contracts[3].filename == "Contract4"
