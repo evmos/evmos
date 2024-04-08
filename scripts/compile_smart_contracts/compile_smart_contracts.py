291012abb5eeeba61cfa440d939af538a2f12d64
MalteHerrmann <malte@evmos.org>
2024-04-09 00:08:13 +0200

    wip

--- scripts/compile_smart_contracts/compile_smart_contracts.py
+++ scripts/compile_smart_contracts/compile_smart_contracts.py
@@ -17,13 +17,16 @@ from typing import List
 # This list contains all files that should be ignored when scanning the repository
 # for Solidity files.
 IGNORED_FILES: List[str] = [
-    "ERC20Minter_OpenZeppelinV5.sol", # Ignored because it uses a different OpenZeppelin contracts version to compile
+    # Ignored because it uses a different OpenZeppelin contracts version to compile
+    "ERC20Minter_OpenZeppelinV5.sol",
 ]
 
 
 # This list contains all folders that should be ignored when scanning the repository
 # for Solidity files.
 IGNORED_FOLDERS: List[str] = [
+    "nix_tests",
+    "node_modules",
     "scripts",
 ]
 
@@ -37,6 +40,7 @@ class Contract:
 
     filename: str
     path: Path
+    # TODO: Maybe this can also be removed again
     compiledJSONPath: Path
 
 
@@ -52,6 +56,9 @@ def find_solidity_contracts(path: Path) -> List[Contract]:
             continue
 
         for file in files:
+            if file in IGNORED_FILES:
+                continue
+
             if re.search(r"(?!\.dbg)\.sol$", file):
                 filename = os.path.splitext(file)[0]
                 compiledJSONPath = os.path.join(root, f"{filename}.json")
@@ -64,7 +71,7 @@ def find_solidity_contracts(path: Path) -> List[Contract]:
                     Contract(
                         filename=file,
                         path=Path(root),
-                        compiledJSONPath=Path(root) / f"{file}.json"
+                        compiledJSONPath=Path(root) / f"{filename}.json"
                     )
                 )
 
@@ -77,3 +84,22 @@ def is_ignored_folder(path: str) -> bool:
     """
 
     return any([re.search(folder, path) for folder in IGNORED_FOLDERS])
+
+
+def copy_to_contracts_directory(
+    contracts_dir: Path,
+    contracts: List[Contract]
+) -> bool:
+    """
+    This function copies the list of Contracts found in the repository
+    to the `contracts` subdirectory, which is necessary to compile them
+    with Hardhat.
+    """
+
+    if not os.path.isdir(contracts_dir) or not os.path.exists(contracts_dir):
+        return False
+
+    for contract in contracts:
+        print("copying file to contracts directory")
+
+    return True
--- scripts/compile_smart_contracts/test_compile_smart_contracts.py
+++ scripts/compile_smart_contracts/test_compile_smart_contracts.py
@@ -1,9 +1,11 @@
-from compile_smart_contracts import *
+import os
 import pytest
+from compile_smart_contracts import *
+from pathlib import Path
 
 
 @pytest.fixture
-def setup_contracts_folder(tmp_path):
+def setup_example_contracts_files(tmp_path):
     """
     This fixture creates a temporary folder with some Solidity files.
     """
@@ -24,14 +26,53 @@ def setup_contracts_folder(tmp_path):
     return tmp_path
 
 
-def test_find_solidity_files(setup_contracts_folder):
-    tmp_path = setup_contracts_folder
+def test_find_solidity_files(setup_example_contracts_files):
+    tmp_path = setup_example_contracts_files
     found_solidity_contracts = find_solidity_contracts(tmp_path)
     assert len(found_solidity_contracts) == 3
 
     assert found_solidity_contracts[0].filename == "Contract1.sol"
     assert found_solidity_contracts[0].path == tmp_path
-    assert found_solidity_contracts[0].compiledJSONPath == tmp_path / "Contract1.json"
-    assert found_solidity_contracts[1].filename == "Contract2"
-    assert found_solidity_contracts[2].filename == "Contract3"
-    assert found_solidity_contracts[3].filename == "Contract4"
+    assert found_solidity_contracts[0].compiledJSONPath == Path(
+        tmp_path / "Contract1.json"
+    )
+
+    assert found_solidity_contracts[1].filename == "Contract4.sol"
+    assert found_solidity_contracts[1].path == Path(tmp_path / "precompiles")
+    assert found_solidity_contracts[1].compiledJSONPath == Path(
+        tmp_path / "precompiles" / "Contract4.json"
+    )
+
+    assert found_solidity_contracts[2].filename == "Contract3.sol"
+    assert found_solidity_contracts[2].path == Path(tmp_path / "contracts")
+    assert found_solidity_contracts[2].compiledJSONPath == Path(
+        tmp_path / "contracts" / "Contract3.json"
+    )
+
+
+@pytest.fixture
+def setup_contracts_directory(tmp_path):
+    """
+    This fixture creates the target contracts folder,
+    where any found smart contracts should be copied to
+    in order to be compiled with Hardhat.
+    """
+
+    # TODO: This could actually be removed if nothing is added
+    # except using tmp_path
+    return tmp_path
+
+
+def test_copy_to_contracts_directory(
+    setup_contracts_directory
+):
+    target = setup_contracts_directory
+    contracts = find_solidity_contracts(os.getcwd())
+
+    assert len(os.listdir(target)) == 0
+    assert copy_to_contracts_directory(
+        target,
+        contracts
+    ) is True
+    # TODO: have better assertion
+    assert len(os.listdir(target)) > 0
