f95a20204f95ffeac42a8bcf58e2463b63918a8a
MalteHerrmann <malte@evmos.org>
2024-04-15 19:29:04 +0200

    move compiled JSON files next to Solidity files

--- scripts/compile_smart_contracts/compile_smart_contracts.py
+++ scripts/compile_smart_contracts/compile_smart_contracts.py
@@ -7,11 +7,19 @@ Usage:
 
 """
 
+from shutil import copy
 import os
 import re
 from dataclasses import dataclass
 from pathlib import Path
 from typing import List
+
+# The path to the main level of the Evmos repository.
+EVMOS_REPO = Path(__file__).parent.parent.parent
+
+
+# This is the main target directory inside of the contracts folder.
+MAIN_CONTRACTS_TARGET = EVMOS_REPO / "contracts" / "contracts"
 
 
 # This list contains all files that should be ignored when scanning the repository
@@ -25,9 +33,11 @@ IGNORED_FILES: List[str] = [
 # This list contains all folders that should be ignored when scanning the repository
 # for Solidity files.
 IGNORED_FOLDERS: List[str] = [
+    "contracts/contracts", # Ignored because the files are already in the correct folder
     "nix_tests",
     "node_modules",
     "scripts",
+    "tests/solidity",
 ]
 
 
@@ -40,6 +50,7 @@ class Contract:
 
     filename: str
     path: Path
+    relative_path: Path
     # TODO: Maybe this can also be removed again
     compiledJSONPath: Path
 
@@ -55,8 +66,11 @@ def find_solidity_contracts(path: Path) -> List[Contract]:
         if is_ignored_folder(root):
             continue
 
+        relative_path = Path(root).relative_to(path)
+
         for file in files:
             if file in IGNORED_FILES:
+                print(f"Ignoring file: {file}")
                 continue
 
             if re.search(r"(?!\.dbg)\.sol$", file):
@@ -70,7 +84,8 @@ def find_solidity_contracts(path: Path) -> List[Contract]:
                 solidity_files.append(
                     Contract(
                         filename=file,
-                        path=Path(root),
+                        path=Path(os.path.join(root, file)),
+                        relative_path=relative_path,
                         compiledJSONPath=Path(root) / f"{filename}.json"
                     )
                 )
@@ -87,19 +102,54 @@ def is_ignored_folder(path: str) -> bool:
 
 
 def copy_to_contracts_directory(
-    contracts_dir: Path,
+    target_dir: Path,
     contracts: List[Contract]
 ) -> bool:
     """
     This function copies the list of Contracts found in the repository
-    to the `contracts` subdirectory, which is necessary to compile them
-    with Hardhat.
+    to the target directory.
+
+    In the context of the fully-functional tool, this is necessary to compile
+    them with Hardhat, which relies on the Solidity files to be nested within
+    the `contracts` directory.
     """
 
-    if not os.path.isdir(contracts_dir) or not os.path.exists(contracts_dir):
+    if not os.path.isdir(target_dir) or not os.path.exists(target_dir):
         return False
 
     for contract in contracts:
-        print("copying file to contracts directory")
+        sub_dir = target_dir / contract.relative_path
+        sub_dir.mkdir(parents=True, exist_ok=True) # if sub dir already exists this is skipped when using exist_ok=True
+        copy(contract.path, sub_dir)
+
+        print(f"copying {contract.path} to contracts directory  relative path: {contract.relative_path}")
 
     return True
+
+def is_evmos_repo(path: Path) -> bool:
+    """
+    This function checks if the given path is the root of the Evmos repository,
+    where this script is designed to be executed.
+    """
+
+    print("Path: ", path)
+    contents = os.listdir(path)
+
+    if not "go.mod" in contents:
+        return False
+
+    with open(path / "go.mod", "r") as go_mod:
+        for line in go_mod.readlines():
+            if "module github.com/evmos/evmos" in line:
+                return True
+
+    return False
+
+
+if __name__ == "__main__":
+    dir_to_execute = Path(__file__).parent.parent.parent
+    if not is_evmos_repo(dir_to_execute):
+        raise ValueError("This script should only be executed in the evmos repository.")
+
+    found_contracts = find_solidity_contracts(dir_to_execute)
+    copy_to_contracts_directory(dir_to_execute / "contracts", found_contracts)
--- scripts/compile_smart_contracts/test_compile_smart_contracts.py
+++ scripts/compile_smart_contracts/test_compile_smart_contracts.py
@@ -32,19 +32,21 @@ def test_find_solidity_files(setup_example_contracts_files):
     assert len(found_solidity_contracts) == 3
 
     assert found_solidity_contracts[0].filename == "Contract1.sol"
-    assert found_solidity_contracts[0].path == tmp_path
+    assert found_solidity_contracts[0].path == tmp_path / "Contract1.sol"
+    assert found_solidity_contracts[0].relative_path == Path(".")
     assert found_solidity_contracts[0].compiledJSONPath == Path(
         tmp_path / "Contract1.json"
     )
 
     assert found_solidity_contracts[1].filename == "Contract4.sol"
-    assert found_solidity_contracts[1].path == Path(tmp_path / "precompiles")
+    assert found_solidity_contracts[1].path == Path(tmp_path / "precompiles" / "Contract4.sol")
+    assert found_solidity_contracts[1].relative_path == Path("precompiles")
     assert found_solidity_contracts[1].compiledJSONPath == Path(
         tmp_path / "precompiles" / "Contract4.json"
     )
 
     assert found_solidity_contracts[2].filename == "Contract3.sol"
-    assert found_solidity_contracts[2].path == Path(tmp_path / "contracts")
+    assert found_solidity_contracts[2].relative_path == Path("contracts")
     assert found_solidity_contracts[2].compiledJSONPath == Path(
         tmp_path / "contracts" / "Contract3.json"
     )
@@ -64,15 +66,16 @@ def setup_contracts_directory(tmp_path):
 
 
 def test_copy_to_contracts_directory(
-    setup_contracts_directory
+    setup_contracts_directory: Path,
 ):
     target = setup_contracts_directory
-    contracts = find_solidity_contracts(os.getcwd())
+    contracts = find_solidity_contracts(Path(os.getcwd()))
 
-    assert len(os.listdir(target)) == 0
+    assert len(os.listdir(target)) == 0, "expected target to be empty before copying files"
     assert copy_to_contracts_directory(
         target,
         contracts
-    ) is True
-    # TODO: have better assertion
-    assert len(os.listdir(target)) > 0
+    ) is True, "expected method to be executed successfully"
+    dir_contents_post = os.listdir(target)
+    assert len(dir_contents_post) > 0, "expected multiple files to have been copied to target directory"
+    assert os.path.exists(target / "precompiles" / "staking" / "testdata" / "StakingCaller.sol")
