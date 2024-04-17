6b189ece9b856e3735fbfe37d92143c810c729c2
MalteHerrmann <malte@evmos.org>
2024-04-17 10:26:13 +0200

    more linter adjustments

--- contracts/hardhat.config.js
+++ contracts/hardhat.config.js
@@ -1,4 +1,4 @@
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: '0.8.19',
-};
+  solidity: '0.8.19'
+}
--- scripts/compile_smart_contracts/compile_smart_contracts.py
+++ scripts/compile_smart_contracts/compile_smart_contracts.py
@@ -93,7 +93,7 @@ def find_solidity_contracts(
 
             if re.search(r"(?!\.dbg)\.sol$", file):
                 filename = os.path.splitext(file)[0]
-                compiledJSONPath = Path(root) / f"{filename}.json"
+                compiledJSONPath: Path | None = Path(root) / f"{filename}.json"
 
                 if (
                     added_contract is not None
@@ -113,8 +113,7 @@ def find_solidity_contracts(
                 )
 
     if not found_added_contract and added_contract is not None:
-        raise ValueError(
-            f"Contract {added_contract} not found in the repository.")
+        raise ValueError(f"Contract {added_contract} not found in the repository.")
 
     return solidity_files
 
@@ -220,8 +219,7 @@ def copy_compiled_contracts_back_to_source(
             dir_with_json = compiled_dir / contract.relative_path
 
         compiled_path = (
-            dir_with_json / f"{contract.filename}.sol" /
-            f"{contract.filename}.json"
+            dir_with_json / f"{contract.filename}.sol" / f"{contract.filename}.json"
         )
 
         if not os.path.exists(compiled_path):
@@ -271,8 +269,7 @@ def compile_files(repo_path: Path, added_contract: str | None = None):
     with Hardhat.
     """
 
-    found_contracts = find_solidity_contracts(
-        REPO_PATH, added_contract=added_contract)
+    found_contracts = find_solidity_contracts(REPO_PATH, added_contract=added_contract)
 
     if not copy_to_contracts_directory(CONTRACTS_TARGET, found_contracts):
         raise ValueError("Failed to copy contracts to target directory.")
