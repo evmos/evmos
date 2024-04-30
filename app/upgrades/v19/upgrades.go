886e4a7ae6e0e94a21297c398eed187ba2acdbb5
Ramiro Carlucho <ramirocarlucho@gmail.com>
2024-04-30 18:37:45 -0300

    add upgrade handler

---
+++ app/upgrades/v19/upgrades.go
@@ -0,0 +1,25 @@
+// Copyright Tharsis Labs Ltd.(Evmos)
+// SPDX-License-Identifier:ENCL-1.0(https://github.com/evmos/evmos/blob/main/LICENSE)
+
+package v19
+
+import (
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/cosmos/cosmos-sdk/types/module"
+	upgradetypes "github.com/cosmos/cosmos-sdk/x/upgrade/types"
+)
+
+// CreateUpgradeHandler creates an SDK upgrade handler for v18
+func CreateUpgradeHandler(
+	mm *module.Manager,
+	configurator module.Configurator,
+) upgradetypes.UpgradeHandler {
+	return func(ctx sdk.Context, _ upgradetypes.Plan, vm module.VersionMap) (module.VersionMap, error) {
+		logger := ctx.Logger().With("upgrade", UpgradeName)
+		// revenue module is deprecated
+		logger.Debug("deleting revenue module from version map...")
+		delete(vm, "revenue")
+		// Leave modules as-is to avoid running InitGenesis.
+		return mm.RunMigrations(ctx, configurator, vm)
+	}
+}
