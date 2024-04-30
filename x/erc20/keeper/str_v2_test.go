63a47fac2b2eede8fbd5846de9b9dc8b6a0cb1d3
Ramiro Carlucho <ramirocarlucho@gmail.com>
2024-04-30 12:01:39 -0300

    str unit tests

--- x/erc20/keeper/str_v2_test.go
+++ x/erc20/keeper/str_v2_test.go
@@ -6,6 +6,82 @@ import (
 	sdk "github.com/cosmos/cosmos-sdk/types"
 )
 
+func (suite *KeeperTestSuite) TestSetDeleteSTRV2Address() {
+	address1 := suite.address.Bytes()
+	address2 := suite.consAddress.Bytes()
+
+	suite.SetupTest()
+
+	// Set the same address twice, and it shouldnt fail
+	suite.app.Erc20Keeper.SetSTRv2Address(suite.ctx, address1)
+	suite.Require().Equal(true, suite.app.Erc20Keeper.HasSTRv2Address(suite.ctx, address1))
+	suite.app.Erc20Keeper.SetSTRv2Address(suite.ctx, address1)
+	suite.Require().Equal(true, suite.app.Erc20Keeper.HasSTRv2Address(suite.ctx, address1))
+
+	// Set a different address and it shouldnt affect the first address
+	suite.app.Erc20Keeper.SetSTRv2Address(suite.ctx, address2)
+	suite.Require().Equal(true, suite.app.Erc20Keeper.HasSTRv2Address(suite.ctx, address2))
+	suite.app.Erc20Keeper.SetSTRv2Address(suite.ctx, address1)
+	suite.Require().Equal(true, suite.app.Erc20Keeper.HasSTRv2Address(suite.ctx, address1))
+
+	// Delete the first address.
+	// - it should delete the first address
+	// - it shouldnt affect the second one
+	suite.app.Erc20Keeper.DeleteSTRv2Address(suite.ctx, address1)
+	suite.Require().Equal(false, suite.app.Erc20Keeper.HasSTRv2Address(suite.ctx, address1))
+	suite.app.Erc20Keeper.DeleteSTRv2Address(suite.ctx, address1)
+	suite.Require().Equal(false, suite.app.Erc20Keeper.HasSTRv2Address(suite.ctx, address1))
+	suite.Require().Equal(true, suite.app.Erc20Keeper.HasSTRv2Address(suite.ctx, address2))
+
+	// Set the deleted address again
+	suite.app.Erc20Keeper.SetSTRv2Address(suite.ctx, address1)
+	suite.Require().Equal(true, suite.app.Erc20Keeper.HasSTRv2Address(suite.ctx, address1))
+	suite.Require().Equal(true, suite.app.Erc20Keeper.HasSTRv2Address(suite.ctx, address2))
+}
+
+func (suite *KeeperTestSuite) TestHasSTRV2Address() {
+	address1 := suite.address.Bytes()
+	address2 := suite.consAddress.Bytes()
+
+	testCases := []struct {
+		name     string
+		malleate func()
+		expected bool
+	}{
+		{
+			"space is empty",
+			func() {
+			},
+			false,
+		},
+		{
+			"one address",
+			func() {
+				suite.app.Erc20Keeper.SetSTRv2Address(suite.ctx, address1)
+			},
+			true,
+		},
+		{
+			"two addresses",
+			func() {
+				suite.app.Erc20Keeper.SetSTRv2Address(suite.ctx, address1)
+				suite.app.Erc20Keeper.SetSTRv2Address(suite.ctx, address2)
+			},
+			true,
+		},
+	}
+	for _, tc := range testCases {
+		suite.Run(fmt.Sprintf("Case %s", tc.name), func() {
+			suite.SetupTest() // reset
+
+			tc.malleate()
+
+			actual := suite.app.Erc20Keeper.HasSTRv2Address(suite.ctx, address1)
+			suite.Require().Equal(tc.expected, actual)
+		})
+	}
+}
+
 func (suite *KeeperTestSuite) TestGetAllSTRV2Address() {
 	address1 := suite.address.Bytes()
 	address2 := suite.consAddress.Bytes()
