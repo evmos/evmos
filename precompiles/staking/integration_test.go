ac72b8a46cf892b4ae93096a95ee4b40e7389ff5
tom <tomasguerraalda@hotmail.com>
2024-06-07 13:49:32 -0300

    add edit validator from sc call test

--- precompiles/staking/integration_test.go
+++ precompiles/staking/integration_test.go
@@ -1855,6 +1855,85 @@ var _ = Describe("Calling staking precompile via Solidity", func() {
 		})
 	})
 
+	Context("edit a validator", func() {
+		var (
+			// NOTE: this has to be populated in the BeforeEach block because the private key is not initialized before
+			defaultEditValArgs contracts.CallArgs
+			valPriv            *ethsecp256k1.PrivKey
+			valAddr            sdk.AccAddress
+
+			defaultDescription = staking.Description{
+				Moniker:         "edit node",
+				Identity:        "[do-not-modify]",
+				Website:         "[do-not-modify]",
+				SecurityContact: "[do-not-modify]",
+				Details:         "[do-not-modify]",
+			}
+			defaultCommissionRate    = big.NewInt(staking.DoNotModifyCommissionRate)
+			defaultMinSelfDelegation = big.NewInt(staking.DoNotModifyMinSelfDelegation)
+		)
+
+		BeforeEach(func() {
+			defaultEditValArgs = defaultCallArgs.WithMethodName("testEditValidator")
+
+			// create a new validator
+			valAddr, valPriv = testutiltx.NewAccAddressAndKey()
+			hexAddr := common.BytesToAddress(valAddr.Bytes())
+			err := evmosutil.FundAccountWithBaseDenom(s.ctx, s.app.BankKeeper, valAddr, 2e18)
+			Expect(err).To(BeNil(), "error while funding account: %v", err)
+
+			description := staking.Description{
+				Moniker:         "new node",
+				Identity:        "",
+				Website:         "",
+				SecurityContact: "",
+				Details:         "",
+			}
+			commission := staking.Commission{
+				Rate:          big.NewInt(100000000000000000),
+				MaxRate:       big.NewInt(100000000000000000),
+				MaxChangeRate: big.NewInt(100000000000000000),
+			}
+			minSelfDelegation := big.NewInt(1)
+			pubkeyBase64Str := "UuhHQmkUh2cPBA6Rg4ei0M2B04cVYGNn/F8SAUsYIb4="
+			value := big.NewInt(1e18)
+
+			createValidatorArgs := contracts.CallArgs{
+				ContractAddr: s.precompile.Address(),
+				ContractABI:  s.precompile.ABI,
+				MethodName:   staking.CreateValidatorMethod,
+				PrivKey:      valPriv,
+				Args:         []interface{}{description, commission, minSelfDelegation, hexAddr, pubkeyBase64Str, value},
+			}
+
+			logCheckArgs := passCheck.WithExpEvents(staking.EventTypeCreateValidator)
+
+			_, _, err = contracts.CallContractAndCheckLogs(s.ctx, s.app, createValidatorArgs, logCheckArgs)
+			Expect(err).To(BeNil(), "error while calling the smart contract: %v", err)
+
+			s.NextBlock()
+		})
+
+		It("should edit a validator", func() {
+			cArgs := defaultEditValArgs.
+				WithPrivKey(valPriv).
+				WithArgs(
+					defaultDescription, defaultCommissionRate, defaultMinSelfDelegation,
+				)
+
+			logCheckArgs := passCheck.
+				WithExpEvents(staking.EventTypeEditValidator)
+
+			_, _, err = contracts.CallContractAndCheckLogs(s.ctx, s.app, cArgs, logCheckArgs)
+			Expect(err).To(BeNil(), "error while calling the smart contract: %v", err)
+
+			validator, found := s.app.StakingKeeper.GetValidator(s.ctx, valAddr.Bytes())
+			Expect(found).To(BeTrue(), "expected validator to be found")
+			Expect(validator.Description.Moniker).To(Equal(defaultDescription.Moniker), "expected validator moniker is updated")
+			Expect(validator.Commission.Rate.BigInt().String()).To(Equal("100000000000000000"), "expected validator commission rate remain unchanged")
+		})
+	})
+
 	Context("delegating", func() {
 		var (
 			// prevDelegation is the delegation that is available prior to the test (an initial delegation is
