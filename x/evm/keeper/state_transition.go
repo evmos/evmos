008065d6c5c44239d5986dd0faa0e9c0a706a3ac
Freddy Caceres <facs95@gmail.com>
2024-06-07 14:31:57 +0200

    imp(evm): Move OpcodeHooks into types

--- x/evm/keeper/state_transition.go
+++ x/evm/keeper/state_transition.go
@@ -60,8 +60,16 @@ func (k *Keeper) NewEVM(
 	vmConfig := k.VMConfig(ctx, msg, cfg, tracer)
 
 	signer := msg.From()
-	AccessControl := types.NewRestrictedPermissionPolicy(&cfg.Params.AccessControl, signer)
-	evmHooks := NewDefaultOpCodesHooks(AccessControl, signer)
+	accessControl := types.NewRestrictedPermissionPolicy(&cfg.Params.AccessControl, signer)
+
+	// Set hooks for the EVM opcodes
+	evmHooks := types.NewDefaultOpCodesHooks()
+	evmHooks.AddCreateHook(
+		accessControl.GetCreateHook(signer),
+	)
+	evmHooks.AddCallHook(
+		accessControl.GetCallHook(signer),
+	)
 	return vm.NewEVMWithHooks(evmHooks, blockCtx, txCtx, stateDB, cfg.ChainConfig, vmConfig)
 }
 
