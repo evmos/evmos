50e029ca3e8b6d625997066f44264ad1dc0a60ca
stepit <stefanofrancesco.pitton@gmail.com>
2024-06-20 16:43:53 +0200

    add missing methods for setting opcodes

--- x/evm/core/vm/custom_eip.go
+++ x/evm/core/vm/custom_eip.go
@@ -3,15 +3,54 @@
 
 package vm
 
+import (
+	"sort"
+)
+
 // ExtendActivators allows to merge the go ethereum activators map
 // with additional activators.
 func ExtendActivators(eips map[int]func(*JumpTable)) {
-	for k, v := range eips {
-		activators[k] = v
+	// Sorting key to ensure deterministic execution.
+	keys := make([]int, 0, len(eips))
+	for k := range eips {
+		keys = append(keys, k)
+	}
+	sort.Ints(keys)
+
+	// If an EIP is already present in the activators type, skip it.
+	for _, k := range keys {
+		if _, exist := activators[k]; !exist {
+			activators[k] = eips[k]
+		}
 	}
 }
 
-// SetConstantGas change the constant gas of the operation.
+// SetExecute sets the execution function of the operation.
+func (o *operation) SetExecute(ef executionFunc) {
+	o.execute = ef
+}
+
+// SetConstantGas changes the constant gas of the operation.
 func (o *operation) SetConstantGas(gas uint64) {
 	o.constantGas = gas
+}
+
+// SetDynamicGas sets the dynamic gas function of the operation.
+func (o *operation) SetDynamicGas(gf gasFunc) {
+	o.dynamicGas = gf
+}
+
+// SetMinStack sets the minimum stack size required for the operation.
+func (o *operation) SetMinStack(minStack int) {
+	o.minStack = minStack
+}
+
+// SetMaxStack sets the maximum stack size for the operation.
+func (o *operation) SetMaxStack(maxStack int) {
+	o.maxStack = maxStack
+}
+
+// SetMemorySize sets the memory size function for the operation.
+func (o *operation) SetMemorySize(msf memorySizeFunc) {
+	o.memorySize = msf
 }
