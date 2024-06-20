fd8892882c561512f0469b593c2e289db8f9341c
MalteHerrmann <malte@evmos.org>
2024-06-20 17:38:03 +0200

    wip adding migration logic

--- app/upgrades/v19/upgrades.go
+++ app/upgrades/v19/upgrades.go
@@ -6,7 +6,12 @@ package v19
 import (
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/cosmos/cosmos-sdk/types/module"
+	authkeeper "github.com/cosmos/cosmos-sdk/x/auth/keeper"
+	authtypes "github.com/cosmos/cosmos-sdk/x/auth/types"
 	upgradetypes "github.com/cosmos/cosmos-sdk/x/upgrade/types"
+	"github.com/ethereum/go-ethereum/common"
+	evmostypes "github.com/evmos/evmos/v18/types"
+	"github.com/evmos/evmos/v18/utils"
 	evmkeeper "github.com/evmos/evmos/v18/x/evm/keeper"
 )
 
@@ -19,7 +24,8 @@ const (
 func CreateUpgradeHandler(
 	mm *module.Manager,
 	configurator module.Configurator,
-	evmKeeper *evmkeeper.Keeper,
+	ak authkeeper.AccountKeeper,
+	ek *evmkeeper.Keeper,
 ) upgradetypes.UpgradeHandler {
 	return func(ctx sdk.Context, _ upgradetypes.Plan, vm module.VersionMap) (module.VersionMap, error) {
 		logger := ctx.Logger().With("upgrade", UpgradeName)
@@ -28,9 +34,11 @@ func CreateUpgradeHandler(
 		delete(vm, "revenue")
 
 		ctxCache, writeFn := ctx.CacheContext()
-		if err := RemoveOutpostsFromEvmParams(ctxCache, evmKeeper); err == nil {
+		if err := RemoveOutpostsFromEvmParams(ctxCache, ek); err == nil {
 			writeFn()
 		}
+
+		MigrateEthAccountsToBaseAccounts(ctx, ak, ek)
 
 		// Leave modules as-is to avoid running InitGenesis.
 		return mm.RunMigrations(ctx, configurator, vm)
@@ -50,4 +58,23 @@ func RemoveOutpostsFromEvmParams(ctx sdk.Context,
 	}
 	params.ActivePrecompiles = newActivePrecompiles
 	return evmKeeper.SetParams(ctx, params)
+}
+
+// MigrateEthAccountsToBaseAccounts is used to store the code hash of the associated
+func MigrateEthAccountsToBaseAccounts(ctx sdk.Context, ak authkeeper.AccountKeeper, ek *evmkeeper.Keeper) {
+	ak.IterateAccounts(ctx, func(account authtypes.AccountI) (stop bool) {
+		ethAcc, ok := account.(*evmostypes.EthAccount)
+		if !ok {
+			// only migrate EthAccounts
+			return false
+		}
+
+		hexAddr := utils.SDKAddrToEthAddr(ethAcc.GetAddress())
+		ek.SetCodeHash(ctx, hexAddr, common.HexToHash(ethAcc.CodeHash))
+
+		// Set the base account in the account keeper instead of the EthAccount
+		ak.SetAccount(ctx, ethAcc.BaseAccount)
+
+		return false
+	})
 }
