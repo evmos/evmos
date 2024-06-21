46f0c9d9bde2e3fdeea0e0753fcb1e5ed4f120ce
stepit <stefanofrancesco.pitton@gmail.com>
2024-06-21 09:03:45 +0200

    add checks and docstring

--- x/evm/config/config.go
+++ x/evm/config/config.go
@@ -7,14 +7,24 @@
 package config
 
 import (
+	"golang.org/x/exp/slices"
+
 	"github.com/evmos/evmos/v18/x/evm/core/vm"
 	"github.com/evmos/evmos/v18/x/evm/types"
 )
 
+// ExtendEips allows to add to the go-ethereum activators map the provided
+// EIP activators.
 func ExtendEips(eips map[int]func(*vm.JumpTable)) {
 	vm.ExtendActivators(eips)
 }
 
+// UpdateDefaultExtraEIPs update the x/evm DefaultExtraEIPs params
+// by adding provided EIP numbers.
 func UpdateDefaultExtraEIPs(eips []int64) {
-	types.DefaultExtraEIPs = append(types.DefaultExtraEIPs, eips...)
+	for _, eip := range eips {
+		if !slices.Contains(types.DefaultExtraEIPs, eip) {
+			types.DefaultExtraEIPs = append(types.DefaultExtraEIPs, eip)
+		}
+	}
 }
