c519fce69ca93c41862ddb719a17627af8c7f14a
stepit <stefanofrancesco.pitton@gmail.com>
2024-07-04 17:22:45 +0200

    update custom eips + test

--- app/eips.go
+++ app/eips.go
@@ -8,29 +8,26 @@ import (
 	"github.com/evmos/evmos/v18/x/evm/core/vm"
 )
 
-// TODO: 2 as global
+const multiplier = 5
 
 // enable0000 contains the logic to modify the CREATE and CREATE2 opcodes
 // constant gas value.
-// TODO: define the multiplier.
 func enable0000(jt *vm.JumpTable) {
 	// Gas cost changes
-	jt[vm.CREATE].SetConstantGas(params.CreateGas * 2)
-	jt[vm.CREATE2].SetConstantGas(params.CreateGas * 2)
+	jt[vm.CREATE].SetConstantGas(params.CreateGas * multiplier)
+	jt[vm.CREATE2].SetConstantGas(params.CreateGas * multiplier)
 }
 
 // enable0001 contains the logic to modify the CALL opcode
 // constant gas value.
-// TODO: define the multiplier.
 func enable0001(jt *vm.JumpTable) {
 	// Gas cost changes
-	jt[vm.CALL].SetConstantGas(params.CallGasEIP150 * 2)
+	jt[vm.CALL].SetConstantGas(params.CallGasEIP150 * multiplier)
 }
 
 // enable0002 contains the logic to modify the SSTORE opcode
 // constant gas value.
-// TODO: define the multiplier.
 func enable0002(jt *vm.JumpTable) {
 	// Gas cost changes
-	jt[vm.SSTORE].SetConstantGas(params.SstoreSetGas * 2)
+	jt[vm.SSTORE].SetConstantGas(params.SstoreSetGas * multiplier)
 }
---
+++ app/testdata/Deploy.sol
@@ -0,0 +1,4 @@
+// SPDX-License-Identifier: LGPL-3.0-only
+pragma solidity >=0.8.17;
+
+contract Deploy {}
