6d0908d845e78412de57f6552fc01517ecc9e06b
hanchon <guillermo.paoletti@gmail.com>
2024-07-09 17:14:28 +0200

    chore: merge main again

--- precompiles/vesting/tx.go
+++ precompiles/vesting/tx.go
@@ -10,10 +10,9 @@ import (
 	"github.com/ethereum/go-ethereum/accounts/abi"
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/evmos/evmos/v18/precompiles/authorization"
-	"github.com/evmos/evmos/v18/x/evm/core/vm"
-
 	cmn "github.com/evmos/evmos/v18/precompiles/common"
 	"github.com/evmos/evmos/v18/utils"
+	"github.com/evmos/evmos/v18/x/evm/core/vm"
 )
 
 const (
