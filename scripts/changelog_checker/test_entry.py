582dae75c7edc95a787c81f954a90b31eb9d7fd3
dev-evmos <dev-evmos@users.noreply.github.com>
2024-07-16 15:45:55 +0000

    run make format-python

--- scripts/changelog_checker/test_entry.py
+++ scripts/changelog_checker/test_entry.py
@@ -1,8 +1,8 @@
 import re
 
-from entry import (ALLOWED_SPELLINGS, Entry, check_category,  # type: ignore
-                   check_description, check_link, check_spelling,
-                   check_whitespace, get_match)
+from entry import check_category  # type: ignore
+from entry import (ALLOWED_SPELLINGS, Entry, check_description, check_link,
+                   check_spelling, check_whitespace, get_match)
 
 
 class TestEntry:
