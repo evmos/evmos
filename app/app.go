260a8d4f5304589062ec716f0063b8c6cab210c3
GAtom22 <GAtom22@users.noreply.github.com>
2024-08-01 12:14:10 +0000

    run make format

--- app/app.go
+++ app/app.go
@@ -1202,7 +1202,7 @@ func (app *Evmos) setupUpgradeHandlers() {
 		// revenue module is deprecated in v19
 		storeUpgrades = &storetypes.StoreUpgrades{
 			Deleted: []string{"revenue"},
-			Added: []string{ratelimittypes.ModuleName},
+			Added:   []string{ratelimittypes.ModuleName},
 		}
 	default:
 		// no-op
