d3c44c99a38ec81c8fab08e16c659040c1648982
stepit <stefanofrancesco.pitton@gmail.com>
2024-08-06 17:00:42 +0200

    remove unused tests

--- tests/integration/ledger/ledger_test.go
+++ tests/integration/ledger/ledger_test.go
@@ -4,8 +4,6 @@ import (
 	"bytes"
 	"context"
 
-	"cosmossdk.io/math"
-
 	"github.com/cosmos/cosmos-sdk/client"
 	"github.com/cosmos/cosmos-sdk/client/flags"
 	"github.com/cosmos/cosmos-sdk/crypto/keyring"
@@ -15,16 +13,12 @@ import (
 	"github.com/evmos/evmos/v18/crypto/hd"
 	"github.com/evmos/evmos/v18/encoding"
 	"github.com/evmos/evmos/v18/tests/integration/ledger/mocks"
-	"github.com/evmos/evmos/v18/testutil"
-	utiltx "github.com/evmos/evmos/v18/testutil/tx"
 
 	"github.com/spf13/cobra"
 
 	sdktestutil "github.com/cosmos/cosmos-sdk/testutil"
 	sdktestutilcli "github.com/cosmos/cosmos-sdk/testutil/cli"
-	sdk "github.com/cosmos/cosmos-sdk/types"
 	sdktestutilmod "github.com/cosmos/cosmos-sdk/types/module/testutil"
-	bankcli "github.com/cosmos/cosmos-sdk/x/bank/client/cli"
 
 	//nolint:revive // dot imports are fine for Ginkgo
 	. "github.com/onsi/ginkgo/v2"
@@ -42,15 +36,14 @@ var (
 
 var _ = Describe("Ledger CLI and keyring functionality: ", func() {
 	var (
-		receiverAccAddr sdk.AccAddress
-		encCfg          sdktestutilmod.TestEncodingConfig
-		kr              keyring.Keyring
-		mockedIn        sdktestutil.BufferReader
-		clientCtx       client.Context
-		ctx             context.Context
-		cmd             *cobra.Command
-		krHome          string
-		keyRecord       *keyring.Record
+		encCfg    sdktestutilmod.TestEncodingConfig
+		kr        keyring.Keyring
+		mockedIn  sdktestutil.BufferReader
+		clientCtx client.Context
+		ctx       context.Context
+		cmd       *cobra.Command
+		krHome    string
+		keyRecord *keyring.Record
 	)
 
 	ledgerKey := "ledger_key"
@@ -167,71 +160,6 @@ var _ = Describe("Ledger CLI and keyring functionality: ", func() {
 					s.Require().Error(err, "false positive result, error expected")
 
 					s.Require().Equal(mocks.ErrMockedSigning.Error(), err.Error(), "original and returned errors are not equal")
-				})
-			})
-			Context("with cli command", func() {
-				BeforeEach(func() {
-					s.ledger = mocks.NewSECP256K1(s.T())
-
-					err := testutil.FundAccount(
-						s.ctx,
-						s.app.BankKeeper,
-						s.accAddr,
-						sdk.NewCoins(
-							sdk.NewCoin("aevmos", math.NewInt(100000000000000)),
-						),
-					)
-					s.Require().NoError(err)
-
-					receiverAccAddr = sdk.AccAddress(utiltx.GenerateAddress().Bytes())
-
-					cmd = bankcli.NewSendTxCmd(s.app.AccountKeeper.AddressCodec())
-					mockedIn = sdktestutil.ApplyMockIODiscardOutErr(cmd)
-
-					kr, clientCtx, ctx = s.NewKeyringAndCtxs(krHome, mockedIn, encCfg)
-
-					// register mocked funcs
-					mocks.MClose(s.ledger)
-					mocks.MGetPublicKeySECP256K1(s.ledger, s.pubKey)
-					mocks.MEnsureExist(s.accRetriever, nil)
-					mocks.MGetAccountNumberSequence(s.accRetriever, 0, 0, nil)
-				})
-				It("should execute bank tx cmd", func() {
-					mocks.MSignSECP256K1(s.ledger, signOkMock, nil)
-
-					cmd.SetContext(ctx)
-					cmd.SetArgs([]string{
-						ledgerKey,
-						receiverAccAddr.String(),
-						sdk.NewCoin("aevmos", math.NewInt(1000)).String(),
-						s.FormatFlag(flags.FlagUseLedger),
-						s.FormatFlag(flags.FlagSkipConfirmation),
-					})
-					out := bytes.NewBufferString("")
-					cmd.SetOutput(out)
-
-					err := cmd.Execute()
-
-					s.Require().NoError(err, "can't execute cli tx command")
-				})
-				It("should return error from ledger device", func() {
-					mocks.MSignSECP256K1(s.ledger, signErrMock, mocks.ErrMockedSigning)
-
-					cmd.SetContext(ctx)
-					cmd.SetArgs([]string{
-						ledgerKey,
-						receiverAccAddr.String(),
-						sdk.NewCoin("aevmos", math.NewInt(1000)).String(),
-						s.FormatFlag(flags.FlagUseLedger),
-						s.FormatFlag(flags.FlagSkipConfirmation),
-					})
-					out := bytes.NewBufferString("")
-					cmd.SetOutput(out)
-
-					err := cmd.Execute()
-
-					s.Require().Error(err, "false positive, error expected")
-					s.Require().Equal(mocks.ErrMockedSigning.Error(), err.Error())
 				})
 			})
 		})
