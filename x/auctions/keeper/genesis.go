0ab37e7f9abe9db1bb7090ccf5999a4e47b50484
stepit <stefanofrancesco.pitton@gmail.com>
2024-08-09 11:47:41 +0200

    move genesis to keeper for proper validation

--- x/auctions/keeper/genesis.go
+++ x/auctions/keeper/genesis.go
@@ -1,19 +1,30 @@
 // Copyright Tharsis Labs Ltd.(Evmos)
 // SPDX-License-Identifier:ENCL-1.0(https://github.com/evmos/evmos/blob/main/LICENSE)
 
-package auctions
+package keeper
 
 import (
+	"fmt"
+
 	errorsmod "cosmossdk.io/errors"
 	sdk "github.com/cosmos/cosmos-sdk/types"
-	"github.com/evmos/evmos/v19/x/auctions/keeper"
 	"github.com/evmos/evmos/v19/x/auctions/types"
 )
 
-func InitGenesis(ctx sdk.Context, k keeper.Keeper, data types.GenesisState) {
+func InitGenesis(ctx sdk.Context, k Keeper, data types.GenesisState) {
 	err := k.SetParams(ctx, data.Params)
 	if err != nil {
 		panic(errorsmod.Wrap(err, "could not set parameters at genesis"))
+	}
+
+	// Bidder address should exists in the account keeper.
+	bidder, err := sdk.AccAddressFromBech32(data.Bid.Sender)
+	if err != nil {
+		panic(errorsmod.Wrap(err, "bidder address is not valid"))
+	}
+
+	if found := k.accountKeeper.HasAccount(ctx, bidder); !found {
+		panic(fmt.Errorf("account associated with %s does not exist", data.Bid.Sender))
 	}
 
 	// Set the highest bid
@@ -25,7 +36,7 @@ func InitGenesis(ctx sdk.Context, k keeper.Keeper, data types.GenesisState) {
 	k.SetRound(ctx, data.Round)
 }
 
-func ExportGenesis(ctx sdk.Context, k keeper.Keeper) *types.GenesisState {
+func ExportGenesis(ctx sdk.Context, k Keeper) *types.GenesisState {
 	return &types.GenesisState{
 		Params: k.GetParams(ctx),
 		Bid:    *k.GetHighestBid(ctx),
