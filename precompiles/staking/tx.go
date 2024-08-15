96a2c460f21121b5811745bd37be953384a990cc
Tom <54514587+GAtom22@users.noreply.github.com>
2024-08-15 07:42:20 -0300

    fix linter errors (#2762)

--- precompiles/staking/tx.go
+++ precompiles/staking/tx.go
@@ -4,6 +4,7 @@
 package staking
 
 import (
+	"errors"
 	"fmt"
 	"time"
 
@@ -76,7 +77,7 @@ func (p Precompile) CreateValidator(
 	// and MsgEditValidator (source: https://github.com/cosmos/cosmos-sdk/blob/4bd73b667f8aed50ad4602ddf862a4ed6e1450a8/x/staking/proto/cosmos/staking/v1beta1/authz.proto#L39-L50)
 	// so, for the time being, we won't allow calls from smart contracts
 	if contract.CallerAddress != origin {
-		return nil, fmt.Errorf(ErrCannotCallFromContract)
+		return nil, errors.New(ErrCannotCallFromContract)
 	}
 
 	// we only allow the tx signer "origin" to create their own validator.
@@ -127,7 +128,7 @@ func (p Precompile) EditValidator(
 	// and MsgEditValidator (source: https://github.com/cosmos/cosmos-sdk/blob/4bd73b667f8aed50ad4602ddf862a4ed6e1450a8/x/staking/proto/cosmos/staking/v1beta1/authz.proto#L39-L50)
 	// so, for the time being, we won't allow calls from smart contracts
 	if contract.CallerAddress != origin {
-		return nil, fmt.Errorf(ErrCannotCallFromContract)
+		return nil, errors.New(ErrCannotCallFromContract)
 	}
 
 	// we only allow the tx signer "origin" to edit their own validator.
