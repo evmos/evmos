f6c850b60e20f268feecd253f4d51faff0011b52
GAtom22 <GAtom22@users.noreply.github.com>
2024-08-22 16:55:16 +0000

    run make format

--- cmd/evmosd/testnet.go
+++ cmd/evmosd/testnet.go
@@ -164,7 +164,7 @@ Example:
 			minGasPrice, _ := cmd.Flags().GetString(flagMinGasPrice)
 
 			args.baseFee, err = math.LegacyNewDecFromStr(baseFee)
-			if err != nil  {
+			if err != nil {
 				return fmt.Errorf("invalid value for --base-fee. expected a int or decimal number greater than or equal to 0 but got %s and err %s", baseFee, err.Error())
 			}
 			if args.baseFee.LT(math.LegacyZeroDec()) {
