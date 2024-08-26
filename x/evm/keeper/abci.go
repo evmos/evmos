ba42f5e19b700349e26a316fc3e0e0e06f4ee1ff
tom <tomasguerraalda@hotmail.com>
2024-08-26 14:54:10 -0300

    check res == nil

--- x/evm/keeper/abci.go
+++ x/evm/keeper/abci.go
@@ -31,7 +31,7 @@ func (k *Keeper) BeginBlock(ctx sdk.Context, _ abci.RequestBeginBlock) {
 	if err != nil {
 		logger.Error("error when getting base fee", "error", err.Error())
 	}
-	if !res.BaseFee.IsNil() {
+	if res != nil && !res.BaseFee.IsNil() {
 		// Store current base fee in event
 		ctx.EventManager().EmitEvents(sdk.Events{
 			sdk.NewEvent(
