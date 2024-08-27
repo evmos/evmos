ddb9203dffd226bb24c1d1ec14f7586411da08b4
tom <tomasguerraalda@hotmail.com>
2024-08-27 14:09:32 -0300

    Revert "refactor"
    This reverts commit 1a5c0cf7be01f17a2f71dc7125d602b1ff509305.

--- x/evm/statedb/interfaces.go
+++ x/evm/statedb/interfaces.go
@@ -5,7 +5,7 @@ package statedb
 import (
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/ethereum/go-ethereum/common"
-
+	
 	"github.com/evmos/evmos/v19/x/evm/core/vm"
 	evmtypes "github.com/evmos/evmos/v19/x/evm/types"
 )
