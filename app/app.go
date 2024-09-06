6b766006fd6d6fb7ea63ec1a2c0ea260cb97325f
ramacarlucho <ramacarlucho@users.noreply.github.com>
2024-09-06 18:22:35 +0000

    run make format

--- app/app.go
+++ app/app.go
@@ -1185,7 +1185,6 @@ func initParamsKeeper(
 }
 
 func (app *Evmos) setupUpgradeHandlers() {
-
 	// When a planned update height is reached, the old binary will panic
 	// writing on disk the height and name of the update that triggered it
 	// This will read that value, and execute the preparations for the upgrade.
