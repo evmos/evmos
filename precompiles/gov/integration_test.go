47fdeea8c68a977cd090e6adc18cb1c8be9883bd
tom <tomasguerraalda@hotmail.com>
2024-09-17 17:43:28 -0300

    add queries integration tests

--- precompiles/gov/integration_test.go
+++ precompiles/gov/integration_test.go
@@ -6,6 +6,7 @@ import (
 	"testing"
 
 	"cosmossdk.io/math"
+	"github.com/cosmos/cosmos-sdk/types/query"
 
 	"github.com/evmos/evmos/v20/precompiles/gov"
 	"github.com/evmos/evmos/v20/precompiles/testutil"
@@ -44,9 +45,13 @@ func TestKeeperIntegrationTestSuite(t *testing.T) {
 	RunSpecs(t, "Keeper Suite")
 }
 
-var _ = Describe("Calling distribution precompile from EOA", func() {
+var _ = Describe("Calling governance precompile from EOA", func() {
 	var s *PrecompileTestSuite
-
+	const (
+		proposalID uint64 = 1
+		option     uint8  = 1
+		metadata          = "metadata"
+	)
 	BeforeEach(func() {
 		s = new(PrecompileTestSuite)
 		s.SetupTest()
@@ -74,9 +79,6 @@ var _ = Describe("Calling distribution precompile from EOA", func() {
 	// =====================================
 	Describe("Execute Vote transaction", func() {
 		const method = gov.VoteMethod
-		const proposalID uint64 = 1
-		const option uint8 = 1
-		const metadata = "metadata"
 
 		BeforeEach(func() {
 			// set the default call arguments
@@ -133,7 +135,105 @@ var _ = Describe("Calling distribution precompile from EOA", func() {
 	// 				QUERIES
 	// =====================================
 	Describe("Execute queries", func() {
-		It("should get proposal info - proposal query", func() {
+		Context("vote query", func() {
+			method := gov.GetVoteMethod
+			BeforeEach(func() {
+				// submit a vote
+				voteArgs := factory.CallArgs{
+					ContractABI: s.precompile.ABI,
+					MethodName:  gov.VoteMethod,
+					Args: []interface{}{
+						s.keyring.GetAddr(0), proposalID, option, metadata,
+					},
+				}
+
+				voterSetCheck := passCheck.WithExpEvents(gov.EventTypeVote)
+
+				_, _, err := s.factory.CallContractAndCheckLogs(s.keyring.GetPrivKey(0), txArgs, voteArgs, voterSetCheck)
+				Expect(err).To(BeNil(), "error while calling the precompile")
+				Expect(s.network.NextBlock()).To(BeNil())
+			})
+			It("should return a vote", func() {
+				callArgs.MethodName = method
+				callArgs.Args = []interface{}{proposalID, s.keyring.GetAddr(0)}
+				txArgs.GasLimit = 200_000
+
+				_, ethRes, err := s.factory.CallContractAndCheckLogs(
+					s.keyring.GetPrivKey(0),
+					txArgs,
+					callArgs,
+					passCheck,
+				)
+				Expect(err).To(BeNil(), "error while calling the smart contract: %v", err)
+
+				var out gov.VoteOutput
+				err = s.precompile.UnpackIntoInterface(&out, method, ethRes.Ret)
+				Expect(err).To(BeNil())
+
+				Expect(out.Vote.Voter).To(Equal(s.keyring.GetAddr(0)))
+				Expect(out.Vote.ProposalId).To(Equal(proposalID))
+				Expect(out.Vote.Metadata).To(Equal(metadata))
+				Expect(out.Vote.Options).To(HaveLen(1))
+				Expect(out.Vote.Options[0].Option).To(Equal(option))
+				Expect(out.Vote.Options[0].Weight).To(Equal(math.LegacyOneDec().String()))
+			})
+		})
+
+		Context("votes query", func() {
+			method := gov.GetVotesMethod
+			BeforeEach(func() {
+				// submit votes
+				for _, key := range s.keyring.GetKeys() {
+					voteArgs := factory.CallArgs{
+						ContractABI: s.precompile.ABI,
+						MethodName:  gov.VoteMethod,
+						Args: []interface{}{
+							key.Addr, proposalID, option, metadata,
+						},
+					}
+
+					voterSetCheck := passCheck.WithExpEvents(gov.EventTypeVote)
+
+					_, _, err := s.factory.CallContractAndCheckLogs(key.Priv, txArgs, voteArgs, voterSetCheck)
+					Expect(err).To(BeNil(), "error while calling the precompile")
+					Expect(s.network.NextBlock()).To(BeNil())
+				}
+			})
+			It("should return all votes", func() {
+				callArgs.MethodName = method
+				callArgs.Args = []interface{}{
+					proposalID,
+					query.PageRequest{
+						Limit:      10,
+						CountTotal: true,
+					},
+				}
+				txArgs.GasLimit = 200_000
+
+				_, ethRes, err := s.factory.CallContractAndCheckLogs(
+					s.keyring.GetPrivKey(0),
+					txArgs,
+					callArgs,
+					passCheck,
+				)
+				Expect(err).To(BeNil(), "error while calling the smart contract: %v", err)
+
+				var out gov.VotesOutput
+				err = s.precompile.UnpackIntoInterface(&out, method, ethRes.Ret)
+				Expect(err).To(BeNil())
+
+				votersCount := len(s.keyring.GetKeys())
+				Expect(out.PageResponse.Total).To(Equal(uint64(votersCount)))
+				Expect(out.PageResponse.NextKey).To(Equal([]byte{}))
+				Expect(out.Votes).To(HaveLen(votersCount))
+				for _, v := range out.Votes {
+					Expect(v.ProposalId).To(Equal(proposalID))
+					Expect(v.Metadata).To(Equal(metadata))
+					Expect(v.Options).To(HaveLen(1))
+					Expect(v.Options[0].Option).To(Equal(option))
+					Expect(v.Options[0].Weight).To(Equal(math.LegacyOneDec().String()))
+				}
+			})
 		})
 	})
 })
