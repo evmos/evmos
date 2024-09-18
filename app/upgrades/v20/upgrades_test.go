8bf49957b51803b7c2d6163057f1eb2960d545a4
tom <tomasguerraalda@hotmail.com>
2024-09-18 14:04:11 -0300

    add expedited proposal params update to upgrade handler

--- app/upgrades/v20/upgrades_test.go
+++ app/upgrades/v20/upgrades_test.go
@@ -3,8 +3,10 @@ package v20_test
 import (
 	"testing"
 
-	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/stretchr/testify/require"
+
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	govv1 "github.com/cosmos/cosmos-sdk/x/gov/types/v1"
 
 	v20 "github.com/evmos/evmos/v20/app/upgrades/v20"
 	"github.com/evmos/evmos/v20/testutil/integration/evmos/network"
@@ -62,4 +64,82 @@ func TestEnableGovPrecompile(t *testing.T) {
 			require.Contains(t, updatedParams.ActiveStaticPrecompiles, types.GovPrecompileAddress)
 		})
 	}
+}
+
+func TestUpdateExpeditedPropsParams(t *testing.T) {
+	var (
+		nw            *network.UnitTestNetwork
+		ctx           sdk.Context
+		initialParams govv1.Params
+		err           error
+	)
+
+	testCases := []struct {
+		name      string
+		setup     func()
+		postCheck func()
+	}{
+		{
+			name:  "pass - default params, no-op",
+			setup: func() {},
+			postCheck: func() {
+				params, err := nw.App.GovKeeper.Params.Get(ctx)
+				require.NoError(t, err)
+				require.Equal(t, initialParams, params)
+			},
+		},
+		{
+			name: "pass - expedited has 'stake' denom",
+			setup: func() {
+				// wrong exp min deposit denom
+				initialParams.ExpeditedMinDeposit[0].Denom = "stake"
+				err := nw.App.GovKeeper.Params.Set(ctx, initialParams)
+				require.NoError(t, err)
+			},
+			postCheck: func() {
+				params, err := nw.App.GovKeeper.Params.Get(ctx)
+				require.NoError(t, err)
+				require.Equal(t, "aevmos", params.ExpeditedMinDeposit[0].Denom)
+				require.Equal(t, initialParams.ExpeditedMinDeposit[0].Amount, params.ExpeditedMinDeposit[0].Amount)
+			},
+		},
+		{
+			name: "pass - updates denom, amount and period",
+			setup: func() {
+				// wrong exp min deposit denom
+				initialParams.ExpeditedMinDeposit[0].Denom = "stake"
+				// wrong exp min deposit amount (< than min_deposit amt)
+				initialParams.ExpeditedMinDeposit[0].Amount = initialParams.MinDeposit[0].Amount.SubRaw(1)
+				// wrong exp voting period (> than voting period)
+				expPeriod := *initialParams.VotingPeriod * 2
+				initialParams.ExpeditedVotingPeriod = &expPeriod
+				err := nw.App.GovKeeper.Params.Set(ctx, initialParams)
+				require.NoError(t, err)
+			},
+			postCheck: func() {
+				params, err := nw.App.GovKeeper.Params.Get(ctx)
+				require.NoError(t, err)
+				require.Equal(t, "aevmos", params.ExpeditedMinDeposit[0].Denom)
+				require.Equal(t, initialParams.MinDeposit[0].Amount.MulRaw(govv1.DefaultMinExpeditedDepositTokensRatio), params.ExpeditedMinDeposit[0].Amount)
+				require.Equal(t, *initialParams.VotingPeriod/2, *params.ExpeditedVotingPeriod)
+			},
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			nw = network.NewUnitTestNetwork()
+			ctx = nw.GetContext()
+			initialParams, err = nw.App.GovKeeper.Params.Get(ctx)
+			require.NoError(t, err)
+			// setup for testcase
+			tc.setup()
+
+			err = v20.UpdateExpeditedPropsParams(ctx, nw.App.GovKeeper)
+			require.NoError(t, err)
+
+			tc.postCheck()
+		})
+	}
+
 }
