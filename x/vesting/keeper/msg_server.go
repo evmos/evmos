a6f8b8bb67d972b25955ee04b8353959f6de9762
tom <tomasguerraalda@hotmail.com>
2024-09-24 16:52:04 -0300

    move addGrant to clawback acc type

--- x/vesting/keeper/msg_server.go
+++ x/vesting/keeper/msg_server.go
@@ -5,7 +5,6 @@ package keeper
 
 import (
 	"context"
-	"time"
 
 	errorsmod "cosmossdk.io/errors"
 	"github.com/cosmos/cosmos-sdk/telemetry"
@@ -156,7 +155,7 @@ func (k Keeper) FundVestingAccount(goCtx context.Context, msg *types.MsgFundVest
 		return nil, errorsmod.Wrapf(errortypes.ErrInvalidRequest, "account %s can only accept grants from account %s", msg.VestingAddress, vestingAcc.FunderAddress)
 	}
 
-	err = k.addGrant(vestingAcc, msg.GetStartTime().Unix(), msg.GetLockupPeriods(), msg.GetVestingPeriods(), vestingCoins)
+	err = vestingAcc.AddGrant(msg.GetStartTime().Unix(), msg.GetLockupPeriods(), msg.GetVestingPeriods(), vestingCoins)
 	if err != nil {
 		return nil, err
 	}
@@ -380,47 +379,6 @@ func (k Keeper) ConvertVestingAccount(
 	k.accountKeeper.SetAccount(ctx, baseAcc)
 
 	return &types.MsgConvertVestingAccountResponse{}, nil
-}
-
-// addGrant merges a new clawback vesting grant into an existing
-// ClawbackVestingAccount.
-func (k Keeper) addGrant(
-	va *types.ClawbackVestingAccount,
-	grantStartTime int64,
-	grantLockupPeriods, grantVestingPeriods sdkvesting.Periods,
-	grantCoins sdk.Coins,
-) error {
-	// check if the clawback vesting account has only been initialized and not yet funded --
-	// in that case it's necessary to update the vesting account with the given start time because this is set to zero in the initialization
-	if len(va.LockupPeriods) == 0 && len(va.VestingPeriods) == 0 {
-		va.StartTime = time.Unix(grantStartTime, 0).UTC()
-	}
-
-	// modify schedules for the new grant
-	accStartTime := va.GetStartTime()
-	newLockupStart, newLockupEnd, newLockupPeriods := types.DisjunctPeriods(accStartTime, grantStartTime, va.LockupPeriods, grantLockupPeriods)
-	newVestingStart, newVestingEnd, newVestingPeriods := types.DisjunctPeriods(
-		accStartTime,
-		grantStartTime,
-		va.GetVestingPeriods(),
-		grantVestingPeriods,
-	)
-
-	if newLockupStart != newVestingStart {
-		return errorsmod.Wrapf(
-			types.ErrVestingLockup,
-			"vesting start time calculation should match lockup start (%d ≠ %d)",
-			newVestingStart, newLockupStart,
-		)
-	}
-
-	va.StartTime = time.Unix(newLockupStart, 0).UTC()
-	va.EndTime = types.Max64(newLockupEnd, newVestingEnd)
-	va.LockupPeriods = newLockupPeriods
-	va.VestingPeriods = newVestingPeriods
-	va.OriginalVesting = va.OriginalVesting.Add(grantCoins...)
-
-	return nil
 }
 
 // transferClawback transfers unvested tokens in a ClawbackVestingAccount to
