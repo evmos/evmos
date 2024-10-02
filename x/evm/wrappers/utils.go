a64c75807d0a2fcd9ad7ed68d0b80ecb92d8b704
hanchon <guillermo.paoletti@gmail.com>
2024-10-02 11:54:12 +0200

    fix linter issues

--- x/evm/wrappers/utils.go
+++ x/evm/wrappers/utils.go
@@ -30,7 +30,7 @@ func MustConvertEvmCoinTo18Decimals(coin sdk.Coin) sdk.Coin {
 // ConvertAmountToLegacy18Decimals convert the given amount into a 18 decimals
 // representation.
 func ConvertAmountTo18DecimalsLegacy(amt sdkmath.LegacyDec) sdkmath.LegacyDec {
-	evmCoinDecimal := config.GetEVMCoinDecimals()
+	evmCoinDecimal := evmtypes.GetEVMCoinDecimals()
 
 	return amt.MulInt(evmCoinDecimal.ConversionFactor())
 }
