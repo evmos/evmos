dab33ddd9cef9dc77d61d997edd090ad05afc9d6
tom <tomasguerraalda@hotmail.com>
2024-10-07 14:15:11 -0300

    update rocksdb version

--- scripts/install_librocksdb.sh
+++ scripts/install_librocksdb.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-ROCKSDB_VERSION=${1:-"9.3.1"}
+ROCKSDB_VERSION=${1:-"9.5.2"}
 
 # Check if RocksDB is already installed
 if [[ $(find /usr/lib -name "librocksdb.so.${ROCKSDB_VERSION}" -print -quit) ]]; then
