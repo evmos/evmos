56ebdd6024aaf484e8fd5c2a27a16a5075577f70
Guillermo Paoletti <guillermo.paoletti@gmail.com>
2024-10-07 12:51:11 +0200

    Update app/config.go
    Co-authored-by: stepit <48993133+0xstepit@users.noreply.github.com>
    Signed-off-by: Guillermo Paoletti <guillermo.paoletti@gmail.com>

--- app/config.go
+++ app/config.go
@@ -18,6 +18,7 @@ import (
 
 var (
 	sealed    = false
+        // firstCall is used to discriminate between the call to the InitializeAppConfiguration for the CLI client or for the node operator.
 	firstCall = true
 )
 
