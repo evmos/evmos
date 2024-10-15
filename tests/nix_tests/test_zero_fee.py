1f8778f9896c920d46ef48a75a0328ccfbd66b40
tom <tomasguerraalda@hotmail.com>
2024-10-15 17:42:00 -0300

    zero fee test

--- tests/nix_tests/test_zero_fee.py
+++ tests/nix_tests/test_zero_fee.py
@@ -6,8 +6,10 @@ from web3 import Web3
 from .network import create_snapshots_dir, setup_custom_evmos
 from .utils import (
     ADDRS,
+    EVMOS_6DEC_CHAIN_ID,
     KEYS,
     eth_to_bech32,
+    evm6dec_config,
     memiavl_config,
     send_transaction,
     wait_for_fn,
@@ -24,6 +26,18 @@ def custom_evmos(tmp_path_factory):
 
 
 @pytest.fixture(scope="module")
+def custom_evmos_6dec(tmp_path_factory):
+    """
+    Setup an evmos chain with
+    an evm denom with 6 decimals
+    """
+    path = tmp_path_factory.mktemp("zero-fee-6dec")
+    yield from setup_custom_evmos(
+        path, 46900, evm6dec_config(path, "zero-fee"), chain_id=EVMOS_6DEC_CHAIN_ID
+    )
+
+
+@pytest.fixture(scope="module")
 def custom_evmos_rocksdb(tmp_path_factory):
     path = tmp_path_factory.mktemp("zero-fee-rocksdb")
     yield from setup_custom_evmos(
@@ -35,8 +49,8 @@ def custom_evmos_rocksdb(tmp_path_factory):
     )
 
 
-@pytest.fixture(scope="module", params=["evmos", "evmos-rocksdb"])
-def evmos_cluster(request, custom_evmos, custom_evmos_rocksdb):
+@pytest.fixture(scope="module", params=["evmos", "evmos-6dec", "evmos-rocksdb"])
+def evmos_cluster(request, custom_evmos, custom_evmos_6dec, custom_evmos_rocksdb):
     """
     run on evmos and
     evmos built with rocksdb (memIAVL + versionDB)
@@ -44,6 +58,8 @@ def evmos_cluster(request, custom_evmos, custom_evmos_rocksdb):
     provider = request.param
     if provider == "evmos":
         yield custom_evmos
+    elif provider == "evmos-6dec":
+        yield custom_evmos_6dec
     elif provider == "evmos-rocksdb":
         yield custom_evmos_rocksdb
     else:
@@ -54,8 +70,8 @@ def test_cosmos_tx(evmos_cluster):
     """
     test basic cosmos transaction works with zero fees
     """
-    denom = "aevmos"
     cli = evmos_cluster.cosmos_cli()
+    denom = cli.evm_denom()
     sender = eth_to_bech32(ADDRS["signer1"])
     receiver = eth_to_bech32(ADDRS["signer2"])
     amt = 1000
@@ -99,7 +115,7 @@ def test_eth_tx(evmos_cluster):
 
     sender = ADDRS["signer1"]
     receiver = ADDRS["signer2"]
-    amt = 1000
+    amt = int(1e18)
 
     old_src_balance = w3.eth.get_balance(sender)
     old_dst_balance = w3.eth.get_balance(receiver)
