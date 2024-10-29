8377b4aefb6c96e781d08ba26737e0152aa1ad7c
stepit <stefanofrancesco.pitton@gmail.com>
2024-10-29 17:10:05 +0100

    add WEVMOS.sol

---
+++ precompiles/werc20/testdata/WEVMOS.sol
@@ -0,0 +1,29 @@
+pragma solidity >=0.4.22 <0.6;
+
+contract WEVMOS9 {
+    string public name = "Wrapped Evmos";
+    string public symbol = "WEVMOS";
+    uint8 public decimals = 18;
+
+    event Deposit(address indexed dst, uint wad);
+    event Withdrawal(address indexed src, uint wad);
+
+    mapping(address => uint) public balanceOf;
+    mapping(address => mapping(address => uint)) public allowance;
+
+    function() external payable {
+        deposit();
+    }
+
+    function deposit() public payable {
+        balanceOf[msg.sender] += msg.value;
+        emit Deposit(msg.sender, msg.value);
+    }
+
+    function withdraw(uint wad) public {
+        require(balanceOf[msg.sender] >= wad);
+        balanceOf[msg.sender] -= wad;
+        msg.sender.transfer(wad);
+        emit Withdrawal(msg.sender, wad);
+    }
+}
