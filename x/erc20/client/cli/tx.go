d24cc3899dd7b95382af4983ebbf548609fe9803
GuillemGarciaDev <guillem.garcia.gil@protonmail.com>
2024-10-29 17:46:58 +0100

    feat(erc20): add `TransferOwnershipProposal` cmd command

--- x/erc20/client/cli/tx.go
+++ x/erc20/client/cli/tx.go
@@ -210,3 +210,66 @@ func NewToggleTokenConversionProposalCmd() *cobra.Command {
 	}
 	return cmd
 }
+
+func NewTransferOwnershipProposalCmd() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:     "transfer-ownership TOKEN NEW_OWNER",
+		Args:    cobra.ExactArgs(2),
+		Short:   "Submit a transfer ownership proposal",
+		Long:    "Submit a proposal to transfer the ownership of a token pair to a new owner.",
+		Example: fmt.Sprintf("$ %s tx gov submit-legacy-proposal transfer-ownership DENOM_OR_CONTRACT NEW_OWNER --from=<key_or_address>", version.AppName),
+		RunE: func(cmd *cobra.Command, args []string) error {
+			clientCtx, err := client.GetClientTxContext(cmd)
+			if err != nil {
+				return err
+			}
+
+			title, err := cmd.Flags().GetString(cli.FlagTitle)
+			if err != nil {
+				return err
+			}
+
+			description, err := cmd.Flags().GetString(cli.FlagDescription) //nolint:staticcheck
+			if err != nil {
+				return err
+			}
+
+			depositStr, err := cmd.Flags().GetString(cli.FlagDeposit)
+			if err != nil {
+				return err
+			}
+
+			deposit, err := sdk.ParseCoinsNormalized(depositStr)
+			if err != nil {
+				return err
+			}
+
+			token, newOwner := args[0], args[1]
+			from := clientCtx.GetFromAddress()
+
+			content := types.NewTransferOwnershipProposal(title, description, token, newOwner)
+
+			msg, err := govv1beta1.NewMsgSubmitProposal(content, deposit, from)
+			if err != nil {
+				return err
+			}
+
+			return tx.GenerateOrBroadcastTxCLI(clientCtx, cmd.Flags(), msg)
+		},
+	}
+
+	cmd.Flags().String(cli.FlagTitle, "", "title of proposal")
+	cmd.Flags().String(cli.FlagDescription, "", "description of proposal") //nolint:staticcheck
+	cmd.Flags().String(cli.FlagDeposit, "1aevmos", "deposit of proposal")
+	if err := cmd.MarkFlagRequired(cli.FlagTitle); err != nil {
+		panic(err)
+	}
+	if err := cmd.MarkFlagRequired(cli.FlagDescription); err != nil { //nolint:staticcheck
+		panic(err)
+	}
+	if err := cmd.MarkFlagRequired(cli.FlagDeposit); err != nil {
+		panic(err)
+	}
+
+	return cmd
+}
