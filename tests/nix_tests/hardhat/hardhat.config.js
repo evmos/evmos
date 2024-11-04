4a48af743c75fbca7453f98522a6e7d64049f131
stepit <stefanofrancesco.pitton@gmail.com>
2024-11-04 17:25:57 +0100

    add changelog

--- tests/nix_tests/hardhat/hardhat.config.js
+++ tests/nix_tests/hardhat/hardhat.config.js
@@ -8,6 +8,10 @@ module.exports = {
         // NOTE: changing compiler version may break tests,
         // as the expected gas and bytecodes may be different
         version: '0.8.18'
+      },
+      // This version is required to compile the werc9 contract.
+      {
+        version: '0.4.22'
       }
     ]
   },
