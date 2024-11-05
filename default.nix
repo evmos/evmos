960c70148a1ac9d864286261b6d3e64adeb18f0e
MalteHerrmann <malte@evmos.org>
2024-11-05 16:09:08 +0100

    fix build on mac os

--- default.nix
+++ default.nix
@@ -1,6 +1,7 @@
 { lib
 , buildGoApplication
 , buildPackages
+, darwin
 , fetchFromGitHub
 , stdenv
 , rev ? "dirty"
@@ -20,7 +21,7 @@ let
     "-X github.com/cosmos/cosmos-sdk/version.Commit=${rev}"
     "-X github.com/cosmos/cosmos-sdk/types.DBBackend=${dbBackend}"
   ]);
-  buildInputs = lib.optionals (dbBackend == "rocksdb") [ rocksdb ];
+  buildInputs = lib.optionals (dbBackend == "rocksdb") [ rocksdb ] ++ lib.optionals stdenv.isDarwin ([ darwin.apple_sdk.frameworks.CoreServices ]);
   # use a newer version of nixpkgs to get go_1_22
   # We're not updating this on the whole setup because breaks other stuff
   # but we can import the needed packages from the newer version
