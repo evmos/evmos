c258797273581e7f74acd96b0a836fb2247b86d1
stepit <stefanofrancesco.pitton@gmail.com>
2024-11-15 17:34:11 +0100

    add chain coins struct

---
+++ testutil/integration/evmos/network/coins.go
@@ -0,0 +1,85 @@
+// Copyright Tharsis Labs Ltd.(Evmos)
+// SPDX-License-Identifier:ENCL-1.0(https://github.com/evmos/evmos/blob/main/LICENSE)
+
+package network
+
+import (
+	"github.com/evmos/evmos/v20/utils"
+	evmtypes "github.com/evmos/evmos/v20/x/evm/types"
+)
+
+type CoinInfo struct {
+	Denom    string
+	Decimals evmtypes.Decimals
+}
+
+// ChainCoins information for the coins required from the chian to operate:
+// - baseCoin: represents the base coin used to pay gas fees and staking in the
+// Cosmos context.
+// - evmCoin: represents the evm coin used to pay Ethereum
+// transactions fees.
+type ChainCoins struct {
+	// TODO: not sure if this is an overkill. Do we want to customize the
+	// decimals of the base denom? Maybe not..
+	baseCoin *CoinInfo
+	evmCoin  *CoinInfo
+}
+
+// DefaultChainCoins returns the default values used for the ChainCoins in which
+// base and evm denom are the same.
+func DefaultChainCoins() ChainCoins {
+	baseCoinInfo := evmtypes.ChainsCoinInfo[utils.MainnetChainID]
+	// baseCoin is used for both base and evm coin as default..
+	baseCoin := CoinInfo{
+		Denom:    baseCoinInfo.Denom,
+		Decimals: baseCoinInfo.Decimals,
+	}
+	evmCoin := baseCoin
+	return ChainCoins{
+		baseCoin: &baseCoin,
+		evmCoin:  &evmCoin,
+	}
+}
+
+func (cc ChainCoins) BaseCoin() CoinInfo {
+	return *cc.baseCoin
+}
+
+func (cc ChainCoins) EVMCoin() CoinInfo {
+	return *cc.evmCoin
+}
+
+func (cc ChainCoins) BaseDenom() string {
+	return cc.baseCoin.Denom
+}
+
+func (cc ChainCoins) EVMDenom() string {
+	return cc.evmCoin.Denom
+}
+
+func (cc ChainCoins) BaseDecimals() evmtypes.Decimals {
+	return cc.baseCoin.Decimals
+}
+
+func (cc ChainCoins) EVMDecimals() evmtypes.Decimals {
+	return cc.evmCoin.Decimals
+}
+
+func (cc ChainCoins) IsBaseEqualToEVM() bool {
+	return cc.BaseDenom() == cc.EVMDenom()
+}
+
+// DenomDecimalsMap returns a map of Denom -> Decimals for the chain coin.
+func (cc ChainCoins) DenomDecimalsMap() map[string]evmtypes.Decimals {
+	chainDenoms := []string{cc.BaseDenom()}
+	chainDenomDecimals := map[string]evmtypes.Decimals{
+		cc.BaseDenom(): cc.BaseDecimals(),
+	}
+
+	// Insert also the evm denom if base and evm denom are different.
+	if !cc.IsBaseEqualToEVM() {
+		chainDenoms = append(chainDenoms, cc.EVMDenom())
+		chainDenomDecimals[cc.EVMDenom()] = cc.EVMDecimals()
+	}
+	return chainDenomDecimals
+}
