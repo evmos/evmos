5f8984f73987933713d9b1d883074128c10eebb7
Vlad <vladislav.varadinov@gmail.com>
2024-11-25 16:50:06 +0200

    fix: update errors

--- precompiles/evidence/errors.go
+++ precompiles/evidence/errors.go
@@ -4,12 +4,8 @@
 package evidence
 
 const (
-	// ErrEvidenceNotFound is raised when the evidence is not found.
-	ErrEvidenceNotFound = "evidence not found"
 	// ErrInvalidEvidenceHash is raised when the evidence hash is invalid.
 	ErrInvalidEvidenceHash = "invalid request; hash is empty"
-	// ErrOriginDifferentFromSubmitter is raised when the origin address is different from the submitter address.
-	ErrOriginDifferentFromSubmitter = "tx origin address %s does not match the submitter address %s"
 	// ErrExpectedEquivocation is raised when the evidence is not an Equivocation.
 	ErrExpectedEquivocation = "invalid evidence type: expected Equivocation"
 )
